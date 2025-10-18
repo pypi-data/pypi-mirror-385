0ba836f70f43eabf2abacd43c99d6aa6a57a3cea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-18 14:05:13 +0000

    Add package batch-tensorsolve/1.0.1/batch_tensorsolve-1.0.1-py3-none-any.whl

---
+++ packages/batch-tensorsolve/batch_tensorsolve-1.0.1-py3-none-any.whl/batch_tensorsolve/__init__.py
@@ -0,0 +1,4 @@
+__version__ = "1.0.1"
+from ._main import AmbiguousBatchAxesWarning, broadcast_without_repeating, btensorsolve
+
+__all__ = ["AmbiguousBatchAxesWarning", "broadcast_without_repeating", "btensorsolve"]
