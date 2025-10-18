9a2256b03306806d4e251b2e70baa193655bf8ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-18 14:04:26 +0000

    Add package BlickUtils/0.5.10.32/blickutils-0.5.10.32-py3-none-any.whl

---
+++ packages/BlickUtils/blickutils-0.5.10.32-py3-none-any.whl/BlickUtils/__init__.py
@@ -0,0 +1,13 @@
+"""
+blick_utils - A collection of utility functions
+"""
+
+__version__ = "0.5.10.32"
+
+from .core import BlickUtils
+
+# Dynamically expose all static methods:
+for name in dir(BlickUtils):
+    attr = getattr(BlickUtils, name)
+    if callable(attr):
+        globals()[name] = attr
