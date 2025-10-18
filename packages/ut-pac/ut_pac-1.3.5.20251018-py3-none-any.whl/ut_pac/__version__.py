9af6157a83d3468d9556746a24c4c0d2029be30f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-18 15:04:32 +0000

    Add package ut-pac/1.3.5.20251018/ut_pac-1.3.5.20251018-py3-none-any.whl

---
+++ packages/ut-pac/ut_pac-1.3.5.20251018-py3-none-any.whl/ut_pac/__version__.py
@@ -0,0 +1,26 @@
+"""Version module
+Attributes:
+    __title__: package title
+    __description__: package description
+    __url__: project url
+    __version__: package version'
+    __build__: package build id
+    __author__: package author
+    __author_email__: email of package author'
+    __license__: package license
+    __copyright__: package copyright
+    __cake__: package cake
+"""
+from datetime import datetime
+_now = datetime.now().strftime("%Y%m%d")
+
+__title__ = 'ut_pac'
+__description__ = 'Utilities for Package Management.'
+__url__ = 'https://ut-pac.readthedocs.io/en/latest'
+__version__ = f'1.3.5.{_now}'
+__build__ = _now
+__author__ = 'Bernd Stroehle'
+__author_email__ = 'bernd.stroehle@gmail.com'
+__license__ = 'GPL-3.0-only WITH Classpath-Exception-2.0 OR BSD-3-Clause'
+__copyright__ = 'Copyright 2025 Bernd Stroehle'
+__cake__ = u'\u2728 \U0001f370 \u2728'
