197c8c19880c4b43c982a0db59342d35f1e030e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-18 14:04:44 +0000

    Add package mace-descriptor/0.9.3/mace_descriptor-0.9.3.tar.gz

---
+++ packages/mace-descriptor/mace_descriptor-0.9.3.tar.gz/mace_descriptor-0.9.3/mace_descriptor/calculators/__init__.py
@@ -0,0 +1,9 @@
+from .foundations_models import mace_anicc, mace_mp, mace_off, mace_omol
+from .mace import MACECalculator
+
+__all__ = [
+    "MACECalculator",
+    "mace_mp",
+    "mace_off",
+    "mace_anicc",
+]
