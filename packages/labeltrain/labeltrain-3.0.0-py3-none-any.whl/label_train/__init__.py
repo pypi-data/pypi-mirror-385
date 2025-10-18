bc4d96495977a0d80d0297a2e2edbc036e52eded
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-18 15:04:03 +0000

    Add package labeltrain/3.0.0/labeltrain-3.0.0-py3-none-any.whl

---
+++ packages/labeltrain/labeltrain-3.0.0-py3-none-any.whl/label_train/__init__.py
@@ -0,0 +1,8 @@
+"""
+LabelTrain - Lightweight Image Annotation Tool
+-----------------------------------------------
+This file marks 'label_train' as a Python package.
+"""
+
+__version__ = "3.0.1"
+__author__ = "Mahdi Mirzakhani"
