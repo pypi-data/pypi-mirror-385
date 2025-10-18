917d5c8fdec9ffde6ef1a94d7bbcc5f634775b2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-18 15:04:14 +0000

    Add package reyfetch/1.0.47/reyfetch-1.0.47-py3-none-any.whl

---
+++ packages/reyfetch/reyfetch-1.0.47-py3-none-any.whl/reyfetch/__init__.py
@@ -0,0 +1,21 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@Time    : 2023-02-19
+@Author  : Rey
+@Contact : reyxbo@163.com
+@Explain : Web data fetch method set.
+
+Modules
+-------
+rali : Ali web fetch methods.
+rall : All methods.
+rbaidu : Baidu web fetch methods.
+rbase : Base methods.
+rdouban : Douban web fetch methods.
+rgeneral : General fetch methods.
+rsina : Sina web fetch methods.
+rtoutiao : Toutiao web fetch methods.
+rweibo : Weibo web fetch methods.
+"""
