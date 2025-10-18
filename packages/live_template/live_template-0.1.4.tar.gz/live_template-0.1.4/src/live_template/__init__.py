48bb656e64c00add37d36e3a90085099934d43fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-18 14:04:21 +0000

    Add package live_template/0.1.4/live_template-0.1.4.tar.gz

---
+++ packages/live_template/live_template-0.1.4.tar.gz/live_template-0.1.4/src/live_template/__init__.py
@@ -0,0 +1,4 @@
+from .adapters import AiogramRouter, AiogramParser
+from .core import TemplateParser
+
+__all__ = ["AiogramRouter", "AiogramParser", "TemplateParser"]
---
+++ packages/live_template/live_template-0.1.4.tar.gz/live_template-0.1.4/src/live_template/adapters/__init__.py
@@ -0,0 +1,4 @@
+from lt_aiogram.router import AiogramRouter
+from lt_aiogram.parser import AiogramParser
+
+__all__ = ["AiogramRouter", "AiogramParser"]
