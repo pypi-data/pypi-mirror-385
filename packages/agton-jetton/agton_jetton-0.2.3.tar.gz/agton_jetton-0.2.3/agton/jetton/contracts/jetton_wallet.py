caa127e42e548bc46bd1f505f535a7fd0434fd34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-18 03:12:56 +0000

    Add package agton-jetton/0.2.3/agton_jetton-0.2.3.tar.gz

---
+++ packages/agton-jetton/agton_jetton-0.2.3.tar.gz/agton_jetton-0.2.3/agton/jetton/contracts/jetton_wallet.py
@@ -0,0 +1,48 @@
+from dataclasses import dataclass
+
+from agton.ton import Contract, MsgAddress, Cell, Slice, MessageRelaxed, CurrencyCollection
+from agton.jetton.messages import JettonTransfer
+
+@dataclass(frozen=True, slots=True)
+class JettonWalletData:
+    balance: int
+    owner: MsgAddress
+    jetton: MsgAddress
+    jetton_wallet_code: Cell
+
+class JettonWallet(Contract):
+    def get_wallet_data(self) -> JettonWalletData:
+        s = self.run_get_method('get_wallet_data')
+        match s:
+            case (
+                int() as balance,
+                Slice() as owner,
+                Slice() as jetton,
+                Cell() as jetton_wallet_code
+            ):
+                return JettonWalletData(
+                    balance,
+                    owner.load_msg_address(),
+                    jetton.load_msg_address(),
+                    jetton_wallet_code
+                )
+            case _:
+                raise TypeError(f"Unexpected result for get_wallet_data: {s!r}")
+    
+    def create_jetton_transfer(self, *,
+                               value: int | CurrencyCollection,
+                               query_id: int,
+                               amount: int,
+                               destination: MsgAddress,
+                               response_destination: MsgAddress,
+                               custom_payload: Cell | None = None,
+                               forward_amount: int = 0,
+                               forward_payload: Cell = Cell.empty()) -> MessageRelaxed:
+        body = JettonTransfer(
+            query_id, amount, destination, response_destination,
+            custom_payload, forward_amount, forward_payload
+        )
+        return self.create_internal_message(
+            value=value,
+            body=body.to_cell(),
+        )
