a10c77aa272dd61661f14d1286706edadc43751f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-18 03:12:51 +0000

    Add package pyadjoint/0.2.3/pyadjoint-0.2.3-py3-none-any.whl

---
+++ packages/pyadjoint/pyadjoint-0.2.3-py3-none-any.whl/pyadjoint/__init__.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""
+:copyright:
+    adjTomo Dev Team (adjtomo@gmail.com), 2022
+    Lion Krischer (krischer@geophysik.uni-muenchen.de), 2015
+:license:
+    BSD 3-Clause ("BSD New" or "BSD Simplified")
+"""
+import logging
+
+__version__ = "0.1.0"
+
+
+class PyadjointError(Exception):
+    """
+    Base class for all Pyadjoint exceptions. Will probably be used for all
+    exceptions to not overcomplicate things as the whole package is pretty
+    small.
+    """
+    pass
+
+
+class PyadjointWarning(UserWarning):
+    """
+    Base class for all Pyadjoint warnings.
+    """
+    pass
+
+
+# setup the logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.WARNING)
+# Prevent propagating to higher loggers.
+logger.propagate = 0
+# Console log handler.
+ch = logging.StreamHandler()
+# Add formatter
+FORMAT = "[%(asctime)s] - %(name)s - %(levelname)s: %(message)s"
+formatter = logging.Formatter(FORMAT)
+ch.setFormatter(formatter)
+ch.setLevel(logging.DEBUG)
+logger.addHandler(ch)
+
+# Main objects and functions available at the top level.
+from .adjoint_source import AdjointSource # NOQA
+from .main import (calculate_adjoint_source, get_example_data,
+                   plot_adjoint_source)  # NOQA
+from .config import get_config, get_function, ADJSRC_TYPES  # NOQA
---
+++ packages/pyadjoint/pyadjoint-0.2.3-py3-none-any.whl/pyadjoint/tests/test_config.py
@@ -0,0 +1,61 @@
+"""
+Test suite for Config class
+"""
+import inspect
+import pytest
+import pkgutil
+import os
+
+from pyadjoint import (get_config, get_function, adjoint_source_types,
+                       ADJSRC_TYPES)
+
+
+def test_all_configs():
+    """Test importing all configs based on available types"""
+    for adj_src_type in ADJSRC_TYPES:
+        get_config(adjsrc_type=adj_src_type, min_period=1, max_period=10)
+
+
+def test_config_set_correctly():
+    """Just make sure that choosing a specific adjoint source type exposes
+    the correct parameters
+    """
+    cfg = get_config(adjsrc_type="multitaper", min_period=1,
+                     max_period=10)
+    # Unique parameter for MTM
+    assert(hasattr(cfg, "phase_step"))
+
+
+def test_incorrect_inputs():
+    """
+    Make sure that incorrect input parameters are error'd
+    """
+    with pytest.raises(AssertionError):
+        get_config(adjsrc_type="cc_timetraveling_muskrat", min_period=1,
+                   max_period=10)
+
+    with pytest.raises(AssertionError):
+        get_config(adjsrc_type="cc_traveltime_misfit", min_period=10,
+                   max_period=1)
+
+
+def test_adjsrc_types():
+    """
+    Check that all adjoint sources have the correct format
+    """
+    path = os.path.join(
+        os.path.dirname(inspect.getfile(inspect.currentframe())),
+        "adjoint_source_types")
+    for importer, modname, _ in pkgutil.iter_modules(
+            [path], prefix=adjoint_source_types.__name__ + "."):
+        m = importer.find_module(modname).load_module(modname)
+        name = m.__name__.split(".")[-1]
+
+        assert hasattr(m, "calculate_adjoint_source"), \
+            f"{name} has not function 'calculate_adjoint_source'"
+
+
+def test_get_functions():
+    """Get correct functions based on adjoint source types"""
+    for adjsrc_type in ADJSRC_TYPES:
+        assert(callable(get_function(adjsrc_type)))
