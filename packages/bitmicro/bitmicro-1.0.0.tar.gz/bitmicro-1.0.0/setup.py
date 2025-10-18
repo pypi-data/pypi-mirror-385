7dc443f8ff98a52026b9a5aaf6d16649297f8ee6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-18 15:04:34 +0000

    Add package bitmicro/1.0.0/bitmicro-1.0.0.tar.gz

---
+++ packages/bitmicro/bitmicro-1.0.0.tar.gz/bitmicro-1.0.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bitmicro",  # your PyPI package name
+    version="1.0.0",  # increment this for new releases
+    author="neoncorp & Volbil",
+    author_email="snurlaelah163@gmail.com",
+    description="MicroBitcoin Python library (Bitcash-style)",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/COXKPER/bitmicro",  # or your project site
+    license="GPLv3",
+    packages=find_packages(),
+    install_requires=[
+        "ecdsa",
+        "base58",
+        "requests",
+    ],
+    python_requires=">=3.7",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+)
