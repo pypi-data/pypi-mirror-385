fb79bd484f540d1892cc97296682c6e2f798c6f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-18 15:04:43 +0000

    Add package jp-medicine-master/2.0.2/jp_medicine_master-2.0.2-py3-none-any.whl

---
+++ packages/jp-medicine-master/jp_medicine_master-2.0.2-py3-none-any.whl/jp_medicine_master/__init__.py
@@ -0,0 +1,15 @@
+__version__ = '2.0.2'
+
+from .master import (
+    read_y,
+    read_price,
+    read_ge,
+    read_hot13,
+    read_hot9,
+)
+
+from .usage import (
+    get_y_all,
+    get_y_with_yj,
+    get_biosimilar,
+)
