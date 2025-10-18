1a9bfb42067968869f0ce4aa1fe0f45425cae930
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-18 14:03:55 +0000

    Add package blackman-client/0.0.5/blackman_client-0.0.5-py3-none-any.whl

---
+++ packages/blackman-client/blackman_client-0.0.5-py3-none-any.whl/blackman_client/api/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+# import apis into api package
+from blackman_client.api.completions_api import CompletionsApi
+from blackman_client.api.feedback_api import FeedbackApi
+from blackman_client.api.semantic_cache_api import SemanticCacheApi
+
