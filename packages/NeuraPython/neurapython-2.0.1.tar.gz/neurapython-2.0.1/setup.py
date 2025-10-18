4808bb076ef84e1a673de3cfbe0d7fbc57de3ca7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-18 15:04:35 +0000

    Add package NeuraPython/2.0.1/neurapython-2.0.1.tar.gz

---
+++ packages/NeuraPython/neurapython-2.0.1.tar.gz/neurapython-2.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="NeuraPython",
+    version="2.0.1",
+    author="Ibrahim Shahid",
+    author_email="ibrahimshahid7767@gmail.com",
+    description="A multi-purpose AI and utility module for Python — includes GUI, file handling, math, media, and OS integration.",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),  # finds your 'neurapython' package automatically
+    install_requires=[
+        "pyttsx3",
+        "speechrecognition",
+        "PyQt6",
+        "pandas",
+        "pygame",
+        "opencv-python",
+        "matplotlib",
+        "pillow",
+    ],
+    python_requires='>=3.7',
+)
