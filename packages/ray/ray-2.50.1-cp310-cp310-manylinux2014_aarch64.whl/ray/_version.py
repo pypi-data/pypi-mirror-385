be9dd6048ab125f9278b6cb285310ef7a1c25ff7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-18 14:04:38 +0000

    Add package ray/2.50.1/ray-2.50.1-cp310-cp310-manylinux2014_aarch64.whl

---
+++ packages/ray/ray-2.50.1-cp310-cp310-manylinux2014_aarch64.whl/ray/_version.py
@@ -0,0 +1,6 @@
+# Replaced with the current commit when building the wheels.
+commit = "7cf6817996f5304b5c808453a997fc1570dcde25"
+version = "2.50.1"
+
+if __name__ == "__main__":
+    print("%s %s" % (version, commit))
