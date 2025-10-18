968e6328280d001e2560fb310778a2b6d15faed9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-18 03:13:09 +0000

    Add package neuroconv/0.8.2/neuroconv-0.8.2.tar.gz

---
+++ packages/neuroconv/neuroconv-0.8.2.tar.gz/neuroconv-0.8.2/src/neuroconv/baseextractorinterface.py
@@ -0,0 +1,48 @@
+"""Abstract class defining the structure of all Extractor-based Interfaces."""
+
+from abc import ABC
+
+from .basetemporalalignmentinterface import BaseTemporalAlignmentInterface
+from .tools import get_package
+
+
+class BaseExtractorInterface(BaseTemporalAlignmentInterface, ABC):
+    """
+    Abstract class defining the structure of all Extractor-based Interfaces.
+    """
+
+    # Manually override any of these attributes in a subclass if needed.
+    # Note that values set at the level of class definition are called upon import.
+    ExtractorModuleName: str | None = None
+    ExtractorName: str | None = None  # Defaults to __name__.replace("Interface", "Extractor").
+    Extractor = None  # Class loads dynamically on first call to .get_extractor()
+
+    @classmethod
+    def get_extractor(cls):
+        """
+        Get the extractor class for this interface.
+
+        Returns
+        -------
+        type
+            The extractor class that will be used to read the data.
+        """
+        if cls.Extractor is not None:
+            return cls.Extractor
+        extractor_module = get_package(package_name=cls.ExtractorModuleName)
+        extractor = getattr(
+            extractor_module,
+            cls.ExtractorName or cls.__name__.replace("Interface", "Extractor"),
+        )
+        cls.Extractor = extractor
+        return extractor
+
+    def __init__(self, **source_data):
+        super().__init__(**source_data)
+        self.extractor = self.get_extractor()
+        self.extractor_kwargs = self._source_data_to_extractor_kwargs(source_data)
+        self._extractor_instance = self.extractor(**self.extractor_kwargs)
+
+    def _source_data_to_extractor_kwargs(self, source_data: dict) -> dict:
+        """This functions maps the source_data to kwargs required to initialize the Extractor."""
+        return source_data
---
+++ packages/neuroconv/neuroconv-0.8.2.tar.gz/neuroconv-0.8.2/src/neuroconv/datainterfaces/ophys/hdf5/hdf5datainterface.py
@@ -0,0 +1,50 @@
+from typing import Literal
+
+from pydantic import ConfigDict, FilePath, validate_call
+
+from ..baseimagingextractorinterface import BaseImagingExtractorInterface
+from ....utils import ArrayType
+
+
+class Hdf5ImagingInterface(BaseImagingExtractorInterface):
+    """Interface for HDF5 imaging data."""
+
+    display_name = "HDF5 Imaging"
+    associated_suffixes = (".h5", ".hdf5")
+    info = "Interface for HDF5 imaging data."
+
+    @validate_call(config=ConfigDict(arbitrary_types_allowed=True))
+    def __init__(
+        self,
+        file_path: FilePath,
+        mov_field: str = "mov",
+        sampling_frequency: float | None = None,
+        start_time: float | None = None,
+        metadata: dict | None = None,
+        channel_names: ArrayType | None = None,
+        verbose: bool = False,
+        photon_series_type: Literal["OnePhotonSeries", "TwoPhotonSeries"] = "TwoPhotonSeries",
+    ):
+        """
+
+        Parameters
+        ----------
+        file_path : FilePath
+            Path to .h5 or .hdf5 file.
+        mov_field : str, default: 'mov'
+        sampling_frequency : float, optional
+        start_time : float, optional
+        metadata : dict, optional
+        channel_names : list of str, optional
+        verbose : bool, default: False
+        """
+        super().__init__(
+            file_path=file_path,
+            mov_field=mov_field,
+            sampling_frequency=sampling_frequency,
+            start_time=start_time,
+            metadata=metadata,
+            channel_names=channel_names,
+            verbose=verbose,
+            photon_series_type=photon_series_type,
+        )
