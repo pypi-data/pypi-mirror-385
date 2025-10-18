fb325580cbaafd8d8f7f5814930baec0bcec11bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-18 14:03:56 +0000

    Add package jetpytools/2.0.0/jetpytools-2.0.0-py3-none-any.whl

---
+++ packages/jetpytools/jetpytools-2.0.0-py3-none-any.whl/jetpytools/_metadata.py
@@ -0,0 +1,12 @@
+"""Collection of stuff that's useful in general python programming"""
+
+__version__ = "2.0.0"
+
+__author_name__, __author_email__ = "Jaded Encoding Thaumaturgy", "jaded.encoding.thaumaturgy@gmail.com"
+__maintainer_name__, __maintainer_email__ = __author_name__, __author_email__
+
+__author__ = f"{__author_name__} <{__author_email__}>"
+__maintainer__ = __author__
+
+if __name__ == "__github__":
+    print(__version__)
