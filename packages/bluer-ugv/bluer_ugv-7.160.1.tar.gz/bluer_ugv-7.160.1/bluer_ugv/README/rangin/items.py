dcb3c5b816f87f53950298968cec887683a32b21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-18 16:04:52 +0000

    Add package bluer-ugv/7.160.1/bluer_ugv-7.160.1.tar.gz

---
+++ packages/bluer-ugv/bluer_ugv-7.160.1.tar.gz/bluer_ugv-7.160.1/bluer_ugv/README/rangin/items.py
@@ -0,0 +1,10 @@
+from bluer_objects.README.items import ImageItems
+
+from bluer_ugv.README.rangin.consts import rangin_assets2
+
+
+items = ImageItems(
+    {
+        f"{rangin_assets2}/rangin.png": "",
+    }
+)
