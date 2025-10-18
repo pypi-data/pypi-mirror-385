814c4cc5f91148e718bd82abbe049ad7cd163022
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-18 15:04:07 +0000

    Add package tremolo-login/1.0.9/tremolo_login-1.0.9-py3-none-any.whl

---
+++ packages/tremolo-login/tremolo_login-1.0.9-py3-none-any.whl/tremolo_login/__init__.py
@@ -0,0 +1,50 @@
+# Copyright (c) 2023 nggit
+
+__version__ = '1.0.9'
+__all__ = ('Session',)
+
+import hashlib  # noqa: E402
+import hmac  # noqa: E402
+
+import tremolo_session  # noqa: E402
+
+Session = tremolo_session.Session
+
+
+class SessionData(tremolo_session.SessionData):
+    def get_signature(self):
+        if b'user-agent' in self.request.headers:
+            ua = self.request.headers[b'user-agent'][0]
+        else:
+            ua = b''
+
+        try:
+            return hmac.digest(
+                bytes.fromhex(self.id),
+                ua,
+                hashlib.sha256
+            ).hex()
+        except AttributeError:
+            return hmac.new(
+                bytes.fromhex(self.id),
+                msg=ua,
+                digestmod=hashlib.sha256
+            ).hexdigest()
+
+    def login(self):
+        self['_login'] = self.get_signature()
+
+        return self['_login']
+
+    def logout(self):
+        if '_login' in self:
+            del self['_login']
+
+    def is_logged_in(self):
+        if '_login' not in self:
+            return False
+
+        return hmac.compare_digest(self['_login'], self.get_signature())
+
+
+tremolo_session.SessionData = SessionData
