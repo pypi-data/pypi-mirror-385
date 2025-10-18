cab2b03b9bf5f79d1459b938e958b4a1dc05508f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-18 03:13:09 +0000

    Add package nomenklatura-mpt/4.1.20/nomenklatura_mpt-4.1.20.tar.gz

---
+++ packages/nomenklatura-mpt/nomenklatura_mpt-4.1.20.tar.gz/nomenklatura_mpt-4.1.20/nomenklatura/__init__.py
@@ -0,0 +1,11 @@
+from nomenklatura.resolver import Resolver
+from nomenklatura.store import Store, View
+from nomenklatura.index import Index
+
+__version__ = "4.1.9"
+__all__ = [
+    "Resolver",
+    "Index",
+    "Store",
+    "View",
+]
