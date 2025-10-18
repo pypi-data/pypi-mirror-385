46999d47dac5e6733da9aed14018db0196fb3f05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-18 15:04:03 +0000

    Add package django-spire/0.17.9/django_spire-0.17.9-py3-none-any.whl

---
+++ packages/django-spire/django_spire-0.17.9-py3-none-any.whl/django_spire/knowledge/intelligence/intel/message_intel.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+from django_spire.ai.chat.message_intel import BaseMessageIntel
+
+
+class KnowledgeMessageIntel(BaseMessageIntel):
+    _template: str = 'django_spire/knowledge/message/knowledge_message_intel.html'
+    body: str
+
+    def content_to_str(self) -> str:
+        return self.body
