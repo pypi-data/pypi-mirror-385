e5eea0657bc908cf3a10a0e6c06a166b47e9099f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-18 14:05:04 +0000

    Add package dsnd-probability-necs-1/1.2/dsnd_probability_necs_1-1.2.tar.gz

---
+++ packages/dsnd-probability-necs-1/dsnd_probability_necs_1-1.2.tar.gz/dsnd_probability_necs_1-1.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dsnd_probability_necs_1",
+    version="1.2",
+    description="Gaussian and Binomial distributions",
+    packages=["dsnd_probability_necs_1"],
+    author="Necs Mobolaji",
+    author_email="goognecs@gmail.com",
+    zip_safe=False,
+)
