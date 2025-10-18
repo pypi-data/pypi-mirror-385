0da7423d60e932dad55c63b0447ad2aa42e5b348
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-18 03:12:44 +0000

    Add package terrascope-api/2025.10.82308/terrascope_api-2025.10.82308-py3-none-any.whl

---
+++ packages/terrascope-api/terrascope_api-2025.10.82308-py3-none-any.whl/terrascope_api/__init__.py
@@ -0,0 +1,8 @@
+# Copyright 2017-2021 Orbital Insight Inc., all rights reserved.
+# Contains confidential and trade secret information.
+# Government Users:  Commercial Computer Software - Use governed by
+# terms of Orbital Insight commercial license agreement.
+
+
+from terrascope_api.sync_client import TerrascopeSyncClient  # noqa
+from terrascope_api.async_client import TerrascopeAsyncClient  # noqa
