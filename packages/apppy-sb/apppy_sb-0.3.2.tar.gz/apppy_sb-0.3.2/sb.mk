3df64b7ef6bb3b85c15abcf3b7cf1c050c9f0b43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-19 04:06:57 +0000

    Add package apppy-sb/0.3.2/apppy_sb-0.3.2.tar.gz

---
+++ packages/apppy-sb/apppy_sb-0.3.2.tar.gz/apppy_sb-0.3.2/sb.mk
@@ -0,0 +1,23 @@
+ifndef APPPY_SB_MK_INCLUDED
+APPPY_SB_MK_INCLUDED := 1
+SB_PKG_DIR := $(patsubst %/,%,$(dir $(abspath $(lastword $(MAKEFILE_LIST)))))
+
+.PHONY: sb sb-dev sb/build sb/clean sb/install sb/install-dev
+
+sb: sb/clean sb/install
+
+sb-dev: sb/clean sb/install-dev
+
+sb/build:
+	cd $(SB_PKG_DIR) && uvx --from build pyproject-build
+
+sb/clean:
+	cd $(SB_PKG_DIR) && rm -rf dist/ *.egg-info .venv
+
+sb/install: sb/build
+	cd $(SB_PKG_DIR) && uv pip install dist/*.whl
+
+sb/install-dev:
+	cd $(SB_PKG_DIR) && uv pip install -e .
+
+endif # APPPY_SB_MK_INCLUDED
