db494cc2e74b22a56e7c93b0fa4a4a0ad1558583
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-19 15:04:30 +0000

    Add package angle-emb/0.6.0/angle_emb-0.6.0.tar.gz

---
+++ packages/angle-emb/angle_emb-0.6.0.tar.gz/angle_emb-0.6.0/docs/conf.py
@@ -0,0 +1,42 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'AnglE'
+copyright = '2024, Sean Lee'
+author = 'Sean Lee'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.doctest',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.githubpages',
+    'sphinx_copybutton',
+    'autoapi.extension',
+]
+
+autosummary_generate = True
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+autoapi_dirs = ['../angle_emb']
---
+++ packages/angle-emb/angle_emb-0.6.0.tar.gz/angle_emb-0.6.0/tests/test_loadding.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+def test_loadding():
+    import numpy as np
+    from angle_emb import AnglE, Prompts
+
+    angle = AnglE.from_pretrained('WhereIsAI/UAE-Large-V1')
+    vecs = angle.encode('hello world')
+    assert isinstance(vecs, np.ndarray)
+    vecs = angle.encode(['hello world', 'hi there👋'])
+    assert isinstance(vecs, np.ndarray)
+    # test prompt
+    vecs = angle.encode(['hello world', 'hi there👋'], prompt=Prompts.C)
+    assert isinstance(vecs, np.ndarray)
+
+
+def test_ese_loadding():
+    import numpy as np
+    from angle_emb import AnglE
+
+    angle = AnglE.from_pretrained('WhereIsAI/UAE-Large-V1').truncate_layer(20)
+    vecs = angle.encode('hello world')
+    assert isinstance(vecs, np.ndarray)
+    vecs = angle.encode(['hello world', 'hi there👋'], embedding_size=512)
+    assert isinstance(vecs, np.ndarray)
+
+
+def test_normalize_embedding():
+    import numpy as np
+    from angle_emb import AnglE
+
+    angle = AnglE.from_pretrained('WhereIsAI/UAE-Large-V1')
+    vecs = angle.encode('hello world', normalize_embedding=True)
+    assert isinstance(vecs, np.ndarray)
