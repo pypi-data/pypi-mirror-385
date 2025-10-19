0cc69b774d1f4e4c74e794c48a68064a76e3efb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-19 16:04:11 +0000

    Add package brtp/0.0.6/brtp-0.0.6-py3-none-any.whl

---
+++ packages/brtp/brtp-0.0.6-py3-none-any.whl/brtp/plotting/canvas/__init__.py
@@ -0,0 +1,8 @@
+"""
+Module providing a 'wrapper' class (Canvas) for plotting in matplotlib figures, which takes care of...
+  - axis transformations (x/y/z)
+  - plotting styles
+  - ...
+"""
+
+from ._linestyle import LineStyle
