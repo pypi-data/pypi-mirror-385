0e971b77eadc3720bd1286690db6cdca54cffcca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-19 14:05:16 +0000

    Add package ryutils/9.0.2/ryutils-9.0.2-py3-none-any.whl

---
+++ packages/ryutils/ryutils-9.0.2-py3-none-any.whl/ryutils/alerts/factory.py
@@ -0,0 +1,30 @@
+"""
+Factory for creating alerts and the associated CLI arguments.
+"""
+
+import argparse
+import typing as T
+
+from ryutils import log
+from ryutils.alerts.alert_types import AlertType
+from ryutils.alerts.alerter import Alerter
+
+
+class AlertFactory:
+    @staticmethod
+    def create_alert(
+        alert_type: AlertType, args: argparse.Namespace, verbose: bool = False
+    ) -> Alerter:
+        alert_class_type = alert_type.value
+
+        if verbose:
+            log.print_normal(f"Attempting to create {alert_type} alerter")
+
+        # Handle different alert types with their specific parameters
+        if alert_type == AlertType.DISCORD:
+            # Discord requires both webhook_url and title
+            title = getattr(args, "title", "Alert")
+            return T.cast(Alerter, alert_class_type(webhook_url=args.webhook_url, title=title))
+
+        # Slack and Mock only need webhook_url
+        return T.cast(Alerter, alert_class_type(webhook_url=args.webhook_url))
---
+++ packages/ryutils/ryutils-9.0.2-py3-none-any.whl/ryutils/alerts/mock.py
@@ -0,0 +1,32 @@
+"""
+Dummy class for testing purposes.
+"""
+
+import typing as T
+from dataclasses import dataclass, field
+
+from ryutils.alerts.alerter import Alerter
+
+
+@dataclass
+class MockAlerter(Alerter):
+    webhook_url: str
+    _callback: T.Callable[[str], None] = field(default_factory=lambda: lambda _: None)
+
+    def __post_init__(self) -> None:
+        # Call parent constructor
+        super().__init__(self.webhook_url, "Mock")
+
+    @property
+    def callback(self) -> T.Callable[[str], None]:
+        return self._callback
+
+    @callback.setter
+    def callback(self, callback: T.Callable[[str], None]) -> None:
+        self._callback = callback
+
+    def send_alert(self, message: str) -> None:
+        self.callback(message)
+
+    async def send_alert_async(self, message: str) -> None:
+        self.send_alert(message)
