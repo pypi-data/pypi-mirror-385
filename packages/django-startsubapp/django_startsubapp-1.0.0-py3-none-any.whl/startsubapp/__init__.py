316b9f306a34eefe5ccda391a6348f67cb1ae803
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-19 14:04:28 +0000

    Add package django-startsubapp/1.0.0/django_startsubapp-1.0.0-py3-none-any.whl

---
+++ packages/django-startsubapp/django_startsubapp-1.0.0-py3-none-any.whl/startsubapp/__init__.py
@@ -0,0 +1,16 @@
+"""
+Django StartSubApp - Create Django apps inside subdirectories
+
+A Django management command that creates Django apps inside a subdirectory
+(default: 'apps/') to keep your project structure cleaner and more organized.
+
+Author: Saman Naruee
+Email: samannaruee@gmail.com
+GitHub: @Saman-naruee
+"""
+
+__version__ = "1.0.0"
+__author__ = "Saman Naruee"
+__email__ = "samannaruee@gmail.com"
+
+default_app_config = "startsubapp.apps.StartsubappConfig"
