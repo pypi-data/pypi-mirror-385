b4fa04884c881d42ae71863b54e75a8223567e14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-19 15:04:16 +0000

    Add package sinfra-tools/0.0.1/sinfra_tools-0.0.1.tar.gz

---
+++ packages/sinfra-tools/sinfra_tools-0.0.1.tar.gz/sinfra_tools-0.0.1/readme.md
@@ -0,0 +1,6 @@
+<!--
+- SPDX-License-Identifier: Apache-2.0
+- Copyright (C) 2025 Jayesh Badwaik <j.badwaik@fz-juelich.de>
+-->
+
+# sinfra-tools : A collection of tools for sinfra framework
---
+++ packages/sinfra-tools/sinfra_tools-0.0.1.tar.gz/sinfra_tools-0.0.1/src/sinfra/cli/__init__.py
@@ -0,0 +1,8 @@
+# ------------------------------------------------------------------------------
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (C) 2025 Jayesh Badwaik <j.badwaik@fz-juelich.de>
+# ------------------------------------------------------------------------------
+
+
+def main():
+    print("Hello, World!")
