d97d977d08e5f69aeeb8b775464531eb0a2276e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-19 14:05:02 +0000

    Add package django-phone-verify/3.2.0/django_phone_verify-3.2.0-py3-none-any.whl

---
+++ packages/django-phone-verify/django_phone_verify-3.2.0-py3-none-any.whl/phone_verify/admin.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+
+# Third Party Stuff
+from django.contrib import admin
+
+from .models import SMSVerification
+
+
+@admin.register(SMSVerification)
+class SMSVerificationAdmin(admin.ModelAdmin):
+    list_display = ("id", "security_code", "phone_number", "is_verified", "failed_attempts", "created_at")
+    search_fields = ("phone_number",)
+    ordering = ("phone_number",)
+    readonly_fields = (
+        "security_code",
+        "phone_number",
+        "session_token",
+        "is_verified",
+        "failed_attempts",
+        "created_at",
+        "modified_at",
+    )
