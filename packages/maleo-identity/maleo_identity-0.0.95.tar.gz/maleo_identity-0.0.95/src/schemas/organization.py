2afe6afca5764db9dc4c3a67e3e3c6463f961a49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-19 03:25:10 +0000

    Add package maleo-identity/0.0.95/maleo_identity-0.0.95.tar.gz

---
+++ packages/maleo-identity/maleo_identity-0.0.95.tar.gz/maleo_identity-0.0.95/src/schemas/organization.py
@@ -0,0 +1,162 @@
+from pydantic import BaseModel, Field
+from typing import Generic, Literal, TypeVar, overload
+from uuid import UUID
+from maleo.enums.organization import (
+    OrganizationType,
+    OptOrganizationType,
+    FullOrganizationTypeMixin,
+    OptListOfOrganizationTypes,
+    FullOrganizationTypesMixin,
+)
+from maleo.enums.status import (
+    ListOfDataStatuses,
+    FULL_DATA_STATUSES,
+)
+from maleo.schemas.mixins.filter import convert as convert_filter
+from maleo.schemas.mixins.identity import (
+    IdentifierTypeValue,
+    Ids,
+    UUIDs,
+    Keys,
+    Names,
+)
+from maleo.schemas.mixins.sort import convert as convert_sort
+from maleo.schemas.parameter import (
+    ReadSingleParameter as BaseReadSingleParameter,
+    ReadPaginatedMultipleParameter,
+    StatusUpdateParameter as BaseStatusUpdateParameter,
+    DeleteSingleParameter as BaseDeleteSingleParameter,
+)
+from maleo.types.dict import StrToAnyDict
+from maleo.types.integer import OptListOfInts
+from maleo.types.string import OptStr, OptListOfStrs
+from maleo.types.uuid import OptListOfUUIDs
+from ..enums.organization import IdentifierType
+from ..mixins.organization import Key, Name
+from ..types.organization import IdentifierValueType
+
+
+class CreateParameter(Name[str], Key[str], FullOrganizationTypeMixin[OrganizationType]):
+    pass
+
+
+class ReadMultipleParameter(
+    ReadPaginatedMultipleParameter,
+    Names[OptListOfStrs],
+    Keys[OptListOfStrs],
+    FullOrganizationTypesMixin[OptListOfOrganizationTypes],
+    UUIDs[OptListOfUUIDs],
+    Ids[OptListOfInts],
+):
+    @property
+    def _query_param_fields(self) -> set[str]:
+        return {
+            "ids",
+            "uuids",
+            "statuses",
+            "organization_types",
+            "keys",
+            "names",
+            "search",
+            "page",
+            "limit",
+            "use_cache",
+        }
+
+    def to_query_params(self) -> StrToAnyDict:
+        params = self.model_dump(
+            mode="json", include=self._query_param_fields, exclude_none=True
+        )
+        params["filters"] = convert_filter(self.date_filters)
+        params["sorts"] = convert_sort(self.sort_columns)
+        params = {k: v for k, v in params.items()}
+        return params
+
+
+class ReadSingleParameter(BaseReadSingleParameter[IdentifierType, IdentifierValueType]):
+    @overload
+    @classmethod
+    def new(
+        cls,
+        identifier_type: Literal[IdentifierType.ID],
+        identifier_value: int,
+        statuses: ListOfDataStatuses = list(FULL_DATA_STATUSES),
+        use_cache: bool = True,
+    ) -> "ReadSingleParameter": ...
+    @overload
+    @classmethod
+    def new(
+        cls,
+        identifier_type: Literal[IdentifierType.UUID],
+        identifier_value: UUID,
+        statuses: ListOfDataStatuses = list(FULL_DATA_STATUSES),
+        use_cache: bool = True,
+    ) -> "ReadSingleParameter": ...
+    @overload
+    @classmethod
+    def new(
+        cls,
+        identifier_type: Literal[IdentifierType.KEY],
+        identifier_value: str,
+        statuses: ListOfDataStatuses = list(FULL_DATA_STATUSES),
+        use_cache: bool = True,
+    ) -> "ReadSingleParameter": ...
+    @classmethod
+    def new(
+        cls,
+        identifier_type: IdentifierType,
+        identifier_value: IdentifierValueType,
+        statuses: ListOfDataStatuses = list(FULL_DATA_STATUSES),
+        use_cache: bool = True,
+    ) -> "ReadSingleParameter":
+        return cls(
+            identifier_type=identifier_type,
+            identifier_value=identifier_value,
+            statuses=statuses,
+            use_cache=use_cache,
+        )
+
+    def to_query_params(self) -> StrToAnyDict:
+        return self.model_dump(
+            mode="json", include={"statuses", "use_cache"}, exclude_none=True
+        )
+
+
+class FullUpdateData(Name[str], Key[str], FullOrganizationTypeMixin[OrganizationType]):
+    pass
+
+
+class PartialUpdateData(
+    Name[OptStr], Key[OptStr], FullOrganizationTypeMixin[OptOrganizationType]
+):
+    pass
+
+
+UpdateDataT = TypeVar("UpdateDataT", FullUpdateData, PartialUpdateData)
+
+
+class UpdateDataMixin(BaseModel, Generic[UpdateDataT]):
+    data: UpdateDataT = Field(..., description="Update data")
+
+
+class UpdateParameter(
+    UpdateDataMixin[UpdateDataT],
+    IdentifierTypeValue[
+        IdentifierType,
+        IdentifierValueType,
+    ],
+    Generic[UpdateDataT],
+):
+    pass
+
+
+class StatusUpdateParameter(
+    BaseStatusUpdateParameter[IdentifierType, IdentifierValueType],
+):
+    pass
+
+
+class DeleteSingleParameter(
+    BaseDeleteSingleParameter[IdentifierType, IdentifierValueType]
+):
+    pass
