04e9278fff569fb4982d45a6618b17cdae6c0cc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-19 15:04:27 +0000

    Add package absfuyu/5.12.1/absfuyu-5.12.1-py3-none-any.whl

---
+++ packages/absfuyu/absfuyu-5.12.1-py3-none-any.whl/absfuyu/extra/data_analysis.py
@@ -0,0 +1,21 @@
+"""
+Absfuyu: Data Analysis [W.I.P]
+------------------------------
+Extension for ``pd.DataFrame``
+(deprecated)
+
+Version: 5.12.0
+Date updated: 17/10/2025 (dd/mm/yyyy)
+"""
+
+# Library
+# ---------------------------------------------------------------------------
+from absfuyu.extra.da.dadf import DADF  # noqa
+from absfuyu.extra.da.df_func import (  # noqa
+    compare_2_list,
+    equalize_df,
+    rename_with_dict,
+)
+
+# Class - DA
+# TODO: drop dups | Combine: row with data, row NaN
