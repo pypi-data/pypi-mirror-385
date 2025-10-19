d007f644d97da521a608e0376f416f05c4479a75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-19 03:25:13 +0000

    Add package yuio/2.0.0b4/yuio-2.0.0b4.tar.gz

---
+++ packages/yuio/yuio-2.0.0b4.tar.gz/yuio-2.0.0b4/docs/source/conf.py
@@ -0,0 +1,56 @@
+import datetime
+import os
+import pathlib
+
+os.environ["__YUIO_SPHINX_BUILD"] = "1"
+
+import yuio
+
+# -- Project information -----------------------------------------------------
+
+project = "Yuio"
+copyright = f"{datetime.date.today().year}, Tamika Nomara"
+author = "Tamika Nomara"
+release = version = yuio.__version__
+
+
+# -- General configuration ---------------------------------------------------
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.githubpages",
+    "sphinx_design",
+    "sphinx_vhs",
+]
+
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3/", None),
+    "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
+}
+nitpick_ignore_regex = [
+    (r"py:class", r"(.*\.)?([A-Z]{1,2}|[A-Z]+_co|Cmp|SupportsLt|Sz|TAst|_[^.]*)")
+]
+autodoc_typehints_format = "short"
+autodoc_member_order = "bysource"
+autodoc_inherit_docstrings = False
+
+vhs_cwd = pathlib.Path(__file__).parent.parent.parent
+vhs_min_version = "0.7.2"
+
+# -- Options for HTML output -------------------------------------------------
+
+html_theme = "furo"
+html_static_path = ["_static"]
+html_js_files = [
+    "mermaid-init.js",
+    (
+        "https://cdn.jsdelivr.net/npm/mermaid@11.4.1/dist/mermaid.min.js",
+        {"defer": "defer"},
+    ),
+]
+html_theme_options = {
+    "source_repository": "https://github.com/taminomara/yuio",
+    "source_branch": "main",
+    "source_directory": "docs/source",
+}
---
+++ packages/yuio/yuio-2.0.0b4.tar.gz/yuio-2.0.0b4/yuio/_typing.py
@@ -0,0 +1,44 @@
+# pyright: reportDeprecated=false
+
+import re as _re
+import types as _types
+import typing as _typing
+from typing import *  # type: ignore
+
+import typing_extensions as _typing_extensions
+from typing_extensions import *  # type: ignore
+
+assert _typing.Union is _typing_extensions.Union
+assert _typing.Annotated is _typing_extensions.Annotated
+
+
+def is_union(origin):
+    return origin is Union or origin is _types.UnionType
+
+
+if TYPE_CHECKING:
+    StrRePattern: TypeAlias = _re.Pattern[str]
+    StrReMatch: TypeAlias = _re.Match[str]
+
+    def type_repr(ty: Any) -> str: ...
+
+else:
+    try:
+        StrRePattern = _re.Pattern[str]
+        StrReMatch = _re.Match[str]
+    except TypeError:
+        StrRePattern = _re.Pattern
+        StrReMatch = _re.Match
+
+    if "type_repr" not in globals():
+
+        def type_repr(ty: Any) -> str:
+            if isinstance(
+                value, (type, _types.FunctionType, _types.BuiltinFunctionType)
+            ):
+                if value.__module__ == "builtins":
+                    return value.__qualname__
+                return f"{value.__module__}.{value.__qualname__}"
+            if value is ...:
+                return "..."
+            return repr(value)
