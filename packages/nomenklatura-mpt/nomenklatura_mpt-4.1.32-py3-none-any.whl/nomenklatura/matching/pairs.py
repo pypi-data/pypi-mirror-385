88e981ed68f99fe341497c9247603827a7fd5921
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-19 04:06:34 +0000

    Add package nomenklatura-mpt/4.1.32/nomenklatura_mpt-4.1.32-py3-none-any.whl

---
+++ packages/nomenklatura-mpt/nomenklatura_mpt-4.1.32-py3-none-any.whl/nomenklatura/matching/pairs.py
@@ -0,0 +1,42 @@
+import json
+from typing import Generator, Dict, Any
+from followthemoney.proxy import EntityProxy
+from followthemoney.util import PathLike
+
+from nomenklatura.judgement import Judgement
+
+
+class JudgedPair(object):
+    """A pair of two entities which have been judged to be the same
+    (or not) by a user."""
+
+    __slots__ = ("left", "right", "weight", "judgement")
+
+    def __init__(
+        self, left: EntityProxy, right: EntityProxy, judgement: Judgement
+    ) -> None:
+        self.left = left
+        self.right = right
+        self.judgement = judgement
+        self.weight = 0.0
+
+    def to_dict(self) -> Dict[str, Any]:
+        return {
+            "left": self.left.to_dict(),
+            "right": self.right.to_dict(),
+            "judgement": self.judgement.value,
+            "weight": self.weight,
+        }
+
+
+def read_pairs(pairs_file: PathLike) -> Generator[JudgedPair, None, None]:
+    """Read judgement pairs (training data) from a JSON file."""
+    with open(pairs_file, "r") as fh:
+        while line := fh.readline():
+            data = json.loads(line)
+            left_entity = EntityProxy.from_dict(data["left"])
+            right_entity = EntityProxy.from_dict(data["right"])
+            judgement = Judgement(data["judgement"])
+            if judgement not in (Judgement.POSITIVE, Judgement.NEGATIVE):
+                continue
+            yield JudgedPair(left_entity, right_entity, judgement)
