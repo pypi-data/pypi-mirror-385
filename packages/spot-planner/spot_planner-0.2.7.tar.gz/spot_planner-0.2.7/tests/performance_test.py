753ac5a598cb2125d974476aa8260b776568ed72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-19 03:25:26 +0000

    Add package spot-planner/0.2.7/spot_planner-0.2.7.tar.gz

---
+++ packages/spot-planner/spot_planner-0.2.7.tar.gz/spot_planner-0.2.7/tests/performance_test.py
@@ -0,0 +1,29 @@
+from decimal import Decimal
+
+import pytest
+
+from spot_planner.main import _is_valid_combination, get_cheapest_periods
+
+def test_performance():
+    prices = [Decimal(f"{i}") for i in range(24)]
+    get_cheapest_periods(
+        prices=prices,
+        low_price_threshold=Decimal("10"),
+        min_selections=12,
+        min_consecutive_selections=1,
+        max_gap_between_periods=1,
+        max_gap_from_start=1,
+    )
+
+
+@pytest.mark.slow
+def test_maximum_range():
+    prices = [Decimal(f"{i}") for i in range(29)]
+    get_cheapest_periods(
+        prices=prices,
+        low_price_threshold=Decimal("10"),
+        min_selections=12,
+        min_consecutive_selections=1,
+        max_gap_between_periods=1,
+        max_gap_from_start=1,
+    )
