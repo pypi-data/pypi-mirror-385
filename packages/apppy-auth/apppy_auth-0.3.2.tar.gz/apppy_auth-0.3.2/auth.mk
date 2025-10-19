c824fd2477641b87be7182adf25d5c309943f2b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-19 04:06:57 +0000

    Add package apppy-auth/0.3.2/apppy_auth-0.3.2.tar.gz

---
+++ packages/apppy-auth/apppy_auth-0.3.2.tar.gz/apppy_auth-0.3.2/auth.mk
@@ -0,0 +1,23 @@
+ifndef APPPY_AUTH_MK_INCLUDED
+APPPY_AUTH_MK_INCLUDED := 1
+AUTH_PKG_DIR := $(patsubst %/,%,$(dir $(abspath $(lastword $(MAKEFILE_LIST)))))
+
+.PHONY: auth auth-dev auth/build auth/clean auth/install auth/install-dev
+
+auth: auth/clean auth/install
+
+auth-dev: auth/clean auth/install-dev
+
+auth/build:
+	cd $(AUTH_PKG_DIR) && uvx --from build pyproject-build
+
+auth/clean:
+	cd $(AUTH_PKG_DIR) && rm -rf dist/ *.egg-info .venv
+
+auth/install: auth/build
+	cd $(AUTH_PKG_DIR) && uv pip install dist/*.whl
+
+auth/install-dev:
+	cd $(AUTH_PKG_DIR) && uv pip install -e .
+
+endif # APPPY_AUTH_MK_INCLUDED
