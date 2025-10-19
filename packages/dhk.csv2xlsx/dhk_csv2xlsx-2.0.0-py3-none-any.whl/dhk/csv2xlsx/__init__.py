4689f405c7bfc4482f70aff48020e22924ce91c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-19 04:07:00 +0000

    Add package dhk.csv2xlsx/2.0.0/dhk_csv2xlsx-2.0.0-py3-none-any.whl

---
+++ packages/dhk.csv2xlsx/dhk_csv2xlsx-2.0.0-py3-none-any.whl/dhk/csv2xlsx/__init__.py
@@ -0,0 +1,10 @@
+#   -*- coding: utf-8 -*-
+
+##############################################################################
+# copyrights and license
+#
+# Copyright (c) 2025 David Harris Kiesel
+#
+# Licensed under the MIT License. See LICENSE in the project root for license
+# information.
+##############################################################################
