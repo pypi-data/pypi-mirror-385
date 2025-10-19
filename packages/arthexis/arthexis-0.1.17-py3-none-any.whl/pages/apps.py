aaacbcf571d6ca24102a2583e1f152316c9b0e82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-19 14:05:07 +0000

    Add package arthexis/0.1.17/arthexis-0.1.17-py3-none-any.whl

---
+++ packages/arthexis/arthexis-0.1.17-py3-none-any.whl/pages/apps.py
@@ -0,0 +1,10 @@
+from django.apps import AppConfig
+
+
+class PagesConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "pages"
+    verbose_name = "7. Experience"
+
+    def ready(self):  # pragma: no cover - import for side effects
+        from . import checks  # noqa: F401
