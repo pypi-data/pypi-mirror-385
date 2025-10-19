27198d6ec3d05d22acd88c0b792b8690a13c9343
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-19 16:04:11 +0000

    Add package checkatlas/0.7.1/checkatlas-0.7.1.tar.gz

---
+++ packages/checkatlas/checkatlas-0.7.1.tar.gz/checkatlas-0.7.1/checkatlas/checkatlas_workflow.py
@@ -0,0 +1,84 @@
+import logging
+import os
+
+try:
+    from . import check
+    from .utils import checkatlas_workflow_arguments, files, folders
+except ImportError:
+    from checkatlas import check
+    from checkatlas.utils import checkatlas_workflow_arguments, files, folders
+
+
+PROCESS_TYPE = ["list_scanpy", "list_cellranger", "list_seurat", "html_report"]
+
+
+def main() -> None:  # pragma: no cover
+    """
+    The main function executes on commands:
+    `python -m checkatlas` and `$ checkatlas `.
+
+    This is checkatlas entry point.
+
+    Arguments are managed here
+    Search fo atlases is managed here
+    Then checkatlas is ran with the list of atlases found
+
+    Returns:
+        None
+
+    """
+    # Set up logging
+    logger = logging.getLogger("checkatlas")
+    logging.basicConfig(format="|--- %(levelname)-8s %(message)s")
+
+    parser = checkatlas_workflow_arguments.create_parser()
+
+    # Parse all args
+    args = parser.parse_args()
+
+    # Set logger level
+    if args.debug:
+        logger.setLevel(getattr(logging, "DEBUG"))
+    else:
+        logger.setLevel(getattr(logging, "INFO"))
+
+    logger.debug(f"Program arguments: {args}")
+
+    process = args.process
+    if process == "list_scanpy":
+        logger.debug(f"Search path {args.path} with checkatlas_workflow")
+        logger.debug(f"Transform path to absolute:{args.path}")
+        args.path = os.path.abspath(args.path)
+        logger.debug(f"Check checkatlas folders in:{args.path}")
+        folders.checkatlas_folders(args.path)
+        logger.info("Searching Seurat, Cellranger and Scanpy files")
+        check.list_scanpy_atlases(args.path)
+    elif process == "list_cellranger":
+        logger.debug(f"Search path {args.path} with checkatlas_workflow")
+        logger.debug(f"Transform path to absolute:{args.path}")
+        args.path = os.path.abspath(args.path)
+        logger.debug(f"Check checkatlas folders in:{args.path}")
+        folders.checkatlas_folders(args.path)
+        logger.info("Searching Seurat, Cellranger and Scanpy files")
+        check.list_cellranger_atlases(args.path)
+    elif process == "list_seurat":
+        logger.debug(f"Search path {args.path} with checkatlas_workflow")
+        logger.debug(f"Transform path to absolute:{args.path}")
+        args.path = os.path.abspath(args.path)
+        logger.debug(f"Check checkatlas folders in:{args.path}")
+        folders.checkatlas_folders(args.path)
+        logger.info("Searching Seurat, Cellranger and Scanpy files")
+        check.list_seurat_atlases(args.path)
+    elif process == "html_report":
+        logger.info(
+            "Generate QC plots html report in "
+            f"{files.get_html_qc_report_path(args.path)}"
+        )
+        check.generate_fig_html(args.path, "qc")
+        logger.info(
+            "Generate UMAP plots html report in "
+            f"{files.get_html_umap_report_path(args.path)}"
+        )
+        check.generate_fig_html(args.path, "reductions")
+    else:
+        logger.debug("TO DO : Spatial Transcriptomics not yet managed.")
---
+++ packages/checkatlas/checkatlas-0.7.1.tar.gz/checkatlas-0.7.1/checkatlas/metrics/annot/dunn_index.py
@@ -0,0 +1,12 @@
+def run(annotation, ref_annotation):
+    """
+    `Dunn readthedocs
+    <https://checkatlas.readthedocs.io/en/latest/metrics/clustering/dunn/>`__
+
+    TO BE IMPLEMENTED
+
+    :param annotation:
+    :param ref_annotation:
+    :return:
+    """
+    return 0
