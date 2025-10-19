4c1c94fb2b0aae28a3586a84c33f5bbd4c14f083
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-19 15:05:00 +0000

    Add package byzh-ai/0.0.9.23/byzh_ai-0.0.9.23-py3-none-any.whl

---
+++ packages/byzh-ai/byzh_ai-0.0.9.23-py3-none-any.whl/byzh/ai/Bmodel/__init__.py
@@ -0,0 +1,9 @@
+from .lenet5 import B_Lenet5
+from .resnet18 import B_ResNet18
+
+from .rnn import B_RNN
+from .lstm import B_LSTM
+
+from .SimpleCNN import B_SimpleCNN
+
+__all__ = ['B_SimpleCNN', 'B_Lenet5', 'B_ResNet18', 'B_RNN', 'B_LSTM']
