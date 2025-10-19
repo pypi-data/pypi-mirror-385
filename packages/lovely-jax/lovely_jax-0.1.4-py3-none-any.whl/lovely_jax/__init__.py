ad38471658ee7906f24f7a4839d7b6638fb3a2cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-19 15:04:19 +0000

    Add package lovely-jax/0.1.4/lovely_jax-0.1.4-py3-none-any.whl

---
+++ packages/lovely-jax/lovely_jax-0.1.4-py3-none-any.whl/lovely_jax/__init__.py
@@ -0,0 +1,13 @@
+__version__ = "0.1.4"
+
+
+from .repr_str import *
+from .repr_rgb import *
+from .repr_plt import *
+from .repr_chans import *
+from .patch import *
+from .utils import *
+
+import os
+if os.environ.get("LOVELY_JAX", "").strip().lower() in {"1", "true", "yes"}:
+    monkey_patch()
