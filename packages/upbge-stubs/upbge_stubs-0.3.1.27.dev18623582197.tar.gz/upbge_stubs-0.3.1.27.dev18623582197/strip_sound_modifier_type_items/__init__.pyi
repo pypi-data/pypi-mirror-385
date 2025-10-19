e68f3b55b4f15470a8d2de7891497fc1a8766737
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-19 14:05:07 +0000

    Add package upbge-stubs/0.3.1.27.dev18623582197/upbge_stubs-0.3.1.27.dev18623582197.tar.gz

---
+++ packages/upbge-stubs/upbge_stubs-0.3.1.27.dev18623582197.tar.gz/upbge_stubs-0.3.1.27.dev18623582197/strip_sound_modifier_type_items/__init__.pyi
@@ -0,0 +1,14 @@
+"""
+
+
+Strip Sound Modifier Type Items
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+:SOUND_EQUALIZER: 
+  Sound Equalizer.
+
+.. _rna-enum-strip-sound-modifier-type-items:
+
+"""
+
+import typing
