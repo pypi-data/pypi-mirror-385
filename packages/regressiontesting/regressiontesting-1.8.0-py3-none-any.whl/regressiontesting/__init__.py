73813615799905606450af695955cf3c27f8859e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-19 15:04:16 +0000

    Add package regressiontesting/1.8.0/regressiontesting-1.8.0-py3-none-any.whl

---
+++ packages/regressiontesting/regressiontesting-1.8.0-py3-none-any.whl/regressiontesting/__init__.py
@@ -0,0 +1,13 @@
+# RegressionTesting
+# Copyright (c) 2025 Lionel Guo
+# Author: Lionel Guo
+# Email: lionelliguo@gmail.com
+# GitHub: https://github.com/lionelliguo/regressiontesting
+
+# __init__.py
+
+from .regressiontesting import RegressionTesting
+
+"""
+This is the regression testing module for testing APIs using Google Sheets and curl commands.
+"""
