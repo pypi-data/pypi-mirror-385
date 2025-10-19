85c3e5c0a2e329d3df5e2a23296955b3717340e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-19 16:04:13 +0000

    Add package animeapi-py/3.7.0/animeapi_py-3.7.0-py3-none-any.whl

---
+++ packages/animeapi-py/animeapi_py-3.7.0-py3-none-any.whl/animeapi/excepts.py
@@ -0,0 +1,14 @@
+"""
+animeapi.excepts
+----------------
+
+This module contains the set of AnimeAPI's exceptions.
+"""
+
+
+class MissingRequirement(Exception):
+    """Raised if there is an error with the request"""
+
+
+class UnsupportedVersion(Exception):
+    """Raised if the version is unsupported"""
