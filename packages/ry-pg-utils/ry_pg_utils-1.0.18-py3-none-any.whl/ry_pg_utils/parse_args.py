8e2f7118d5e2f75f47fcea7c59acb54186225846
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-19 04:05:51 +0000

    Add package ry-pg-utils/1.0.18/ry_pg_utils-1.0.18-py3-none-any.whl

---
+++ packages/ry-pg-utils/ry_pg_utils-1.0.18-py3-none-any.whl/ry_pg_utils/parse_args.py
@@ -0,0 +1,14 @@
+import argparse
+
+from .config import get_config
+
+
+def add_postgres_db_args(parser: argparse.ArgumentParser) -> None:
+    postgres_parser = parser.add_argument_group("postgres-options")
+    config = get_config()
+    postgres_parser.add_argument("--postgres-host", default=config.postgres_host)
+    postgres_parser.add_argument("--postgres-port", type=int, default=config.postgres_port)
+    postgres_parser.add_argument("--postgres-db", default=config.postgres_db)
+    postgres_parser.add_argument("--postgres-user", default=config.postgres_user)
+    postgres_parser.add_argument("--postgres-password", default=config.postgres_password)
+    postgres_parser.add_argument("--do-publish-db", action="store_true", default=False)
