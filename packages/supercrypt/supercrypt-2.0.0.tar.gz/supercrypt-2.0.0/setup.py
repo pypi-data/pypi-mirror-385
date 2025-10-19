efaea9effad5d7cfa6008ae3344d6552a3fc82eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-19 03:25:31 +0000

    Add package supercrypt/2.0.0/supercrypt-2.0.0.tar.gz

---
+++ packages/supercrypt/supercrypt-2.0.0.tar.gz/supercrypt-2.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='supercrypt',
+    version='2.0.0',
+    author='ATHALLAH RAJENDRA PUTRA JUNIARTO',
+    description='Ultra-secure custom mathematical encryption library with file encryption support',
+    packages=find_packages(),
+    python_requires='>=3.8',
+)
