06c52575a92c1daba2970f8bdc9e0ab1274e7c8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-19 03:25:38 +0000

    Add package quantjourney/1.0.2/quantjourney-1.0.2-py3-none-any.whl

---
+++ packages/quantjourney/quantjourney-1.0.2-py3-none-any.whl/quantjourney/sdk/connectors/cftc.py
@@ -0,0 +1,10 @@
+from ..client import ConnectorEndpoint
+
+
+class CFTCEndpoint(ConnectorEndpoint):
+    """SDK endpoints for CFTC connector.
+
+    Relies on dynamic fallback for get_*/search_* methods.
+    """
+    pass
+
