9e16cd3806f387d605efda0f466c50f50a67fb66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-19 15:04:21 +0000

    Add package lyric-lang/0.0.1/lyric_lang-0.0.1.tar.gz

---
+++ packages/lyric-lang/lyric_lang-0.0.1.tar.gz/lyric_lang-0.0.1/lyric/__init__.py
@@ -0,0 +1,12 @@
+def main():
+    banner = """
+    ────────────────────────────────────────────────
+               Lyric Programming Language
+             © 2025 MiraNova Studios
+
+        Lyric is coming soon.
+        Visit http://miranova.studio/lyric.html
+    ────────────────────────────────────────────────
+    """
+    print(banner.strip())
+
