db739f47cbb43a0f72c079a81139b63e9a49e15d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-19 03:25:15 +0000

    Add package Treeed/0.3.0/treeed-0.3.0.tar.gz

---
+++ packages/Treeed/treeed-0.3.0.tar.gz/treeed-0.3.0/py3dgraphics/__init__.py
@@ -0,0 +1,4 @@
+from .opengl_graphics import Point3D, Object3D, Scene3D, create_cube, quick_scene, load_obj, create_object_from_file
+
+__version__ = "0.3.0"
+__all__ = ["Point3D", "Object3D", "Scene3D", "create_cube", "quick_scene", "load_obj", "create_object_from_file"]
