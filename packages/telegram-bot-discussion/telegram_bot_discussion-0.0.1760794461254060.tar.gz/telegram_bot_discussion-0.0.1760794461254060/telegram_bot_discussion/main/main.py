2173ca19e173601d48b42bad60ebc4bc45ab1eba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-19 03:25:31 +0000

    Add package telegram-bot-discussion/0.0.1760794461254060/telegram_bot_discussion-0.0.1760794461254060.tar.gz

---
+++ packages/telegram-bot-discussion/telegram_bot_discussion-0.0.1760794461254060.tar.gz/telegram_bot_discussion-0.0.1760794461254060/telegram_bot_discussion/main/main.py
@@ -0,0 +1,29 @@
+from optparse import OptionParser
+from os import path
+from shutil import copytree
+
+
+def main():
+    usage = "Params: -n/--new project_path"
+    parser = OptionParser(usage=usage)
+    parser.add_option(
+        "-n",
+        "--new",
+        dest="new",
+        help="new project directory name",
+        metavar="FILE",
+    )
+    (options, _) = parser.parse_args()
+    if not options.new:
+        print(usage)
+        exit(1)
+    from_dir = path.join(path.dirname(path.abspath(__file__)), "template_bot")
+    to_dir = path.join(path.abspath("./"), str(options.new))
+    if path.exists(to_dir):
+        print(f"Dir `{to_dir}` already exists")
+        exit(1)
+    copytree(
+        from_dir,
+        to_dir,
+        False,
+    )
