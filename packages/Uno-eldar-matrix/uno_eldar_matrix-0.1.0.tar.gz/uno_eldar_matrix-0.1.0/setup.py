f77d308f43f56afae3096c4c2cc8a3f859c91532
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-19 03:25:20 +0000

    Add package Uno-eldar-matrix/0.1.0/uno_eldar_matrix-0.1.0.tar.gz

---
+++ packages/Uno-eldar-matrix/uno_eldar_matrix-0.1.0.tar.gz/uno_eldar_matrix-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Uno-eldar-matrix",
+    version="0.1.0",
+    packages=find_packages(),
+    description="Uno card game package",
+    author="Eldar",
+    license="MIT",
+)
