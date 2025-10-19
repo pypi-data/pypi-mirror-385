9297d133095e7467eab1051210ea5d0d69f691cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-19 14:04:43 +0000

    Add package sys-mcp/0.2.1/sys_mcp-0.2.1-py3-none-any.whl

---
+++ packages/sys-mcp/sys_mcp-0.2.1-py3-none-any.whl/sys_mcp/server.py
@@ -0,0 +1,37 @@
+"""Module to define system-related MCP server."""
+
+import logging
+from mcp.server.fastmcp import FastMCP
+from sys_mcp.tools.system import SystemToolGroup
+
+
+logger = logging.getLogger(__name__)
+
+
+_INSTRUCTIONS = """
+You are a system management MCP server designed to help users manage and interact with UNIX-like systems.
+You can execute safe to run system commands and retrieve system information.
+"""
+
+
+def create_server() -> FastMCP:
+    """Create and return a FastMCP server instance.
+
+    Returns:
+        An instance of FastMCP.
+    """
+    logger.info("Creating SysMCP server instance...")
+
+    server = FastMCP(
+        name="SysMCP",
+        instructions=_INSTRUCTIONS.strip(),
+    )
+
+    SystemToolGroup(server).register_tools()
+
+    return server
+
+
+if __name__ == "__main__":
+    server = create_server()
+    server.run(transport="stdio")
