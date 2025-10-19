30a4546f5f471f55629b2307384e3d6013e918df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-19 15:04:47 +0000

    Add package kq-lmpc-quadrotor/1.0.9/kq_lmpc_quadrotor-1.0.9.tar.gz

---
+++ packages/kq-lmpc-quadrotor/kq_lmpc_quadrotor-1.0.9.tar.gz/kq_lmpc_quadrotor-1.0.9/kq_lmpc_quadrotor/__init__.py
@@ -0,0 +1,17 @@
+
+from .QuadrotorKoopman import KoopmanLift, UtilsFunctions
+from .Utils import Gen_ref_dfb, init_moving_pos, setup_ref
+from .Utils import quadrotor_nl_plant, rand_state
+from .Utils import lqr_init, compute_lqr_control
+
+from .Viz import animate_quadrotor
+from .Viz import Plot_Results as pltt
+
+#from .mpc import setup_ocp_kqlmpc, lift_constr
+from .mpc import init_guide, shift_guide, get_state_input_bounds
+#from .mpc import setup_ocp_nmpc
+
+from .lqr_demo import lqr_demo
+
+
+__all__ = ["lqr_demo"]
