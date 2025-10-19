bd934f7835f5b475968708a50eb11cdc02793ba1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-19 03:25:10 +0000

    Add package eykturkish/0.1.0b0/eykturkish-0.1.0b0.tar.gz

---
+++ packages/eykturkish/eykturkish-0.1.0b0.tar.gz/eykturkish-0.1.0b0/eykturkish/__init__.py
@@ -0,0 +1,5 @@
+print("\033[31mWarning: NOT THREAD-SAFE. GLOBAL VARIABLES ARE USED FOR SIMPLICITY. DO NOT USE IN WEB SERVERS. \nUyarı: THREAD-SAFE DEĞİLDİR. BASİTLİK İÇİN GLOBAL DEĞİŞKENLER KULLANILMIŞTIR. WEB SUNUCULARINDA KULLANMAYIN.\033[0m",flush=True)
+from .eykturkish import *
+from .unicodelib import *
+from .matematik import *
+__version__ = "0.1.0b"
