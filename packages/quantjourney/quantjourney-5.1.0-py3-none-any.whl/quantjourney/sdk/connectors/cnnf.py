c42015751856052ac39cc5b0be7dd2cf5ca93194
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-19 03:25:30 +0000

    Add package quantjourney/5.1.0/quantjourney-5.1.0-py3-none-any.whl

---
+++ packages/quantjourney/quantjourney-5.1.0-py3-none-any.whl/quantjourney/sdk/connectors/cnnf.py
@@ -0,0 +1,10 @@
+from ..client import ConnectorEndpoint
+
+
+class CNNFEndpoint(ConnectorEndpoint):
+    """SDK endpoints for CNN Fear & Greed connector.
+
+    Relies on dynamic fallback for get_*/search_* methods.
+    """
+    pass
+
