010786a7d58177a7af23239725ffefbf3c423c01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-19 04:06:24 +0000

    Add package indipydriver/2.5.0/indipydriver-2.5.0-py3-none-any.whl

---
+++ packages/indipydriver/indipydriver-2.5.0-py3-none-any.whl/indipydriver/__init__.py
@@ -0,0 +1,21 @@
+
+import sys
+
+from .ipydriver import IPyDriver, Device
+from .propertyvectors import SwitchVector, LightVector, TextVector, BLOBVector, NumberVector
+from .propertymembers import SwitchMember, LightMember, TextMember, BLOBMember, NumberMember, getfloat
+from .events import getProperties, newSwitchVector, newTextVector, newNumberVector, newBLOBVector, Message, delProperty, defSwitchVector, defTextVector, defNumberVector, defLightVector, defBLOBVector, setSwitchVector, setTextVector, setNumberVector, setLightVector, setBLOBVector
+from .ipyserver import IPyServer
+
+if sys.version_info < (3, 11):
+    raise ImportError('indipydriver requires Python >= 3.11')
+
+version = "2.5.0"
+
+
+__all__ = ["version", "IPyDriver", "Device", "getfloat", "IPyServer",
+           "SwitchVector", "LightVector", "TextVector", "BLOBVector", "NumberVector",
+           "SwitchMember", "LightMember", "TextMember", "BLOBMember", "NumberMember",
+           "getProperties", "newSwitchVector", "newTextVector", "newNumberVector", "newBLOBVector",
+           "Message", "delProperty", "defSwitchVector", "defTextVector", "defNumberVector", "defLightVector", "defBLOBVector",
+           "setSwitchVector", "setTextVector", "setNumberVector", "setLightVector", "setBLOBVector"]
