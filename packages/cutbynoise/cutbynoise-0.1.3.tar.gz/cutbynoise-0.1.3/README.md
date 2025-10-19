ca1663355c483f8fb92511b56ffd6dd6bf5dca0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-19 04:07:00 +0000

    Add package cutbynoise/0.1.3/cutbynoise-0.1.3.tar.gz

---
+++ packages/cutbynoise/cutbynoise-0.1.3.tar.gz/cutbynoise-0.1.3/README.md
@@ -0,0 +1,186 @@
+This repository contains cutbynoise - a tool for cutting regions
+that are delimited by certain samples (a.k.a. jingles, markers or
+templates) out of larger sound files.
+
+For example, you can use it on a multi-hour long podcast episode
+MP3 file where the producer inserts audio advertisements and
+marks such inserts with static samples.
+
+That way you can easily listen again and again to an
+advertisement you found interesting.
+
+Alternatively, you can listen to an episode without having to
+manually pause and skip over advertisements you have grown to
+hate and whose markers have conditioned you to do so, already.
+
+Example usage:
+
+```
+$ ./cutbynoise.py -b ldn-begin-short.flac -o ldn372-clean.mp3 ldn372.mp3 --trash del -v
+004.7 INF Aligning ldn-begin-short.flac with ldn372.mp3 (overlap=True) ...    [__main__]
+006.5 INF Template matches at: [1918.26029091 1994.97614545 4029.22829091 4112.20814545] (s)    [__main__]
+006.5 INF All template match positions: [1918.26029091 1994.97614545 4029.22829091 4112.20814545] (s)    [__main__]
+006.5 INF Noisy regions: [[1918.26029091 1994.97614545]  [4029.22829091 4112.20814545]] (s)    [__main__]
+006.5 INF Duration(s) of noisy regions: [76.71585455 82.97985455] (s)    [__main__]
+006.5 INF Writing noise to del/01.mp3 ...    [__main__]
+006.9 INF Writing noise to del/02.mp3 ...    [__main__]
+007.1 INF Writing cut ldn372-clean.mp3 ...    [__main__]
+```
+
+NB: In this case the begin template also matches the end one.
+In case the region is delimited by truly different templates you
+can specify the second one via `-e`.
+
+
+## How it works
+
+Cutbynoise [cross-correlates][cc] each template with the input
+sound file. To speed this up and reduce memory usage, only the
+mono signal is cross-correlated, the signals are down-sampled,
+the cross-correlation is computed via [convolution][conv], using
+[FFT][fft] and the [overlap-add method][oa].
+
+In a direct implementation, [cross-correlating][cc] a shorter
+signal with a longer one basically means computing the
+dot product over a sliding window.
+Peaks in the resulting vector are possible template matches.
+Thus, it can be seen as a variant of the sequence alignment
+problem.
+
+For the heavy-lifting it uses [ffmpeg][ffmpeg],
+i.e. for reading and resampling the
+waveform signals and cutting the input file, and [SciPy][scipy]
+for the cross-correlation.
+
+Optionally, cutbynoise is able to correlate the templates in a
+moving window (cf. `--window`). This reduces the memory usage
+further (e.g. 145 Mib RSS instead of 4.5 GiB RSS, on a 4 h long
+mp3 file) and is thus recommended on memory constrained systems.
+Note that the window mode trades memory savings against an
+increased runtime (e.g. 28 s instead of 23 s, on a 2023 laptop
+CPU, using the same example input).
+
+
+## Related Methods
+
+In case the begin and end templates of regions aren't very static
+one can look into standard methods used in speech recognition,
+i.e. especially [Dynamic Time Warping (DTW)][dtw]. See also e.g.
+the [Dynamic Time Warping Suite][dtws] which provides packages
+for Python and R (FWIW, I haven't tested them, yet).
+
+When it comes to podcasts with ads, some of them provide a
+premium feed that also contains all the episodes without any ads.
+Thus, if it fits your budget and you want to support the podcast
+hosts you can consider to simply pay for such a premium feed on a
+regular basis, instead of fiddling with cutbynoise.
+
+However, not all podcasts (understandably) can be bothered to
+provide multiple feeds. For example, AFAICS, the c't uplink
+podcast is only available with ads, i.e. even if you have a c't
+Plus paid subscription.
+
+Also, at some point, everyone's budget is limited, i.e. one can
+only financially support so many projects.
+
+As always, one can also simply unsubscribe podcasts that contain
+too many annoying ads. Arguably, there are enough podcasts
+available that don't contain any advertisements.
+
+
+## Getting Started
+
+Cutbynoise can be installed via pip or directly be executed,
+given that the required dependencies (cf. `pyproject.toml`) are installed.
+
+On Fedora, you can install the dependencies system-wide like this:
+
+```
+dnf install python3-scipy python3-numpy ffmpeg
+```
+
+For creating suitable templates (samples) that delimit your
+regions you can use your favourite audio editor. For example,
+you can use the open-source [Audacity][audacity] for this task
+(cf.  File -> Export -> Export Selected Audio ...), which is
+packaged by many Linux distributions.
+
+
+## Combining Filters
+
+For filtering the noise out of input files where several
+different kinds of markers are used you can chain multiple
+cutbynoise invocations together with `chain`, which is also part
+of this repository.
+
+For example, a podcast may regularly insert a super annoying
+pre-roll that doesn't have a dedicated start sample, i.e. just an
+end sample, i.e. _in addition_ to mid-rolls that _can_ be
+recognized by start and stop samples.
+In such a case you can invoke cutbynoise first and use the
+start-of-file as begin marker and run it a second time on the
+result to cut out the mid-rolls, as well.
+
+The same approach is available for post-rolls, i.e. to specify
+the end-of-file as end marker.
+
+Also, some podcast cutters aren't that disciplined to always use
+the same pair of markers, e.g. start and end marker might be
+randomly switched or a set of samples is in use.
+All this can be covered by a chain, where cutbynoise is invoked
+for a few variations. The `chain` command ignores failing steps,
+by default, as long at least one step succeeded. This is useful
+for a chain of cutbynoise invocations where a part is expected to
+fail when no noise regions are found.
+
+See also the comments at the top of `chain.py` for an extreme,
+but unfortunately real world, chaining example.
+
+
+## Limitations
+
+As is, cutbynoise doesn't take extra care of additional meta-data
+contained in the input file.
+Especially chapter marks aren't adjusted, i.e. in the output
+file some chapter marks might be off by whatever amount was cut.
+Also, embedded images might not remain in the output file.
+
+FWIW, both limitations might be fixed by moderate extra effort,
+e.g. by finding the right ffmpeg options and possibly adjusting
+chapter marks programmatically.
+
+OTOH, the ad business is very dynamic and wants to target
+victims as much as possible, most ads are often automatically
+inserted on-the-fly for each download. That means depending on
+the date, ad-auction outcomes and phase of the moon, a certain
+podcast episode download link might contain quite different ads
+at quite different locations, including none.
+Since developers working in the ad industry allegedly couldn't care
+less, their ad pasting code likely doesn't adjust any chapter
+marks, in the first place. Thus, when simply cutting out the ad
+noise _not_ adjusting any existing chapter marks might be the
+right thing and actually improve the chapter marks accuracy.
+
+
+## License
+
+Cutbynoise is licensed under the [GPL version 3 or later][gpl].
+
+
+## See Also
+
+Cutbynoise can be integrated with [castproxy](https://github.com/gsauthof/feed-util#castproxypy),
+Atom feed generator that aggregates audiocasts (podcasts).
+
+
+[cc]: https://en.wikipedia.org/wiki/Cross-correlation
+[conv]: https://en.wikipedia.org/wiki/Cross-correlation#Properties
+[fft]: https://en.wikipedia.org/wiki/Fast_Fourier_transform
+[oa]: https://en.wikipedia.org/wiki/Overlap%E2%80%93add_method
+[ffmpeg]: https://en.wikipedia.org/wiki/FFmpeg
+[scipy]: https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.correlate.html
+[gpl]: https://en.wikipedia.org/wiki/GNU_General_Public_License
+[dtw]: https://en.wikipedia.org/wiki/Dynamic_time_warping
+[dtws]: https://dynamictimewarping.github.io/
+[audacity]: https://en.wikipedia.org/wiki/Audacity_(audio_editor)
+
---
+++ packages/cutbynoise/cutbynoise-0.1.3.tar.gz/cutbynoise-0.1.3/cutbynoise.py
@@ -0,0 +1,441 @@
+#!/usr/bin/env python3
+
+
+# cutbynoise - cut sample enclosed noise regions out of sound files
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+# SPDX-FileCopyrightText: © 2024 Georg Sauthoff <mail@gms.tf>
+
+
+import argparse
+import itertools
+import json
+import logging
+import numpy as np
+import os
+import scipy.signal as signal
+import subprocess
+import sys
+import tempfile
+import traceback
+
+
+log = logging.getLogger(__name__)
+
+
+def setup_logging(level):
+    old_f = logging.getLogRecordFactory()
+    def f(*xs, **kw):
+        r = old_f(*xs, **kw)
+        r.epoch = r.relativeCreated / 1000.0
+        level_dict = { 10 : 'DBG',  20 : 'INF', 30 : 'WRN', 40 : 'ERR', 50 : 'CRI' }
+        r.lvl = level_dict[r.levelno]
+        return r
+    logging.setLogRecordFactory(f)
+    log_format = '{epoch:05.1f} {lvl} {message}    [{name}]'
+    logging.basicConfig(format=log_format, level=level, style='{')
+
+    np.set_printoptions(linewidth=np.inf, threshold=np.inf)
+
+
+def parse_args():
+    p = argparse.ArgumentParser()
+    p.add_argument('input',           help='input audio filename')
+    p.add_argument('--output',  '-o', metavar='FILENAME', help='trimmed output filename')
+    p.add_argument('--begin',   '-b', help='sample that starts a noise region')
+    p.add_argument('--end',     '-e', help='sample that ends a noise region')
+    p.add_argument('--thresh',  '-t', default=0.69, type=float, help='correlation threshold (default: %(default)f)')
+    p.add_argument('--trash',         metavar='DIRECTORY', help='store noise snippets in directory (default: they are discarded)')
+    p.add_argument('--cut-file',      metavar='FILENAME', help='filename for ffmpeg input list (default: temporary file)')
+    p.add_argument('--overlap',       default=True, action='store_true', help='use faster overlap-add method (default: on)')
+    p.add_argument('--std',           dest='overlap', action='store_false', help='use standard fft cross-correlation (default: --overlap)')
+    p.add_argument('--length',  '-l', default=400, type=int, help='maximum lenghth of a noisy region in seconds (default: %(default)d)')
+    p.add_argument('--verbose', '-v', action='store_true', help='print verbose messages')
+    p.add_argument('--debug',   '-d', action='store_true', help='print debug messages')
+    p.add_argument('--down',          action='store_true', default=True, help='down-sample inputs to 13.75 kHz to speed up processing (default: yes)')
+    p.add_argument('--hq',            dest='down', action='store_false', help='read inputs at full sample rate (default: --down)')
+    # for some samples/inputs, 4000 or even 2000 is good enough
+    p.add_argument('--hz',            default=13750, type=int, help='sample rate to use when --down is specified (default: %(default)d)')
+    p.add_argument('--json',          metavar='FILENAME', help='write positions to json file')
+    p.add_argument('--window',  '-w', action='store_true', help='limit memory usage by aligning in a moving window')
+    p.add_argument('--silence',       nargs='?', const=1.0, metavar='SECONDS', type=float, help='search for n second silence as end marker (default: 1 s if specified)')
+    p.add_argument('--bof',           action='store_true', help='include begin-of-file as begin of first noise region')
+    p.add_argument('--eof',           action='store_true', help='include end-of-file as end of last noise region')
+
+    args = p.parse_args()
+
+    if args.bof and args.begin:
+        raise RuntimeError('--bof and --begin conflicts')
+    if args.eof and args.end:
+        raise RuntimeError('--eof and --end conflicts')
+    if args.bof:
+        args.marks = []
+    else:
+        args.marks = [ args.begin ]
+    args.level = logging.WARNING
+    if args.verbose:
+        args.level = logging.INFO
+    if args.debug:
+        args.level = logging.DEBUG
+    if args.end:
+        args.marks.append(args.end)
+    if args.silence:
+        if args.end:
+            raise RuntimeError('--end conflicts with --silence')
+    if not ((args.begin) or (args.begin and args.end) or (args.bof and args.end) or (args.begin and args.eof) or (args.begin and args.silence)):
+        raise Runtime('unsupported combination of --begin/--end/--bof/--eof/--silence')
+
+    return args
+
+
+def ffprobe(filename):
+    r = subprocess.run(['ffprobe', '-loglevel', 'warning', '-show_format', '-show_streams',
+                        '-of', 'json', filename],
+                   capture_output=True, text=True, check=True)
+    d = json.loads(r.stdout)
+    return d
+
+
+def probe_wav(filename):
+    h = ffprobe(filename)
+    rate = int(h['streams'][0]['sample_rate'])
+    return rate
+
+
+def ffmpeg_read(filename, rate=None):
+    ms = []
+    if rate is not None:
+        ms.extend(['-ar', str(rate)])
+    r = subprocess.run(( ['ffmpeg', '-loglevel', 'warning', '-vn', '-i', filename, '-f', 'wav', '-acodec', 'pcm_s16le']
+                       + ms
+                       # only left channel
+                       + ['-filter', 'pan=1c|c0=c0', 'pipe:'] ),
+                       capture_output=True, check=True)
+    return r.stdout
+
+
+def read_wav(filename, target_rate=None):
+    rate = probe_wav(filename)
+    extra = {}
+    if target_rate and str(target_rate) != str(rate):
+        extra['rate'] = str(target_rate)
+    bs = ffmpeg_read(filename, **extra)
+    wav = np.frombuffer(bs, np.int16)
+    wav = wav.astype('float32', casting='safe')
+    return wav, rate
+
+
+# copied from https://github.com/scipy/scipy/blob/4edfcaa3ce8a387450b6efce968572def71be089/scipy/signal/_signaltools.py#L1101
+# cf. https://github.com/scipy/scipy/blob/4edfcaa3ce8a387450b6efce968572def71be089/scipy/signal/_signaltools.py#L243
+def _reverse_and_conj(x):
+    """
+    Reverse array `x` in all dimensions and perform the complex conjugate
+    """
+    reverse = (slice(None, None, -1),) * x.ndim
+    return x[reverse].conj()
+
+
+
+def pair_positions(prev, offs_s):
+    # might be true when end sample is used elsewhere, besides pairings with the begin sample ...
+    if prev is not None and prev.shape < offs_s.shape:
+        rs = []
+        for p in prev:
+            t = [ x for x in offs_s if x > p ]
+            if t:
+                k = min(t)
+                rs.append(k)
+        log.info(f'Found more end positions than beginnings ({offs_s}) - filtering to: {rs}')
+        offs_s = np.array(rs)
+    return offs_s
+
+
+def align(hay, needle, rate, thresh, pos, prev=None, overlap=False):
+    y = np.dot(needle, needle)
+    if overlap:
+        xs = signal.oaconvolve(hay, _reverse_and_conj(needle), mode='full')
+        yP = np.max(signal.oaconvolve(needle, _reverse_and_conj(needle), mode='full'))
+    else:
+        xs = signal.correlate(hay, needle, mode='full', method='fft')
+        yP = np.max(signal.correlate(needle, needle, mode='full', method='fft'))
+    log.debug('Self correlation of sample: {}'.format(abs(1 - y / yP)))
+    assert abs(1 - y / yP) < 1e-5
+    h = thresh * y
+    ks, hs = signal.find_peaks(xs, height=h, distance=10*rate)
+    if pos[0] == 0:
+        if pos[1] == 1:
+            ks[::2] -= needle.size
+        else: # i.e. pos[1] == 2
+            ks -= needle.size
+
+    offs_s = ks / rate
+
+    offs_s = pair_positions(prev, offs_s)
+
+    return offs_s
+
+
+def yield_window(filename, window_size, overlap_size, rate):
+    def to_wav(bs):
+        bale = np.frombuffer(bs, np.int16)
+        bale = bale.astype('float32', casting='safe')
+        return bale
+
+    # NB: each sample is 2 byte big ...
+    n, k = window_size * 2, overlap_size * 2
+    bs = bytearray(n + k)
+    xs = memoryview(bs)[k:]
+    ys = memoryview(bs)[-k:]
+    zs = memoryview(bs)[:k]
+    off = 0
+    off_inc = n // 2
+    with subprocess.Popen(['ffmpeg', '-loglevel', 'warning', '-vn', '-i', filename,
+                           # only left channel
+                           '-filter', 'pan=1c|c0=c0', '-f', 'wav',
+                           '-acodec', 'pcm_s16le', '-ar', str(rate), 'pipe:'],
+                          stdout=subprocess.PIPE, stdin=subprocess.DEVNULL) as p:
+        while True:
+            l = p.stdout.readinto(xs)
+            if off == 0:
+                yield off, to_wav(xs)
+                off = (n - k) // 2
+            elif l < len(xs):
+                yield off, to_wav(memoryview(bs)[:k + l])
+                break
+            else:
+                yield off, to_wav(bs)
+                off += off_inc
+            zs[:] = ys
+        # NB: context exit also waits but without a timeout
+        p.wait(60)
+
+
+def find_silence(xs, needle, rate):
+    ys = np.square(xs)
+    n = len(needle)
+    # NB: if we scale ys by `/n` and `np.sqrt()` zs
+    #     we would get the the sliding RMS of the input signal
+    zs = signal.correlate(ys, needle, mode='full', method='fft')
+    t = 0.01 * np.max(zs) # multiply by 0.05 or so when using the RMS
+    # i.e. filter out  bottoms that lie below the threshold t
+    # and stretch over at least n samples, i.e. our silence
+    rs = np.nonzero(np.diff(np.nonzero(zs > t)[0]) > n)[0]
+    rs += n
+    return rs
+
+
+def align_window(hay_fn, needles, rate, thresh):
+    ys = [ np.dot(needle, needle) for needle in needles ]
+    #n = needle.size * 100
+    k = max(int(needle * rate) if type(needle) is float else needle.size for needle in needles)
+    n = k * 50
+    ysP = [ np.max(signal.correlate(needle, needle, mode='full', method='fft')) for needle in needles ]
+    if type(needles[0]) is not float:
+        assert abs(1 - ys[0] / ysP[0]) < 1e-5
+    if len(needles) > 1:
+        if type(needles[1]) is not float:
+            assert abs(1 - ys[1] / ysP[1]) < 1e-5
+    hs = [ thresh * y for y in ys ]
+    kks = [ [ np.zeros(0, dtype='int64') ] for _ in needles ]
+    if len(needles) == 2 and type(needles[1]) is float and needles[1] > 0.0:
+        silent_needle = np.ones(int(needles[1] * rate))
+    for off, bale in yield_window(hay_fn, n, k, rate):
+        for i, needle in enumerate(needles):
+            if type(needle) is float:
+                if needle == 0.0:
+                    if off == 0:
+                        ks = np.array([0], dtype='int64')
+                        kks[i].append(ks + off)
+                elif needle > 0.0:
+                    ks = find_silence(bale, silent_needle, rate)
+                    if ks.size:
+                        kks[i].append(ks + off)
+            else:
+                xs = signal.correlate(bale, needle, mode='full', method='fft')
+                ks, _ = signal.find_peaks(xs, height=hs[i], distance=10*rate)
+                if ks.size:
+                    kks[i].append(ks + off)
+
+    for i, needle in enumerate(needles):
+        if type(needle) is float:
+            if needle < 0.0:
+                ks = np.array([int(needle * -1.0 * rate)], dtype='int64')
+                kks[i].append(ks)
+
+    for i in range(len(kks)):
+        kks[i] = np.concatenate(kks[i])
+        if i == 0:
+            if len(kks) == 1:
+                kks[i][::2] -= min(int(needles[i]*rate), 0) if type(needles[i]) is float else needles[i].size
+            else:
+                kks[i] -= min(int(needles[i]*rate), 0) if type(needles[i]) is float else needles[i].size
+        kks[i] = kks[i] / rate
+
+    if len(needles) == 1:
+        return kks
+
+    kks[1] = pair_positions(kks[0], kks[1])
+
+    return kks
+
+
+def merge(rs):
+    if len(rs) == 1:
+        return rs[0]
+    xs, ys = rs
+    if xs.size != ys.size:
+        raise RuntimeError(f'Not all markers pair: {xs.size} beginnings vs. {ys.size} ends')
+    v = np.empty((xs.size + ys.size,), dtype=xs.dtype)
+    v[0::2] = xs
+    v[1::2] = ys
+    return v
+
+
+def ffmpeg_cutout(filename, begin, end, ofilename):
+    # XXX `-acodec copy` basically equivalent to `-c copy` when dealing with podcast files, right ...
+    r = subprocess.run(['ffmpeg', '-loglevel', 'warning',
+                        '-ss', str(begin), '-to', str(end), '-i', filename,
+                        '-acodec', 'copy', ofilename, '-y'],
+                       capture_output=True, text=True, check=True)
+
+
+def write_noise(ifilename, off_pairs, odir):
+    os.makedirs(odir, exist_ok=True)
+    _, ext = os.path.splitext(ifilename)
+    for i, (b, e) in enumerate(off_pairs, 1):
+        ofilename = f'{odir}/{i:02}{ext}'
+        log.info(f'Writing noise to {ofilename} ...')
+        ffmpeg_cutout(ifilename, b, e, ofilename)
+
+
+def ffmpeg_punchout(filename, ofilename):
+    # XXX `-acodec copy` basically equivalent to `-c copy` when dealing with podcast files, right ...
+    r = subprocess.run(['ffmpeg', '-loglevel', 'warning', '-f', 'concat', '-safe', '0',
+                        '-i', filename,
+                        '-acodec', 'copy', ofilename, '-y'],
+                       capture_output=True, text=True, check=True)
+
+
+def write_output(f, ifilename, offs_s, ofilename):
+    ls = itertools.cycle(('outpoint', 'inpoint'))
+    fn = os.path.abspath(ifilename)
+    print(f"file '{fn}'", file=f)
+    for l, off in zip(ls, offs_s):
+        print(f'{l} {off}', file=f)
+        if l == 'outpoint':
+            print(f"file '{fn}'", file=f)
+    if sys.version_info < (3, 12):
+        f.flush()
+    else:
+        f.close()
+    ffmpeg_punchout(f.name, ofilename)
+
+
+def write_json(offs_s, filename):
+    with open(filename, 'w') as f:
+        d = { 'offs_s': list(offs_s) }
+        json.dump(d, f)
+
+
+def main():
+    args = parse_args()
+    setup_logging(args.level)
+
+    if args.window:
+        if args.down:
+            hay_rate = args.hz
+        else:
+            hay_rate = probe_wav(args.input)
+        needles = []
+        if args.bof:
+            needles.append(0.0)
+        for i, needle_fn in enumerate(args.marks):
+            needle, needle_rate = read_wav(needle_fn, hay_rate)
+            if not args.down and hay_rate != needle_rate:
+                log.warning(f'Sample rate mismatch: {hay_rate} ({args.input}) vs. {needle_rate} ({needle_fn})')
+            needles.append(needle)
+        if args.silence:
+            needles.append(args.silence)
+        if args.eof:
+            d = float(ffprobe(args.input)['format']['duration'])
+            needles.append(d * -1)
+        rs = align_window(args.input, needles, hay_rate, args.thresh)
+        log.info(f'Template matches at: {rs} (s)')
+    else:
+        if args.down:
+            hay, hay_rate = read_wav(args.input, args.hz)
+            hay_rate = args.hz
+        else:
+            hay, hay_rate = read_wav(args.input)
+        rs = []
+        n = len(args.marks)
+        off = 0
+        if args.bof:
+            rs.append(np.array([0], dtype='float64'))
+            off += 1
+        for i, needle_fn in enumerate(args.marks, start=off):
+            needle, needle_rate = read_wav(needle_fn, hay_rate)
+            if not args.down and hay_rate != needle_rate:
+                log.warning(f'Sample rate mismatch: {hay_rate} ({args.input}) vs. {needle_rate} ({needle_fn})')
+            log.info(f'Aligning {os.path.basename(needle_fn)} with {os.path.basename(args.input)} (overlap={args.overlap}) ...')
+            offs_s = align(hay, needle, hay_rate, args.thresh, pos=(i, n), prev=(None if i==0 else rs[-1]), overlap=args.overlap)
+            rs.append(offs_s)
+            log.info(f'Template matches at: {offs_s} (s)')
+        if args.eof:
+            d = float(ffprobe(args.input)['format']['duration'])
+            rs.append(np.array([d], dtype='float64'))
+        if args.silence:
+            silent_needle = np.ones(int(args.silence * hay_rate))
+            ks = find_silence(hay, silent_needle, hay_rate)
+            if ks.size:
+                offs_s = ks / hay_rate
+                rs.append(offs_s)
+                log.info(f'Silence matched at: {offs_s} (s)')
+
+    offs_s = merge(rs)
+    log.info(f'All template match positions: {offs_s} (s)')
+    if offs_s.size == 0:
+        raise RuntimeError("Didn't find any noise regions")
+    if offs_s.size % 2 != 0:
+        raise RuntimeError('Found unbalanced markers')
+    if offs_s.size > 10:
+        raise RuntimeError(f'Found too many regions: {offs_s.size/2}')
+    off_pairs = offs_s.reshape((-1, 2))
+    pp = str(off_pairs).replace('\n', ' ')
+    log.info(f'Noisy regions: {pp} (s)')
+    ds = np.diff(off_pairs, axis=1).squeeze()
+    log.info(f'Duration(s) of noisy regions: {ds} (s)')
+    if np.max(ds) > args.length:
+        raise RuntimeError(f'Noise regions are longer than expected: {ds}')
+    if np.min(ds) < 0:
+        raise RuntimeError(f'Noise markers switched: {ds}')
+    if args.trash:
+        write_noise(args.input, off_pairs, args.trash)
+    if args.json:
+        write_json(offs_s, args.json)
+    if args.output:
+        log.info(f'Writing cut {args.output} ...')
+        if args.cut_file:
+            with open(args.cut_file, 'w') as f:
+                write_output(f, args.input, offs_s, args.output)
+        else:
+            if sys.version_info < (3, 12):
+                kw = {}
+            else:
+                kw = { 'delete_on_close': False }
+            with tempfile.NamedTemporaryFile('w', **kw) as f:
+                write_output(f, args.input, offs_s, args.output)
+
+
+def mainP():
+    try:
+        return main()
+    except subprocess.CalledProcessError as e:
+        print(f'Command {e.cmd} failed ({e.returncode}): {e.stderr}', file=sys.stderr)
+        traceback.print_exception(e, file=sys.stderr)
+        return 1
+
+
+if __name__ == '__main__':
+    sys.exit(mainP())
+
