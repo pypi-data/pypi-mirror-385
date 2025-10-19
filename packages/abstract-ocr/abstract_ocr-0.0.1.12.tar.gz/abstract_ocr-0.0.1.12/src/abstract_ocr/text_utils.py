1aaf66191437d0b270573f42d7425026a2724f78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-19 04:07:00 +0000

    Add package abstract-ocr/0.0.1.12/abstract_ocr-0.0.1.12.tar.gz

---
+++ packages/abstract-ocr/abstract_ocr-0.0.1.12.tar.gz/abstract_ocr-0.0.1.12/src/abstract_ocr/text_utils.py
@@ -0,0 +1,24 @@
+from .functions import (logger,
+                        Counter,
+                        get_generator as generator,
+                        get_summary,
+                        generate_with_bigbird,
+                        refine_with_gpt,
+                        generate_media_url,
+                        split_to_chunk,
+                        calculate_keyword_density,
+                        refine_keywords,
+                        extract_keywords_nlp,
+                        get_keybert_model,
+                        refine_keywords,
+                        chunk_summaries,
+                        generate_media_url,
+                        chunk_summaries,
+                        get_content_length,
+                        refine_keywords as get_keybert)
+from .ocr_utils import extract_text_from_image
+
+
+
+
+
