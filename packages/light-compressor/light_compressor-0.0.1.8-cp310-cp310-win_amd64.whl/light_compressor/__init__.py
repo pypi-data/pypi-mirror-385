0e116aa7a15d268afff7da2e19769962b7a9dbda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-19 03:25:31 +0000

    Add package light-compressor/0.0.1.8/light_compressor-0.0.1.8-cp310-cp310-win_amd64.whl

---
+++ packages/light-compressor/light_compressor-0.0.1.8-cp310-cp310-win_amd64.whl/light_compressor/__init__.py
@@ -0,0 +1,29 @@
+"""Library for read compressed stream and write compressed chunks."""
+
+from .compressor_method import (
+    auto_detector,
+    CompressionMethod,
+)
+from .compressors import (
+    LZ4Compressor,
+    ZSTDCompressor,
+)
+from .decompressors import (
+    LZ4Decompressor,
+    ZSTDDecompressor,
+)
+from .reader import define_reader
+from .writer import define_writer
+
+
+__all__ = (
+    "auto_detector",
+    "define_reader",
+    "define_writer",
+    "CompressionMethod",
+    "LZ4Compressor",
+    "LZ4Decompressor",
+    "ZSTDCompressor",
+    "ZSTDDecompressor",
+)
+__version__ = "0.0.1.8"
