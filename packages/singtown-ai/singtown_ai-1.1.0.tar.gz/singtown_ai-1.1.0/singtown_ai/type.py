6398ab0eef9b12e4802a3dc7d59073afbde5efd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-20 16:05:20 +0000

    Add package singtown-ai/1.1.0/singtown_ai-1.1.0.tar.gz

---
+++ packages/singtown-ai/singtown_ai-1.1.0.tar.gz/singtown_ai-1.1.0/singtown_ai/type.py
@@ -0,0 +1,39 @@
+from pydantic import BaseModel
+from typing import List, Literal
+
+
+TaskStatus = Literal["PENDING", "RUNNING", "SUCCESS", "FAILED"]
+ProjectType = Literal["CLASSIFICATION", "OBJECT_DETECTION"]
+TaskType = Literal["TRAIN", "DEPLOY"]
+DatasetSubset = Literal["train", "val", "test"]
+
+
+class ObjectDetectionEntry(BaseModel):
+    label: str
+    bbox: List[float]
+
+
+class Annotation(BaseModel):
+    file: str
+    subset: DatasetSubset
+    classification: str = ""
+    object_detection: List[ObjectDetectionEntry] = []
+
+
+class Project(BaseModel):
+    labels: List[str]
+    type: ProjectType
+
+
+class TaskResponse(BaseModel):
+    project: Project
+    type: TaskType
+    status: TaskStatus
+    params: dict
+    trained_file: str = ""
+    metrics: List[dict] = []
+
+
+class LogEntry(BaseModel):
+    timestamp: float
+    content: str
