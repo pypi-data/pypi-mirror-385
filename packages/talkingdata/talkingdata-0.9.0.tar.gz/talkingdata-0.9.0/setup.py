1b81c92c6db1e4e209ae8c02a04c648db04c26d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-20 04:08:53 +0000

    Add package talkingdata/0.9.0/talkingdata-0.9.0.tar.gz

---
+++ packages/talkingdata/talkingdata-0.9.0.tar.gz/talkingdata-0.9.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+setup(
+    name="talkingdata",
+    version="0.9.0",
+    author="Mohammad Abdullah",
+    author_email="mabdullaha407@gmail.com",
+    description="A lightweight, multi-provider AI Data Analysis library using SQL reasoning on pandas DataFrames.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://mabdullah40.github.io/Open-Analysis/",
+    packages=find_packages(),
+    install_requires=[
+        "pandas",
+        "duckdb",
+        "html2text",
+        "python-dotenv",
+        "streamlit",
+    ],
+    extras_require={
+        "groq": ["groq"],
+        "gemini": ["google-genai"],
+        "openai": ["openai"]
+    }
+)
