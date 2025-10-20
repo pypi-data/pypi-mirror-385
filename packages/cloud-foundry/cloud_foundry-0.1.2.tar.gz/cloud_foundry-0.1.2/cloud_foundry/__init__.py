19dc3f63901e2cd0f665937823affe91219f28dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-20 16:05:28 +0000

    Add package cloud-foundry/0.1.2/cloud_foundry-0.1.2.tar.gz

---
+++ packages/cloud-foundry/cloud_foundry-0.1.2.tar.gz/cloud_foundry-0.1.2/cloud_foundry/__init__.py
@@ -0,0 +1,17 @@
+from cloud_foundry.utils.logger import logger  # noqa: F401
+from cloud_foundry.pulumi.function import Function  # noqa: F401
+from cloud_foundry.pulumi.function import import_function  # noqa: F401
+from cloud_foundry.pulumi.function import function  # noqa: F401
+from cloud_foundry.pulumi.python_function import python_function  # noqa: F401
+from cloud_foundry.pulumi.rest_api import RestAPI  # noqa: F401
+from cloud_foundry.pulumi.rest_api import rest_api  # noqa: F401
+
+from cloud_foundry.utils.openapi_editor import OpenAPISpecEditor  # noqa: F401
+from cloud_foundry.pulumi.site_bucket import site_bucket  # noqa: F401
+from cloud_foundry.pulumi.document_repository import document_repository  # noqa: F401
+from cloud_foundry.pulumi.cdn import cdn  # noqa: F401
+from cloud_foundry.pulumi.domain import domain  # noqa: F401
+
+from cloud_foundry.utils.names import resource_id  # noqa: F401
+
+# from cloud_foundry.mail_publisher import mail_publisher
