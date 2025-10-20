2f687676417c87bb03b565e94955359a73d45013
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-20 03:26:05 +0000

    Add package ai-snake-lab/0.10.1/ai_snake_lab-0.10.1-py3-none-any.whl

---
+++ packages/ai-snake-lab/ai_snake_lab-0.10.1-py3-none-any.whl/ai_snake_lab/constants/DColors.py
@@ -0,0 +1,22 @@
+"""
+constants/DColors.py
+
+    AI Snake Game Simulator
+    Author: Nadim-Daniel Ghaznavi
+    Copyright: (c) 2024-2025 Nadim-Daniel Ghaznavi
+    GitHub: https://github.com/NadimGhaznavi/ai
+    License: GPL 3.0
+"""
+
+from ai_snake_lab.utils.ConstGroup import ConstGroup
+
+
+class DColor(ConstGroup):
+    """Color constants"""
+
+    GREEN: str = "green"
+    RED: str = "red"
+    BLUE: str = "blue"
+    YELLOW: str = "yellow"
+    WHITE: str = "white"
+    BLACK: str = "black"
