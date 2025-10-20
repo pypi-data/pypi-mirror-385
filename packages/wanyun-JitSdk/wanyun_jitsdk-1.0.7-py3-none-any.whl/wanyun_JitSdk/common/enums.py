ea15c2cd3d23b6bb05f4606f1f9621cc0f62fa88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-20 15:05:18 +0000

    Add package wanyun-JitSdk/1.0.7/wanyun_jitsdk-1.0.7-py3-none-any.whl

---
+++ packages/wanyun-JitSdk/wanyun_jitsdk-1.0.7-py3-none-any.whl/wanyun_JitSdk/common/enums.py
@@ -0,0 +1,15 @@
+# -*-coding:utf-8-*-
+"""
+Created on 2024/11/13
+
+@author: 臧韬
+
+@desc: 默认描述
+"""
+
+
+class RequestMethodEnum:
+    GET = "GET"
+    POST = "POST"
+    DELETE = "DELETE"
+    PUT = "PUT"
