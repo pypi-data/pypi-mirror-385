cb540fdd5021f242e145237c0e1c306ee2f9e50b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-20 03:26:00 +0000

    Add package codecomplexity/0.1.2/codecomplexity-0.1.2.tar.gz

---
+++ packages/codecomplexity/codecomplexity-0.1.2.tar.gz/codecomplexity-0.1.2/codecomplexity/__init__.py
@@ -0,0 +1,21 @@
+"""
+CodeComplexity - A Python library for analyzing code complexity metrics
+"""
+
+__version__ = "0.1.2"
+
+from .analyzer import (
+    analyze_code,
+    analyze_file,
+    format_report,
+    ComplexityMetrics,
+    ComplexityAnalyzer,
+)
+
+__all__ = [
+    "analyze_code",
+    "analyze_file",
+    "format_report",
+    "ComplexityMetrics",
+    "ComplexityAnalyzer",
+]
---
+++ packages/codecomplexity/codecomplexity-0.1.2.tar.gz/codecomplexity-0.1.2/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="codecomplexity",
+    version="0.1.2",
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="A simple code complexity analyzer for Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/codecomplexity",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Quality Assurance",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
+    python_requires=">=3.8",
+    install_requires=[],
+    entry_points={
+        "console_scripts": [
+            "codecomplexity=codecomplexity.cli:main",
+        ],
+    },
+    extras_require={
+        "dev": [
+            "pytest>=7.0",
+            "pytest-cov>=4.0",
+            "black>=23.0",
+            "flake8>=6.0",
+        ],
+    },
+)
