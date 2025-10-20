1d615f34ac469bae9b3311f718248dae3c2d44c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-20 03:25:43 +0000

    Add package faim-sdk/0.1.1/faim_sdk-0.1.1.tar.gz

---
+++ packages/faim-sdk/faim_sdk-0.1.1.tar.gz/faim_sdk-0.1.1/faim_client/models/__init__.py
@@ -0,0 +1,9 @@
+"""Contains all the data models used in inputs/outputs"""
+
+from .error_response import ErrorResponse
+from .model_name import ModelName
+
+__all__ = (
+    "ErrorResponse",
+    "ModelName",
+)
