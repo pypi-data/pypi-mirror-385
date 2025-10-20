078f672b647a3b1bd1770e95163238ca72ac9d44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-20 14:04:51 +0000

    Add package geoseeq/0.7.11/geoseeq-0.7.11-py3-none-any.whl

---
+++ packages/geoseeq/geoseeq-0.7.11-py3-none-any.whl/geoseeq/blob_constructors.py
@@ -0,0 +1,6 @@
+"""Alias for id_constructors.from_blobs and id_constructors.from_uuids
+
+For backwards compatibility.
+"""
+from .id_constructors.from_blobs import *
+from .id_constructors.from_uuids import *
