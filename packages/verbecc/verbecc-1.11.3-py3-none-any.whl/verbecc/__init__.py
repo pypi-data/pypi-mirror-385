447c2fbd5018eda769fa228b5511fab55d8d6e75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-20 14:05:05 +0000

    Add package verbecc/1.11.3/verbecc-1.11.3-py3-none-any.whl

---
+++ packages/verbecc/verbecc-1.11.3-py3-none-any.whl/verbecc/__init__.py
@@ -0,0 +1,46 @@
+from verbecc.src.conjugator.conjugator import Conjugator
+from verbecc.src.defs.types.alternates_behavior import AlternatesBehavior
+from verbecc.src.defs.types.conjugation import (
+    MoodsConjugation,
+    MoodConjugation,
+    TenseConjugation,
+    PersonConjugation,
+    ConjugationInfo,
+    Conjugation,
+)
+from verbecc.src.defs.types.gender import Gender
+from verbecc.src.defs.types.person import Person, is_plural, is_singular
+from verbecc.src.defs.types.partiple_inflection import ParticipleInflection
+from verbecc.src.defs.types.language_codes import LangCodeISO639_1
+from verbecc.src.defs.types.mood import (
+    Mood,
+    MoodCa,
+    MoodEs,
+    MoodFr,
+    MoodIt,
+    MoodPt,
+    MoodRo,
+)
+from verbecc.src.defs.types.tense import (
+    Tense,
+    TenseCa,
+    TenseEs,
+    TenseFr,
+    TenseIt,
+    TensePt,
+    TenseRo,
+)
+import verbecc.src.defs.constants.localization as localization
+import verbecc.src.defs.constants.grammar_defines as grammar_defines
+from verbecc.src.parsers.mood_template import MoodTemplate
+from verbecc.src.parsers.verb import Verb
+from verbecc.src.defs.types.exceptions import (
+    ConjugatorError,
+    ConjugationTemplateError,
+    ConjugationsParserError,
+    VerbNotFoundError,
+    TemplateNotFoundError,
+    InvalidLangError,
+    InvalidMoodError,
+    InvalidTenseError,
+)
