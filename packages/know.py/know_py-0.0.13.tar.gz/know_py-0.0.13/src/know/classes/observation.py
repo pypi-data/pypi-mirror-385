64509ceb6d8f5368a800f8ed26a654a61901901b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-20 04:08:17 +0000

    Add package know.py/0.0.13/know_py-0.0.13.tar.gz

---
+++ packages/know.py/know_py-0.0.13.tar.gz/know_py-0.0.13/src/know/classes/observation.py
@@ -0,0 +1,24 @@
+# This is free and unencumbered software released into the public domain.
+
+from pydantic import BaseModel, Field
+from .percept import VisualPercept
+
+
+class Observation(BaseModel):
+    type: str = Field("Observation", alias="@type")
+    id: str = Field(..., serialization_alias="@id")
+    source: str | None = None
+    percepts: list[VisualPercept] = []
+
+    def to_json(self) -> str:
+        import json
+
+        return json.dumps(self.to_dict(), separators=(",", ":"))
+
+    def to_dict(self) -> dict[str, object]:
+        return self.model_dump(
+            by_alias=True,
+            exclude_unset=True,
+            exclude_none=True,
+            exclude_computed_fields=True,
+        )
