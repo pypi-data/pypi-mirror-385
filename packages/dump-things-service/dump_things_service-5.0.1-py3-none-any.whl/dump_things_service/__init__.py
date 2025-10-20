5d920d37e9cf6da66c16b09a9ffdbeb59fbe808a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-20 16:05:17 +0000

    Add package dump-things-service/5.0.1/dump_things_service-5.0.1-py3-none-any.whl

---
+++ packages/dump-things-service/dump_things_service-5.0.1-py3-none-any.whl/dump_things_service/__init__.py
@@ -0,0 +1,44 @@
+from enum import Enum
+from typing import (
+    Any,
+    Union,
+)
+
+from starlette.status import (
+    HTTP_200_OK,
+    HTTP_300_MULTIPLE_CHOICES,
+    HTTP_400_BAD_REQUEST,
+    HTTP_401_UNAUTHORIZED,
+    HTTP_403_FORBIDDEN,
+    HTTP_404_NOT_FOUND,
+    HTTP_413_REQUEST_ENTITY_TOO_LARGE as HTTP_413_CONTENT_TOO_LARGE,
+    HTTP_422_UNPROCESSABLE_ENTITY as HTTP_422_UNPROCESSABLE_CONTENT,
+    HTTP_500_INTERNAL_SERVER_ERROR,
+)
+
+__all__ = [
+    'Format',
+    'HTTP_200_OK',
+    'HTTP_300_MULTIPLE_CHOICES',
+    'HTTP_400_BAD_REQUEST',
+    'HTTP_401_UNAUTHORIZED',
+    'HTTP_403_FORBIDDEN',
+    'HTTP_404_NOT_FOUND',
+    'HTTP_413_CONTENT_TOO_LARGE',
+    'HTTP_422_UNPROCESSABLE_CONTENT',
+    'HTTP_500_INTERNAL_SERVER_ERROR',
+    'JSON',
+    'YAML',
+    'config_file_name',
+]
+
+
+class Format(str, Enum):
+    json = 'json'
+    ttl = 'ttl'
+
+
+JSON = Union[dict[str, Any], list[Any], str, int, float, None]
+YAML = JSON
+
+config_file_name = '.dumpthings.yaml'
