56ec09a65e684866ec408213986e1f568921b85a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-20 03:26:04 +0000

    Add package arcadedb-embedded/0.0.0/arcadedb_embedded-0.0.0-py3-none-any.whl

---
+++ packages/arcadedb-embedded/arcadedb_embedded-0.0.0-py3-none-any.whl/arcadedb_embedded/__init__.py
@@ -0,0 +1,7 @@
+"""ArcadeDB Embedded - Placeholder Release
+
+This is a placeholder release to reserve the package name on PyPI.
+The real package with full functionality is coming soon.
+"""
+
+__version__ = "0.0.0"
