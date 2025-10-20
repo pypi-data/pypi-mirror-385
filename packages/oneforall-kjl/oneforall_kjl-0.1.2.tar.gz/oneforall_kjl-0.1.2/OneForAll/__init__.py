a40e3d7db606461088c25b06047448c365b8f678
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-20 15:05:00 +0000

    Add package oneforall-kjl/0.1.2/oneforall_kjl-0.1.2.tar.gz

---
+++ packages/oneforall-kjl/oneforall_kjl-0.1.2.tar.gz/oneforall_kjl-0.1.2/OneForAll/__init__.py
@@ -0,0 +1,27 @@
+"""
+oneforall打包
+"""
+
+__version__ = "0.1.2"
+__author__ = "Your Name"
+__email__ = "your.email@example.com"
+
+import common
+import config
+import data
+import images
+import modules
+import thirdparty
+import oneforall, brute, takeover
+
+__all__ = [
+    "common",
+    "config",
+    "data",
+    "images",
+    "modules",
+    "thirdparty",
+    "oneforall",
+    "brute",
+    "takeover",
+]
