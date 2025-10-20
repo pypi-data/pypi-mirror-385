1bac36931d4aa29e9b3c72b6661581c99cd34708
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-20 15:06:16 +0000

    Add package oneforall-kjl/0.1.8/oneforall_kjl-0.1.8-py3-none-any.whl

---
+++ packages/oneforall-kjl/oneforall_kjl-0.1.8-py3-none-any.whl/oneforall_kjl/__init__.py
@@ -0,0 +1,21 @@
+"""
+oneforall打包
+"""
+
+__version__ = "0.1.8"
+__author__ = "Your Name"
+__email__ = "your.email@example.com"
+
+import oneforall_kjl.config
+import oneforall_kjl.modules
+import oneforall_kjl.oneforall, oneforall_kjl.brute, oneforall_kjl.takeover,oneforall_kjl.export
+
+# __all__ = [
+#     "common",
+#     "config",
+#     "modules",
+#     "oneforall",
+#     "export",
+#     "brute",
+#     "takeover",
+# ]
