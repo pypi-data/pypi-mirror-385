2345dbe2665a19983d2d712c5049c94d8a9a0d37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-20 16:04:59 +0000

    Add package flake8-pytest-style/2.2.0/flake8_pytest_style-2.2.0-py3-none-any.whl

---
+++ packages/flake8-pytest-style/flake8_pytest_style-2.2.0-py3-none-any.whl/flake8_pytest_style/visitors/__init__.py
@@ -0,0 +1,26 @@
+from .assertion import AssertionVisitor, UnittestAssertionVisitor
+from .fail import FailVisitor
+from .fixtures import FixturesVisitor
+from .imports import ImportsVisitor
+from .marks import MarksVisitor
+from .parametrize import ParametrizeVisitor
+from .patch import PatchVisitor
+from .raises import RaisesVisitor
+from .t_functions import TFunctionsVisitor
+from .try_except import TryExceptVisitor
+from .warns import WarnsVisitor
+
+__all__ = (
+    "AssertionVisitor",
+    "FailVisitor",
+    "FixturesVisitor",
+    "ImportsVisitor",
+    "MarksVisitor",
+    "ParametrizeVisitor",
+    "PatchVisitor",
+    "RaisesVisitor",
+    "TFunctionsVisitor",
+    "TryExceptVisitor",
+    "UnittestAssertionVisitor",
+    "WarnsVisitor",
+)
---
+++ packages/flake8-pytest-style/flake8_pytest_style-2.2.0-py3-none-any.whl/flake8_pytest_style/visitors/raises.py
@@ -0,0 +1,72 @@
+import ast
+
+from flake8_plugin_utils import Visitor
+
+from flake8_pytest_style.config import Config
+from flake8_pytest_style.errors import (
+    RaisesTooBroad,
+    RaisesWithMultipleStatements,
+    RaisesWithoutException,
+)
+from flake8_pytest_style.utils import (
+    get_qualname,
+    get_simple_call_args,
+    is_empty_string,
+    is_none,
+    is_nontrivial_with_statement,
+    is_raises_call,
+    is_raises_with,
+)
+
+
+class RaisesVisitor(Visitor[Config]):
+    def _check_raises_call(self, node: ast.Call) -> None:
+        """
+        Checks for violations regarding `pytest.raises` call args (PT010 and PT011).
+        """
+        args = get_simple_call_args(node)
+        exception = args.get_argument("expected_exception", position=0)
+        if not exception:
+            self.error_from_node(RaisesWithoutException, node)
+            return
+
+        exception_name = get_qualname(exception)
+        if exception_name not in self.config.raises_require_match_for:
+            return
+        match = args.get_argument("match")
+        if match is None or is_none(match) or is_empty_string(match):
+            self.error_from_node(RaisesTooBroad, node, exception=exception_name)
+
+    def _check_raises_with(self, node: ast.With) -> None:
+        """Checks for PT012."""
+        body = node.body
+
+        is_complex_body = False
+        if len(body) != 1:
+            is_complex_body = True
+        elif isinstance(
+            body[0],
+            (
+                ast.If,
+                ast.For,
+                ast.AsyncFor,
+                ast.While,
+                ast.Try,
+            ),
+        ):
+            is_complex_body = True
+        elif is_nontrivial_with_statement(body[0]):
+            is_complex_body = True
+
+        if is_complex_body:
+            self.error_from_node(RaisesWithMultipleStatements, node)
+
+    def visit_Call(self, node: ast.Call) -> None:
+        if is_raises_call(node):
+            self._check_raises_call(node)
+
+    def visit_With(self, node: ast.With) -> None:
+        if is_raises_with(node):
+            self._check_raises_with(node)
+
+        self.generic_visit(node)
