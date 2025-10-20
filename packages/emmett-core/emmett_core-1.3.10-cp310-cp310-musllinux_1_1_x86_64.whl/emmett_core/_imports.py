8eb061e91e8cd1e8aa9c7c2c84ded2f1750173e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-20 04:08:31 +0000

    Add package emmett-core/1.3.10/emmett_core-1.3.10-cp310-cp310-musllinux_1_1_x86_64.whl

---
+++ packages/emmett-core/emmett_core-1.3.10-cp310-cp310-musllinux_1_1_x86_64.whl/emmett_core/_imports.py
@@ -0,0 +1,12 @@
+try:
+    import orjson
+except ImportError:
+    orjson = None
+try:
+    import rapidjson
+except ImportError:
+    rapidjson = None
+try:
+    import granian
+except ImportError:
+    granian = None
