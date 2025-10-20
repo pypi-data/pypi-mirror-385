826ebe0c9b572c33b25ecb302406fa07ae03c172
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-20 03:25:28 +0000

    Add package inlayr/0.1.0/inlayr-0.1.0-py3-none-any.whl

---
+++ packages/inlayr/inlayr-0.1.0-py3-none-any.whl/inlayr/rpcs/solana.py
@@ -0,0 +1,23 @@
+"""
+Implementation of solana chain connector.
+
+Dependencies
+------------
+* `solana` (RPC client + transaction objects)
+"""
+
+from __future__ import annotations
+
+from ..utils.constants import ConstDict
+
+from solana.rpc.api import Client
+
+class RPC:
+    def __init__(self, chain_id: int | None, headers: dict = {}):
+        self.name = "Solana"
+
+        self.rpc_url = "https://api.mainnet-beta.solana.com"
+        self.chain_id = chain_id
+
+        self.client = Client(self.rpc_url)
+        self.headers = headers
