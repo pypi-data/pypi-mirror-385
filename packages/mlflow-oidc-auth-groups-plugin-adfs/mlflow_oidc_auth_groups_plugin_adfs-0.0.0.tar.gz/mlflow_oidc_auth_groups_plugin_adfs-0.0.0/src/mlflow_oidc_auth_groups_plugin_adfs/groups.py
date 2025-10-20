f050d671bb08f26106853bca3f20bd68063c8ec4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-20 17:04:46 +0000

    Add package mlflow-oidc-auth-groups-plugin-adfs/0.0.0/mlflow_oidc_auth_groups_plugin_adfs-0.0.0.tar.gz

---
+++ packages/mlflow-oidc-auth-groups-plugin-adfs/mlflow_oidc_auth_groups_plugin_adfs-0.0.0.tar.gz/mlflow_oidc_auth_groups_plugin_adfs-0.0.0/src/mlflow_oidc_auth_groups_plugin_adfs/groups.py
@@ -0,0 +1,16 @@
+
+def _normalize_list(groups) -> list:
+    """
+        Return always list from str or list
+    """
+    if isinstance(groups, str):
+        return [groups]
+    elif isinstance(groups, list):
+        return groups
+    else:
+        raise TypeError("Input must be a string or a list")
+
+
+def get_user_groups(token) -> list:
+    groups = token["userinfo"]["groups"]
+    _normalize_list(groups)
