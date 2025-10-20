50b5b6db85962825492e9d9e0a0afd4b4b9088f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-20 16:04:59 +0000

    Add package flake8-pytest-style/2.2.0/flake8_pytest_style-2.2.0.tar.gz

---
+++ packages/flake8-pytest-style/flake8_pytest_style-2.2.0.tar.gz/flake8_pytest_style-2.2.0/flake8_pytest_style/config.py
@@ -0,0 +1,51 @@
+from enum import Enum
+from typing import Any, List, NamedTuple, Type
+
+
+def enum_choices(enum: Type[Enum]) -> List[Any]:
+    return [member.value for member in enum]
+
+
+class ParametrizeNamesType(Enum):
+    CSV = "csv"
+    TUPLE = "tuple"
+    LIST = "list"
+
+
+class ParametrizeValuesType(Enum):
+    TUPLE = "tuple"
+    LIST = "list"
+
+
+class ParametrizeValuesRowType(Enum):
+    TUPLE = "tuple"
+    LIST = "list"
+
+
+class Config(NamedTuple):
+    fixture_parentheses: bool
+    raises_require_match_for: List[str]
+    warns_require_match_for: List[str]
+    parametrize_names_type: ParametrizeNamesType
+    parametrize_values_type: ParametrizeValuesType
+    parametrize_values_row_type: ParametrizeValuesRowType
+    mark_parentheses: bool
+
+
+DEFAULT_CONFIG = Config(
+    fixture_parentheses=False,
+    raises_require_match_for=[
+        "BaseException",
+        "Exception",
+        "ValueError",
+        "IOError",
+        "OSError",
+        "EnvironmentError",
+        "socket.error",
+    ],
+    warns_require_match_for=["Warning", "UserWarning", "DeprecationWarning"],
+    parametrize_names_type=ParametrizeNamesType.TUPLE,
+    parametrize_values_type=ParametrizeValuesType.LIST,
+    parametrize_values_row_type=ParametrizeValuesRowType.TUPLE,
+    mark_parentheses=False,
+)
---
+++ packages/flake8-pytest-style/flake8_pytest_style-2.2.0.tar.gz/flake8_pytest_style-2.2.0/flake8_pytest_style/visitors/fail.py
@@ -0,0 +1,35 @@
+import ast
+
+from flake8_plugin_utils import Visitor
+
+from flake8_pytest_style.config import Config
+from flake8_pytest_style.errors import AssertAlwaysFalse, FailWithoutMessage
+from flake8_pytest_style.utils import (
+    get_simple_call_args,
+    is_empty_string,
+    is_fail_call,
+    is_falsy_constant,
+)
+
+
+class FailVisitor(Visitor[Config]):
+    def _check_fail_call(self, node: ast.Call) -> None:
+        """Checks for PT016."""
+        args = get_simple_call_args(node)
+        # Since pytest 7.0 the argument is named 'reason', and 'msg' is deprecated but
+        # supported (at the time of writing this code).  We check 'reason', then first
+        # positional argument, and then 'msg'.  The edge cases like
+        # `pytest.fail('foo', msg='bar')` and `pytest.fail(reason='foo', msg='bar')`
+        # are deliberately ignored.
+        message_argument = args.get_argument("reason", 0) or args.get_argument("msg")
+        if not message_argument or is_empty_string(message_argument):
+            self.error_from_node(FailWithoutMessage, node)
+
+    def visit_Assert(self, node: ast.Assert) -> None:
+        """Checks for PT015."""
+        if is_falsy_constant(node.test):
+            self.error_from_node(AssertAlwaysFalse, node)
+
+    def visit_Call(self, node: ast.Call) -> None:
+        if is_fail_call(node):
+            self._check_fail_call(node)
