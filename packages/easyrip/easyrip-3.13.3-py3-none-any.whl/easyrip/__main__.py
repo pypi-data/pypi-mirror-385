67ffefaf312ed9eff45b2c54ca6de3dfac66fed8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-20 14:05:16 +0000

    Add package easyrip/3.13.3/easyrip-3.13.3-py3-none-any.whl

---
+++ packages/easyrip/easyrip-3.13.3-py3-none-any.whl/easyrip/__main__.py
@@ -0,0 +1,41 @@
+import sys
+from typing import NoReturn
+
+import Crypto
+import fontTools
+
+from .easyrip_main import Ripper, get_input_prompt, init, log, run_command
+
+
+def run() -> NoReturn:
+    init(True)
+
+    log.debug(f"pycryptodome: v{Crypto.__version__}")
+    log.debug(f"fonttools: v{fontTools.__version__}")
+
+    Ripper.ripper_list = []
+
+    if len(sys.argv) > 1:
+        run_command(sys.argv[1:])
+        if len(Ripper.ripper_list) == 0:
+            sys.exit()
+
+    while True:
+        try:
+            command = input(get_input_prompt(is_color=True))
+            sys.stdout.flush()
+            sys.stderr.flush()
+        except KeyboardInterrupt:
+            print(
+                f"\033[{91 if log.default_background_color == 41 else 31}m^C\033[{log.default_foreground_color}m"
+            )
+            continue
+        except EOFError:
+            log.debug("Manually force exit")
+            sys.exit()
+
+        if not run_command(command):
+            log.warning("Stop run command")
+
+
+run()
