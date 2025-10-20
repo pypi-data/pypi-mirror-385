eef55c9bc99d84f0d49149506bb6a61515e848fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-20 03:25:55 +0000

    Add package ml-trainer-sdk/0.2.2/ml_trainer_sdk-0.2.2.tar.gz

---
+++ packages/ml-trainer-sdk/ml_trainer_sdk-0.2.2.tar.gz/ml_trainer_sdk-0.2.2/ml_trainer/tasks/task_factory.py
@@ -0,0 +1,19 @@
+import abc
+from abc import abstractmethod
+
+# Abstract factory design pattern
+class AbstractTaskFactory(abc.ABC):
+    @abstractmethod
+    def create_dataset(self, config):
+        msg = "create dataset not implemented"
+        raise ValueError(msg)
+    
+    @abstractmethod
+    def create_model(self, config):
+        msg = "create model not implemented"
+        raise ValueError(msg)
+
+    @abstractmethod
+    def create_trainer(self, config):
+        msg = "create trainer not implemented"
+        raise ValueError(msg)
