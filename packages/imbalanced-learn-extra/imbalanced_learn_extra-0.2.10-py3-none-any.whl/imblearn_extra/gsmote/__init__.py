520502cd8adb71798247763506b6703622715f32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-20 15:05:48 +0000

    Add package imbalanced-learn-extra/0.2.10/imbalanced_learn_extra-0.2.10-py3-none-any.whl

---
+++ packages/imbalanced-learn-extra/imbalanced_learn_extra-0.2.10-py3-none-any.whl/imblearn_extra/gsmote/__init__.py
@@ -0,0 +1,10 @@
+"""Implementation of the Geometric SMOTE algorithm.
+
+A geometrically enhanced drop-in replacement for SMOTE. It is compatible with scikit-learn and imbalanced-learn.
+"""
+
+from __future__ import annotations
+
+from .geometric_smote import SELECTION_STRATEGIES, GeometricSMOTE, make_geometric_sample
+
+__all__: list[str] = ['SELECTION_STRATEGIES', 'GeometricSMOTE', 'make_geometric_sample']
