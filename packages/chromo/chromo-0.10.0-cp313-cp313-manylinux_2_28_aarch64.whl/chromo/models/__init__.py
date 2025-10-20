f13dd540f68c97451fc47427c999991a4fa8a8b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-20 15:05:13 +0000

    Add package chromo/0.10.0/chromo-0.10.0-cp313-cp313-manylinux_2_28_aarch64.whl

---
+++ packages/chromo/chromo-0.10.0-cp313-cp313-manylinux_2_28_aarch64.whl/chromo/models/__init__.py
@@ -0,0 +1,42 @@
+from chromo.models.dpmjetIII import DpmjetIII191, DpmjetIII193, DpmjetIII307
+from chromo.models.epos import EposLHC, EposLHCR, EposLHCRHadrRescattering
+from chromo.models.phojet import Phojet112, Phojet191, Phojet193
+from chromo.models.pythia6 import Pythia6
+from chromo.models.pythia8 import Pythia8
+from chromo.models.qgsjet import QGSJet01d, QGSJetII03, QGSJetII04, QGSJetIII
+from chromo.models.sibyll import (
+    Sibyll21,
+    Sibyll23c,
+    Sibyll23d,
+    Sibyll23dStarMixed,
+    Sibyll23e,
+    Sibyll23eStarMixed,
+)
+from chromo.models.sophia import Sophia20
+from chromo.models.urqmd import UrQMD34
+
+__all__ = (
+    "DpmjetIII191",
+    "DpmjetIII193",
+    "DpmjetIII307",
+    "EposLHC",
+    "EposLHCR",
+    "EposLHCRHadrRescattering",
+    "Phojet112",
+    "Phojet191",
+    "Phojet193",
+    "Pythia6",
+    "Pythia8",
+    "QGSJet01d",
+    "QGSJetII03",
+    "QGSJetII04",
+    "QGSJetIII",
+    "Sibyll21",
+    "Sibyll23c",
+    "Sibyll23d",
+    "Sibyll23dStarMixed",
+    "Sibyll23e",
+    "Sibyll23eStarMixed",
+    "Sophia20",
+    "UrQMD34",
+)
