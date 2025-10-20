4a71520df1f213f631e5c9af6cbc9fa8b217133b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-20 05:05:17 +0000

    Add package dtpyfw/0.5.9/dtpyfw-0.5.9.tar.gz

---
+++ packages/dtpyfw/dtpyfw-0.5.9.tar.gz/dtpyfw-0.5.9/dtpyfw/core/__init__.py
@@ -0,0 +1,22 @@
+from ..core.require_extra import require_extra
+
+__all__ = (
+    "async",
+    "chunking",
+    "enums",
+    "env",
+    "exception",
+    "file_folder",
+    "hashing",
+    "jsonable_encoder",
+    "request",
+    "require_extra",
+    "retry",
+    "safe_access",
+    "singleton",
+    "slug",
+    "url",
+    "validation",
+)
+
+require_extra("core", "requests")
