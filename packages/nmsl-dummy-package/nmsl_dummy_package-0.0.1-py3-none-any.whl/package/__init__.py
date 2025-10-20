f5c31c17f3cdc9bfb7922d1790af8cb504254e49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-20 16:04:59 +0000

    Add package nmsl-dummy-package/0.0.1/nmsl_dummy_package-0.0.1-py3-none-any.whl

---
+++ packages/nmsl-dummy-package/nmsl_dummy_package-0.0.1-py3-none-any.whl/package/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+__version__ = '0.0.1'
+from .module import sum
+
+__all__ = ['sum'] # 패키지로 배포할 함수명 작성
+
+__version__ = '0.0.1' # 권장 선택사항
+__author__ = 'sohyun Yang' # 선택사항
+__email__ = '1234@dgist.ac,kr' # 선택사항
---
+++ packages/nmsl-dummy-package/nmsl_dummy_package-0.0.1-py3-none-any.whl/package/module.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+모듈 파일에는 패키지로 만들고자 하는 기능이 담긴 함수를 정의
+"""
+def sum(a: int, b: int) -> int:
+  return a + b
