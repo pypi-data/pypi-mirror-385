6fd0665129fd84a73cfdbcfddacf1d59542976d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-20 06:04:43 +0000

    Add package francisco-diaz/0.0.4/francisco_diaz-0.0.4-py3-none-any.whl

---
+++ packages/francisco-diaz/francisco_diaz-0.0.4-py3-none-any.whl/francisco_diaz/__main__.py
@@ -0,0 +1,9 @@
+from .speakers import Francisco
+
+
+def main():
+    Francisco().print_name()
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/francisco-diaz/francisco_diaz-0.0.4-py3-none-any.whl/francisco_diaz/speakers/francisco.py
@@ -0,0 +1,5 @@
+from francisco_diaz.speaker import Speaker
+
+
+class Francisco(Speaker):
+    name = "Francisco"
