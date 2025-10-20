e81435ce00d771a32fce31c253752b567176c127
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-20 03:25:43 +0000

    Add package id-translation/0.15.3/id_translation-0.15.3.tar.gz

---
+++ packages/id-translation/id_translation-0.15.3.tar.gz/id_translation-0.15.3/src/id_translation/dio/__init__.py
@@ -0,0 +1,30 @@
+"""Integration for insertion and extraction of IDs and translations to and from various data structures.
+
+See :doc:`/documentation/translation-io` for help.
+"""
+
+from ._data_structure_io import DataStructureIO
+from ._resolve import (
+    _ENTRYPOINT_GROUP,
+    get_resolution_order,
+    is_registered,
+    load_integrations,
+    register_io,
+    resolve_io,
+)
+
+ENTRYPOINT_GROUP: str = _ENTRYPOINT_GROUP  # Public reexport. Makes Sphinx happy.
+"""Group used to discover :class:`DataStructureIO` integrations.
+
+See :func:`load_integrations` and :py:func:`importlib.metadata.entry_points` for details.
+"""
+
+__all__ = [
+    "ENTRYPOINT_GROUP",
+    "DataStructureIO",
+    "get_resolution_order",
+    "is_registered",
+    "load_integrations",
+    "register_io",
+    "resolve_io",
+]
---
+++ packages/id-translation/id_translation-0.15.3.tar.gz/id_translation-0.15.3/src/id_translation/mapping/exceptions.py
@@ -0,0 +1,75 @@
+"""Mapping errors."""
+
+from typing import Any as _Any
+
+
+class MappingError(Exception):
+    """Base exception class for all mapping-related issues."""
+
+    def __init__(self, msg: str, ref: str = "") -> None:
+        link = "https://id-translation.readthedocs.io/en/stable/documentation/mapping-primer.html"
+        if ref:
+            link += f"#{ref}"
+        super().__init__(f"{msg}\n\nFor help, please refer to the {link} page.")
+
+
+class UnmappedValuesError(MappingError):
+    """Raised when there are unmapped values left after filtering and on_unmapped='raise'."""
+
+
+class ScoringDisabledError(MappingError):
+    """Indicates that the scoring logic has been disabled. Raised by :func:`.score_functions.disabled`."""
+
+    def __init__(self, value: _Any, candidates: _Any, context: _Any) -> None:
+        super().__init__(
+            "Scoring disabled.\n"
+            f"The Mapper is working in strict override-only mode, so the {value=} in {context=} "
+            f"cannot be mapped to any of the {candidates=}. Possible solutions:\n"
+            "    * Add an override or filter for this value, or\n"
+            "    * Set strict=False (silently refuse to map instead of raising), or\n"
+            "    * Choose an appropriate score function to use.",
+            ref="override-only-mapping",
+        )
+        self.value = value
+        self.candidates = candidates
+        self.context = context
+
+
+class AmbiguousScoreError(MappingError):
+    """Indicates that the scoring logic has produces ambiguous scores."""
+
+    def __init__(self, kind: str, key: _Any, match0: _Any, match1: _Any, cardinality: str, scores: str) -> None:
+        hint = f"\n{scores}\nInspect the matrix above for details. You may wish to use a different scoring method."
+        super().__init__(
+            f"Ambiguous mapping of {kind}={key!r}; matches ({match0}) and ({match1}) "
+            f"are in conflict since {cardinality=}.{hint}"
+        )
+
+
+class UserMappingError(MappingError):
+    """A user-defined mapping function did something forbidden."""
+
+    def __init__(self, msg: str, value: _Any, candidates: set[_Any]) -> None:
+        super().__init__(msg)
+        self.value = value
+        self.candidates = candidates
+
+
+class CardinalityError(MappingError):
+    """Base class for cardinality issues."""
+
+
+class MappingWarning(UserWarning):
+    """Base warning class for all mapping-related issues."""
+
+
+class UnmappedValuesWarning(MappingWarning):
+    """Raised when there are unmapped values left after filtering and on_unmapped='raise'."""
+
+
+class UserMappingWarning(MappingWarning):
+    """A user-defined mapping function did something strange."""
+
+
+class BadFilterError(MappingError):
+    """Invalid filter."""
