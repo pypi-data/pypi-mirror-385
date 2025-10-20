20bb08f7373a75cd1b6c48aae59a9ad2305f4889
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-20 03:25:49 +0000

    Add package rushrunning/0.1.2/rushrunning-0.1.2.tar.gz

---
+++ packages/rushrunning/rushrunning-0.1.2.tar.gz/rushrunning-0.1.2/rushrunning/__init__.py
@@ -0,0 +1,19 @@
+﻿import sys
+from functools import wraps
+
+from rushlib.output import print_red
+
+
+def launch(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except Exception as e:
+            print_red(f"Error: {e}")
+            sys.exit(1)
+        except KeyboardInterrupt:
+            print_red("\n\n操作已取消")
+            sys.exit(1)
+
+    return wrapper
