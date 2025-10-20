9e3ededb9535f0a71be5ca85590103861b6169e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-20 04:08:13 +0000

    Add package sagar-varma/0.1.0/sagar_varma-0.1.0.tar.gz

---
+++ packages/sagar-varma/sagar_varma-0.1.0.tar.gz/sagar_varma-0.1.0/README.md
@@ -0,0 +1,15 @@
+# TSnake
+
+A simple terminal-based snake game written in Python.
+
+## Installation
+
+```bash
+pip install tsnake
+```
+
+## Usage
+
+```bash
+tsnake
+```
---
+++ packages/sagar-varma/sagar_varma-0.1.0.tar.gz/sagar_varma-0.1.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sagar-varma',
+    version='0.1.0',
+    author='huiiy',
+    description='A simple terminal-based snake game.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/your_username/tsnake',  # Replace with your GitHub URL
+    packages=find_packages(),
+    install_requires=[
+        'unicurses',
+    ],
+    entry_points={
+        'console_scripts': [
+            'tsnake = tsnake.main:main',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Topic :: Games/Entertainment',
+    ],
+)
