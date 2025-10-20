a3691a2297ef92ba225e97ab0b84f840b7d2f0c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-20 05:05:05 +0000

    Add package ai-storyteller/0.0.1/ai_storyteller-0.0.1-py3-none-any.whl

---
+++ packages/ai-storyteller/ai_storyteller-0.0.1-py3-none-any.whl/storyteller/modules/st/enums.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+__all__ = ("ImageGeneratorEnum",)
+
+
+class ImageGeneratorEnum(Enum):
+    FLUX_TEXT_TO_IMAGE = "Flux text to image"
+    # DALL_E_3_TEXT_TO_IMAGE = "Dall-E 3 text to image"
+    # GEMINI_TEXT_TO_IMAGE = "Gemini text to image"
+    # FLUX_IMAGE_TO_IMAGE = "Flux image to image"
+    # DALL_E_3_IMAGE_TO_IMAGE = "Dall-E 3 image to image"
+    # GEMINI_IMAGE_TO_IMAGE = "Gemini image to image"
