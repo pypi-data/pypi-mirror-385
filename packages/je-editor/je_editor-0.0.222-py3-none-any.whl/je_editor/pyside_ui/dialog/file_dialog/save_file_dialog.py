a523c24be9eb28f706b6219a27e2641fd9895ebb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-20 03:25:53 +0000

    Add package je-editor/0.0.222/je_editor-0.0.222-py3-none-any.whl

---
+++ packages/je-editor/je_editor-0.0.222-py3-none-any.whl/je_editor/pyside_ui/dialog/file_dialog/save_file_dialog.py
@@ -0,0 +1,45 @@
+from __future__ import annotations
+
+import os
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+from je_editor.pyside_ui.code.auto_save.auto_save_manager import file_is_open_manager_dict
+from je_editor.utils.logging.loggin_instance import jeditor_logger
+
+if TYPE_CHECKING:
+    from je_editor.pyside_ui.main_ui.main_editor import EditorMain
+from PySide6.QtWidgets import QFileDialog
+
+from je_editor.pyside_ui.main_ui.editor.editor_widget import EditorWidget
+from je_editor.utils.file.save.save_file import write_file
+
+
+def choose_file_get_save_file_path(parent_qt_instance: EditorMain) -> bool:
+    """
+    :param parent_qt_instance: Pyside parent
+    :return: save code edit content to file
+    """
+    jeditor_logger.info("save_file_dialog.py choose_file_get_save_file_path"
+                        f" parent_qt_instance: {parent_qt_instance}")
+    widget = parent_qt_instance.tab_widget.currentWidget()
+    if isinstance(widget, EditorWidget):
+        file_path = QFileDialog().getSaveFileName(
+            parent=parent_qt_instance,
+            dir=os.getcwd(),
+            filter="""Python file (*.py);;
+            HTML file (*.html);;
+            File (*.*)"""
+        )[0]
+        if file_path is not None and file_path != "":
+            widget.current_file = file_path
+            write_file(file_path, widget.code_edit.toPlainText())
+            path = Path(file_path)
+            file_is_open_manager_dict.update({str(path): str(path.name)})
+            if widget.code_save_thread is not None:
+                widget.code_save_thread.file = file_path
+                widget.code_save_thread.editor = widget.code_edit
+            widget.rename_self_tab()
+            return True
+        return False
+    return False
---
+++ packages/je-editor/je_editor-0.0.222-py3-none-any.whl/je_editor/utils/redirect_manager/redirect_manager_class.py
@@ -0,0 +1,74 @@
+import logging
+import queue
+import sys
+
+from je_editor.utils.logging.loggin_instance import jeditor_logger
+
+
+class RedirectStdOut(logging.Handler):
+
+    # redirect logging std output to queue
+
+    def __init__(self):
+        super().__init__()
+
+    def write(self, content_to_write) -> None:
+        redirect_manager_instance.std_out_queue.put(content_to_write)
+
+    def emit(self, record: logging.LogRecord) -> None:
+        redirect_manager_instance.std_out_queue.put(self.format(record))
+
+
+class RedirectStdErr(logging.Handler):
+
+    # redirect logging stderr output to queue
+
+    def __init__(self):
+        super().__init__()
+
+    def write(self, content_to_write) -> None:
+        redirect_manager_instance.std_err_queue.put(content_to_write)
+
+    def emit(self, record: logging.LogRecord) -> None:
+        redirect_manager_instance.std_err_queue.put(self.format(record))
+
+
+class RedirectManager(object):
+    # Redirect all output to queue
+    def __init__(self):
+        jeditor_logger.info("Init RedirectManager")
+        self.std_err_queue = queue.Queue()
+        self.std_out_queue = queue.Queue()
+
+    @staticmethod
+    def set_redirect() -> None:
+        """
+        :return: None
+        """
+        jeditor_logger.info("RedirectManager set_redirect")
+        redirect_out = RedirectStdOut()
+        redirect_err = RedirectStdErr()
+        sys.stdout = redirect_out
+        sys.stderr = redirect_err
+        default_logger = logging.getLogger("JEditor_RedirectManager")
+        default_logger.addHandler(redirect_err)
+        skip_logger_list = ["JEditor", "FrontEngine",
+                            "AutomationIDE", "TestPioneer", "langchain", "langchain_core", "langchain_openai"]
+        for name in logging.root.manager.loggerDict.keys():
+            if name in skip_logger_list:
+                continue
+            else:
+                logging.getLogger(name).addHandler(redirect_err)
+
+    @staticmethod
+    def restore_std() -> None:
+        """
+        reset redirect
+        :return: None
+        """
+        jeditor_logger.info("RedirectManager restore_std")
+        sys.stdout = sys.__stdout__
+        sys.stderr = sys.__stderr__
+
+
+redirect_manager_instance = RedirectManager()
