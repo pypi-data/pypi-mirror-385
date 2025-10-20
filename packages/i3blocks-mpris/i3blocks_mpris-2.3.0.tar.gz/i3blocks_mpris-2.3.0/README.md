4c9cb5b8d67749ff7da9eb3ee9d209176a1ab1a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-20 04:08:36 +0000

    Add package i3blocks-mpris/2.3.0/i3blocks_mpris-2.3.0.tar.gz

---
+++ packages/i3blocks-mpris/i3blocks_mpris-2.3.0.tar.gz/i3blocks_mpris-2.3.0/README.md
@@ -0,0 +1,234 @@
+# i3blocks-mpris
+
+A persistent [i3blocks][i3blocks] blocklet for the [MPRIS][mpris-spec] D-Bus interface.
+
+[![screenshot][screenshot]][screencast]
+
+Click the image above to watch a [screencast][screencast].
+
+This project was previously known as **i3blocks-spotify-persist**.
+
+
+## Features
+
+* near-immediate updates thanks to the event-driven model: the blocket is a constantly running process receiving D-Bus signals
+* configurable output
+* configurable mouse click actions (i3blocks [version 1.5][i3blocks-1.5] or later is required)
+
+
+## Installation
+
+**Python version 3.8 or later is required.**
+
+The blocket can be installed from PyPI using `pip`:
+
+```shell
+python3 -m pip install [--user] i3blocks-mpris
+```
+
+Once the package is installed, there will be a blocket script named `i3blocks-mpris` somewhere depending on the presence of a `--user` pip flag (e.g., `/usr/local/bin/i3blocks-mpris` or `~/.local/bin/i3blocks-mpris`).
+
+To avoid dependecy hell, [pipx][pipx] can be used:
+
+```shell
+pipx install i3blocks-mpris
+```
+
+In this case the blocket script will be placed in `~/.local/bin` directory.
+
+
+## Dependencies
+
+Required (installed automatically):
+  * [PyGObject][pygobject]
+  * [dbus-python][dbus-python]
+
+Optional (installed manually):
+  * [Font Awesome][font-awesome] (for status icons)
+
+
+## Usage
+
+Add the following lines to your i3blocks config:
+
+```
+[mpris]
+command=/path/to/bin/i3blocks-mpris -c /path/to/config.json
+interval=persist
+```
+
+
+## Configuration
+
+The blocket can be configured using a JSON config file and/or command line arguments. The only required parameter is `player`. It must be specified using either the config or the command line argument. Other config parameters and the config itself are optional.
+
+### Config parameters
+
+#### player
+
+*Type:* string
+
+*Default value:* no default value, must be specified
+
+A name of the player, either a full bus name — `org.mpris.MediaPlayer2.<player>[.<instance>]` — or its `<player>[.<instance>]` part.
+
+Examples:
+
+  * org.mpris.MediaPlayer2.spotify
+  * org.mpris.MediaPlayer2.vlc.instance7389
+  * spotify
+  * vlc.instance7389
+
+#### format
+
+*Type:* string
+
+*Default value:* `{status}: {artist} – {title}`
+
+A template string with placeholders. Placeholder formats are `{field}` and `{field:filter}`.
+
+Supported fields:
+
+  * `status`, one of [enum][mpris-playbackstatus-type] values: `Playing`, `Paused`, `Stopped`
+  * `artist`
+  * `title`
+
+Supported filters:
+
+| Filter | Description | Example |
+|--------------|-----------------------------------------------------------------------------|-------------------------------------------|
+| `upper`      | [`str.upper`][python-docs-str-upper]                                        | “lorem Ipsum DOLor” → “LOREM IPSUM DOLOR” |
+| `lower`      | [`str.lower`][python-docs-str-lower]                                        | “lorem Ipsum DOLor” → “lorem ipsum dolor” |
+| `capitalize` | [`str.capitalize`][python-docs-str-capitalize]                              | “lorem Ipsum DOLor” → “Lorem ipsum dolor” |
+| `title`      | [`str.title`][python-docs-str-title]                                        | “lorem Ipsum DOLor” → “Lorem Ipsum Dolor” |
+| `icon`       | converts a textual `status` to an icon, see the `status_icons` option below | “Paused” → “⏸”                           |
+
+Any other Python 3.8+ format spec is also supported, [here are some examples][python-docs-str-format-examples].
+
+In particular, a long `artist` or `title` name can be shortened, centered, padded in a few ways. A new format spec has been added to
+truncate and add a suffix but only if the string has been shortened, the syntax for this case is `.<max_length>,<suffix>` and the
+last row of examples in this table use it:
+
+|              Artist/Title              |               Format              |                 Result                 |
+|----------------------------------------|-----------------------------------|----------------------------------------|
+| `Long Theater`                         | `{artist:.9}`                     | `Long Thea`                            |
+| `Toooooooooooool`                      | `{artist:…<10.9}`                 | `Toooooooo…`                           |
+| `Godzilla` / `Golderia`                | `{artist: ^10} - {title: ^10.4}`  | ` Godzilla  -    Gold   `              |
+| `Apparatus Superiority` / `Player Two` | `{artist:…<16.15} - {title:>15}`  | `Apparatus Super… -      Player Two`   |
+| `In Fire` / `Lan Connected`            | `{artist:.10,…} - {title:.10,…}`  | `In Fire - Lan Connec…`                |
+
+#### placeholder
+
+*Type:* string
+
+*Default value:* empty string
+
+A message displayed when there is no player. If an empty string (the default), the blocklet completely disappears.
+
+#### markup_escape
+
+*Type:* boolean
+
+*Default value:* `false`
+
+This option specifies whether to escape special characters (such as `<`, `>`, `&`) using corresponding XML entities. Set to `true` if Pango markup is used (`markup=pango` in your `i3blocks` config), `false` otherwise.
+
+#### status_icons
+
+*Type:* object
+
+*Default value:* `{"Playing": "\uf04b", "Paused": "\uf04c", "Stopped": "\uf04d"}`
+
+This option provides a mapping for the `icon` filter (see above). The default value uses icons from [Font Awesome][font-awesome].
+
+#### mouse_buttons
+
+*Type:* object
+
+*Default value:* `{"1": "PlayPause"}`
+
+This option provides a mapping of X11 mouse buttons numbers to [MPRIS methods][mpris-methods]. You can use the `xev` program to determine button numbers.
+
+#### sanitize_unicode
+
+*Type:* boolean
+
+*Default value:* `true`
+
+If this option is set to `true`, the blocklet removes some unicode characters (more specifically, characters belonging to `Cc`, `Cs`, `Co`, and `Cn` [general categories][sanitize-unicode-categories]). See [issue #9][sanitize-unicode-issue] for details.
+
+
+#### dedupe
+
+*Type:* boolean
+
+*Default value:* `true`
+
+For some reason, the Spotify app emits several identical signals for one action/event (e.g., it produces **four** `PropertiesChanged` signals when a track is played or paused). If this option is set `true`, the blocklet will compare the updated message with the previous one and print it only if it has changed. There is no reason to turn off deduplication except for debugging.
+
+### Config example
+
+```json
+{
+    "player": "spotify",
+    "format": "<span font_family='monospace' color='#ffa651' weight='bold'>{status:icon} {status:upper}</span> <span color='#72bf44' weight='bold'>{artist}</span><span color='#ffa651'>᛫</span><span color='#b2d235'>{title}</span>",
+    "markup_escape": true,
+    "status_icons": {
+        "Playing": "|>",
+        "Paused": "||",
+        "Stopped": "[]"
+    },
+    "mouse_buttons": {
+        "1": "PlayPause",
+        "9": "Previous",
+        "8": "Next"
+    }
+}
+
+```
+
+
+## Command line arguments
+
+  * `-h`, `--help` — show all command line arguments and exit
+  * `-c`, `--config` — a path to the config file (see above)
+
+The following arguments override corresponding config options or defaults (that is, command line arguments have the highest precedence):
+
+  * `-p`, `--player`
+  * `-f`, `--format`
+  * `--markup-escape` / `--no-markup-escape`
+  * `--sanitize-unicode` / `--no-sanitize-unicode`
+  * `--dedupe` / `--no-dedupe`
+
+
+## Changelog
+
+See [CHANGELOG.md][changelog].
+
+
+## License
+
+The [MIT License][license].
+
+
+[screenshot]: https://tinystash.undef.im/il/3wQUgnuCRyADYHZ4Vi6qN29p65njk1DdsjUu5WePUBNmUak7Z9y6CqNRnEzMN2pVBVsZvBDJ9GDyJUGGYd3Fgbqd.png
+[screencast]: https://tinystash.undef.im/il/2Xscwkh3rAhw2iqSr9XxJ2Meph57eXiHwkkWiAgroiuGPXB9fYnPJqgdYR7nR4B9U5hHvxxGtr8Sc3QaquwjHT38.mp4
+[license]: https://github.com/un-def/i3blocks-mpris/blob/master/LICENSE
+[changelog]: https://github.com/un-def/i3blocks-mpris/blob/master/CHANGELOG.md
+[i3blocks]: https://github.com/vivien/i3blocks
+[i3blocks-1.5]: https://github.com/vivien/i3blocks/releases/tag/1.5
+[dbus-python]: https://dbus.freedesktop.org/doc/dbus-python/
+[pygobject]: https://pygobject.readthedocs.io/en/latest/
+[font-awesome]: https://fontawesome.com/
+[pipx]: https://pipxproject.github.io/pipx/
+[mpris-spec]: https://specifications.freedesktop.org/mpris-spec/latest/
+[mpris-playbackstatus-type]: https://specifications.freedesktop.org/mpris-spec/latest/Player_Interface.html#Enum:Playback_Status
+[mpris-methods]: https://specifications.freedesktop.org/mpris-spec/latest/Player_Interface.html#methods
+[sanitize-unicode-issue]: https://github.com/un-def/i3blocks-mpris/issues/9
+[sanitize-unicode-categories]: https://en.wikipedia.org/wiki/Unicode_character_property#General_Category
+[python-docs-str-upper]: https://docs.python.org/3/library/stdtypes.html#str.upper
+[python-docs-str-lower]: https://docs.python.org/3/library/stdtypes.html#str.lower
+[python-docs-str-capitalize]: https://docs.python.org/3/library/stdtypes.html#str.capitalize
+[python-docs-str-title]: https://docs.python.org/3/library/stdtypes.html#str.title
+[python-docs-str-format-examples]: https://docs.python.org/3.8/library/string.html#format-examples
---
+++ packages/i3blocks-mpris/i3blocks_mpris-2.3.0.tar.gz/i3blocks_mpris-2.3.0/i3blocks_mpris.py
@@ -0,0 +1,493 @@
+import argparse
+import enum
+import html
+import json
+import os
+import re
+import string
+import sys
+import unicodedata
+from copy import deepcopy
+
+import dbus
+from dbus.mainloop.glib import DBusGMainLoop, threads_init
+from gi.repository import Gio, GioUnix, GLib
+
+
+__version__ = '2.3.0'
+__author__ = 'Dmitry Meyer <me@undef.im>'
+
+
+class MatchMode(enum.Enum):
+    UNKNOWN = 0
+    EXACT = 1
+    PREFIX = 2
+
+
+class Formatter(string.Formatter):
+
+    _FORMAT_FUNCS = {
+        'upper': str.upper,
+        'lower': str.lower,
+        'capitalize': str.capitalize,
+        'title': str.title,
+        'icon': 'status_icon',
+    }
+
+    _TRUNCATE_STR_WITH_SUFFIX_REGEX = re.compile(
+        r'^(?P<base_truncate>\.\d+),(?P<suffix>.+)$'
+    )
+
+    @classmethod
+    def truncate_with_suffix_func_generator(cls, format_spec):
+        truncate_match = cls._TRUNCATE_STR_WITH_SUFFIX_REGEX.fullmatch(
+            format_spec)
+        if truncate_match is None:
+            return None
+
+        def inner(value):
+            base_truncate = truncate_match.group('base_truncate')
+            suffix = truncate_match.group('suffix')
+            truncated = f'{value:{base_truncate}}'
+            if len(truncated) < len(value):
+                return truncated + suffix
+            return truncated
+
+        return inner
+
+    def __init__(
+        self, status_icons: dict[str, str] | None = None,
+        markup_escape: bool = False, sanitize_unicode: bool = True,
+    ):
+        self._status_icons = status_icons.copy() if status_icons else dict()
+        self._markup_escape = markup_escape
+        self._sanitize_unicode = sanitize_unicode
+
+    def format_field(self, value, format_spec: str):
+        if self._sanitize_unicode and isinstance(value, str):
+            value = self._do_sanitize_unicode(value)
+        format_func = self._FORMAT_FUNCS.get(format_spec)
+        if not format_func and isinstance(value, str):
+            format_func = self.truncate_with_suffix_func_generator(format_spec)
+        if format_func:
+            if isinstance(format_func, str):
+                format_func = getattr(self, '_format_func__' + format_func)
+            value = format_func(value)
+        else:
+            value = super().format_field(value, format_spec)
+        if self._markup_escape:
+            value = html.escape(value)
+        return value
+
+    def _do_sanitize_unicode(self, value: str) -> str:
+        """Removes all characters belonging to the `C` (“other”) categories
+        save for the `Cf` (“format”) category.
+        """
+        return ''.join(
+            char for char in value
+            if unicodedata.category(char) not in {'Cc', 'Cs', 'Co', 'Cn'}
+        )
+
+    def _format_func__status_icon(self, status) -> str:
+        return self._status_icons.get(status, '?')
+
+
+class MPRISBlocklet:
+
+    DEFAULT_CONFIG = {
+        # Format: {field} or {field:filter}
+        # Fields: status, artist, title
+        # Filters: icon (from status only), upper, lower, capitalize, title
+        'format': '{status}: {artist} – {title}',
+        # A message displayed when there is no player
+        'placeholder': '',
+        # Escape special characters (such as `<>&`) for Pango markup
+        'markup_escape': False,
+        # Remove `C` category unicode characters (except for `Cf`)
+        'sanitize_unicode': True,
+        # MPRIS `PlaybackStatus` property to icon mapping
+        'status_icons': {
+            'Playing': '\uf04b',   # 
+            'Paused': '\uf04c',   # 
+            'Stopped': '\uf04d',   # 
+        },
+        # X11 mouse button number to MPRIS method mapping
+        'mouse_buttons': {
+            '1': 'PlayPause',
+        },
+        # Do not print the same info multiple times if True
+        'dedupe': True,
+    }
+
+    MPRIS_BUS_NAME_PREFIX = 'org.mpris.MediaPlayer2.'
+    MPRIS_OBJECT_PATH = '/org/mpris/MediaPlayer2'
+    MPRIS_PLAYER_INTERFACE = 'org.mpris.MediaPlayer2.Player'
+
+    DBUS_BUS_NAME = 'org.freedesktop.DBus'
+    DBUS_OBJECT_PATH = '/org/freedesktop/DBus'
+    DBUS_ROOT_INTERFACE = 'org.freedesktop.DBus'
+    DBUS_PROPERTIES_INTERFACE = 'org.freedesktop.DBus.Properties'
+
+    _loop = None
+    _stdin_stream = None
+    _bus: dbus.SessionBus | None = None
+    _properties_changed_signal_match = None
+    _specific_name_owner_changed_signal_match = None
+    _any_name_owner_changed_signal_match = None
+    _player_connected = False
+    _match_mode: MatchMode
+
+    def __init__(self, bus_name, config=None):
+        if not bus_name.startswith(self.MPRIS_BUS_NAME_PREFIX):
+            bus_name = f'{self.MPRIS_BUS_NAME_PREFIX}{bus_name}'
+        # the current bus name; may be changed if the player allow multiple
+        # instances and the user specified only the player name part without
+        # the specific instance suffix
+        self._bus_name = bus_name
+        # for single-instance players or multi-instance players when the
+        # user specified the exact instance, this is actually the full name;
+        # for multi-instance players when the user provided only the non-unique
+        # player name without the instance, this is the bus name without
+        # the instance suffix
+        self._bus_name_prefix = bus_name
+        _config = deepcopy(self.DEFAULT_CONFIG)
+        if config:
+            for key, value in config.items():
+                if isinstance(value, dict):
+                    _config[key].update(value)
+                else:
+                    _config[key] = value
+        self._formatter = Formatter(
+            status_icons=_config['status_icons'],
+            markup_escape=_config['markup_escape'],
+            sanitize_unicode=_config['sanitize_unicode'],
+        )
+        self._format_string = _config['format']
+        self._placeholder = _config['placeholder']
+        self._mouse_buttons = _config['mouse_buttons']
+        self._dedupe = _config['dedupe']
+        self._last_info = None
+        self._last_status = None
+        self._last_metadata = None
+        # a dict used as an ordered set, keys — well-known names with unique
+        # instance suffixes, values — True
+        self._instances = {}
+
+    @classmethod
+    def create_loop(cls):
+        loop = GLib.MainLoop()
+        # See: https://dbus.freedesktop.org/doc/dbus-python/
+        # dbus.mainloop.html?highlight=thread#dbus.mainloop.glib.threads_init
+        threads_init()
+        DBusGMainLoop(set_as_default=True)
+        return loop
+
+    def bus_name_has_owner(self, bus_name: str):
+        return self._bus.name_has_owner(bus_name)
+
+    def init_bus(self):
+        self._bus = dbus.SessionBus()
+
+    def _connect_to_player(self):
+        self._player_connected = True
+        self._connect_to_properties_changed_signal()
+        self._connect_to_specific_name_owner_changed_signal()
+        self.show_initial_info()
+
+    def _disconnect_from_player(self):
+        self._player_connected = False
+        if self._match_mode != MatchMode.EXACT:
+            # _bus_name is volatile since it contains unique instance suffix,
+            # we need to connect to each instance each time
+            self._disconnect_from_properties_changed_signal()
+            self._disconnect_from_specific_name_owner_changed_signal()
+
+    def run(self, *, loop=None, read_stdin=True, nowait=False):
+        if loop is None:
+            self._loop = self.create_loop()
+        else:
+            self._loop = loop
+        self.init_bus()
+        # initially, we don't know which match mode to use
+        match_mode = MatchMode.UNKNOWN
+        player_found = False
+        if self.bus_name_has_owner(self._bus_name):
+            # either the player don't allow multiple instance, e.g.,
+            # `org.mpris.MediaPlayer2.spotify`, or the user specified the
+            # exact instance, e.g., `org.mpris.MediaPlayer2.chromium.instance2`
+            # in both cases, the exact name match is used
+            match_mode = MatchMode.EXACT
+            player_found = True
+        else:
+            self._find_instances()
+            instance_bus_name = self._pick_instance()
+            if instance_bus_name:
+                player_found = True
+                match_mode = MatchMode.PREFIX
+                self._bus_name = instance_bus_name
+        if not player_found and nowait:
+            return
+        self._match_mode = match_mode
+        if player_found:
+            self._connect_to_player()
+        else:
+            self.show_placeholder(only_if_not_empty=True)
+        if match_mode != MatchMode.EXACT:
+            self._connect_to_any_name_owner_changed_signal()
+        if read_stdin:
+            self.start_stdin_read_loop()
+        try:
+            self._loop.run()
+        finally:
+            self.stop_stdin_read_loop()
+
+    def _find_instances(self) -> None:
+        for name in self._bus.list_names():
+            self._maybe_add_instance(name)
+
+    def _maybe_add_instance(self, name: str) -> bool:
+        name_prefix = self._bus_name_prefix
+        if not name.startswith(name_prefix):
+            return False
+        maybe_prefix, _, _ = name.rpartition('.')
+        if maybe_prefix != name_prefix:
+            return False
+        self._instances[name] = True
+        return True
+
+    def _maybe_remove_instance(self, name: str) -> None:
+        name_prefix = self._bus_name_prefix
+        if not name.startswith(name_prefix):
+            return
+        maybe_prefix, _, _ = name.rpartition('.')
+        if maybe_prefix == name_prefix:
+            self._instances.pop(name, None)
+
+    def _pick_instance(self) -> str | None:
+        for bus_name in reversed(tuple(self._instances)):
+            if self.bus_name_has_owner(bus_name):
+                return bus_name
+            del self._instances[bus_name]
+        return None
+
+    def start_stdin_read_loop(self):
+        self._stdin_stream = Gio.DataInputStream.new(
+            GioUnix.InputStream.new(sys.stdin.fileno(), False))
+        self._stdin_stream.set_close_base_stream(True)
+        self._read_stdin_once()
+
+    def stop_stdin_read_loop(self):
+        if not self._stdin_stream:
+            return
+        self._stdin_stream.close_async(
+            io_priority=GLib.PRIORITY_DEFAULT,
+            callback=lambda *args: self._loop.quit(),
+        )
+        self._loop.run()
+        self._stdin_stream = None
+
+    def _read_stdin_once(self):
+        self._stdin_stream.read_line_async(
+            io_priority=GLib.PRIORITY_DEFAULT, callback=self._on_stdin_line)
+
+    def _on_stdin_line(self, stream, task):
+        try:
+            result = stream.read_line_finish(task)
+        except GLib.Error:
+            return
+        try:
+            button = result[0].decode()
+        except ValueError:
+            button = None
+        if button and self._player_connected:
+            method_name = self._mouse_buttons.get(button)
+            if method_name:
+                self._bus.call_async(
+                    bus_name=self._bus_name,
+                    object_path=self.MPRIS_OBJECT_PATH,
+                    dbus_interface=self.MPRIS_PLAYER_INTERFACE,
+                    method=method_name, signature='', args=[],
+                    reply_handler=None, error_handler=None,
+                )
+        self._read_stdin_once()
+
+    def _connect_to_properties_changed_signal(self):
+        if self._properties_changed_signal_match:
+            return
+        self._properties_changed_signal_match = self._bus.add_signal_receiver(
+            bus_name=self._bus_name,
+            dbus_interface=self.DBUS_PROPERTIES_INTERFACE,
+            signal_name='PropertiesChanged',
+            handler_function=self._on_properties_changed,
+        )
+
+    def _on_properties_changed(self, interface_name, changed_properties, _):
+        self.show_info(
+            status=changed_properties.get('PlaybackStatus'),
+            metadata=changed_properties.get('Metadata'),
+            only_if_changed=self._dedupe,
+        )
+
+    def _disconnect_from_properties_changed_signal(self):
+        if self._properties_changed_signal_match:
+            self._properties_changed_signal_match.remove()
+            self._properties_changed_signal_match = None
+
+    def _connect_to_specific_name_owner_changed_signal(self):
+        if self._specific_name_owner_changed_signal_match:
+            return
+        signal_match = self._bus.add_signal_receiver(
+            bus_name=self.DBUS_BUS_NAME,
+            path=self.DBUS_OBJECT_PATH,
+            dbus_interface=self.DBUS_ROOT_INTERFACE,
+            signal_name='NameOwnerChanged',
+            arg0=self._bus_name,
+            handler_function=self._on_specific_name_owner_changed,
+        )
+        self._specific_name_owner_changed_signal_match = signal_match
+
+    def _on_specific_name_owner_changed(self, _name, old_owner, new_owner):
+        if not old_owner and new_owner:
+            if not self._player_connected:
+                self._connect_to_player()
+        elif old_owner and not new_owner:
+            self._disconnect_from_player()
+            next_instance_bus_name: str | None = None
+            if self._match_mode == MatchMode.PREFIX:
+                next_instance_bus_name = self._pick_instance()
+            if next_instance_bus_name:
+                self._bus_name = next_instance_bus_name
+                self._connect_to_player()
+            else:
+                self.show_placeholder()
+                self._last_info = None
+
+    def _disconnect_from_specific_name_owner_changed_signal(self):
+        if self._specific_name_owner_changed_signal_match:
+            self._specific_name_owner_changed_signal_match.remove()
+            self._specific_name_owner_changed_signal_match = None
+
+    def _connect_to_any_name_owner_changed_signal(self):
+        if self._any_name_owner_changed_signal_match:
+            return
+        signal_match = self._bus.add_signal_receiver(
+            bus_name=self.DBUS_BUS_NAME,
+            path=self.DBUS_OBJECT_PATH,
+            dbus_interface=self.DBUS_ROOT_INTERFACE,
+            signal_name='NameOwnerChanged',
+            handler_function=self._on_any_name_owner_changed,
+        )
+        self._any_name_owner_changed_signal_match = signal_match
+
+    def _on_any_name_owner_changed(self, name, old_owner, new_owner):
+        if not old_owner and new_owner:
+            if name == self._bus_name_prefix:
+                self._match_mode = MatchMode.EXACT
+                self._bus_name = name
+                self._disconnect_from_any_name_owner_changed_signal()
+                self._connect_to_player()
+            else:
+                instance_added = self._maybe_add_instance(name)
+                if instance_added:
+                    self._match_mode = MatchMode.PREFIX
+                    if not self._player_connected:
+                        self._bus_name = name
+                        self._connect_to_player()
+        elif old_owner and not new_owner:
+            self._maybe_remove_instance(name)
+
+    def _disconnect_from_any_name_owner_changed_signal(self):
+        if self._any_name_owner_changed_signal_match:
+            self._any_name_owner_changed_signal_match.remove()
+            self._any_name_owner_changed_signal_match = None
+
+    def get_property(self, property_name):
+        return self._bus.call_blocking(
+            bus_name=self._bus_name,
+            object_path=self.MPRIS_OBJECT_PATH,
+            dbus_interface=self.DBUS_PROPERTIES_INTERFACE,
+            method='Get', signature='ss',
+            args=[self.MPRIS_PLAYER_INTERFACE, property_name],
+        )
+
+    def show_initial_info(self):
+        self.show_info(
+            status=self.get_property('PlaybackStatus'),
+            metadata=self.get_property('Metadata'),
+        )
+
+    def show_info(self, status=None, metadata=None, *, only_if_changed=False):
+        if status is None:
+            status = self._last_status
+        else:
+            self._last_status = status
+        if metadata is None:
+            metadata = self._last_metadata
+        else:
+            self._last_metadata = metadata
+        if status is None or metadata is None:
+            return
+        artist = ', '.join(metadata.get('xesam:artist', ()))
+        title = metadata.get('xesam:title', '')
+        info = self._formatter.format(
+            self._format_string,
+            status=status,
+            artist=artist,
+            title=title,
+        )
+        if not only_if_changed or self._last_info != info:
+            print(info, flush=True)
+            self._last_info = info
+
+    def show_placeholder(self, *, only_if_not_empty: bool = False):
+        if only_if_not_empty and not self._placeholder:
+            return
+        print(self._placeholder, flush=True)
+
+
+def _add_boolean_flag_group(
+    parser: argparse.ArgumentParser, name: str, dest: str | None = None,
+) -> None:
+    if dest is None:
+        dest = name.replace('-', '_')
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument(
+        f'--{name}', action='store_true', default=None, dest=dest)
+    group.add_argument(
+        f'--no-{name}', action='store_false', default=None, dest=dest)
+
+
+def _parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--config')
+    parser.add_argument('-p', '--player')
+    parser.add_argument('-f', '--format')
+    parser.add_argument('-n', '--placeholder')
+    _add_boolean_flag_group(parser, 'markup-escape')
+    _add_boolean_flag_group(parser, 'sanitize-unicode')
+    _add_boolean_flag_group(parser, 'dedupe')
+    parser.add_argument('--version', action='version', version=__version__)
+    args = parser.parse_args()
+    return args
+
+
+def _main():
+    args = _parse_args()
+    if args.config:
+        with open(os.path.abspath(args.config)) as fp:
+            config = json.load(fp)
+        player_from_config = config.pop('player', None)
+    else:
+        config = {}
+        player_from_config = None
+    player = args.player or player_from_config
+    if not player:
+        sys.exit('player is not specified')
+    for key, value in vars(args).items():
+        if key not in ['config', 'player'] and value is not None:
+            config[key] = value
+    MPRISBlocklet(bus_name=player, config=config).run()
+
+
+if __name__ == '__main__':
+    _main()
