548b7b35e40f681754d6c2fc80e20facfa01632f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-20 03:26:15 +0000

    Add package artifetch/0.0.0/artifetch-0.0.0-py3-none-any.whl

---
+++ packages/artifetch/artifetch-0.0.0-py3-none-any.whl/artifetch/providers/gitlab.py
@@ -0,0 +1,10 @@
+"""Download artifacts or job outputs from GitLab."""
+
+class GitLabProvider:
+    def __init__(self):
+        """Initialize GitLab API client."""
+        pass
+
+    def fetch(self, path: str, dest: str = ".", **kwargs):
+        """Download job artifact or other file from GitLab."""
+        pass
---
+++ packages/artifetch/artifetch-0.0.0-py3-none-any.whl/artifetch/utils.py
@@ -0,0 +1,9 @@
+"""Shared helpers: logging, filesystem, env, checksums, etc."""
+
+def setup_logging():
+    """Configure global logging."""
+    pass
+
+def ensure_dir(path: str):
+    """Ensure that a directory exists."""
+    pass
