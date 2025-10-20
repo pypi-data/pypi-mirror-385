1834ed64a38c6c5e3d0c481cf09f7c231da5f89e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-20 03:26:04 +0000

    Add package badldap/0.6.7/badldap-0.6.7-py3-none-any.whl

---
+++ packages/badldap/badldap-0.6.7-py3-none-any.whl/badldap/_version.py
@@ -0,0 +1,8 @@
+
+__version__ = "0.6.7"
+__banner__ = \
+"""
+# badldap %s 
+# Original Author: Tamas Jos (@skelsec)
+# Current Maintainer: Baptiste Crépin (baptiste@cravaterouge.com)
+""" % __version__
