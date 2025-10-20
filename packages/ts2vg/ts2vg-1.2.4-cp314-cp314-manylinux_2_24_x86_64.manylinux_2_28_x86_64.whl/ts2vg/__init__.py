f66f3de9783de8a3a774929622f27e7f2d2b73db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-20 03:25:46 +0000

    Add package ts2vg/1.2.4/ts2vg-1.2.4-cp314-cp314-manylinux_2_24_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/ts2vg/ts2vg-1.2.4-cp314-cp314-manylinux_2_24_x86_64.manylinux_2_28_x86_64.whl/ts2vg/__init__.py
@@ -0,0 +1,8 @@
+from ts2vg._version import __version__
+from ts2vg.graph.natural import NaturalVG
+from ts2vg.graph.horizontal import HorizontalVG
+
+__all__ = [
+    'NaturalVG',
+    'HorizontalVG',
+]
