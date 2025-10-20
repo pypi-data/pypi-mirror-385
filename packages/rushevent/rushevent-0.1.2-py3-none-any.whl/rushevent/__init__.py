2a4c55ce1f1d0a2a4e0b5a49bee2f4f2ca9720b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-20 03:25:55 +0000

    Add package rushevent/0.1.2/rushevent-0.1.2-py3-none-any.whl

---
+++ packages/rushevent/rushevent-0.1.2-py3-none-any.whl/rushevent/__init__.py
@@ -0,0 +1,59 @@
+﻿import pygame
+
+
+class _EventObj:
+    def __init__(self, event: pygame.event.Event):
+        self._event = event
+        if self._event is not None:
+            for prop, value in event.__dict__.items():
+                setattr(self, prop, value)
+
+    def __bool__(self):
+        return not self._event is None
+
+    def __str__(self):
+        return str(self._event)
+
+
+class _EventManager:
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+            cls._instance.initialized = False
+        return cls._instance
+
+    def __init__(self):
+        if not self.initialized:
+            self.initialized = True
+            self._events = {}
+
+    def _init(self):
+        self._quit = False
+        self._window_close = None
+
+    def update(self):
+        self._init()
+
+        for event in pygame.event.get():
+            self._events[event.type] = event
+
+    def get(self, key) -> _EventObj:
+        return _EventObj(self._events.get(key, None))
+
+
+class EventFinder:
+    @staticmethod
+    def init() -> bool:
+        pygame.init()
+        _EventManager()
+        return True
+
+    @staticmethod
+    def update() -> None:
+        _EventManager().update()
+
+    @staticmethod
+    def get(key) -> _EventObj:
+        return _EventManager().get(key)
