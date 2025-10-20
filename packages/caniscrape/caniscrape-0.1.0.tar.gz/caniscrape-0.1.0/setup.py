020ead30578d5dacfc86d6007faaa46f7f0489d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-20 06:04:44 +0000

    Add package caniscrape/0.1.0/caniscrape-0.1.0.tar.gz

---
+++ packages/caniscrape/caniscrape-0.1.0.tar.gz/caniscrape-0.1.0/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='caniscrape',
+    version='0.1.0',
+    author='Zaid Ahmed',
+    author_email='zaahme18@gmail.com',
+    description='Analyze website anti-bot protections before you scrape',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/ZA1815/caniscrape',
+    packages=find_packages(),
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+    ],
+    python_requires='>=3.9',
+    install_requires=[
+        'click>=8.0.0',
+        'rich>=13.0.0',
+        'aiohttp>=3.8.0',
+        'beautifulsoup4>=4.11.0',
+        'playwright>=1.40.0',
+        'curl-cffi>=0.5.0',
+        'requests>=2.28.0',
+    ],
+    entry_points={
+        'console_scripts': [
+            'caniscrape=caniscrape.cli:cli',
+        ],
+    },
+)
