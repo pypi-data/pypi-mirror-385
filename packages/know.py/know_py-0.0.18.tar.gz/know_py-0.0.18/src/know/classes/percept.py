e149d970e9ac71c211856233dabbd971bbf1f95e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-20 15:04:55 +0000

    Add package know.py/0.0.18/know_py-0.0.18.tar.gz

---
+++ packages/know.py/know_py-0.0.18.tar.gz/know_py-0.0.18/src/know/classes/percept.py
@@ -0,0 +1,22 @@
+# This is free and unencumbered software released into the public domain.
+
+from pydantic import Field
+from .thing import Thing
+
+
+class Percept(Thing):
+    type: str = Field("Percept", alias="@type")
+
+    def __init__(self, id: str | None = None, **kwargs: object):
+        super().__init__(id, **kwargs)
+
+
+class VisualPercept(Percept):
+    type: str = Field("VisualPercept", alias="@type")
+    source: str | None = None
+    subject: str
+    confidence: float | None = None
+    box: str | None = None
+
+    def __init__(self, id: str | None = None, **kwargs: object):
+        super().__init__(id, **kwargs)
