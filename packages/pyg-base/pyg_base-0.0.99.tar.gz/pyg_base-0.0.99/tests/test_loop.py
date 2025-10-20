dc4ee7b65bcf259f6044936ae78ab4a4f72151e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-20 17:04:41 +0000

    Add package pyg-base/0.0.99/pyg_base-0.0.99.tar.gz

---
+++ packages/pyg-base/pyg_base-0.0.99.tar.gz/pyg_base-0.0.99/tests/test_loop.py
@@ -0,0 +1,303 @@
+from pyg_base import loop, eq, drange, Dict, dictattr, pd2np, tree_loop, is_tree_deep, loop_last_axis, loop_and_reduce_last_axis
+from pyg_base._loop import _item_by_key
+import pandas as pd; import numpy as np
+import pytest
+from numpy import array
+from collections import OrderedDict
+
+SP = lambda a, b: Dict(s = a+b, p = a*b)
+AB = lambda a, b: a+b
+
+def S(v):
+    if isinstance(v, list):
+        return [S(w) for w in v]
+    else:
+        return v.s
+
+
+def test_loop_dict():
+    f = loop(dict)(AB)
+    assert f(1,2) == 3
+    assert f(1, b=2) == 3
+    assert f(a=1, b=2) ==3
+    assert f(dict(a=1,b=2), 2) == dict(a = 3, b = 4)
+    assert f(dict(a=1,b=2), dict(a=2, b=3)) == dict(a = 3, b = 5)
+    b = pd.Series([2,3], ['a','b'])
+    assert f(dict(a=1,b=2), b) == dict(a = 3, b = 5)
+    b = pd.DataFrame([[2,3],[3,4]], columns = ['a','b'])
+    assert eq(f(dict(a=1,b=2), b), dict(a = 1 + b.a, b = 2 + b.b))
+    
+    with pytest.raises(TypeError):
+        f(dict(a=1,b=2), dict(a=2, b=3, c=6))
+    with pytest.raises(TypeError):
+        f(dict(a=1,b=2), [2,2]) 
+        
+    
+
+def test__item_by_key():
+    value = dict(x = 1, y = 2, z = 3)
+    key = 'x'
+    keys = ['x', 'y']
+    i = None
+    assert _item_by_key(value, key, keys, i) == 1
+    assert _item_by_key(pd.Series(value), key, keys, i) == 1
+    assert list(_item_by_key(pd.DataFrame([value]), key, keys, i).values) == [1]
+    assert list(_item_by_key(pd.DataFrame(pd.Series(value)), key, keys, i).values) == [1]
+    
+
+def test_loop_just_dicts():
+    f = loop(dict)(AB)
+    res = f(dictattr(x = 1), b = 2)
+    assert res == dictattr(x = 3) and type(res) == dictattr
+    res = f(Dict(x = 1, y = 2), b = 2)
+    assert res == Dict(x = 3, y = 4) and type(res) == Dict
+    res = f(OrderedDict([('x',1), ('y',2),]), b = 3)
+    assert res == OrderedDict([('x',4), ('y',5),]) and type(res) == OrderedDict
+
+    class FakeDict(dict):
+        pass
+    with pytest.raises(TypeError):
+        f(FakeDict(x = 1, y = 2), b = 2)
+    f = loop(dict, FakeDict)(AB)
+    res = f(FakeDict(x = 1, y = 2), b = 2)
+    assert res == FakeDict(x = 3, y = 4) and type(res) == FakeDict
+
+
+def test_loop_list():
+    f = loop(list)(AB)
+    assert f(1,2) == 3
+    assert f(1, b=2) == 3
+    assert f(a=1, b=2) ==3
+    assert f([1,2], 2) == [3,4]
+    assert f([1,2], [2,3]) == [3,5]
+    assert f([1,2], (2,3)) == [3,5]
+    assert f([1,2], np.array([2,3])) == [3,5]
+    assert f([1,2], pd.Series([2,3])) == [3,5]
+    b = pd.Series([2,3], drange(-1)) # a timeseries is considered a single object
+    assert eq(f([1,2], b), [1+b, 2+b])
+    b = pd.DataFrame([[2,3],[3,4]], columns = ['a','b'])
+    assert eq(f([1,2], b), [1+b.a, 2+b.b])
+    b = pd.DataFrame([[2,3,4],[3,4,5], [5,6,7]], columns = ['a','b','c'])
+    assert eq(f([1,2], b), [1+b, 2+b])
+    with pytest.raises(TypeError):
+        f([1,2], [2,2,3]) 
+
+    assert f((1,2),(3,4)) == (1,2,3,4)
+
+
+def test_loop_list_dict():
+    f = loop(list)(SP)
+    assert f(1,2).s == 3
+    assert f(1, b=2).s == 3
+    assert f(a=1, b=2).s ==3
+    assert S(f([1,2], 2)) == [3,4]
+    assert S(f([1,2], [2,3])) == [3,5]
+    assert S(f([1,2], (2,3))) == [3,5]
+    assert S(f([1,2], np.array([2,3]))) == [3,5]
+    assert S(f([1,2], pd.Series([2,3]))) == [3,5]
+    b = pd.Series([2,3], drange(-1)) # a timeseries is considered a single object
+    assert eq(S(f([1,2], b)), [1+b, 2+b])
+    b = pd.DataFrame([[2,3],[3,4]], columns = ['a','b'])
+    assert eq(S(f([1,2], b)), [1+b.a, 2+b.b])
+    b = pd.DataFrame([[2,3,4],[3,4,5], [5,6,7]], columns = ['a','b','c'])
+    assert eq(S(f([1,2], b)), [1+b, 2+b])
+    with pytest.raises(TypeError):
+        f([1,2], [2,2,3]) 
+
+
+
+def test_loop_tuple():
+    f = loop(tuple)(AB)
+    assert f(1,2) == 3
+    assert f(1, b=2) == 3
+    assert f(a=1, b=2) ==3
+    assert f((1,2), 2) == (3,4)
+    assert f((1,2), [2,3]) == (3,5)
+    assert f((1,2), np.array([2,3])) == (3,5)
+    assert f((1,2), pd.Series([2,3])) == (3,5)
+    b = pd.Series([2,3], drange(-1)) # a timeseries is considered a single object
+    assert eq(f((1,2), b), (1+b, 2+b))
+    b = pd.DataFrame([[2,3],[3,4]], columns = ['a','b'])
+    assert eq(f((1,2), b), (1+b.a, 2+b.b))
+    b = pd.DataFrame([[2,3,4],[3,4,5], [5,6,7]], columns = ['a','b','c'])
+    assert eq(f((1,2), b), (1+b, 2+b))
+    with pytest.raises(TypeError):
+        f((1,2), [2,2,3]) 
+
+    assert f([1,2],[3,4]) == [1,2,3,4]
+
+
+def test_loop_series():
+    f = loop(pd.Series)(AB)
+    assert f(1,2) == 3
+    assert f(1, b=2) == 3
+    assert f(a=1, b=2) ==3
+    a = pd.Series(dict(a=1,b=2))
+    assert eq(f(a, 2) , a+2)
+    assert eq(f(a,dict(a=2, b=3)) , pd.Series(dict(a = 3, b = 5)))
+    ats = pd.Series([1,2], drange(-1))    
+    assert eq(f(ats, 1) , ats+1)
+    assert eq(list(f(a, [1,2]).values), [array([2, 3]), array([3, 4])])
+    assert eq(list(f(a, array([1,2])).values), [array([2, 3]), array([3, 4])])
+    f = loop(pd.Series)(np.std)
+    assert eq(f(a) , pd.Series(dict(a=0, b=0)))
+    assert f(ats) == 0.5
+    
+    
+    
+def test_loop_df():
+    f = loop(pd.DataFrame)(AB)
+    assert f(1,2) == 3
+    assert f(1, b=2) == 3
+    assert f(a=1, b=2) ==3
+    a = pd.DataFrame(dict(a=[1,2],b=[2,3]))
+    assert eq(f(a, 1), a + 1)
+    assert eq(f(a, [1,2]), pd.DataFrame(dict(a=[2,3],b=[2+2,3+2])))
+    assert eq(f(a, dict(a=1,b=2)), pd.DataFrame(dict(a=[2,3],b=[2+2,3+2])))
+    ats = pd.DataFrame(dict(a=[1,2],b=[2,3]), drange(-1))
+    f = loop(pd.Series)(np.std)
+    assert eq(f(a), pd.Series(dict(a=0.5, b=0.5)))
+    assert eq(f(ats), pd.Series(dict(a=0.5, b=0.5)))
+
+    a = pd.DataFrame(dict(a=[1,2,3,4],b=[2,3,0,1]))
+    b = np.array([1,2,3,4])
+    f = loop(pd.DataFrame)(AB)
+    assert eq(f(a,b).a.values, a.a.values + b)
+    assert eq(f(a,b).b.values, a.b.values + b)
+
+    b = np.array([[1,2,3,4],[0,-1,-2,-3]]).T
+    assert eq(f(a,b).a.values, a.a.values + b.T[0])
+    assert eq(f(a,b).b.values, a.b.values + b.T[1])
+
+def test_loop_df_dict():
+    f = loop(pd.DataFrame)(SP)
+    assert f(1,2).s == 3
+    assert f(1, b=2).s == 3
+    assert f(a=1, b=2).s ==3
+    a = pd.DataFrame(dict(a=[1,2],b=[2,3]))
+    assert eq(f(a, 1).s, a + 1)
+    assert eq(f(a, [1,2]).s, pd.DataFrame(dict(a=[2,3],b=[2+2,3+2])))
+    assert eq(f(a, dict(a=1,b=2)).s, pd.DataFrame(dict(a=[2,3],b=[2+2,3+2])))
+    ats = pd.DataFrame(dict(a=[1,2],b=[2,3]), drange(-1))
+    f = loop(pd.Series)(lambda a: Dict(s = np.std(a)))
+    assert eq(f(a).s, pd.Series(dict(a=0.5, b=0.5)))
+    assert eq(f(ats).s, pd.Series(dict(a=0.5, b=0.5)))
+
+    a = pd.DataFrame(dict(a=[1,2,3,4],b=[2,3,0,1]))
+    b = np.array([1,2,3,4])
+    f = loop(pd.DataFrame)(SP)
+    assert eq(f(a,b).s.a.values, a.a.values + b)
+    assert eq(f(a,b).s.b.values, a.b.values + b)
+
+    b = np.array([[1,2,3,4],[0,-1,-2,-3]]).T
+    assert eq(f(a,b).s.a.values, a.a.values + b.T[0])
+    assert eq(f(a,b).s.b.values, a.b.values + b.T[1])
+
+
+    
+
+def test_loop_index_and_columns():
+    a = pd.DataFrame(dict(a=[1,2,3,4],b=[2,3,0,1]), index = drange(3))
+    b = np.array([1,2,3,4])
+    f = loop(pd.DataFrame, pd.Series)(AB)
+    assert eq(f(a,b).index, a.index)
+    assert eq(f(a,b).columns, a.columns)
+    s = pd.Series([1,2,3,4], drange(3))
+    assert eq(f(s,b).index, s.index)
+
+def test_loop_index_and_columns_dict():
+    f = loop(pd.DataFrame, pd.Series)(SP)
+    a = pd.DataFrame(dict(a=[1,2,3,4],b=[2,3,0,1]), index = drange(3))
+    b = np.array([1,2,3,4])
+    assert eq(f(a,b).s.index, a.index)
+    assert eq(f(a,b).s.columns, a.columns)
+    s = pd.Series([1,2,3,4], drange(3))
+    assert eq(f(s,b).s.index, s.index)
+
+
+    
+def test_loop_df_axis1():
+    f = loop(pd.DataFrame)(AB)
+    a = pd.DataFrame(dict(a=[1,2,3,4],b=[2,3,0,1]))
+    b = np.array([1,2])
+    assert eq(f(a,b, axis=1).a.values, a.a.values + 1)
+    assert eq(f(a,b, axis=1).b.values, a.b.values + 2)
+
+def test_loop_df_axis1_dict():
+    f = loop(pd.DataFrame)(SP)
+    a = pd.DataFrame(dict(a=[1,2,3,4],b=[2,3,0,1]))
+    b = np.array([1,2])
+    assert eq(f(a,b, axis=1).s.a.values, a.a.values + 1)
+    assert eq(f(a,b, axis=1).s.b.values, a.b.values + 2)
+
+
+
+        
+def test_loop_df_ndarray():
+    f = loop(np.ndarray)(AB)
+    a = pd.DataFrame(dict(a=[1,2,3,4],b=[2,3,0,1]))
+    b = np.array([[1,2,3,4],[0,-1,-2,-3]]).T
+    assert eq(f(b,a).T[0], a.a.values + b.T[0])
+    assert eq(f(b,a).T[1], a.b.values + b.T[1])
+    a = [1,2]        
+    assert eq(f(b,a).T[0], b.T[0]+1)
+    assert eq(f(b,a).T[1], b.T[1]+2)
+    a = pd.DataFrame(dict(a=[1,2,3,4]))
+    assert eq(f(b,a), np.array([[2,4,6,8],[1,1,1,1]]).T)
+    a = a.values
+    assert eq(f(b,a), np.array([[2,4,6,8],[1,1,1,1]]).T)
+
+def test_loop_df_ndarray_dict():
+    f = loop(np.ndarray)(SP)
+    a = pd.DataFrame(dict(a=[1,2,3,4],b=[2,3,0,1]))
+    b = np.array([[1,2,3,4],[0,-1,-2,-3]]).T
+    assert eq(f(b,a).s.T[0], a.a.values + b.T[0])
+    assert eq(f(b,a).s.T[1], a.b.values + b.T[1])
+    a = [1,2]        
+    assert eq(f(b,a).s.T[0], b.T[0]+1)
+    assert eq(f(b,a).s.T[1], b.T[1]+2)
+    a = pd.DataFrame(dict(a=[1,2,3,4]))
+    assert eq(f(b,a).s, np.array([[2,4,6,8],[1,1,1,1]]).T)
+    a = a.values
+    assert eq(f(b,a).s, np.array([[2,4,6,8],[1,1,1,1]]).T)
+    
+ 
+
+def test_pd2np_exc():
+    assert pd2np(exc = 'a')(lambda x, a: type(a))(x = pd.Series([1,2,3]), a = pd.Series([1,2,3])) == pd.Series
+    assert pd2np(exc = None)(lambda x, a: type(a))(x = pd.Series([1,2,3]), a = pd.Series([1,2,3])) == np.ndarray
+    assert pd2np(lambda x, a: type(a))(x = pd.Series([1,2,3]), a = pd.Series([1,2,3])) == np.ndarray
+    
+
+def test_tree_loop():
+    weighted_sum = lambda d, w = {}: sum([d[key]*w.get(key,1) for key in d])
+    assert weighted_sum(dict(x = 1, y = 2)) == 3
+    assert weighted_sum(dict(x = 1, y = 2), w = dict(x = 0.4, y = 0.6)) == 1.6
+    
+    arg = dict(a = dict(x=1, y=1), b = dict(x = 1, y = 2, z = 3), c = dict(u = dict(x = 1, y = 2), v = dict(x = 1, y = 2, z = 3)))
+    w = dict(a = dict(x = 0.1, y = 0.9), b = dict(x = 1, y = 1, z = 2), c = {})
+
+    assert tree_loop(weighted_sum)(arg) == dict(a = 2, b = 6, c = dict(u = 3, v = 6))
+    assert tree_loop(weighted_sum)(arg, w) == dict(a = 1, b = 9, c = dict(u = 3, v = 6))
+
+
+def test_is_tree_deep():
+    assert is_tree_deep(dict(a = 1), 0)
+    assert is_tree_deep(dict(a = dict(b = 1)), 1)
+    assert is_tree_deep(dict(a = dict(b = 1), c = dict(x = 1, y = 2)), 1)
+    assert not is_tree_deep(dict(a = 1), 1)
+    assert not is_tree_deep(dict(a = dict(b = 1), c = dict(x = 1, y = 2)), 2)
+
+
+def test_loop_last_axis():
+    a = np.random.normal(0,1,(10,20,30))
+    b = loop_and_reduce_last_axis(lambda a: len(a))(a)
+    assert b.shape == a.shape[:2]
+    assert b[0][0] == 30
+    f = loop_and_reduce_last_axis(lambda a: np.mean(a))
+    b = f(a)
+    assert eq(b, np.mean(a, axis=-1))
+    f = loop_and_reduce_last_axis(lambda *a: len(a))
+    assert eq(f(a,1), f(a, a))
+    f = loop_last_axis(lambda a ,b : a + b)
+    assert eq(f(a,a), a+a)
---
+++ packages/pyg-base/pyg_base-0.0.99.tar.gz/pyg_base-0.0.99/tests/test_pandas.py
@@ -0,0 +1,255 @@
+import pandas as pd
+import numpy as np
+import datetime
+from pyg_base import shape, df_slice, drange, dt, dt_bump, dt2str, eq, dictable, df_unslice, nona, df_sync, Dict, add_, div_, mul_, sub_, pow_, df_fillna, df_reindex, min_, max_
+from operator import add, itruediv, sub, mul, pow
+from pyg_base._pandas import _align_columns
+
+def test_df_slice():
+    df = pd.Series(np.random.normal(0,1,1000), drange(-999, 2000))
+    assert len(df_slice(df, None, dt_bump(2000,'-1m'))) == 969
+    assert len(df_slice(df, dt_bump(2000,'-1m'), None)) == 31
+
+
+    df = pd.Series(np.random.normal(0,1,1000), drange(-999, 2020))
+    jan1 = drange(2018, dt(2021,11,1), '1y')
+    feb1 = drange(dt(2018,2,1), dt(2021,11,1), '1y')
+    res = df_slice(df, jan1, feb1, openclose = '[)')
+    assert set(res.index.month) == {1}
+
+
+def test_min_max_with_num():
+    a = 2
+
+    b = pd.Series([1,2,3])
+    assert eq(min_(a, b), pd.Series([1,2,2]))
+    assert eq(max_(a, b), pd.Series([2,2,3]))
+    assert eq(min_(b, a), pd.Series([1,2,2]))
+    assert eq(max_(b, a), pd.Series([2,2,3]))
+
+    b = pd.DataFrame(dict(x=[1,2,3]))
+    assert eq(min_(a, b), pd.DataFrame(dict(x = [1,2,2])))
+    assert eq(max_(a, b), pd.DataFrame(dict(x = [2,2,3])))
+    assert eq(min_(b, a), pd.DataFrame(dict(x = [1,2,2])))
+    assert eq(max_(b, a), pd.DataFrame(dict(x = [2,2,3])))
+
+    b = pd.DataFrame(dict(x=[1,2,3], y = [3,2,1]))
+    assert eq(min_(a, b), pd.DataFrame(dict(x = [1,2,2], y = [2,2,1])))
+    assert eq(max_(a, b), pd.DataFrame(dict(x = [2,2,3], y = [3,2,2])))
+    assert eq(min_(b, a), pd.DataFrame(dict(x = [1,2,2], y = [2,2,1])))
+    assert eq(max_(b, a), pd.DataFrame(dict(x = [2,2,3], y = [3,2,2])))
+
+def test_min_max_with_series():
+    a = pd.Series([2,2,2])
+    b = pd.Series([1,2,3])
+    assert eq(min_(a, b), pd.Series([1,2,2]))
+    assert eq(max_(a, b), pd.Series([2,2,3]))
+    assert eq(min_(b, a), pd.Series([1,2,2]))
+    assert eq(max_(b, a), pd.Series([2,2,3]))
+
+    b = pd.DataFrame(dict(x=[1,2,3]))
+    assert eq(min_(a, b), pd.Series([1,2,2]))
+    assert eq(max_(a, b), pd.Series([2,2,3]))
+    assert eq(min_(b, a), pd.Series([1,2,2]))
+    assert eq(max_(b, a), pd.Series([2,2,3]))
+
+    b = pd.DataFrame(dict(x=[1,2,3], y = [3,2,1]))
+    assert eq(min_(a, b), pd.DataFrame(dict(x = [1,2,2], y = [2,2,1])))
+    assert eq(max_(a, b), pd.DataFrame(dict(x = [2,2,3], y = [3,2,2])))
+    assert eq(min_(b, a), pd.DataFrame(dict(x = [1,2,2], y = [2,2,1])))
+    assert eq(max_(b, a), pd.DataFrame(dict(x = [2,2,3], y = [3,2,2])))
+
+def test_min_max_with_pseudoseries():
+    a = pd.DataFrame(dict(a = [2,2,2]))
+    b = pd.Series([1,2,3])
+    assert eq(min_(a, b), pd.Series([1,2,2]))
+    assert eq(max_(a, b), pd.Series([2,2,3]))
+    assert eq(min_(b, a), pd.Series([1,2,2]))
+    assert eq(max_(b, a), pd.Series([2,2,3]))
+
+    b = pd.DataFrame(dict(x=[1,2,3]))
+    assert eq(min_(a, b), pd.Series([1,2,2]))
+    assert eq(max_(a, b), pd.Series([2,2,3]))
+    assert eq(min_(b, a), pd.Series([1,2,2]))
+    assert eq(max_(b, a), pd.Series([2,2,3]))
+
+    b = pd.DataFrame(dict(a=[1,2,3]))
+    assert eq(min_(a, b), pd.DataFrame(dict(a = [1,2,2])))
+    assert eq(max_(a, b), pd.DataFrame(dict(a = [2,2,3])))
+    assert eq(min_(b, a), pd.DataFrame(dict(a = [1,2,2])))
+    assert eq(max_(b, a), pd.DataFrame(dict(a = [2,2,3])))
+
+    b = pd.DataFrame(dict(x=[1,2,3], y = [3,2,1]))
+    assert eq(min_(a, b), pd.DataFrame(dict(x = [1,2,2], y = [2,2,1])))
+    assert eq(max_(a, b), pd.DataFrame(dict(x = [2,2,3], y = [3,2,2])))
+    assert eq(min_(b, a), pd.DataFrame(dict(x = [1,2,2], y = [2,2,1])))
+    assert eq(max_(b, a), pd.DataFrame(dict(x = [2,2,3], y = [3,2,2])))
+
+def test_min_max_with_dataframe():
+    a = pd.DataFrame(dict(a = [2,2,2], b = [2,2,2]))
+    b = pd.DataFrame(dict(x = [1,2,3], y = [3,2,1]))
+    assert eq(min_(a, b), pd.DataFrame(index=[0,1,2]))
+    assert eq(max_(a, b), pd.DataFrame(index=[0,1,2]))
+    a = pd.DataFrame(dict(x = [2,2,2], y = [2,2,2]))
+    assert eq(min_(a, b), pd.DataFrame(dict(x = [1,2,2], y = [2,2,1])))
+    assert eq(max_(a, b), pd.DataFrame(dict(x = [2,2,3], y = [3,2,2])))
+
+
+def test_df_slice_time():
+    dates = drange(-5, 2020, '5n')
+    df = pd.Series(np.random.normal(0,1,12*24*5+1), dates)
+    assert len(df_slice(df, None, datetime.time(hour = 10))) == 606
+    assert len(df_slice(df, datetime.time(hour = 5), datetime.time(hour = 10))) == 300
+    assert len(df_slice(df, lb = datetime.time(hour = 10), ub = datetime.time(hour = 5))) == len(dates) - 300
+
+
+def test_df_slice_roll():
+    ub = drange(1980, 2000, '3m')
+    df = [pd.Series(np.random.normal(0,1,1000), drange(-999, date)) for date in ub]
+    res = df_slice(df, ub = ub)
+    assert len(res) == 8305
+    ub = drange(1980, 2000, '3m')
+    df = [pd.Series(np.random.normal(0,1,1000), drange(-999, date)) for date in ub]
+    res = df_slice(df, ub = ub, n = 5).iloc[500:]
+    res.shape == (7805,5)
+# -*- coding: utf-8 -*-
+
+
+def test_df_slice_roll_symbol():
+    ub = drange(1980, 2000, '3m')
+    df = [dt2str(date) for date in ub]    
+    res = df_slice(df, ub = ub, n = 3)
+    assert list(res.iloc[-3].values) == ['19990701', '19991001', '20000101']
+    assert res.index[-3] == dt('19990701')
+    res = df_slice(df, lb = ub, n = 3, openclose = '[)')
+    assert list(res.iloc[-3].values) == ['19990701', '19991001', '20000101']
+    assert res.index[-3] == dt('19990701')
+
+def test_df_unslice():
+    ub = drange(1980, 2000, '3m')
+    dfs = [pd.Series(date.year * 100 + date.month, drange(-999, date)) for date in ub]
+    df = df_slice(dfs, ub = ub, n = 10)
+    res = df_unslice(df, ub)
+    rs = dictable(res.items(), ['ub', 'df'])
+    assert eq(df_slice(df = rs.df, ub = rs.ub, n = 10), df)
+    assert len(rs.inc(lambda df: len(set(nona(df)))>1)) == 0
+
+def test_df_sync():
+    a = pd.DataFrame(np.random.normal(0,1,(100,5)), drange(-100,-1), list('abcde'))
+    b = pd.DataFrame(np.random.normal(0,1,(100,5)), drange(-99), list('bcdef'))
+    c = 'not a timeseries'
+    d = pd.DataFrame(np.random.normal(0,1,(100,1)), drange(-98,1), ['single_column_df'])
+    s = pd.Series(np.random.normal(0,1,105), drange(-104))
+    
+    dfs = [a,b,c,d,s]
+    res = df_sync(dfs, 'ij')
+    assert len(res[0]) == len(res[1]) == len(res[-1]) == 98
+    assert res[2] == 'not a timeseries'
+    assert list(res[0].columns) == list('bcde')
+
+    res = df_sync(dfs, 'oj')
+    assert len(res[0]) == len(res[1]) == len(res[-1]) == 106; 
+    assert res[2] == 'not a timeseries'
+    assert list(res[0].columns) == list('bcde')
+
+    res = df_sync(dfs, join = 'oj', method = 1)
+    assert res[0].iloc[0].sum() == 4
+
+    res = df_sync(dfs, join = 'oj', method = 1, columns = 'oj')
+    assert res[0].iloc[0].sum() == 5
+    assert list(res[0].columns) == list('abcdef')
+    assert list(res[-2].columns) == ['single_column_df'] # single column unaffected
+
+    dfs = Dict(a = a, b = b, c = c, d = d, s = s)
+    res = df_sync(dfs, join = 'oj', method = 1, columns = 'oj')
+    assert res.c == 'not a timeseries'
+    assert res.a.shape == (106,6)
+
+def test_bi():
+    s = pd.Series([1,2,3.], drange(-2,2000))
+    a = pd.DataFrame(dict(a = [1,2,3.], b = [4,5,6.]), drange(-2,2000))
+    b = pd.DataFrame(dict(c = [1,2,3.], b = [4,5,6.]), drange(-3,2000)[:-1])
+    c = 5
+    
+    assert eq(add_(s,a), pd.DataFrame(dict(a = [2,4,6.], b = [5,7,9.]), drange(-2,2000)))
+ 
+    for f in [add_,sub_,div_,mul_,pow_]:
+        assert f(s,b).shape == (2,2)
+        assert f(s,b, 'oj').shape == (4,2)
+
+        assert f(a,b).shape == (2,1)
+        assert f(a,b, 'oj').shape == (4,1)
+        assert f(a,b, 'oj', 0, 'oj').shape == (4,3)
+
+    for f in [add_,mul_]: # support for list of values
+        for x in [s,a,b,c]:
+            for y in [s, a, b, c]:
+                assert eq(f([x,y]), f(x,y))
+    
+    assert add_([a,b,c,s], columns = 'oj').shape == (2,3)
+    assert mul_([a,b,c,s], columns = 'oj').shape == (2,3)
+
+    # operations with a constant
+    for f,o in zip([add_,sub_,div_,mul_,pow_], [add, sub, itruediv,mul, pow]):
+        for v in [a,b,s,c]:
+            assert eq(f(v,c), o(v,c))
+
+
+def test_df_fillna():
+    from numpy import nan
+    dates =  drange(dt(2000), dt(2000,1,14))
+    df = pd.Series([0,nan, 1, nan, nan, 2, nan, nan, nan, 3, nan, nan, nan, nan, ], dates)
+    df0 = df.copy()
+    df0[np.isnan(df)] = 0
+    assert eq(df_fillna(df), df)
+    assert eq(df_fillna(df, 0), df0)
+    assert eq(df_fillna(df, 'ffill_na'), pd.Series([0,0, 1, 1,1, 2, 2,2,2,3, nan, nan, nan, nan, ], dates))
+    assert eq(df_fillna(df, 'ffill_0'), pd.Series([0,0, 1, 1,1, 2, 2,2,2,3] + [0] * 4, dates))
+    assert eq(df_fillna(df, 'bfill'), pd.Series([0,1, 1, 2, 2, 2, 3, 3, 3, 3, nan, nan, nan, nan, ], dates))
+    assert eq(df_fillna(df, 'linear'), pd.Series([0,.5, 1, 1+1/3, 1+2/3, 2, 2.25, 2.5, 2.75] + [3]*5, dates))
+    
+
+    
+def test_sub_column_names_for_df():
+    df = pd.DataFrame(range(10), index = drange(9), columns = ['df'])
+    for f in (sub_, add_, div_, mul_):
+        res = f(df,df)
+        assert isinstance(res, pd.DataFrame)
+        assert list(res.columns) == ['df']
+    df2 = pd.DataFrame(range(10), index = drange(9), columns = ['df2'])
+    for f in (sub_, add_, div_, mul_):
+        res = f(df,df2)
+        assert isinstance(res, pd.DataFrame)
+        assert list(res.columns) == [0]
+    
+
+def test_align_columns():
+    As = [1, 
+          np.array([1.,1.]),
+          np.array([1.,1.]).reshape((2,1)),
+          np.array([[1.,1.],[1.,1.]]),
+          pd.Series([1.,1.]),
+          pd.DataFrame(dict(a = [1,1], b = [1,1]))
+          ]
+          
+    Bs = As
+    for a in As:
+        for b in Bs:
+            res = max_(a,b)
+            assert len(shape(res)) == max(len(shape(a)), len(shape(b)))
+            res = min_(a,b)
+            assert len(shape(res)) == max(len(shape(a)), len(shape(b)))
+
+def test_reindex_unindexable():
+    for t in [1, 'not a pd', dt(4)]:
+        assert df_reindex(t,  [dt(9), 'whatever']) == t
+
+
+def test_reindex_with_ffill_for_dataframe():
+    dates =  drange(dt(2000), 4)
+    df = pd.DataFrame(dict(a = [np.nan,2,np.nan,4,5],b = [1,2,3,np.nan, np.nan]), dates)
+    index = drange(dt(2000,1,0), dt(2000,1,6))    
+    res = df_reindex(df, index, method = 'ffill')
+    assert eq(res, pd.DataFrame(dict(a = [np.nan,np.nan,2,2,4,5,5],b = [np.nan,1,2,3,3,3,3]), index))
+    res = df_reindex(df[['a']], index, method = ['ffill', 'bfill'])
+    assert eq(res, pd.DataFrame(dict(a = [2,2,2,2,4,5,5]), index))
