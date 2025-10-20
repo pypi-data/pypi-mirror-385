cdb0d74e8301f5cb07c60d46a7eb259bb2c5ebbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-20 16:04:44 +0000

    Add package crosschexcloudapi/0.1.0/crosschexcloudapi-0.1.0-py3-none-any.whl

---
+++ packages/crosschexcloudapi/crosschexcloudapi-0.1.0-py3-none-any.whl/crosschexcloudapi/__init__.py
@@ -0,0 +1,14 @@
+"""
+pycosec package
+
+
+Expose the main client class at package level for convenience.
+"""
+
+
+from .api import CrossChexCloudAPI
+
+
+
+__all__ = ["CrossChexCloudAPI"]
+__version__ = "0.1.0"
