764b5b159230cc0305535fa0410d26548b5f220d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-20 05:05:42 +0000

    Add package math2remember/0.1.8/math2remember-0.1.8.tar.gz

---
+++ packages/math2remember/math2remember-0.1.8.tar.gz/math2remember-0.1.8/math2remember/__init__.py
@@ -0,0 +1,7 @@
+from .isPrime import isPrime
+from .getPrimeGen import getPrimeGenFrom, getPrimeGenTo
+
+__version__ = '0.1.8'
+__all__ = ["isPrime", "getPrimeGenFrom", "getPrimeGenTo"]
+
+print(isPrime(1))
