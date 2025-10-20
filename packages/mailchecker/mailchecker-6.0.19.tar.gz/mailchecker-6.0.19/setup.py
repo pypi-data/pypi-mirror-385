cc4075adec16fc39edbb172948ab563649147336
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-20 03:26:06 +0000

    Add package mailchecker/6.0.19/mailchecker-6.0.19.tar.gz

---
+++ packages/mailchecker/mailchecker-6.0.19.tar.gz/mailchecker-6.0.19/setup.py
@@ -0,0 +1,19 @@
+# coding=utf-8
+
+import setuptools
+
+setuptools.setup(
+  name="mailchecker",
+  version = "6.0.19",
+  author='Francois-Guillaume Ribreau',
+  author_email='github@fgribreau.com',
+  description="Cross-language temporary email detection library. Stop users from signing up with temporary email addresses.",
+  url="https://github.com/FGRibreau/mailchecker",
+  py_modules=["MailChecker"],
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+  ],
+  python_requires='>=3.6',
+)
