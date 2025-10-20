913c7383fa6a957c13afaf8bd50bdf2252031417
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-20 17:04:40 +0000

    Add package aioamazondevices/6.4.5/aioamazondevices-6.4.5.tar.gz

---
+++ packages/aioamazondevices/aioamazondevices-6.4.5.tar.gz/aioamazondevices-6.4.5/src/aioamazondevices/__init__.py
@@ -0,0 +1,17 @@
+"""aioamazondevices library."""
+
+__version__ = "6.4.5"
+
+
+from .api import AmazonDevice, AmazonEchoApi
+from .exceptions import (
+    CannotAuthenticate,
+    CannotConnect,
+)
+
+__all__ = [
+    "AmazonDevice",
+    "AmazonEchoApi",
+    "CannotAuthenticate",
+    "CannotConnect",
+]
