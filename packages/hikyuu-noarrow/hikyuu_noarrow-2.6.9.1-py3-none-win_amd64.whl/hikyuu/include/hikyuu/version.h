012e37f9b70bc7af1a4d2ecd847b289503d4229a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-20 15:05:20 +0000

    Add package hikyuu-noarrow/2.6.9.1/hikyuu_noarrow-2.6.9.1-py3-none-win_amd64.whl

---
+++ packages/hikyuu-noarrow/hikyuu_noarrow-2.6.9.1-py3-none-win_amd64.whl/hikyuu/include/hikyuu/version.h
@@ -0,0 +1,24 @@
+/*
+ * version.h
+ *
+ *  Copyright (c) 2020, hikyuu.org
+ *
+ *  Created on: 2020-4-19
+ *      Author: fasiondog
+ */
+
+#pragma once
+#ifndef HKU_VERSION_H
+#define HKU_VERSION_H
+
+// clang-format off
+#define HKU_VERSION "2.6.9"
+#define HKU_VERSION_MAJOR 2
+#define HKU_VERSION_MINOR 6
+#define HKU_VERSION_ALTER 9
+#define HKU_VERSION_BUILD 202510201329
+#define HKU_VERSION_MODE "RELEASE"
+#define HKU_VERSION_GIT "2.6.9 release.ee4977f1 (RELEASE)"
+// clang-format on
+
+#endif /* HKU_VERSION_H */
