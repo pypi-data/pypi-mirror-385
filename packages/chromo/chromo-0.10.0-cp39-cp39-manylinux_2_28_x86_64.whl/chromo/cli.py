e89122a7e308227c5f29482f3d32a7a89e7eb28b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-20 15:05:16 +0000

    Add package chromo/0.10.0/chromo-0.10.0-cp39-cp39-manylinux_2_28_x86_64.whl

---
+++ packages/chromo/chromo-0.10.0-cp39-cp39-manylinux_2_28_x86_64.whl/chromo/cli.py
@@ -0,0 +1,396 @@
+"""
+A commandline interface that mimics CRMC to ease transition from CRMC.
+
+CRMC (Cosmic Ray Monte Carlo package) https://web.ikp.kit.edu/rulrich/crmc.html
+"""
+
+import argparse
+import os
+from math import sqrt
+from pathlib import Path
+
+from particle import Particle
+from rich.progress import (
+    BarColumn,
+    MofNCompleteColumn,
+    Progress,
+    ProgressColumn,
+    Task,
+    TaskProgressColumn,
+    TimeRemainingColumn,
+)
+from rich.text import Text
+
+from chromo import __version__ as version
+from chromo import models, writer
+from chromo.constants import GeV, MeV
+from chromo.kinematics import CenterOfMass, FixedTarget, Momentum
+from chromo.util import AZ2pdg, get_all_models, name2pdg, tolerant_string_match
+
+
+class SpeedColumn(ProgressColumn):
+    """Renders generation speed."""
+
+    def render(self, task: "Task") -> Text:
+        """Show generation speed."""
+        speed = task.finished_speed or task.speed
+        if speed is None:
+            return Text("?", style="progress.data.speed")
+        return Text(f"{speed:.0f}/s", style="progress.data.speed")
+
+
+# Only add numbers here for backward-compatibility with CRMC.
+# Chromo-exclusive models do not get a number and should not be added here.
+MODELS = {
+    0: models.EposLHC,
+    # 1: models.Epos199,
+    2: models.QGSJet01d,
+    # 3: models.Gheisha,
+    4: models.Pythia6,
+    # 5: models.Hijing138,
+    6: models.Sibyll23d,
+    7: models.QGSJetII04,
+    8: models.Phojet193,
+    11: models.QGSJetII03,
+    # 12 refers to different DPMJet versions in CRMC
+    12: models.DpmjetIII307,
+}
+
+VALID_MODELS = []
+for M in get_all_models():
+    for k, v in MODELS.items():
+        if v is M:
+            VALID_MODELS.append(f"{M.label} [{k}]")
+            break
+    else:
+        VALID_MODELS.append(M.label)
+VALID_MODELS = ", ".join(sorted(VALID_MODELS))
+
+FORMATS = {
+    "hepmc": writer.Hepmc,
+    "hepmc:gz": writer.Hepmc,
+    "root": writer.Root,
+    "root:vertex": lambda *args: writer.Root(*args, write_vertices=True),
+    "svg": writer.Svg,
+    "null": writer.Null,
+    # "lhe",
+    # "lhegz",
+}
+VALID_FORMATS = f"{', '.join(FORMATS)}"
+
+
+def extension(format):
+    extension, *options = format.split(":")
+    if "gz" in options:
+        return f"{extension}.gz"
+    return extension
+
+
+def process_particle(x):
+    try:
+        x = int(x)
+        if x > 10000:
+            z = x // 10000
+            a = (x % 10000) / 10
+            return AZ2pdg(a, z)
+        return x  # noqa: TRY300
+    except ValueError:
+        pass
+
+    # handle any special names recognised by CRMC here
+    # ...
+
+    try:
+        return name2pdg(x)
+    except KeyError:
+        msg = f"particle name {x} not recognized"
+        raise SystemExit(msg)
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-v", "--version", action="store_true", help="print version")
+    parser.add_argument(
+        "-s",
+        "--seed",
+        type=int,
+        default=0,
+        help="seed between 1 and 1e9 (default is 0, which generates random seed)",
+    )
+    parser.add_argument(
+        "-n",
+        "--number",
+        type=int,
+        default=1,
+        help="number of collisions (default is 1)",
+    )
+    parser.add_argument(
+        "-m",
+        "--model",
+        default=0,
+        help=(
+            f"select model via number or via tolerant string match (case-insensitive), "
+            f"allowed values: {VALID_MODELS}"
+        ),
+    )
+    parser.add_argument(
+        "-p",
+        "--projectile-momentum",
+        type=float,
+        default=0,
+        help="projectile momentum in GeV/c",
+    )
+    parser.add_argument(
+        "-P",
+        "--target-momentum",
+        type=float,
+        default=0,
+        help="target momentum in GeV/c",
+    )
+    parser.add_argument("-S", "--sqrts", type=float, default=0, help="sqrt(s) in GeV")
+    parser.add_argument(
+        "-i",
+        "--projectile-id",
+        default="p",
+        help="PDG ID or particle name (default is proton)",
+    )
+    parser.add_argument(
+        "-I",
+        "--target-id",
+        default="p",
+        help="PDG ID or particle name (default is proton)",
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        default="",
+        help=f"output format: {VALID_FORMATS} (default is hepmc)",
+    )
+    parser.add_argument(
+        "-f",
+        "--out",
+        help="output file name (generated if none provided); "
+        "format is guessed from the file extension if --output is not specified",
+    )
+    parser.add_argument(
+        "-c",
+        "--config",
+        help="configuration file for generator; configuration is Python code which "
+        "interacts with the variable `model` that represents the model instance",
+    )
+
+    args = parser.parse_args()
+
+    if args.version:
+        print(f"chromo {version}")  # noqa: T201
+        raise SystemExit
+
+    if args.seed <= 0:
+        args.seed = int.from_bytes(os.urandom(4), "little")
+
+    if args.number <= 0:
+        raise SystemExit("Error: number must be positive")
+
+    try:
+        model_number = int(args.model)
+        Model = MODELS[model_number]
+    except KeyError:
+        msg = f"Error: model={args.model} is invalid ({VALID_MODELS})"
+        raise SystemExit(msg)
+    except ValueError:
+        # args.model is not a number.
+        # Find model that matches string spec.
+        matches = []
+        x = args.model.lower()
+        for M in get_all_models():
+            y = M.label.lower()
+            # If exact match found, stop search. This rule is necessary to e.g. select
+            # Sibyll-2.3, since Sibyll-2.3d would also match otherwise, always leading to
+            # ambiguity.
+            if x == y:
+                Model = M
+                break
+            if tolerant_string_match(x, y):
+                matches.append(M)
+        else:
+            if len(matches) == 1:
+                Model = matches[0]
+            elif len(matches) == 0:
+                msg = f"Error: model={args.model} has no match ({VALID_MODELS})"
+                raise SystemExit(msg)
+            else:
+                msg = (
+                    f"Error: model={args.model} is ambiguous, "
+                    f"matches ({', '.join(v.label for v in matches)})"
+                )
+                raise SystemExit(msg)
+    args.model = Model
+
+    args.projectile_id = process_particle(args.projectile_id)
+    args.target_id = process_particle(args.target_id)
+
+    args.projectile_momentum *= GeV
+    args.target_momentum *= GeV
+    args.sqrts *= GeV
+    pr = args.projectile_momentum
+    ta = args.target_momentum
+    if args.sqrts < 0:
+        msg = f"Error: sqrt(s) is negative {args.sqrts / GeV} GeV"
+        raise SystemExit(msg)
+    if (pr != 0 or ta != 0) and args.sqrts != 0:
+        raise SystemExit("Error: either set sqrts or momenta, but not both")
+    if pr == 0 and ta == 0 and args.sqrts == 0:
+        raise SystemExit(
+            "Error: you need to specify particle momenta or the CMS energy"
+        )
+
+    if pr != 0 or ta != 0:
+        # compute sqrt(s)
+        m1 = Particle.from_pdgid(args.projectile_id).mass * MeV
+        m2 = Particle.from_pdgid(args.target_id).mass * MeV
+        e1 = sqrt(m1**2 + pr**2)
+        e2 = sqrt(m2**2 + ta**2)
+        a = e1 + e2
+        b = pr + ta
+        s = (a + b) * (a - b)
+        if s <= 0:
+            raise SystemExit("Error: s <= 0")
+        args.sqrts = sqrt(s)
+
+    if args.out == "-":
+        args.output = "hepmc"
+        raise SystemExit("Error: output to stdout not yet supported")
+    if args.out:  # filename was provided
+        args.out = Path(args.out)
+        suffixes = args.out.suffixes
+        if suffixes and suffixes[-1] in [".gz", ".bz2"]:
+            format = ":".join(x[1:] for x in args.out.suffixes[-2:])
+        else:
+            format = suffixes[-1].lstrip(".") if suffixes else ""
+        # check if both format and args.output are defined and are different
+        if format and args.output and format != args.output:
+            msg = f"Error: File extension of {args.out} does not match {args.output}"
+            raise SystemExit(msg)
+        if not args.output:
+            args.output = format or "hepmc"
+    else:  # no filename provided
+        if not args.output:
+            args.output = "hepmc"
+        # generate filename like CRMC
+        ext = extension(args.output)
+        pid1 = args.projectile_id
+        pid2 = args.target_id
+        en = f"{args.sqrts / GeV:.0f}"
+        mn = Model.pyname.lower()
+        fn = f"chromo_{mn}_{args.seed}_{int(pid1)}_{int(pid2)}_{en}.{ext}"
+        odir = os.environ.get("CRMC_OUT", ".")
+        args.out = Path(odir) / fn
+        # append extension
+        if not args.out.suffixes[-2:]:
+            ext = "." + args.output
+            if ext.endswith("gz"):
+                ext = ext[:-2] + ".gz"
+            args.out = Path(args.out).with_suffix(ext)
+
+    if args.output not in FORMATS:
+        msg = f"Error: unknown format {args.output} ({VALID_FORMATS})"
+        raise SystemExit(msg)
+
+    configuration = ""
+    if args.config:
+        fn = Path(args.config)
+        if not fn.exists():
+            msg = f"Error: configuration file {args.config} does not exist"
+            raise SystemExit(msg)
+
+        lines = ["def configure(model):\n"]
+        with open(fn) as f:
+            lines.extend([f"    {line}" for line in f])
+        configuration = "".join(lines)
+
+    return args, configuration
+
+
+def main():
+    from rich.console import Console
+    from rich.panel import Panel
+
+    args, configuration = parse_arguments()
+
+    p1 = Particle.from_pdgid(args.projectile_id)
+    p2 = Particle.from_pdgid(args.target_id)
+    pr = args.projectile_momentum
+    ta = args.target_momentum
+
+    msg = f"""\
+  [repr.str]Model[/repr.str]\t\t[bold]{args.model.label}[/bold]
+  [repr.str]Projectile[/repr.str]\t[bold]{p1.name}[/bold]\
+ ([repr.number]{int(args.projectile_id)}[/repr.number])
+  [repr.str]Target[/repr.str]\t[bold]{p2.name}[/bold]\
+ ([repr.number]{int(args.target_id)}[/repr.number])\
+"""
+    if pr != 0 or ta != 0:
+        msg += f"""
+  [repr.str]Projectile momentum[/repr.str]\t[magenta]{pr:g} GeV/c[/magenta]
+  [repr.str]Target momentum[/repr.str]\t[magenta]{ta:g} GeV/c[/magenta]\
+"""
+    msg += f"""
+  [repr.str]sqrt(s)[/repr.str]\t[magenta]{args.sqrts:g} GeV[/magenta]
+  [repr.str]Collisions[/repr.str]\t[repr.number]{args.number}[/repr.number]
+  [repr.str]Seed[/repr.str]\t\t[repr.number]{args.seed}[/repr.number]
+  [repr.str]Format[/repr.str]\t{args.output}\
+"""
+    if args.config:
+        msg += f"\n  [repr.str]Configuration[/repr.str]\t{args.config}"
+
+    console = Console()
+    console.print(
+        Panel(msg, title=f"[bold]chromo [green]{version}[/green][/bold]", width=78)
+    )
+
+    if pr > 0 and ta == 0:  # fixed target mode
+        evt_kin = FixedTarget(Momentum(pr), args.projectile_id, args.target_id)
+    else:  # cms mode
+        evt_kin = CenterOfMass(args.sqrts, args.projectile_id, args.target_id)
+
+    task_id = None
+    try:
+        model = args.model(evt_kin, seed=args.seed)
+        if configuration:
+            try:
+                d = {}
+                exec(configuration, d)
+                d["configure"](model)
+            except Exception as e:
+                msg = f"Error in configuration code:\n\n{configuration}, {e}"
+                raise RuntimeError(msg) from e
+        ofile = FORMATS[args.output](args.out, model)
+        with ofile:
+            # workaround: several models generate extra print when first
+            # event is generated, this interferes with progress bar so we
+            # create bar only after second event is generated
+            with Progress(
+                MofNCompleteColumn(),
+                BarColumn(),
+                TaskProgressColumn(),
+                "ETA",
+                TimeRemainingColumn(elapsed_when_finished=True),
+                SpeedColumn(),
+            ) as bar:
+                for event in model(args.number):
+                    ofile.write(event)
+                    if task_id is None:
+                        task_id = bar.add_task("", total=args.number)
+                    bar.advance(task_id, 1)
+    except Exception:
+        if int(os.environ.get("DEBUG", "0")) > 0:
+            raise
+
+        import traceback
+
+        msg = traceback.format_exc(chain=False, limit=0)
+        raise SystemExit(msg.strip())
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/chromo/chromo-0.10.0-cp39-cp39-manylinux_2_28_x86_64.whl/chromo/models/pythia8.py
@@ -0,0 +1,267 @@
+import warnings
+from collections.abc import Collection
+from os import environ
+
+import numpy as np
+from particle import literals as lp
+
+from chromo.common import CrossSectionData, EventData, MCRun
+from chromo.constants import standard_projectiles
+from chromo.kinematics import EventFrame
+from chromo.util import _cached_data_dir, name2pdg
+
+
+class PYTHIA8Event(EventData):
+    """Wrapper for Pythia8 event stack."""
+
+    def __init__(self, generator):
+        pythia = generator._pythia
+        event = pythia.event
+        super().__init__(
+            (generator.name, generator.version),
+            generator.kinematics,
+            generator.nevents,
+            self._get_impact_parameter(pythia),
+            self._get_n_wounded(pythia),
+            generator._inel_or_prod_cross_section,
+            event.pid(),
+            event.status(),
+            pythia.charge(),
+            event.px(),
+            event.py(),
+            event.pz(),
+            event.en(),
+            event.m(),
+            event.vx(),
+            event.vy(),
+            event.vz(),
+            event.vt(),
+            np.maximum(event.mothers() - 1, -1),
+            np.maximum(event.daughters() - 1, -1),
+        )
+
+    @staticmethod
+    def _get_impact_parameter(pythia):
+        hi = pythia.info.hiInfo
+        if hi is None:
+            return np.nan
+        return hi.b
+
+    @staticmethod
+    def _get_n_wounded(pythia):
+        hi = pythia.info.hiInfo
+        if hi is None:
+            return (0, 0)
+        return hi.nPartProj, hi.nPartTarg
+
+    def _prepare_for_hepmc(self):
+        model, version = self.generator
+        warnings.warn(
+            f"{model}-{version}: only part of the history " "available in HepMC3 event",
+            RuntimeWarning,
+        )
+
+        # We must apply some workarounds so that HepMC3 conversion and IO works
+        # for all models. This should be revisited once the fundamental issues
+        # with particle histories have been fixed.
+
+        # to get a valid GenEvent we must
+        # 1) select only particles produced after the parton shower
+        # 2) connect particles attached to a single beam particle
+        # (diffractive events) to the common interaction vertex (1, 2)
+        # TODO check if this costs significant amount of time and speed it up if so
+        mask = (self.status == 1) | (self.status == 2) | (self.status == 4)
+        ev = self[mask]
+        mask = (ev.mothers[:, 0] == 0) | (ev.mothers[:, 0] == 1)
+        ev.mothers[mask] = (0, 1)
+        return ev
+
+
+class Pythia8(MCRun):
+    _name = "Pythia"
+    _version = "8.315"
+    _library_name = "_pythia8"
+    _event_class = PYTHIA8Event
+    _frame = EventFrame.CENTER_OF_MASS
+    _projectiles = standard_projectiles | {
+        lp.photon.pdgid,
+        lp.e_plus.pdgid,
+        lp.e_minus.pdgid,
+    }
+    # Nuclei are supported in principle, but generation is very slow.
+    # Support for more nuclei can be added with ParticleData.addParticle.
+    _targets = (
+        _projectiles
+        | {
+            name2pdg(x)
+            for x in ("He4", "Li6", "C12", "O16", "Cu63", "Xe129", "Au197", "Pb208")
+        }
+        | {lp.e_plus.pdgid, lp.e_minus.pdgid}
+    )
+    _restartable = True
+    _data_url = (
+        "https://github.com/impy-project/chromo"
+        "/releases/download/zipped_data_v1.0/Pythia8_v005.zip"
+    )
+
+    def __init__(self, evt_kin, *, seed=None, config=None, banner=True):
+        """
+
+        Parameters
+        ----------
+        config: str or list of str, optional
+            Pass standard Pythia configuration here. You can use this to change
+            the physics processes which are enabled in Pythia. You can pass a
+            single string that is read from a configuration file or a list of
+            strings, where each string is a single configuration command.
+            If config is not set, 'SoftQCD:inelastic = on' is used to get the
+            equivalent of other generators in chromo.
+        """
+
+        super().__init__(seed)
+
+        datdir = _cached_data_dir(self._data_url) + "xmldoc"
+
+        # Must delete PYTHIA8DATA from environ if it exists, since it overrides
+        # our argument here. When you install Pythia8 with conda, it sets
+        # PYTHIA8DATA. If that version does not match this version, readString()
+        # or init() may fail.
+        if "PYTHIA8DATA" in environ:
+            del environ["PYTHIA8DATA"]
+        self._pythia = self._lib.Pythia(datdir, banner)
+
+        if config is None:
+            if evt_kin.p1 == lp.photon.pdgid and evt_kin.p2 == lp.photon.pdgid:
+                self._config = ["PhotonCollision:all = on"]
+            else:
+                # includes gamma p processes
+                self._config = ["SoftQCD:inelastic = on"]
+        else:
+            self._config = self._parse_config(config)
+
+        # Common settings
+        self._config += [
+            # use our random seed
+            "Random:setSeed = on",
+            # Pythia's RANMAR PRNG accepts only seeds smaller than 900_000_000,
+            # this may change in the future if they switch to a different PRNG
+            f"Random:seed = {self.seed % 900_000_000}",
+        ]
+
+        # Add "Print:quiet = on" if no "Print:quiet" is in config
+        if not any("Print:quiet" in s for s in self._config):
+            self._config.append("Print:quiet = on")
+
+        # must come last
+        if evt_kin is None:
+            # Decay mode
+            self._init_pythia(self._config)
+        else:
+            self.kinematics = evt_kin
+        self._set_final_state_particles()
+
+    def _cross_section(self, kin=None, max_info=False):
+        st = self._pythia.info.sigmaTot
+
+        if (self.kinematics.p1.is_lepton) and (self.kinematics.p2.is_lepton):
+            return CrossSectionData(
+                total=st.sigmaTot,
+                inelastic=st.sigmaTot - st.sigmaEl,
+                elastic=st.sigmaEl,
+            )
+        return CrossSectionData(
+            total=st.sigmaTot,
+            inelastic=st.sigmaTot - st.sigmaEl,
+            elastic=st.sigmaEl,
+            diffractive_ax=st.sigmaAX,
+            diffractive_xb=st.sigmaXB,
+            diffractive_xx=st.sigmaXX,
+            diffractive_axb=st.sigmaAXB,
+        )
+
+    def _set_kinematics(self, kin):
+        config = self._config[:]
+
+        # TODO use numpy PRNG instead of Pythia's
+        config += [
+            # use center-of-mass frame
+            "Beams:frameType = 1",
+            # do not print progress
+            "Next:numberCount = 0",
+        ]
+
+        if (kin.p1.A or 0) > 1 or (kin.p2.A or 1) > 1:
+            import warnings
+
+            warnings.warn(
+                "Support for nuclei is experimental; event generation takes a long time",
+                RuntimeWarning,
+            )
+
+            # speed-up initialization by not fitting nuclear cross-section
+            config.append("HeavyIon:SigFitNGen = 0")
+            config.append("HeavyIon:SigFitDefPar = 10.79,1.75,0.30,0.0,0.0,0.0,0.0,0.0")
+
+        config += [
+            f"Beams:idA = {int(kin.p1)}",
+            f"Beams:idB = {int(kin.p2)}",
+            f"Beams:eCM = {kin.ecm}",
+        ]
+
+        self._init_pythia(config)
+
+    def _init_pythia(self, config):
+        pythia = self._pythia
+        pythia.settings.resetAll()
+
+        for line in config:
+            if not pythia.readString(line):
+                msg = f"readString({line!r}) failed"
+                raise RuntimeError(msg)
+
+        # calling init several times is allowed
+        if not pythia.init():
+            raise RuntimeError("Pythia8 initialization failed")
+
+    def _set_stable(self, pdgid, stable):
+        self._pythia.particleData.mayDecay(pdgid, not stable)
+
+    def _get_stable(self):
+        r = set()
+        for p in self._pythia.particleData.all():
+            # the p.tau0 > 1e-5 cut should not be necessary, but without it,
+            # Pythia8 reports nuclei and some BSM particles like tau' as stable
+            if p.tau0 > 1e-5 and not p.mayDecay:
+                r.add(p.id)
+                if p.hasAnti:
+                    r.add(-p.id)
+        return r
+
+    def _generate(self):
+        return self._pythia.next()
+
+    @staticmethod
+    def _parse_config(config) -> list[str]:
+        # convert config to lines and filter out lines that
+        # we override with our settings in _set_kinematics
+        result: list[str] = []
+        if isinstance(config, str):
+            for line in config.split("\n"):
+                line = line.strip()
+                if not line or line.startswith("#"):
+                    continue
+                result.append(line)
+        elif isinstance(config, Collection):
+            result.extend([item.strip() for item in config])
+
+        ignored = ("Random:", "Beams:", "Next:")
+
+        result2: list[str] = []
+        for line in result:
+            line = line.strip()
+            for ig in ignored:
+                if line.startswith(ig):
+                    warnings.warn(f"configuration ignored: {line!r}", RuntimeWarning)
+                    break
+            result2.append(line)
+        return result2
