19c017532a13f106fe467522a179e58c356c2192
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-20 14:04:58 +0000

    Add package qolsys-controller/0.0.35/qolsys_controller-0.0.35.tar.gz

---
+++ packages/qolsys-controller/qolsys_controller-0.0.35.tar.gz/qolsys_controller-0.0.35/qolsys_controller/database/table_eu_event.py
@@ -0,0 +1,24 @@
+import logging  # noqa: INP001
+import sqlite3
+
+from .table import QolsysTable
+
+LOGGER = logging.getLogger(__name__)
+
+
+class QolsysTableEuEvent(QolsysTable):
+
+    def __init__(self, db: sqlite3.Connection, cursor: sqlite3.Cursor) -> None:
+        super().__init__(db, cursor)
+        self._uri = "content://com.qolsys.qolsysprovider.EUEventContentProvider/eu_event"
+        self._table = "eu_event"
+        self._abort_on_error = False
+        self._implemented = False
+
+
+        self._columns = [
+            "_id",
+        ]
+
+        self._create_table()
+
