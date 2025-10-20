d61cbae1b5c1a09f4ff6e880a9ee6ba44c525edc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-20 14:04:52 +0000

    Add package ejd/0.1.8/ejd-0.1.8.tar.gz

---
+++ packages/ejd/ejd-0.1.8.tar.gz/ejd-0.1.8/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+# Read the README for long description
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="ejd",
+    version="0.1.8",
+    author="Ahmed Samir",
+    author_email="ahmedsamirhelmy2003@gmail.com",
+    description="A CLI tool for static code analysis and fixing using Gemini AI",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Quality Assurance",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    python_requires=">=3.8",
+    install_requires=[
+        "google-generativeai>=0.3.0",
+    ],
+    entry_points={
+        'console_scripts': [
+            'ejd=ejad.cli:main',
+        ],
+    },
+)
