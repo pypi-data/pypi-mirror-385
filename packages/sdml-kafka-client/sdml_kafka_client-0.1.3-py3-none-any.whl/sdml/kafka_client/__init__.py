5e76c13fa42c31c6b3ee2653489835120bf31c11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-20 03:25:24 +0000

    Add package sdml-kafka-client/0.1.3/sdml_kafka_client-0.1.3-py3-none-any.whl

---
+++ packages/sdml-kafka-client/sdml_kafka_client-0.1.3-py3-none-any.whl/sdml/kafka_client/__init__.py
@@ -0,0 +1,11 @@
+from .clients import KafkaBaseClient, KafkaListener, KafkaRPC
+from .types import AssignmentSpec, ParserSpec, TypeStream
+
+__all__ = [
+    "KafkaBaseClient",
+    "KafkaListener",
+    "KafkaRPC",
+    "AssignmentSpec",
+    "ParserSpec",
+    "TypeStream",
+]
---
+++ packages/sdml-kafka-client/sdml_kafka_client-0.1.3-py3-none-any.whl/sdml/kafka_client/clients/__init__.py
@@ -0,0 +1,5 @@
+from .base_client import KafkaBaseClient
+from .listener import KafkaListener
+from .rpc import KafkaRPC
+
+__all__ = ["KafkaBaseClient", "KafkaListener", "KafkaRPC"]
