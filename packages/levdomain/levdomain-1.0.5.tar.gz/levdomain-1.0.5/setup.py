258b32f4e1a2b515197e7cbeee119b1aaf8b8b48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-20 15:04:55 +0000

    Add package levdomain/1.0.5/levdomain-1.0.5.tar.gz

---
+++ packages/levdomain/levdomain-1.0.5.tar.gz/levdomain-1.0.5/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="levdomain",  
+    version="1.0.5",
+    author="somx",
+    description="A simple FastAPI app",
+    long_description_content_type="text/markdown",
+    packages=["src"],  
+    package_dir={"src": "src"},
+    include_package_data=True,
+    python_requires=">=3.7",
+    install_requires=[
+        "fastapi",
+        "uvicorn",
+        "pydantic",
+        "pydantic-settings",
+        "requests",
+        "python-dotenv"
+    ],
+    entry_points={
+        "console_scripts": [
+            "levdomain=src.app:main"
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
