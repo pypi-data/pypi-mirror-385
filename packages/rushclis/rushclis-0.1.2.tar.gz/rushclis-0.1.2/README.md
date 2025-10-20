14f8b17343bacabdaf335b15a028815645dc5287
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-20 03:25:48 +0000

    Add package rushclis/0.1.2/rushclis-0.1.2.tar.gz

---
+++ packages/rushclis/rushclis-0.1.2.tar.gz/rushclis-0.1.2/README.md
@@ -0,0 +1,13 @@
+﻿# RushCli
+
+Python cli class tools
+
+## Install
+
+``
+python.exe scripts/install.py
+``
+
+## Use
+
+继承RushCli类即可.
---
+++ packages/rushclis/rushclis-0.1.2.tar.gz/rushclis-0.1.2/rushclis/command.py
@@ -0,0 +1,14 @@
+﻿from argparse import ArgumentParser
+
+from rushlib.output import print_red
+
+
+class Command(ArgumentParser):
+    def __init__(self,
+                 print_error=False, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.print_error = print_error
+
+    def error(self, message):
+        if self.print_error:
+            print_red(f"{message}")
