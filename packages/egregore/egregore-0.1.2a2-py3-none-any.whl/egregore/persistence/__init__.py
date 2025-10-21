0181e91b9bb5a7ef8475efc002d4605f40fd6eeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 17:13:07 +0000

    Add package egregore/0.1.2a2/egregore-0.1.2a2-py3-none-any.whl

---
+++ packages/egregore/egregore-0.1.2a2-py3-none-any.whl/egregore/persistence/__init__.py
@@ -0,0 +1,5 @@
+"""Persistence engines and bridges for the Egregore core.
+
+The persistence layer will host adapters for databases, Redis, vector stores,
+and any other durable backends that need to plug into agent workflows.
+"""
