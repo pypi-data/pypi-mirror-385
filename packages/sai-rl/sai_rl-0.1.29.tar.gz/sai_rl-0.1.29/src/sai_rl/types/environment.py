b9d71d8ec150cb5352b207cc08c2840d1b59c2fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 05:06:06 +0000

    Add package sai-rl/0.1.29/sai_rl-0.1.29.tar.gz

---
+++ packages/sai-rl/sai_rl-0.1.29.tar.gz/sai_rl-0.1.29/src/sai_rl/types/environment.py
@@ -0,0 +1,18 @@
+from typing import Literal
+from pydantic import BaseModel
+
+from sai_rl.types.package import PackageType
+
+EnvironmentActionType = Literal["discrete", "continuous"]
+EnvironmentStandardType = Literal[
+    "gymnasium", "gym-v26", "gym-v21", "pettingzoo", "pufferlib"
+]
+
+
+class EnvironmentType(BaseModel):
+    id: str
+    name: str
+    gymId: str
+    type: EnvironmentStandardType
+    actionType: EnvironmentActionType
+    package: PackageType
