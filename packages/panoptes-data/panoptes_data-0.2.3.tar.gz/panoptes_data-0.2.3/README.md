5d01046d77fc9cf67c95f0a149c5baf97bdcd290
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 09:06:25 +0000

    Add package panoptes-data/0.2.3/panoptes_data-0.2.3.tar.gz

---
+++ packages/panoptes-data/panoptes_data-0.2.3.tar.gz/panoptes_data-0.2.3/README.md
@@ -0,0 +1,100 @@
+[![Documentation Status](https://readthedocs.org/projects/panoptes-data/badge/?version=latest)](https://panoptes-data.readthedocs.io/en/latest/?badge=latest)
+
+# PANOPTES Data tools
+
+Tools for searching and downloading PANOPTES data.
+
+## Install
+
+Install from pip:
+
+```bash
+pip install panoptes-data
+```
+
+## Examples
+
+See the example Jupyter Notebooks in the `notebooks/` directory.
+
+### Finding observations
+
+```py
+from panoptes.data.search import search_observations
+from panoptes.data.observations import ObservationInfo
+
+# Find some observations
+results = search_observations(by_name='M42')
+
+# Use last result entry to create ObservationInfo object.
+obs_info = ObservationInfo(meta=results.iloc[0])
+print(obs_info.meta)
+
+# Create an ObservationInfo object directly from a sequence_id.
+obs_info = ObservationInfo('PAN001_14d3bd_20180113T052325')
+# But then there is no metadata:
+print(obs_info.meta)
+```
+
+```text
+Sample output (truncated):
+
+camera_id                                           14d3bd
+camera_lens_serial_number                        HA0028608
+camera_serial_number                           12070048413
+coordinates_mount_dec                            -6.229778
+coordinates_mount_ra                               76.0815
+exptime                                              120.0
+field_name                                         Wasp 35
+num_images                                            28.0
+sequence_id                  PAN001_14d3bd_20180113T052325
+software_version                                POCSv0.6.0
+time                             2018-01-13 05:23:25+00:00
+total_exptime                                       3360.0
+unit_id                                             PAN001
+Name: 6121, dtype: object
+```
+
+### Downloading images
+
+The `ObservationInfo` object makes it easy to download the files:
+
+```py
+obs_info.download_images()
+```
+
+### Command-line tools
+
+There is a simple command line tool that allows for both searching and downloading of images and metadata.
+
+#### Search for observations:
+
+```bash
+panoptes-data search --name M42 --min-num-images 90
+```
+
+Example table output:
+
+```text
+| sequence_id                   | field_name   | unit_id   |   coordinates_mount_ra |   coordinates_mount_dec |   num_images |   exptime |   total_exptime | time                      |
+|:------------------------------|:-------------|:----------|-----------------------:|------------------------:|-------------:|----------:|----------------:|:--------------------------|
+| PAN022_977c86_20220108T090553 | M42          | PAN022    |                83.8221 |                -5.39111 |           95 |   90      |            8550 | 2022-01-08 09:05:53+00:00 |
+| PAN022_538cc6_20220108T090553 | M42          | PAN022    |                83.8221 |                -5.39111 |           95 |   89      |            8455 | 2022-01-08 09:05:53+00:00 |
+| PAN019_42433a_20220114T085722 | M42          | PAN019    |                83.8221 |                -5.39111 |           90 |   90      |            8100 | 2022-01-14 08:57:22+00:00 |
+| PAN019_c623e9_20220114T085722 | M42          | PAN019    |                83.8221 |                -5.39111 |           90 |   89.0222 |            8012 | 2022-01-14 08:57:22+00:00 |
+| PAN019_c623e9_20220115T082108 | M42          | PAN019    |                83.8221 |                -5.39111 |          105 |   89.019  |            9347 | 2022-01-15 08:21:08+00:00 |
+| PAN019_42433a_20220115T082108 | M42          | PAN019    |                83.8221 |                -5.39111 |          105 |   90.0095 |            9451 | 2022-01-15 08:21:08+00:00 |
+```
+
+#### Downloading all images for an observation:
+
+```bash
+panoptes-data download PAN022_977c86_20220108T090553
+```
+
+#### Get all metadata for a unit in a given date range:
+
+```bash
+panoptes-data get-metadata --unit-id PAN022 --start-date '2022-01-08'
+```
+
+See `panoptes-data --help` for more options.
---
+++ packages/panoptes-data/panoptes_data-0.2.3.tar.gz/panoptes_data-0.2.3/src/panoptes/data/utils/cli/main.py
@@ -0,0 +1,190 @@
+from pathlib import Path
+
+import pandas as pd
+import typer
+from astropy.time import Time
+from rich import print
+from tqdm import tqdm
+
+from panoptes.data.observations import ObservationInfo
+from panoptes.data.search import search_observations
+from panoptes.utils.time import current_time, flatten_time
+
+app = typer.Typer(pretty_exceptions_enable=False)
+
+
+@app.command()
+def download(sequence_id: str | None = typer.Argument(..., help='Sequence ID for the Observation.'),
+             output_dir: Path = typer.Option(
+                 None,
+                 '--output-dir', '-o',
+                 help='Output directory for images, defaults to sequence_id.'
+             ),
+             image_query: str = typer.Option(
+                 'status!="ERROR"',
+                 '--image-query', '-q',
+                 help='Query for images, default \'status!="ERROR"\''
+             ),
+             ) -> list[str]:
+    """Downloads all FITS images for the observation."""
+    local_files = list()
+
+    if output_dir is None:
+        output_dir = Path(sequence_id)
+
+    print(f'Downloading images for {sequence_id} to {output_dir}.')
+
+    try:
+        obs_info = ObservationInfo(sequence_id=sequence_id, image_query=image_query)
+        print(f'Found {len(obs_info.image_metadata)} images for {sequence_id}.')
+        local_files = obs_info.download_images(output_dir=output_dir)
+        print(f'Downloaded {len(local_files)} images to {output_dir}.')
+
+    except Exception as e:
+        print(f'[red]Error downloading images for {sequence_id}: {e}')
+
+    return local_files
+
+
+@app.command()
+def get_metadata(
+    sequence_id: str | None = typer.Option(
+        None, '--sequence-id', '-s',
+        help='Sequence ID for the Observation.'
+    ),
+    unit_id: str | None = typer.Option(
+        None, '--unit-id', '-u',
+        help='Unit ID for the Observation. '
+             'Use with a date range to download all metadata for a unit.'
+    ),
+    start_date: str | None = typer.Option(
+        None, '--start-date', '-s',
+        help='Start date for downloading metadata in form YYYY-MM-DD'
+    ),
+    end_date: str | None = typer.Option(
+        None, '--end-date', '-s',
+        help='End date for downloading metadata, defaults to now.'
+    ),
+    output_dir: Path = typer.Option(
+        Path('.'), '--output-dir', '-o',
+        help='Output directory for metadata file.'
+    ),
+):
+    """Download metadata.
+
+    If given a single sequence_id, will download the metadata for that observation.
+    If given a unit_id and date range, will download all metadata for that unit.
+    """
+
+    if sequence_id is not None:
+        output_fn = output_dir / f'{sequence_id}-metadata.csv'
+        try:
+            obs_info = ObservationInfo(sequence_id=sequence_id)
+            obs_info.image_metadata.to_csv(output_fn)
+            print(f'[green]Metadata saved to {output_fn}')
+        except Exception as e:
+            print(f'[red]Error downloading metadata for {sequence_id}: {e}')
+    else:
+        if unit_id is None:
+            print('[red]Must provide a unit_id if not providing a sequence_id.')
+            return
+
+        if start_date is None:
+            print('[red]Must provide a start_date if not providing a sequence_id.')
+            return
+        else:
+            start_date = flatten_time(Time(start_date))[:8]
+
+        if end_date is None:
+            end_date = current_time(flatten=True)[:8]
+        else:
+            end_date = flatten_time(Time(end_date))[:8]
+
+        output_fn = output_dir / f'{unit_id}-{start_date}-{end_date}-metadata.csv'
+        try:
+            results_df = search_observations(
+                unit_id=unit_id.upper(),
+                start_date=start_date,
+                by_name='M42',
+                radius=290
+            )
+
+            dfs = list()
+            for idx, rec in (pbar := tqdm(results_df.iterrows(), total=len(results_df))):
+                try:
+                    pbar.set_description(f'Getting metadata for {rec["sequence_id"]}')
+                    dfs.append(ObservationInfo(meta=rec).image_metadata)
+                except Exception:
+                    pbar.write(f'Error in {idx} {rec["sequence_id"]}')
+
+            pd.concat(dfs).to_csv(output_fn)
+
+        except ValueError as e:
+            print(f'Error downloading metadata for {unit_id}: {e}', fg='red')
+
+    print(f'Metadata saved to [green]{output_fn}')
+
+    return output_fn
+
+
+@app.command()
+def search(
+    name: str = typer.Option(
+        None, '--name', '-n',
+        help='Name of object to search for.'
+    ),
+    unit_id: str = typer.Option(
+        None, '--unit-id', '-u',
+        help='Unit ID for the Observation.'
+    ),
+    start_date: str = typer.Option(
+        None, '--start-date', '-s',
+        help='Start date for downloading metadata in form YYYY-MM-DD'
+    ),
+    end_date: str = typer.Option(
+        None, '--end-date', '-s',
+        help='End date for downloading metadata, defaults to now.'
+    ),
+    ra: float = typer.Option(
+        None, '--ra', '-r',
+        help='RA in degrees for search.'
+    ),
+    dec: float = typer.Option(
+        None, '--dec', '-d',
+        help='Dec in degrees for search.'
+    ),
+    radius: int = typer.Option(
+        10, '--radius', '-r',
+        help='Radius in degrees for search.'
+    ),
+    min_num_images: int = typer.Option(
+        1, '--min-num-images', '-m',
+        help='Minimum number of images.'
+    ),
+
+):
+    """Search for observations."""
+    results = search_observations(
+        unit_id=unit_id,
+        start_date=start_date,
+        end_date=end_date,
+        by_name=name,
+        ra=ra,
+        dec=dec,
+        min_num_images=min_num_images,
+        radius=radius
+    )
+
+    if len(results) == 0:
+        print('[red]No results found.')
+        return
+
+    display_cols = ['field_name', 'unit_id', 'coordinates.mount_ra', 'coordinates.mount_dec', 'num_images', 'exptime',
+                    'total_exptime', 'time']
+    markdown_table = results.set_index('sequence_id')[display_cols].to_markdown()
+    print(markdown_table)
+    print(f'Found {len(results)} observations.')
+
+
+if __name__ == "__main__":
+    app()
