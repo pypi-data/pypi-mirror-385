63b4f364acf4310e5615d88bd74310ce4f620ddc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 16:05:56 +0000

    Add package latticeflow-go-sdk/0.18.1/latticeflow_go_sdk-0.18.1-py3-none-any.whl

---
+++ packages/latticeflow-go-sdk/latticeflow_go_sdk-0.18.1-py3-none-any.whl/latticeflow/go/types/__init__.py
@@ -0,0 +1,7 @@
+from latticeflow.go._generated.types import ApiError
+from latticeflow.go._generated.types import File
+from latticeflow.go._generated.types import Response
+from latticeflow.go._generated.types import Unset
+
+
+__all__ = ("ApiError", "File", "Response", "Unset")
