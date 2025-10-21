ea8196d12de0409ea14ae90dd3675d9039680c94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 18:09:14 +0000

    Add package ACID-code-v2/0.2.3/acid_code_v2-0.2.3.tar.gz

---
+++ packages/ACID-code-v2/acid_code_v2-0.2.3.tar.gz/acid_code_v2-0.2.3/example/tutorial_code.py
@@ -0,0 +1,45 @@
+#%%
+from astropy.io import fits
+import importlib, os, sys
+import multiprocessing as mp
+import numpy as np
+import matplotlib.pyplot as plt
+os.chdir(os.path.dirname(__file__))
+os.chdir("..")  # ensures we are in the main directory
+try:
+    import ACID_code_v2 as acid
+except:
+    SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+    PROJECT_ROOT = os.path.dirname(SCRIPT_DIR)
+    sys.path.append(PROJECT_ROOT)
+    from src import ACID_code_v2 as acid
+    print("pip module failed to import, imported from local instead")
+importlib.reload(acid)
+
+spec_file = fits.open('example/sample_spec_1.fits')
+
+wavelength = spec_file[0].data   # Wavelengths in Angstroms
+spectrum = spec_file[1].data     # Spectral Flux
+error = spec_file[2].data        # Spectral Flux Errors
+sn = spec_file[3].data           # SN of Spectrum
+
+linelist = 'example/example_linelist.txt' # Insert path to line list
+
+# choose a velocity grid for the final profile(s)
+deltav = 0.82   # velocity pixel size must not be smaller than the spectral pixel size
+velocities = np.arange(-25, 25, deltav)
+
+# run ACID function
+result = acid.ACID([wavelength], [spectrum], [error], linelist, [sn], velocities,
+                   parallel=True, cores=None, nsteps=4000, verbose=True)
+
+# extract profile and errors
+profile = result[0, 0, 0]
+profile_error = result[0, 0, 1]
+
+# plot results
+plt.figure()
+plt.errorbar(velocities, profile, profile_error)
+plt.xlabel('Velocities (km/s)')
+plt.ylabel('Flux')
+plt.show()
