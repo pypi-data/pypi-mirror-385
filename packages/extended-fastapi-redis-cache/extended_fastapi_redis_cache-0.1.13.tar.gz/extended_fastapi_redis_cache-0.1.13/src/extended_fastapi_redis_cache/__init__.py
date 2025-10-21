b05102ddc06ee9e020df03af73f61865520c5e7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 18:10:32 +0000

    Add package extended-fastapi-redis-cache/0.1.13/extended_fastapi_redis_cache-0.1.13.tar.gz

---
+++ packages/extended-fastapi-redis-cache/extended_fastapi_redis_cache-0.1.13.tar.gz/extended_fastapi_redis_cache-0.1.13/src/extended_fastapi_redis_cache/__init__.py
@@ -0,0 +1,11 @@
+# flake8: noqa
+from extended_fastapi_redis_cache.cache import (
+    cache,
+    cache_one_day,
+    cache_one_hour,
+    cache_one_minute,
+    cache_one_month,
+    cache_one_week,
+    cache_one_year,
+)
+from extended_fastapi_redis_cache.client import FastApiRedisCache
