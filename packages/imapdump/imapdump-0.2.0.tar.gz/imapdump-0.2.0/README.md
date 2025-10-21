750afa28dc925956ca2e63ca1b08614d9e41eb2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 04:04:55 +0000

    Add package imapdump/0.2.0/imapdump-0.2.0.tar.gz

---
+++ packages/imapdump/imapdump-0.2.0.tar.gz/imapdump-0.2.0/README.md
@@ -0,0 +1,75 @@
+# imapdump
+[![Upload Python Package](https://github.com/das-kaesebrot/imapdump/actions/workflows/python-publish.yml/badge.svg)](https://github.com/das-kaesebrot/imapdump/actions/workflows/python-publish.yml)
+
+`imapdump` is a cli based tool to export imap accounts to a local folder.
+
+## Installation
+
+Install the module from [PyPI](https://pypi.org/project/imapdump/):
+```bash
+pip install imapdump
+```
+
+## Usage
+Launch the application via the included command `imapdump`.
+
+```bash
+user@machine:~$ imapdump -h
+usage: dump.py [-h] [-l {critical,fatal,error,warn,info,debug}] [--host HOST] [-f DATABASE_FILE] [-p PORT] [-u USERNAME]
+                   [--password PASSWORD] [--encryption-mode {none,ssl,starttls}] [--folder-regex FOLDER_REGEX] [--force-dump]
+                   [--dump-folder DUMP_FOLDER] [--config CONFIG]
+
+Dump IMAP accounts to a local directory
+
+options:
+  -h, --help            show this help message and exit
+  -l, --logging {critical,fatal,error,warn,info,debug}
+                        set the log level (default: info)
+  --host HOST           Hostname of the IMAP server (default: None)
+  -f, --file DATABASE_FILE
+                        Database file (default: None)
+  -p, --port PORT       Port of the IMAP server (default: 993)
+  -u, --username USERNAME
+                        Username for the IMAP account (default: None)
+  --password PASSWORD   Password of the IMAP account (default: None)
+  --encryption-mode {none,ssl,starttls}
+                        IMAP encryption mode (default: ssl)
+  --folder-regex FOLDER_REGEX
+                        Pattern to match against for including folders (default: ^.*$)
+  --force-dump          Force dump all matching messages without checking against existing database (default: False)
+  --dump-folder DUMP_FOLDER
+                        Where to dump .eml files to (default: None)
+  --config CONFIG       Supply a config file (default: None)
+```
+
+## Configuration
+Create a `config.yml`.
+
+Example configuration:
+```yaml
+host: imap.example.com
+username: user
+password: supers3cr3tp4ssw0rd
+loglevel: info
+dump_folder: /path/to/dump/folder
+```
+
+Then run the application:
+```bash
+user@machine~$ imapdump -l debug --config config.yml
+```
+
+## Open Source License Attribution
+
+This application uses Open Source components. You can find the source code of their open source projects along with license information below. We acknowledge and are grateful to these developers for their contributions to open source.
+### [dacite](https://github.com/konradhalas/dacite)
+- Copyright (c) [Konrad Hałas](https://github.com/konradhalas) and contributors
+- [MIT License](https://github.com/konradhalas/dacite/blob/master/LICENSE)
+
+### [PyYAML](https://pyyaml.org/)
+- Copyright (c) [The YAML Project](https://github.com/yaml) and contributors
+- [MIT License](https://github.com/yaml/pyyaml/blob/main/LICENSE)
+
+### [imapclient](https://github.com/mjs/imapclient/)
+- Copyright (c) 2014 [Menno Smits](https://github.com/mjs) and contributors
+- [New BSD License](https://github.com/mjs/imapclient/blob/master/COPYING)
---
+++ packages/imapdump/imapdump-0.2.0.tar.gz/imapdump-0.2.0/imapdump/dump.py
@@ -0,0 +1,130 @@
+import argparse
+import logging
+import os
+import yamlargparse
+
+from . import __version__
+from .imap.dumper import ImapDumper
+from .enums.imap_encryption_mode import ImapEncryptionMode
+from .config.imap_config import ImapDumpConfig
+
+
+def main():
+    available_levels = [
+        level.lower() for level in logging.getLevelNamesMapping().keys()
+    ]
+    available_levels.remove(logging.getLevelName(logging.NOTSET).lower())
+    available_levels.remove(logging.getLevelName(logging.WARNING).lower())
+
+    parser = yamlargparse.ArgumentParser(
+        description="Dump IMAP accounts to a local directory",
+        error_handler=yamlargparse.usage_and_exit_error_handler,
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+
+    parser.add_argument(
+        "-l",
+        "--logging",
+        help="set the log level",
+        dest="loglevel",
+        type=str,
+        choices=available_levels,
+        default=logging.getLevelName(logging.INFO).lower(),
+    )
+
+    parser.add_argument(
+        "--host",
+        help="Hostname of the IMAP server",
+        type=str,
+    )
+
+    parser.add_argument(
+        "-f",
+        "--file",
+        help="Database file",
+        type=str,
+        dest="database_file",
+        default=None,
+    )
+
+    parser.add_argument(
+        "-p",
+        "--port",
+        help="Port of the IMAP server",
+        type=int,
+        default=993,
+    )
+
+    parser.add_argument(
+        "-u",
+        "--username",
+        help="Username for the IMAP account",
+        type=str,
+        default=None,
+    )
+
+    parser.add_argument(
+        "--password",
+        help="Password of the IMAP account",
+        type=str,
+        default=None,
+    )
+
+    parser.add_argument(
+        "--encryption-mode",
+        help="IMAP encryption mode",
+        type=ImapEncryptionMode,
+        required=False,
+        choices=ImapEncryptionMode.list(),
+        default=ImapEncryptionMode.SSL,
+    )
+
+    parser.add_argument(
+        "--folder-regex",
+        help="Pattern to match against for including folders",
+        type=str,
+        default="^.*$",
+    )
+
+    parser.add_argument(
+        "--force-dump",
+        help="Force dump all matching messages without checking against existing database",
+        action="store_true",
+    )
+
+    parser.add_argument(
+        "--dump-folder",
+        help="Where to dump .eml files to",
+        type=str,
+        default=None,
+    )
+
+    parser.add_argument("--config", help="Supply a config file", action=yamlargparse.ActionConfigFile)
+    
+    print(f"\n[imapdump v{__version__}]\n")
+    
+    args = parser.parse_args()
+
+    logging.basicConfig(
+        format="[%(asctime)s] [%(name)s] [%(levelname)s] %(message)s",
+        level=args.loglevel.upper(),
+    )
+    logger = logging.getLogger("main")
+    logger.debug(f"Running as UID {os.getuid()}")
+
+    config = ImapDumpConfig(**vars(args))
+
+    try:
+        dumper = ImapDumper(config=config)
+        dumper.dump()
+
+    except KeyboardInterrupt as e:
+        logger.info("Got KeyboardInterrupt")
+    except Exception as e:
+        logger.exception("Exception encountered")
+    finally:
+        logger.info("Shutting down")
+
+
+if __name__ == "__main__":
+    main()
