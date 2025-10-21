0642407803f665af9ae1cf5c4b4afbc677268ef4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 03:20:32 +0000

    Add package MetObs-toolkit/0.4.7/metobs_toolkit-0.4.7.tar.gz

---
+++ packages/MetObs-toolkit/metobs_toolkit-0.4.7.tar.gz/metobs_toolkit-0.4.7/src/metobs_toolkit/settings_collection/__init__.py
@@ -0,0 +1,10 @@
+# flake8: noqa: F401
+from .label_defenitions import (
+    label_def,
+    label_to_color_map,
+    gapfill_label_group,
+    failed_gapfill_label_group,
+    qc_label_group,
+)
+
+from .version import __version__
