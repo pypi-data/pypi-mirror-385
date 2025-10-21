8599277e00ea5a9d2cc89f2bb1261ac7557e74c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 06:05:58 +0000

    Add package guardiumATK/1.1.3/guardiumATK-1.1.3.tar.gz

---
+++ packages/guardiumATK/guardiumATK-1.1.3.tar.gz/guardiumATK-1.1.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='guardiumATK',
+    version='1.1.3',
+    author='Josh Klahn',
+    author_email='joshua.d.klahn@gmail.com',
+    url='https://github.com/jklahn/guardiumATK',
+    packages=['guardiumATK']
+)
