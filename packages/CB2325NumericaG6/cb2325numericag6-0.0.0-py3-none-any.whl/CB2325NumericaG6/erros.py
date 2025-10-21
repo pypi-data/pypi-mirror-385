65544d17d9cbcc9aaa6d128180c3440c55b4a3f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 04:05:12 +0000

    Add package CB2325NumericaG6/0.0.0/cb2325numericag6-0.0.0-py3-none-any.whl

---
+++ packages/CB2325NumericaG6/cb2325numericag6-0.0.0-py3-none-any.whl/CB2325NumericaG6/erros.py
@@ -0,0 +1,9 @@
+
+
+def erro_absoluto(valor_real: float, valor_aprox: float) -> float:
+    # TODO: Implement this
+    raise NotImplementedError
+
+def erro_relativo(valor_real: float, valor_aprox: float) -> float:
+    # TODO: Implement this
+    raise NotImplementedError
