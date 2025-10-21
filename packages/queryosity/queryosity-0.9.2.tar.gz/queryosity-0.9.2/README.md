d4a44f5069106957716140201b8a95d7b11fa0ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 16:06:10 +0000

    Add package queryosity/0.9.2/queryosity-0.9.2.tar.gz

---
+++ packages/queryosity/queryosity-0.9.2.tar.gz/queryosity-0.9.2/README.md
@@ -0,0 +1,102 @@
+# Queryosity
+
+![Version](https://img.shields.io/badge/Version-0.9.2-blue.svg)
+![C++ Standard](https://img.shields.io/badge/C++-17-blue.svg)
+[![Ubuntu](https://github.com/taehyounpark/queryosity/actions/workflows/test-on-ubuntu.yml/badge.svg?branch=master)](https://github.com/taehyounpark/queryosity/actions/workflows/test-on-ubuntu.yml)
+[![macOS](https://github.com/taehyounpark/queryosity/actions/workflows/test-on-macos.yml/badge.svg?branch=master)](https://github.com/taehyounpark/queryosity/actions/workflows/test-on-macos.yml)
+[![MIT License](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+
+Queryosity is a row-wise data analysis library for (semi-)structured data.
+
+- Dataflow interface.
+- Arbitrary data types.
+- Lazy, multithreaded actions.
+- Sensitivity analysis.
+
+<p align="center">
+	<strong> <a href="https://queryosity.readthedocs.io/">Documentation</a></strong>
+</p>
+
+## Hello World
+
+```cpp
+#include <fstream>
+#include <sstream>
+#include <vector>
+
+#include <queryosity.hpp>
+#include <queryosity/boost/histogram.hpp>
+#include <queryosity/nlohmann/json.hpp>
+
+using dataflow = qty::dataflow;
+namespace multithread = qty::multithread;
+namespace dataset = qty::dataset;
+namespace column = qty::column;
+namespace query = qty::query;
+
+using json = qty::nlohmann::json;
+using h1d = qty::boost::histogram::histogram<double>;
+using linax = qty::boost::histogram::axis::regular;
+
+int main() {
+  dataflow df(multithread::enable(10));
+
+  std::ifstream data("data.json");
+  auto [x, w] = df.read(
+      dataset::input<json>(data), dataset::column<std::vector<double>>("x"), dataset::column<double>("w"));
+
+  auto zero = df.define(column::constant(0));
+  auto x0 = x[zero];
+
+  auto sel =
+      df.weight(w)
+          .filter(column::expression(
+              [](std::vector<double> const &v) { return v.size(); }))(x);
+
+  auto h_x0_w = df.get(query::output<h1d>(linax(20, 0.0, 200.0)))
+                    .fill(x0)
+                    .at(sel)
+                    .result();
+
+  std::ostringstream os;
+  os << *h_x0_w;
+  std::cout << os.str() << std::endl;
+}
+```
+
+## Installation
+
+### [Single-header](https://raw.githubusercontent.com/taehyounpark/queryosity/master/queryosity.h)
+```cpp
+#include <queryosity.hpp>
+```
+### CMake
+```sh
+git clone https://github.com/taehyounpark/queryosity.git
+``````
+#### External
+```sh
+cd queryosity/ && mkdir build/ && cd build/
+cmake -DQUERYOSITY_INSTALL=ON ../
+cmake --build .
+cmake --install .
+```
+```cmake
+find_package(queryosity 0.9.2 REQUIRED)
+...
+add_library(Analysis ...)
+...
+target_link_libraries(Analysis INTERFACE queryosity::queryosity)
+```
+```cpp
+#include <queryosity.hpp>
+```
+#### Integrated
+```cmake
+add_subdirectory(queryosity)
+...
+add_library(Analysis ...)
+...
+target_link_libraries(Analysis INTERFACE queryosity::queryosity)
+```
---
+++ packages/queryosity/queryosity-0.9.2.tar.gz/queryosity-0.9.2/qtypy/selection.py
@@ -0,0 +1,137 @@
+import cppyy
+
+from abc import ABC, abstractmethod
+
+from .cpp import cpp_binding 
+from .cpp import parse_cpp_identifiers
+
+class at:
+    """
+    Apply a selection using a specific preselection.
+
+    By default, selections are applied sequentially, meaning each subsequent
+    call compounds on the previous selection as its preselection. Use
+    ``at(preselection_name)`` to override this behavior and explicitly set the
+    preselection for a cut or weight.
+
+    Examples
+    --------
+    .. code-block:: python
+
+        # compounding cuts, a -> (a && b) -> (a && b && c)
+        df.apply({
+            "a": cut("x > 0"),
+            "b": cut("y < 0"),   # a && b
+            "c": cut("z != 0")   # a && b && c
+        })
+
+    .. code-block:: python
+
+        # branching cuts, a -> (a && b, a && c)
+        df.apply({
+            "a": cut("x > 0"),
+            "b": cut("y < 0"),          # a && b
+            "c": at("a").cut("z != 0")  # a && c
+        })
+    """
+
+    def __init__(self, preselection):
+        self.preselection_name = preselection
+
+    def cut(self, expr : str):
+        return cut(expr, preselection=self.preselection_name)
+
+    def weight(self, expr: str):
+        return weight(expr, preselection=self.preselection_name)
+
+class selection(ABC, cpp_binding):
+
+    def __init__(self, expr: str, preselection : str = None):
+        super().__init__()
+        self.cpp_prefix += '_df_selection_'
+        self.expr = expr
+        self.args = parse_cpp_identifiers(expr)
+        self.preselection_name = preselection
+
+    def __str__(self):
+        return f'{self.node_operation}({self.expr})'
+
+    @property
+    @abstractmethod
+    def node_operation(self):
+        pass
+
+    @property
+    @abstractmethod
+    def value_type(self):
+        pass
+
+    def instantiate(self, df):
+        column_args = [arg for arg in self.args if arg in df.columns]
+        lmbd_args = [f'{df.columns[arg].value_type} const& {arg}' for arg in column_args]
+        lmbd_defn = '[](' + ', '.join(lmbd_args) + '){return (' + self.expr + ');}'
+        lazy_args = [df.columns[arg].cpp_identifier for arg in column_args]
+
+        if self.preselection_name is None:
+            self.preselection = df.current_selection
+            if df.current_selection == df: self.preselection_name = '--'
+            else: self.preselection_name = df.current_selection.name
+        else:
+            self.preselection = df.selections[self.preselection_name]
+            self.preselection_name = self.preselection.name
+
+        return cppyy.cppdef(f'auto {self.cpp_identifier} = {self.preselection.cpp_identifier}.{self.node_operation}(qty::column::expression({lmbd_defn})).apply({", ".join(lazy_args)});')
+
+class cut(selection):
+    """
+    Represents a floating-point weight applied to a row, considered only
+    when a cut is passed.
+
+    A ``weight`` evaluates a numerical expression for each row representing
+    its statistical significance. Weights compound by multiplication onto
+    existing preselection(s).
+    
+    Parameters
+    ----------
+    expr : str
+        A boolean expression to evaluate on the dataset rows.
+    preselection : str, optional
+        Name of a prior selection to use as the base for this cut.
+    """
+    def __init__(self, expr: str, preselection : str = None):
+        super().__init__(expr, preselection)
+
+    @property
+    def node_operation(self):
+        return 'filter'
+
+    @property
+    def value_type(self):
+        return 'bool'
+
+class weight(selection):
+    """
+    Represents a floating-point weight applied to a row, considered only when
+    a cut is passed.
+
+    A ``weight`` evaluates a numerical expression for each row representing
+    its statistical significance. Weights compound by multiplication onto
+    existing preselection(s).
+
+    Parameters
+    ----------
+    expr : str
+        An expression returning a floating-point value for each row.
+    preselection : str, optional
+        Name of a prior selection (cut) to restrict the weight application.
+    """
+    def __init__(self, expr: str, preselection : str = None):
+        super().__init__(expr, preselection)
+
+    @property
+    def node_operation(self):
+        return 'weight'
+
+    @property
+    def value_type(self):
+        return f'double'
