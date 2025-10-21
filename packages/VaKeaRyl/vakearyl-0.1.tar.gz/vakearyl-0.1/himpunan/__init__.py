12b73be2ece44fc1c0071955cbf536788daadf00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 16:06:14 +0000

    Add package VaKeaRyl/0.1/vakearyl-0.1.tar.gz

---
+++ packages/VaKeaRyl/vakearyl-0.1.tar.gz/vakearyl-0.1/himpunan/__init__.py
@@ -0,0 +1,1 @@
+from .Himpunan import Himpunan
---
+++ packages/VaKeaRyl/vakearyl-0.1.tar.gz/vakearyl-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='VaKeaRyl',
+    version='0.1',
+    packages=find_packages(),
+    description='Implementasi konsep himpunan menggunakan Python list dan magic methods.',
+    author='VaKeaRyl',
+    license='MIT',
+    python_requires='>=3.7'
+)
