eb803d5b2342e609a4a5b67071fdbce58ad8f72d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 14:06:36 +0000

    Add package euroncalc/0.1.1/euroncalc-0.1.1.tar.gz

---
+++ packages/euroncalc/euroncalc-0.1.1.tar.gz/euroncalc-0.1.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='euroncalc',
+    version='0.1.1',
+    author='tejan',
+    author_email='tejan@gmail.com',
+    description='A simple calculator package',
+    long_description=open('README.md', 'r', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'euroncalc=euroncalc.calculator:main',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
