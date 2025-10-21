9a327b420c226991ef8a7785180a55e721d6fbe7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 16:05:56 +0000

    Add package jupyter-hurl-kernel/0.0.10/jupyter_hurl_kernel-0.0.10.tar.gz

---
+++ packages/jupyter-hurl-kernel/jupyter_hurl_kernel-0.0.10.tar.gz/jupyter_hurl_kernel-0.0.10/src/jupyter_hurl_kernel/labextension/lib/index.d.ts
@@ -0,0 +1,9 @@
+/**
+ * JupyterLab extension for Hurl syntax highlighting
+ */
+import { JupyterFrontEndPlugin } from '@jupyterlab/application';
+/**
+ * Initialization data for the extension
+ */
+declare const plugin: JupyterFrontEndPlugin<void>;
+export default plugin;
