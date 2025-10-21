f226c35fab2821612f19f456181fa2dababb37df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 03:21:09 +0000

    Add package imapdump/0.1.0/imapdump-0.1.0-py3-none-any.whl

---
+++ packages/imapdump/imapdump-0.1.0-py3-none-any.whl/imapdump/models/mail.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+from sqlalchemy import DateTime
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.sql import func
+from .base import Base
+from hashlib import sha256
+
+
+class Mail(Base):
+    __tablename__ = "mails"
+    id: Mapped[str] = mapped_column(primary_key=True)
+    uid: Mapped[str] = mapped_column()
+    folder: Mapped[str] = mapped_column()
+    rfc822: Mapped[bytes] = mapped_column()
+    size: Mapped[int] = mapped_column()
+    date: Mapped[datetime] = mapped_column(DateTime, nullable=True)
+    modified: Mapped[datetime] = mapped_column(
+        DateTime, onupdate=func.now(), default=func.now()
+    )
+    created: Mapped[datetime] = mapped_column(DateTime, default=func.now())
+
+    @staticmethod
+    def generate_id(folder_name: str, message_id: str) -> str:
+        return sha256(f"{folder_name}_{message_id}".encode()).hexdigest()
