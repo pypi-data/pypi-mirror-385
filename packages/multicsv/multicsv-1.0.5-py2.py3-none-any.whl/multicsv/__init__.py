94063b461478b0378702068a95211ab6ba30ba0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 09:06:17 +0000

    Add package multicsv/1.0.5/multicsv-1.0.5-py2.py3-none-any.whl

---
+++ packages/multicsv/multicsv-1.0.5-py2.py3-none-any.whl/multicsv/__init__.py
@@ -0,0 +1,14 @@
+
+# flake8: noqa: F401
+
+from .open import multicsv_open as open
+from .open import multicsv_wrap as wrap
+from .file import MultiCSVFile
+from .section import MultiCSVSection
+
+__all__ = [
+    "open",
+    "wrap",
+    "MultiCSVFile",
+    "MultiCSVSection",
+]
---
+++ packages/multicsv/multicsv-1.0.5-py2.py3-none-any.whl/multicsv/exceptions.py
@@ -0,0 +1,54 @@
+
+
+class SubTextIOErrror(Exception):
+    """Base class for all SubTextIO custom exceptions."""
+    pass
+
+
+class OpOnClosedError(SubTextIOErrror, ValueError):
+    pass
+
+
+class InvalidWhenceError(SubTextIOErrror, ValueError):
+    pass
+
+
+class InvalidSubtextCoordinates(SubTextIOErrror, ValueError):
+    pass
+
+
+class BaseMustBeSeekable(SubTextIOErrror, ValueError):
+    pass
+
+
+class BaseMustBeReadable(SubTextIOErrror, ValueError):
+    pass
+
+
+class EndsBeyondBaseContent(SubTextIOErrror, ValueError):
+    pass
+
+
+class BaseIOClosed(SubTextIOErrror, ValueError):
+    pass
+
+
+class MultiCSVFileError(Exception):
+    """Base class for all MultiCSVFile custom exceptions."""
+    pass
+
+
+class OpOnClosedCSVFileError(MultiCSVFileError, ValueError):
+    pass
+
+
+class CSVFileBaseIOClosed(MultiCSVFileError, ValueError):
+    pass
+
+
+class SectionNotFound(MultiCSVFileError, KeyError):
+    pass
+
+
+class BrokenTell(MultiCSVFileError, IOError):
+    pass
