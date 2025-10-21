dbe0f466985c55e7476aa74d9cf55a2e82b03291
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 03:20:37 +0000

    Add package Orange3-Survival-Analysis/0.6.2/orange3_survival_analysis-0.6.2.tar.gz

---
+++ packages/Orange3-Survival-Analysis/orange3_survival_analysis-0.6.2.tar.gz/orange3_survival_analysis-0.6.2/CHANGELOG.md
@@ -0,0 +1,73 @@
+Change Log
+==========
+
+[next] - TBA
+------------
+* ...
+
+[0.6.2] - 2025-10-20
+--------------------
+##### Enhancements
+* Kaplan-Meier Plot - Hide median column if all curves have infinite median ([89209c04](../../commit/89209c0496c9395866efa6f1c59f870f9d1f7fe6))
+
+
+[0.6.1] - 2023-12-08
+--------------------
+##### Enhancements
+* CI - User reusable workflow for automated release ([#68](../../pull/68))
+
+[0.6.0] - 2023-12-08
+--------------------
+
+todo
+
+[0.5.1] - 2023-01-20
+--------------------
+##### Fixes
+* owassurvivaldata: fix setting initialzation ([#61](../../pull/61))
+
+[0.5.0] - 2022-12-06
+--------------------
+##### Enhancements
+* owcohors: add model and coefficients output ([#52](../../pull/52))
+* owranksurvivalfeatures: add log-rank scorer ([#57](../../pull/57))
+* owranksurvivalfeatures: add Features output ([#58](../../pull/58))
+
+##### Fixes
+* Remove calls to deprecated get_column_view method ([#59](../../pull/59))
+
+[0.4.0] - 2022-04-02
+--------------------
+
+
+[0.3.0] - 2021-10-28
+--------------------
+##### Enhancements
+* owkaplanmeier: add log-rank to legend footer ([#17](../../pull/17))
+* New widget: Rank Survival Features ([#16](../../pull/16))
+* New widget: Stepwise Cox Regression ([#18](../../pull/18))
+* New widget: Cox Regression ([#23](../../pull/23))
+
+
+[0.2.0] - 2021-03-29
+--------------------
+##### Enhancements
+* Kaplan-Meier Plot icon ([#11](../../pull/11))
+* owkaplanmeier: enable widget report, add description ([#12](../../pull/12))
+* owkaplanmeier: change the symbol of censored data ([#13](../../pull/13))
+* owkaplanmeier: add more information to the plot legend ([#14](../../pull/14))
+
+
+[0.1.0] - 2021-01-12
+--------------------
+##### Enhancements
+* New widget: Kaplan-Meier Plot
+
+
+[next]: https://github.com/biolab/orange3-survival-analysis/compare/0.5.1...master
+[0.5.1]: https://github.com/biolab/orange3-survival-analysis/compare/0.5.0...0.5.1
+[0.5.0]: https://github.com/biolab/orange3-survival-analysis/compare/0.4.0...0.5.0
+[0.4.0]: https://github.com/biolab/orange3-survival-analysis/compare/0.3.0...0.4.0
+[0.3.0]: https://github.com/biolab/orange3-survival-analysis/compare/0.2.0...0.3.0
+[0.2.0]: https://github.com/biolab/orange3-survival-analysis/compare/0.1.0...0.2.0
+[0.1.0]: https://github.com/biolab/orange3-survival-analysis/compare/20b2bd8ea02876eb9cad3925e0954054da2690de...0.1.0
---
+++ packages/Orange3-Survival-Analysis/orange3_survival_analysis-0.6.2.tar.gz/orange3_survival_analysis-0.6.2/orangecontrib/survival_analysis/modeling/cox.py
@@ -0,0 +1,96 @@
+import numpy as np
+import pandas as pd
+
+from lifelines import CoxPHFitter
+from Orange.base import Learner, Model
+from Orange.data import Table, Domain, table_from_frame
+
+from orangecontrib.survival_analysis.widgets.data import (
+    contains_survival_endpoints,
+    get_survival_endpoints,
+    MISSING_SURVIVAL_DATA,
+)
+
+
+def to_data_frame(table: Table) -> pd.DataFrame:
+    columns = table.domain.attributes + table.domain.class_vars
+    df = pd.DataFrame({col.name: table.get_column(col) for col in columns})
+    df = df.dropna(axis=0)
+    return df
+
+
+class CoxRegressionModel(Model):
+    def __init__(self, model):
+        super().__init__()
+        self._model = model
+        self.params = vars()
+
+    @property
+    def covariates(self):
+        return self._model.summary.index.to_list()
+
+    @property
+    def coefficients(self):
+        return self._model.summary.coef.to_list()
+
+    def ll_ratio_log2p(self):
+        """return -log2p from log-likelihood ratio test"""
+        return -np.log2(self._model.log_likelihood_ratio_test().p_value)
+
+    def predict(self, x):
+        """Predict risk scores."""
+        return self._model.predict_partial_hazard(x)
+
+    def summary_to_table(self) -> Table:
+        df = self._model.summary
+
+        if 'cmp to' in df.columns:
+            df = df.drop(['cmp to'], axis=1)
+
+        table = table_from_frame(df)
+        table.name = 'model summary'
+        domain = Domain(
+            [table.domain['coef']],
+            metas=[var for var in table.domain if var.name != 'coef'],
+        )
+        return table.transform(domain)
+
+    def __call__(self, data, ret=Model.Value):
+        return self.predict(data.X).array
+
+
+class CoxRegressionLearner(Learner):
+    __returns__ = CoxRegressionModel
+    supports_multiclass = True
+
+    def __init__(self, preprocessors=None, **kwargs):
+        self.params = vars()
+        super().__init__(preprocessors=preprocessors)
+
+    def incompatibility_reason(self, domain):
+        if not contains_survival_endpoints(domain):
+            return MISSING_SURVIVAL_DATA
+
+    def fit_storage(self, data):
+        return self.fit(data)
+
+    def _fit_model(self, data):
+        if type(self).fit is Learner.fit:
+            return self.fit_storage(data)
+        else:
+            return self.fit(data)
+
+    def fit(self, data):
+        if not contains_survival_endpoints(data.domain):
+            raise ValueError(MISSING_SURVIVAL_DATA)
+        time_var, event_var = get_survival_endpoints(data.domain)
+
+        df = to_data_frame(data)
+        cph = CoxPHFitter(**self.params['kwargs'])
+        cph = cph.fit(df, duration_col=time_var.name, event_col=event_var.name)
+        return CoxRegressionModel(cph)
+
+    def __call__(self, data, progress_callback=None):
+        m = super().__call__(data, progress_callback)
+        m.params = self.params
+        return m
