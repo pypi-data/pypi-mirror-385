e3f42438c9d573e81558eb1843e7a524d9e8f3af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 06:05:57 +0000

    Add package beekeeper-observers-watsonx/1.0.2/beekeeper_observers_watsonx-1.0.2.tar.gz

---
+++ packages/beekeeper-observers-watsonx/beekeeper_observers_watsonx-1.0.2.tar.gz/beekeeper_observers_watsonx-1.0.2/beekeeper/observers/watsonx/__init__.py
@@ -0,0 +1,21 @@
+from beekeeper.observers.watsonx.base import (
+    CloudPakforDataCredentials,
+    IntegratedSystemCredentials,
+    WatsonxCustomMetric,
+    WatsonxExternalPromptObserver,
+    WatsonxLocalMetric,
+    WatsonxMetric,
+    WatsonxMetricThreshold,
+    WatsonxPromptObserver,
+)
+
+__all__ = [
+    "CloudPakforDataCredentials",
+    "IntegratedSystemCredentials",
+    "WatsonxExternalPromptObserver",
+    "WatsonxPromptObserver",
+    "WatsonxLocalMetric",
+    "WatsonxMetricThreshold",
+    "WatsonxMetric",
+    "WatsonxCustomMetric",
+]
