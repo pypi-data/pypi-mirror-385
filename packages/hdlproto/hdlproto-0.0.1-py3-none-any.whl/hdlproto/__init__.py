e58a6a2f57e51246ef9b84e5b5038b4dc6568965
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 03:20:31 +0000

    Add package hdlproto/0.0.1/hdlproto-0.0.1-py3-none-any.whl

---
+++ packages/hdlproto/hdlproto-0.0.1-py3-none-any.whl/hdlproto/__init__.py
@@ -0,0 +1,12 @@
+# hdlproto/__init__.py
+"""
+HDLProto - Placeholder Package
+
+This package is currently under development.
+Please check back later for the full release.
+"""
+
+__version__ = "0.0.1"
+
+def main():
+    print("HDLProto is under development. Stay tuned!")
