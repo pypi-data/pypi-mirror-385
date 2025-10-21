57f3be563f63d5d27ffc09e0b8ecf7cccea60b9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 07:06:00 +0000

    Add package vaiz-sdk/0.7.1/vaiz_sdk-0.7.1.tar.gz

---
+++ packages/vaiz-sdk/vaiz_sdk-0.7.1.tar.gz/vaiz_sdk-0.7.1/vaiz/api/__init__.py
@@ -0,0 +1,5 @@
+from vaiz.api.base import BaseAPIClient
+from vaiz.api.tasks import TasksAPIClient
+from vaiz.api.documents import DocumentsAPIClient
+
+__all__ = ['BaseAPIClient', 'TasksAPIClient', 'DocumentsAPIClient']
