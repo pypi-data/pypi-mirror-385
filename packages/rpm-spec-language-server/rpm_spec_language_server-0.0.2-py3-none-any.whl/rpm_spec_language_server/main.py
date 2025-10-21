aeb183cb05b633846e8d578008767cf7450cc277
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 06:05:43 +0000

    Add package rpm-spec-language-server/0.0.2/rpm_spec_language_server-0.0.2-py3-none-any.whl

---
+++ packages/rpm-spec-language-server/rpm_spec_language_server-0.0.2-py3-none-any.whl/rpm_spec_language_server/main.py
@@ -0,0 +1,58 @@
+import logging
+
+
+def main() -> None:
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="count",
+        help="Set the logging verbosity, defaults to warning",
+    )
+    parser.add_argument("--log_file", type=str, help="File to log in", nargs=1)
+    parser.add_argument(
+        "--stdio", action="store_true", help="Use stdio instead of the TCP server"
+    )
+    parser.add_argument(
+        "--host", type=str, default="127.0.0.1", help="Bind to this address"
+    )
+    parser.add_argument("--port", type=int, default=2087, help="Bind to this port")
+    parser.add_argument(
+        "--runtime-type-checks",
+        action="store_true",
+        help="Add typeguard runtime type checking",
+    )
+    parser.add_argument(
+        "--ctr-mount-path",
+        type=str,
+        nargs=1,
+        help="Directory that is mounted ",
+        default=[""],
+    )
+
+    args = parser.parse_args()
+
+    if args.runtime_type_checks:
+        from typeguard import install_import_hook
+
+        install_import_hook("rpm_spec_language_server")
+
+    from rpm_spec_language_server.logging import LOG_LEVELS, LOGGER
+    from rpm_spec_language_server.server import create_rpm_lang_server
+
+    log_level = LOG_LEVELS[min(args.verbose or 0, len(LOG_LEVELS) - 1)]
+
+    if args.log_file:
+        LOGGER.removeHandler(LOGGER.handlers[0])
+        LOGGER.addHandler(logging.FileHandler(args.log_file[0]))
+
+    LOGGER.setLevel(log_level)
+
+    server = create_rpm_lang_server(args.ctr_mount_path[0])
+
+    if args.stdio:
+        server.start_io()
+    else:
+        server.start_tcp(args.host, args.port)
