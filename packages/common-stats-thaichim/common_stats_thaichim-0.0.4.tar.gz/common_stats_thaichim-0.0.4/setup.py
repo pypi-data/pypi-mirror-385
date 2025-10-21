68783437451662319fbfdd95b87bd74a9c0ae0bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 15:06:14 +0000

    Add package common-stats-thaichim/0.0.4/common_stats_thaichim-0.0.4.tar.gz

---
+++ packages/common-stats-thaichim/common_stats_thaichim-0.0.4.tar.gz/common_stats_thaichim-0.0.4/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='common_stats-thaichim',
+    version='0.0.4',
+    description='A simple library for basic statistical calculations',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Nawawan Thaichim',
+    author_email='woonzy09@email.com',
+    packages=find_packages(),
+    install_requires=[
+        "pytest"
+    ],
+    license='MIT',
+    python_requires='>=3.7',
+    
+)
