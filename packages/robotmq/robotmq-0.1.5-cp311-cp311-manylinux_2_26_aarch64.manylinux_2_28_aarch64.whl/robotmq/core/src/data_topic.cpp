d7d53f281827e83b83c37cf3a9a10066507b2aa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 07:05:57 +0000

    Add package robotmq/0.1.5/robotmq-0.1.5-cp311-cp311-manylinux_2_26_aarch64.manylinux_2_28_aarch64.whl

---
+++ packages/robotmq/robotmq-0.1.5-cp311-cp311-manylinux_2_26_aarch64.manylinux_2_28_aarch64.whl/robotmq/core/src/data_topic.cpp
@@ -0,0 +1,241 @@
+/**
+ * Copyright (c) 2024 Yihuai Gao
+ *
+ * This software is released under the MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
+#include "data_topic.h"
+#include "common.h"
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <unistd.h>
+DataTopic::DataTopic(const std::string &topic_name, double message_remaining_time_s)
+    : message_remaining_time_s_(message_remaining_time_s), topic_name_(topic_name), is_shm_topic_(false),
+      shm_size_gb_(0)
+{
+    data_.clear();
+}
+
+DataTopic::DataTopic(const std::string &topic_name, double message_remaining_time_s, const std::string server_name,
+                     double shared_memory_size_gb)
+    : message_remaining_time_s_(message_remaining_time_s), topic_name_(topic_name), server_name_(server_name),
+      is_shm_topic_(true), shm_size_gb_(shared_memory_size_gb)
+{
+    data_.clear();
+
+    std::string shm_name = server_name + "_" + topic_name;
+
+    // Create shared memory
+    shm_size_ = shm_size_gb_ * 1024 * 1024 * 1024;
+    occupied_shm_size_ = 0;
+    current_shm_offset_ = 0;
+    shm_fd_ = shm_open(("rmq_" + shm_name).c_str(), O_CREAT | O_RDWR, 0666);
+    ftruncate(shm_fd_, shm_size_);
+    shm_ptr_ = mmap(0, shm_size_, PROT_WRITE, MAP_SHARED, shm_fd_, 0);
+
+    // Create shared memory mutex
+    shm_mutex_fd_ = shm_open(("rmq_" + shm_name + "_mutex").c_str(), O_CREAT | O_RDWR, 0666);
+    ftruncate(shm_mutex_fd_, sizeof(pthread_mutex_t));
+    shm_mutex_ptr_ = (pthread_mutex_t *)mmap(0, sizeof(pthread_mutex_t), PROT_WRITE, MAP_SHARED, shm_mutex_fd_, 0);
+
+    pthread_mutexattr_t attr;
+    pthread_mutexattr_init(&attr);
+    pthread_mutexattr_setpshared(&attr, PTHREAD_PROCESS_SHARED);
+    pthread_mutex_init(shm_mutex_ptr_, &attr);
+}
+
+void DataTopic::copy_data_to_shm(const pybind11::bytes &data, double timestamp)
+{
+
+    // Extract the raw bytes and size from py::bytes
+    char *new_data_buffer;
+    ssize_t length;
+    // This gives you a pointer to the underlying buffer and the size
+    PYBIND11_BYTES_AS_STRING_AND_SIZE(data.ptr(), &new_data_buffer, &length);
+
+    // Store the original data into shared memory and the shm_data_info into data_
+    int64_t data_size = length;
+    if (data_size > shm_size_)
+    {
+        printf("Data size %d is larger than shared memory size %d. New data will be ignored\n", data_size, shm_size_);
+        return;
+    }
+    std::string shm_name = server_name_ + "_" + topic_name_;
+    BytesPtr info_ptr =
+        std::make_shared<Bytes>(SharedMemoryDataInfo(shm_name, shm_size_, current_shm_offset_, data_size).serialize());
+    data_.push_back({info_ptr, timestamp});
+    occupied_shm_size_ += data_size;
+    while (occupied_shm_size_ > shm_size_)
+    {
+        BytesPtr old_info_ptr = std::get<0>(data_.front());
+        SharedMemoryDataInfo old_info(*old_info_ptr);
+        if (old_info.shm_name() == shm_name)
+            occupied_shm_size_ -= old_info.data_size_bytes();
+        data_.pop_front();
+    }
+
+    // Copy data to shared memory: 76MB takes 0.02s
+
+    pthread_mutex_lock(shm_mutex_ptr_);
+    if (current_shm_offset_ + data_size > shm_size_)
+    {
+        uint64_t shm_remaining_size = shm_size_ - current_shm_offset_;
+        memcpy(shm_ptr_ + current_shm_offset_, new_data_buffer, shm_remaining_size);
+        current_shm_offset_ = data_size - shm_remaining_size;
+        memcpy(shm_ptr_, new_data_buffer + shm_remaining_size, current_shm_offset_);
+    }
+    else
+    {
+        memcpy(shm_ptr_ + current_shm_offset_, new_data_buffer, data_size);
+        current_shm_offset_ += data_size;
+    }
+    pthread_mutex_unlock(shm_mutex_ptr_);
+
+    while (!data_.empty() && timestamp - std::get<1>(data_.front()) > message_remaining_time_s_)
+    {
+        BytesPtr old_info_ptr = std::get<0>(data_.front());
+        SharedMemoryDataInfo old_info(*old_info_ptr);
+        if (old_info.shm_name() == shm_name)
+            occupied_shm_size_ -= old_info.data_size_bytes();
+        data_.pop_front();
+    }
+}
+
+void DataTopic::add_data_ptr(const BytesPtr data_ptr, double timestamp)
+{
+    data_.push_back({data_ptr, timestamp});
+    while (!data_.empty() && timestamp - std::get<1>(data_.front()) > message_remaining_time_s_)
+    {
+        data_.pop_front();
+    }
+}
+
+std::vector<TimedPtr> DataTopic::peek_data_ptrs(Order order, int32_t n)
+{
+    if (data_.empty())
+    {
+        return std::vector<TimedPtr>();
+    }
+    if (n < 0 || n > data_.size())
+    {
+        n = data_.size();
+    }
+    if (order == Order::LATEST)
+    {
+        std::vector<TimedPtr> result(data_.end() - n, data_.end());
+        std::reverse(result.begin(), result.end());
+        return result;
+    }
+    else if (order == Order::EARLIEST)
+    {
+        return std::vector<TimedPtr>(data_.begin(), data_.begin() + n);
+    }
+    else
+    {
+        throw std::runtime_error("Invalid end type");
+    }
+}
+
+std::vector<TimedPtr> DataTopic::pop_data_ptrs(Order order, int32_t n)
+{
+    if (data_.empty())
+    {
+        return std::vector<TimedPtr>();
+    }
+    if (n < 0 || n > data_.size())
+    {
+        n = data_.size();
+    }
+    std::vector<TimedPtr> ret = peek_data_ptrs(order, n);
+
+    if (order == Order::LATEST)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            if (is_shm_topic_)
+            {
+                BytesPtr old_info_ptr = std::get<0>(data_.back());
+                SharedMemoryDataInfo old_info(*old_info_ptr);
+                if (old_info.shm_name() == server_name_ + "_" + topic_name_)
+                    occupied_shm_size_ -= old_info.data_size_bytes();
+            }
+            data_.pop_back();
+        }
+    }
+    else if (order == Order::EARLIEST)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            if (is_shm_topic_)
+            {
+                BytesPtr old_info_ptr = std::get<0>(data_.front());
+                SharedMemoryDataInfo old_info(*old_info_ptr);
+                if (old_info.shm_name() == server_name_ + "_" + topic_name_)
+                    occupied_shm_size_ -= old_info.data_size_bytes();
+            }
+            data_.pop_front();
+        }
+    }
+    else
+    {
+        throw std::runtime_error("Invalid end type");
+    }
+    return ret;
+}
+
+void DataTopic::clear_data()
+{
+    data_.clear();
+    if (is_shm_topic_)
+    {
+        occupied_shm_size_ = 0;
+        current_shm_offset_ = 0;
+    }
+}
+
+int DataTopic::size() const
+{
+    return data_.size();
+}
+
+pybind11::bytes DataTopic::get_shared_memory_data(const SharedMemoryDataInfo &shm_data_info)
+{
+    pybind11::bytes data;
+    pthread_mutex_lock(shm_mutex_ptr_);
+    if (shm_data_info.shm_start_idx() + shm_data_info.data_size_bytes() > shm_size_)
+    {
+        char *a = reinterpret_cast<char *>(shm_ptr_) + shm_data_info.shm_start_idx();
+        size_t a_len = shm_size_ - shm_data_info.shm_start_idx();
+        char *b = reinterpret_cast<char *>(shm_ptr_);
+        size_t b_len = shm_data_info.data_size_bytes() - a_len;
+        data = concat_to_pybytes(a, a_len, b, b_len);
+    }
+    else
+    {
+        data = pybind11::bytes(reinterpret_cast<char *>(shm_ptr_) + shm_data_info.shm_start_idx(),
+                               shm_data_info.data_size_bytes());
+    }
+    pthread_mutex_unlock(shm_mutex_ptr_);
+    return data;
+}
+
+bool DataTopic::is_shm_topic() const
+{
+    return is_shm_topic_;
+}
+
+void DataTopic::delete_shm()
+{
+    if (is_shm_topic_)
+    {
+        printf("deleting shared memory: %s\n", ("rmq_" + server_name_ + "_" + topic_name_).c_str());
+        munmap(shm_ptr_, shm_size_);
+        munmap(shm_mutex_ptr_, sizeof(pthread_mutex_t));
+        shm_unlink(("rmq_" + server_name_ + "_" + topic_name_).c_str());
+        shm_unlink(("rmq_" + server_name_ + "_" + topic_name_ + "_mutex").c_str());
+        close(shm_fd_);
+        close(shm_mutex_fd_);
+    }
+}
---
+++ packages/robotmq/robotmq-0.1.5-cp311-cp311-manylinux_2_26_aarch64.manylinux_2_28_aarch64.whl/robotmq/core/src/rmq_client.cpp
@@ -0,0 +1,251 @@
+/**
+ * Copyright (c) 2024 Yihuai Gao
+ *
+ * This software is released under the MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
+#include "rmq_client.h"
+#include "common.h"
+#include <cstring>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <unistd.h>
+
+RMQClient::RMQClient(const std::string &client_name, const std::string &server_endpoint)
+    : client_name_(client_name), context_(1), socket_(context_, zmq::socket_type::req),
+      steady_clock_start_time_us_(steady_clock_us()), last_retrieved_ptrs_()
+{
+    logger_ = spdlog::get(client_name);
+    if (!logger_)
+    {
+        logger_ = spdlog::stdout_color_mt(client_name);
+    }
+    logger_->set_pattern("[%H:%M:%S %n %^%l%$] %v");
+    socket_.connect(server_endpoint);
+}
+
+RMQClient::~RMQClient()
+{
+    socket_.close();
+    context_.close();
+}
+
+int RMQClient::get_topic_status(const std::string &topic, double timeout_s)
+{
+    RMQMessage message(topic, CmdType::GET_TOPIC_STATUS, Order::NONE, get_timestamp(), "");
+    std::string serialized = message.serialize();
+    zmq::message_t request(serialized.data(), serialized.size());
+    socket_.send(request, zmq::send_flags::none);
+
+    zmq::pollitem_t items[] = {{socket_, 0, ZMQ_POLLIN, 0}};
+    zmq::poll(&items[0], 1, timeout_s * 1000);
+    if (items[0].revents & ZMQ_POLLIN)
+    {
+        zmq::message_t reply;
+        socket_.recv(reply);
+        RMQMessage reply_message(std::string(reply.data<char>(), reply.data<char>() + reply.size()));
+        if (reply_message.cmd() == CmdType::GET_TOPIC_STATUS)
+        {
+            std::string data_str = reply_message.data_str();
+            int32_t size = bytes_to_int32(data_str.substr(0, 4));
+            if (data_str.size() == 8)
+            {
+                topic_using_shared_memory_[topic] = bytes_to_int32(data_str.substr(4, 4));
+            }
+            return size;
+        }
+        else if (reply_message.cmd() == CmdType::ERROR)
+        {
+            throw std::runtime_error("Server returned error: " + reply_message.data_str());
+        }
+        else
+        {
+            throw std::runtime_error("Invalid command type: " + std::to_string(static_cast<int>(reply_message.cmd())) +
+                                     " for get_topic_status on topic: " + topic);
+        }
+    }
+
+    std::string endpoint = socket_.get(zmq::sockopt::last_endpoint);
+    socket_.close();
+    socket_ = zmq::socket_t(context_, zmq::socket_type::req);
+    socket_.connect(endpoint);
+
+    return -2;
+}
+
+pybind11::tuple RMQClient::peek_data(const std::string &topic, std::string order, int32_t n)
+{
+    std::string data_str = int32_to_bytes(n);
+    RMQMessage message(topic, CmdType::PEEK_DATA, str_to_order(order), get_timestamp(), data_str);
+    std::vector<TimedPtr> reply_ptrs = send_request_(message);
+    if (reply_ptrs.empty())
+    {
+        logger_->debug("No data available for topic: {}", topic);
+    }
+    return ptrs_to_tuple_(reply_ptrs);
+}
+
+pybind11::tuple RMQClient::pop_data(const std::string &topic, std::string order, int32_t n)
+{
+    std::string data_str = int32_to_bytes(n);
+    RMQMessage message(topic, CmdType::POP_DATA, str_to_order(order), get_timestamp(), data_str);
+    std::vector<TimedPtr> reply_ptrs = send_request_(message);
+    if (reply_ptrs.empty())
+    {
+        logger_->debug("No data available for topic: {}", topic);
+    }
+    return ptrs_to_tuple_(reply_ptrs);
+}
+
+void RMQClient::put_data(const std::string &topic, const pybind11::bytes &data)
+{
+    if (pybind11::len(data) == 0)
+    {
+        throw std::invalid_argument("Cannot pass empty bytes string");
+    }
+    std::vector<TimedPtr> timed_ptrs;
+    BytesPtr data_ptr = std::make_shared<Bytes>(data);
+    TimedPtr timed_ptr = std::make_tuple(data_ptr, get_timestamp());
+    timed_ptrs.push_back(timed_ptr);
+    RMQMessage message(topic, CmdType::PUT_DATA, Order::NONE, get_timestamp(), timed_ptrs);
+    std::vector<TimedPtr> reply_ptrs = send_request_(message);
+}
+
+pybind11::bytes RMQClient::request_with_data(const std::string &topic, const pybind11::bytes &data)
+{
+    if (pybind11::len(data) == 0)
+    {
+        throw std::invalid_argument("Cannot pass empty bytes string");
+    }
+
+    if (topic_using_shared_memory_.find(topic) == topic_using_shared_memory_.end())
+    {
+        get_topic_status(topic, 1.0);
+    }
+    double timestamp = get_timestamp();
+    std::vector<TimedPtr> reply_ptrs;
+
+    if (topic_using_shared_memory_[topic])
+    {
+        // Extract the raw bytes and size from py::bytes
+        char *new_data_buffer;
+        ssize_t length;
+        PYBIND11_BYTES_AS_STRING_AND_SIZE(data.ptr(), &new_data_buffer, &length);
+
+        std::string request_shm_name = client_name_ + "_" + topic + "_request";
+        int shm_fd = shm_open(("rmq_" + request_shm_name).c_str(), O_CREAT | O_RDWR, 0666);
+        ftruncate(shm_fd, length);
+        void *shm_ptr = mmap(0, length, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+        memcpy(shm_ptr, new_data_buffer, length);
+        SharedMemoryDataInfo data_info(request_shm_name, length, 0, length);
+        BytesPtr data_ptr = std::make_shared<Bytes>(data_info.serialize());
+
+        TimedPtr timed_ptr = std::make_tuple(data_ptr, timestamp);
+        std::vector<TimedPtr> timed_ptrs;
+        timed_ptrs.push_back(timed_ptr);
+
+        RMQMessage message(topic, CmdType::REQUEST_WITH_DATA, Order::NONE, get_timestamp(), timed_ptrs);
+        reply_ptrs = send_request_(message);
+        munmap(shm_ptr, length);
+        shm_unlink(("rmq_" + request_shm_name).c_str());
+        close(shm_fd);
+    }
+
+    else
+    {
+        std::vector<TimedPtr> timed_ptrs;
+        BytesPtr data_ptr = std::make_shared<Bytes>(data);
+        TimedPtr timed_ptr = std::make_tuple(data_ptr, timestamp);
+        timed_ptrs.push_back(timed_ptr);
+
+        RMQMessage message(topic, CmdType::REQUEST_WITH_DATA, Order::NONE, get_timestamp(), timed_ptrs);
+        reply_ptrs = send_request_(message);
+    }
+
+    if (reply_ptrs.empty())
+    {
+        logger_->error("No response from server for request with data on topic: {}", topic);
+    }
+    if (reply_ptrs.size() != 1)
+    {
+        throw std::runtime_error("Expected 1 reply pointer, but received " + std::to_string(reply_ptrs.size()));
+    }
+    BytesPtr reply_data_ptr = std::get<0>(reply_ptrs[0]);
+    if (SharedMemoryDataInfo::is_shm_data_info(*reply_data_ptr))
+    {
+        SharedMemoryDataInfo data_info(*reply_data_ptr);
+        return data_info.get_shm_data_with_mutex();
+    }
+    else
+    {
+        return pybind11::bytes(*reply_data_ptr);
+    }
+}
+
+pybind11::tuple RMQClient::get_last_retrieved_data()
+{
+    return ptrs_to_tuple_(last_retrieved_ptrs_);
+}
+
+pybind11::tuple RMQClient::ptrs_to_tuple_(const std::vector<TimedPtr> &ptrs)
+{
+    pybind11::list data;
+    pybind11::list timestamps;
+    for (const TimedPtr ptr : ptrs)
+    {
+        if (SharedMemoryDataInfo::is_shm_data_info(*std::get<0>(ptr)))
+        {
+            SharedMemoryDataInfo data_info(*std::get<0>(ptr));
+            data.append(data_info.get_shm_data_with_mutex());
+        }
+        else
+        {
+            data.append(pybind11::bytes(*std::get<0>(ptr)));
+        }
+        timestamps.append(std::get<1>(ptr));
+    }
+    return pybind11::make_tuple(data, timestamps);
+}
+
+double RMQClient::get_timestamp()
+{
+    return static_cast<double>(steady_clock_us() - steady_clock_start_time_us_) / 1e6;
+}
+
+void RMQClient::reset_start_time(int64_t system_time_us)
+{
+    logger_->info("Resetting start time. Will clear all data retrieved before this time");
+    last_retrieved_ptrs_.clear();
+    steady_clock_start_time_us_ = steady_clock_us() + (system_time_us - system_clock_us());
+}
+
+std::vector<TimedPtr> RMQClient::send_request_(RMQMessage &message)
+{
+    std::string serialized = message.serialize();
+    zmq::message_t request(serialized.data(), serialized.size());
+    socket_.send(request, zmq::send_flags::none);
+    zmq::message_t reply;
+    socket_.recv(reply);
+    RMQMessage reply_message(std::string(reply.data<char>(), reply.data<char>() + reply.size()));
+    if (reply_message.cmd() == CmdType::ERROR)
+    {
+        throw std::runtime_error("Server returned error: " + reply_message.data_str());
+    }
+    if (reply_message.cmd() != message.cmd())
+    {
+        throw std::runtime_error("Command type mismatch. Sent " + std::to_string(static_cast<int>(message.cmd())) +
+                                 " but received " + std::to_string(static_cast<int>(reply_message.cmd())));
+    }
+    if (reply_message.topic() != message.topic())
+    {
+        throw std::runtime_error("Topic mismatch. Sent " + message.topic() + " but received " + reply_message.topic());
+    }
+    if (reply_message.cmd() == CmdType::PEEK_DATA || reply_message.cmd() == CmdType::POP_DATA ||
+        reply_message.cmd() == CmdType::REQUEST_WITH_DATA || reply_message.cmd() == CmdType::PUT_DATA)
+    {
+        last_retrieved_ptrs_ = reply_message.data_ptrs();
+        return reply_message.data_ptrs();
+    }
+    throw std::runtime_error("Invalid command type: " + std::to_string(static_cast<int>(reply_message.cmd())));
+}
