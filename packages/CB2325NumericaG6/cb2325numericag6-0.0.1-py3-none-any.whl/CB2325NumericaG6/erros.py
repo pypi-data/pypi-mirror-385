7fffbaf320703a818b6912801c6e6c0ebc68bc2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 05:05:51 +0000

    Add package CB2325NumericaG6/0.0.1/cb2325numericag6-0.0.1-py3-none-any.whl

---
+++ packages/CB2325NumericaG6/cb2325numericag6-0.0.1-py3-none-any.whl/CB2325NumericaG6/erros.py
@@ -0,0 +1,9 @@
+
+
+def erro_absoluto(valor_real: float, valor_aprox: float) -> float:
+    """Return the absolute Error of an aproximations by calculating the absolute of the difference between the values"""
+    return abs(valor_real-valor_aprox)
+
+def erro_relativo(valor_real: float, valor_aprox: float) -> float:
+    # TODO: Implement this
+    raise NotImplementedError
---
+++ packages/CB2325NumericaG6/cb2325numericag6-0.0.1-py3-none-any.whl/CB2325NumericaG6/integracao.py
@@ -0,0 +1,6 @@
+from typing import Callable
+
+
+def integral(f:Callable, start: float, end: float, divisions: int) -> float:
+    # TODO: Implement this
+    raise NotImplementedError
