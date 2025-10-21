ce0766e705a06067df807fb7341e725de78b5564
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 08:07:05 +0000

    Add package my-test-ml/0.1.2/my_test_ml-0.1.2-py3-none-any.whl

---
+++ packages/my-test-ml/my_test_ml-0.1.2-py3-none-any.whl/mtml/__init__.py
@@ -0,0 +1,50 @@
+"""
+my-test-ml (mtml)
+A test package for ML explanation utilities
+"""
+
+__version__ = "0.1.2"
+
+
+# def describe():
+#     return describe_text
+
+# def get_pd():
+#     return pd_text
+
+# def get_np():
+#     return np_text
+
+# def get_pipe():
+#     return pipe_text
+
+# def get_plt_sb():
+#     return plt_sb_text
+
+# def get_decision():
+#     return decision_text
+
+# def get_imb():
+#     return imb_text
+
+# def get_knn_nb_svm():
+#     return knn_nb_svm_text
+
+# def get_lab1():
+#     return lab1_text
+
+# def get_reg():
+#     return reg_text
+
+# def get_scipy():
+#     return scipy_text
+
+# def get_midterm():
+#     return midterm_text
+
+# def get_assign2():
+#     return assign2_text
+
+from .core import describe, get_pd, get_np, get_pipe, get_plt_sb, get_decision, get_imb, get_knn_nb_svm, get_lab1, get_reg, get_scipy, get_midterm, get_assign2, get_ml_clf, get_ml_reg
+
+__all__ = ["describe", "get_pd", "get_np", "get_pipe", "get_plt_sb", "get_decision", "get_imb", "get_knn_nb_svm", "get_lab1", "get_reg", "get_scipy", "get_midterm", "get_assign2", "get_ml_clf", "get_ml_reg"]
