18ed0cd17dd0c2fbd6cf519fdf8e5ca9d13328de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 08:06:50 +0000

    Add package my-package-lorenzouriel/0.1.0/my_package_lorenzouriel-0.1.0.tar.gz

---
+++ packages/my-package-lorenzouriel/my_package_lorenzouriel-0.1.0.tar.gz/my_package_lorenzouriel-0.1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='my_package_lorenzouriel',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[],
+    author='Lorenzo Uriel',
+)
