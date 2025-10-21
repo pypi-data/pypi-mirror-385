7b7c6fc6a99fdc8e01a5d8c6d27e13c85d687b4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 17:12:49 +0000

    Add package interfacy/0.3.3/interfacy-0.3.3.tar.gz

---
+++ packages/interfacy/interfacy-0.3.3.tar.gz/interfacy-0.3.3/interfacy/appearance/__init__.py
@@ -0,0 +1,12 @@
+from interfacy.appearance.layout import HelpLayout, InterfacyColors, InterfacyLayout
+from interfacy.appearance.layouts import Aligned, AlignedTyped, ArgparseLayout, Modern
+
+__all__ = [
+    "InterfacyColors",
+    "HelpLayout",
+    "InterfacyLayout",
+    "Aligned",
+    "AlignedTyped",
+    "Modern",
+    "ArgparseLayout",
+]
