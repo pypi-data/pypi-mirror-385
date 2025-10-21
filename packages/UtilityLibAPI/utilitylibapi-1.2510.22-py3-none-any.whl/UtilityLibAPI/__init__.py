8d13142533a20677342ae498866975f8f00b9827
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 16:06:28 +0000

    Add package UtilityLibAPI/1.2510.22/utilitylibapi-1.2510.22-py3-none-any.whl

---
+++ packages/UtilityLibAPI/utilitylibapi-1.2510.22-py3-none-any.whl/UtilityLibAPI/__init__.py
@@ -0,0 +1,8 @@
+from .EnDeCodeLib   import CLASS_EnDeCodeLib
+from .SessionVARLib import CLASS_SessionVAR
+from .MailSenderLib import CLASS_MailSender
+
+__all__ = ["CLASS_EnDeCodeLib", 
+           "CLASS_SessionVAR", 
+           "CLASS_MailSender"
+          ]
