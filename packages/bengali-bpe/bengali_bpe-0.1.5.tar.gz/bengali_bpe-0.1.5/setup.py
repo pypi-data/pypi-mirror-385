631474e9cad843ad044ccafefb16c4b2bab19af5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 14:06:07 +0000

    Add package bengali-bpe/0.1.5/bengali_bpe-0.1.5.tar.gz

---
+++ packages/bengali-bpe/bengali_bpe-0.1.5.tar.gz/bengali_bpe-0.1.5/setup.py
@@ -0,0 +1,28 @@
+#setup.py
+
+from setuptools import setup, find_packages
+from pathlib import Path
+
+# Read README.md for PyPI long description
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name="bengali_bpe",
+    version="0.1.5",  # ⬅️ bump version number
+    description="A Byte Pair Encoding (BPE) library for the Bengali language.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",  # ⬅️ for Markdown rendering
+    author="Firoj Ahmmed Patwary",
+    author_email="firoj.stat@gmail.com", 
+    url="https://github.com/firojap/bengali_bpe",  
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Natural Language :: Bengali",
+    ],
+    python_requires='>=3.6',
+)
