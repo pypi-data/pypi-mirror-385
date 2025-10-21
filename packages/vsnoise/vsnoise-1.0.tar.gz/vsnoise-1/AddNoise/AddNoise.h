2842cab3b9835c103d7259882c74dc06a846fe8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 04:04:55 +0000

    Add package vsnoise/1/vsnoise-1.0.tar.gz

---
+++ packages/vsnoise/vsnoise-1.0.tar.gz/vsnoise-1/AddNoise/AddNoise.h
@@ -0,0 +1,65 @@
+#pragma once
+
+#include <type_traits>
+#include <vector>
+
+#include <VSHelper4.h>
+#include <VapourSynth4.h>
+
+#include "pcg.h"
+#include <algorithm>
+#include <random>
+
+#include "perlin.h"
+#include "poisson.h"
+#include "simplex.h"
+
+#ifdef __x86_64__
+#include "VCL2/vectorclass.h"
+#endif
+
+// max # of noise planes
+static constexpr int MAXP = 2;
+
+// offset in pixels of the fake plane MAXP relative to plane MAXP-1
+static constexpr int OFFSET_FAKEPLANE = 32;
+
+struct AddNoiseData final {
+  VSNode *node;
+  const VSVideoInfo *vi;
+  int type;
+
+  // Gaussian
+  float var;
+  float uvar;
+  float hcorr;
+  float vcorr;
+
+  // Perlin
+  float xsize;
+  float ysize;
+
+  uint64_t idum;
+  bool constant;
+  int every;
+
+  float scale;
+  int planesNoise;
+  bool process[3];
+
+  int nStride[MAXP];
+  int nHeight[MAXP];
+  int nSize[MAXP];
+  int peak;
+  int step;
+  int storedFrames;
+
+  std::default_random_engine generator;
+
+  std::vector<uint64_t> pNoiseSeeds;
+  std::vector<std::vector<int8_t>> pN;
+  void (*updateFrame)(const void *_srcp, void *_dstp, const int width,
+                      const int height, const ptrdiff_t stride,
+                      const int noisePlane, void *pNW,
+                      const AddNoiseData *const VS_RESTRICT d) noexcept;
+};
