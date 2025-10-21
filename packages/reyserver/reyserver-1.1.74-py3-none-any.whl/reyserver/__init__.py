0b8969ac6afb96ecce77722b0f40592e080b9ce7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 06:05:40 +0000

    Add package reyserver/1.1.74/reyserver-1.1.74-py3-none-any.whl

---
+++ packages/reyserver/reyserver-1.1.74-py3-none-any.whl/reyserver/__init__.py
@@ -0,0 +1,21 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@Time    : 2023-02-19
+@Author  : Rey
+@Contact : reyxbo@163.com
+@Explain : Backend server method set.
+
+Modules
+-------
+rall : All methods.
+rauth : Authentication methods.
+rbase : Base methods.
+rclient : Client methods.
+rfile : File methods.
+rserver : Server methods.
+"""
+
+
+from .rserver import Server
---
+++ packages/reyserver/reyserver-1.1.74-py3-none-any.whl/reyserver/rall.py
@@ -0,0 +1,16 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@Time    : 2024-01-11
+@Author  : Rey
+@Contact : reyxbo@163.com
+@Explain : All methods.
+"""
+
+
+from .rauth import *
+from .rbase import *
+from .rclient import *
+from .rfile import *
+from .rserver import *
