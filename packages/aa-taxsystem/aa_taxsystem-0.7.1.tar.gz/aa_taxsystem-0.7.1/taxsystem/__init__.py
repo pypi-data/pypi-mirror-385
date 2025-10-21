41b589f290c4d962efe30bcee8a25d3c496ef911
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 16:05:48 +0000

    Add package aa-taxsystem/0.7.1/aa_taxsystem-0.7.1.tar.gz

---
+++ packages/aa-taxsystem/aa_taxsystem-0.7.1.tar.gz/aa_taxsystem-0.7.1/taxsystem/__init__.py
@@ -0,0 +1,9 @@
+"""Initialize the app"""
+
+__version__ = "0.7.1"
+__title__ = "Tax System"
+
+__package_name__ = "aa-taxsystem"
+
+__app_name_useragent__ = "AA-TaxSystem"
+__github_url__ = f"https://github.com/Geuthur/{__package_name__}"
