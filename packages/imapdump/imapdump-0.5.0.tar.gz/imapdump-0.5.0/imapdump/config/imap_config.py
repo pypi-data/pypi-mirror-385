1cffe0aa61a88dd98c82cccd2d8bb6e9b4b65f17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 07:06:41 +0000

    Add package imapdump/0.5.0/imapdump-0.5.0.tar.gz

---
+++ packages/imapdump/imapdump-0.5.0.tar.gz/imapdump-0.5.0/imapdump/config/imap_config.py
@@ -0,0 +1,17 @@
+from ..enums.imap_encryption_mode import ImapEncryptionMode
+from dataclasses import dataclass
+
+
+@dataclass
+class ImapDumpConfig:
+    loglevel: str
+    host: str
+    port: int = 993
+    username: str = None
+    password: str = None
+    database_file: str = None
+    encryption_mode: ImapEncryptionMode = ImapEncryptionMode.SSL
+    folder_regex: str = "^.*$"
+    config: dict = None
+    force_dump: bool = False
+    dump_folder: str = None
