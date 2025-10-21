2a7df17a09244b84c5a543e5a23b1802047f2e4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 14:06:31 +0000

    Add package format-docstring/0.2.2/format_docstring-0.2.2.tar.gz

---
+++ packages/format-docstring/format_docstring-0.2.2.tar.gz/format_docstring-0.2.2/format_docstring/line_wrap_google.py
@@ -0,0 +1,14 @@
+from format_docstring.line_wrap_utils import ParameterMetadata
+
+
+def wrap_docstring_google(
+        docstring: str,
+        line_length: int,
+        leading_indent: int | None = None,
+        fix_rst_backticks: bool = True,
+        parameter_metadata: ParameterMetadata | None = None,
+        return_annotation: str | None = None,
+        attribute_metadata: ParameterMetadata | None = None,
+) -> str:
+    """A placeholder for now."""  # noqa: D401
+    return ''
