5cb20afa1fa6e32db8563e17fe64226f1e303de1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 16:05:59 +0000

    Add package SigProfilerExtractor/1.2.4/sigprofilerextractor-1.2.4.tar.gz

---
+++ packages/SigProfilerExtractor/sigprofilerextractor-1.2.4.tar.gz/sigprofilerextractor-1.2.4/SigProfilerExtractor/version.py
@@ -0,0 +1,7 @@
+
+# THIS FILE IS GENERATED FROM SIGPROFILEREXTRACTOR SETUP.PY
+short_version = '1.2.4'
+version = '1.2.4'
+Update = 'v1.2.4: Added assignment_cpu parameter for SigProfilerAssignment v1.0.0 support.'
+    
+    
