11313b06e49b3cd3b574728d591694432532ef87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 14:06:50 +0000

    Add package SigProfilerAssignment/1.0.1/sigprofilerassignment-1.0.1.tar.gz

---
+++ packages/SigProfilerAssignment/sigprofilerassignment-1.0.1.tar.gz/sigprofilerassignment-1.0.1/SigProfilerAssignment/version.py
@@ -0,0 +1,7 @@
+
+# THIS FILE IS GENERATED FROM SigProfilerAssignment SETUP.PY
+short_version = '1.0.1'
+version = '1.0.1'
+Update = 'v1.0.1:  Refine CPU parameter logic to pass directly instead of via devopts'
+
+    
