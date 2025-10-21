ef347f1dd04ddbca121bc9a5fc90e0e0f665e06d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 03:21:14 +0000

    Add package lyceum-cli/1.0.11/lyceum_cli-1.0.11-py3-none-any.whl

---
+++ packages/lyceum-cli/lyceum_cli-1.0.11-py3-none-any.whl/lyceum_cloud_execution_api_client/api/authentication/login_api_v2_external_auth_login_post.py
@@ -0,0 +1,176 @@
+from http import HTTPStatus
+from typing import Any, Optional, Union
+
+import httpx
+
+from ... import errors
+from ...client import AuthenticatedClient, Client
+from ...models.http_validation_error import HTTPValidationError
+from ...models.login_request import LoginRequest
+from ...models.login_response import LoginResponse
+from ...types import Response
+
+
+def _get_kwargs(
+    *,
+    body: LoginRequest,
+) -> dict[str, Any]:
+    headers: dict[str, Any] = {}
+
+    _kwargs: dict[str, Any] = {
+        "method": "post",
+        "url": "/api/v2/external/auth/login",
+    }
+
+    _kwargs["json"] = body.to_dict()
+
+    headers["Content-Type"] = "application/json"
+
+    _kwargs["headers"] = headers
+    return _kwargs
+
+
+def _parse_response(
+    *, client: Union[AuthenticatedClient, Client], response: httpx.Response
+) -> Optional[Union[HTTPValidationError, LoginResponse]]:
+    if response.status_code == 200:
+        response_200 = LoginResponse.from_dict(response.json())
+
+        return response_200
+    if response.status_code == 422:
+        response_422 = HTTPValidationError.from_dict(response.json())
+
+        return response_422
+    if client.raise_on_unexpected_status:
+        raise errors.UnexpectedStatus(response.status_code, response.content)
+    else:
+        return None
+
+
+def _build_response(
+    *, client: Union[AuthenticatedClient, Client], response: httpx.Response
+) -> Response[Union[HTTPValidationError, LoginResponse]]:
+    return Response(
+        status_code=HTTPStatus(response.status_code),
+        content=response.content,
+        headers=response.headers,
+        parsed=_parse_response(client=client, response=response),
+    )
+
+
+def sync_detailed(
+    *,
+    client: Union[AuthenticatedClient, Client],
+    body: LoginRequest,
+) -> Response[Union[HTTPValidationError, LoginResponse]]:
+    """Login
+
+     Login with email and password, return JWT tokens.
+    This JWT can be used directly with MinIO STS.
+
+    Args:
+        body (LoginRequest):
+
+    Raises:
+        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+        httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+    Returns:
+        Response[Union[HTTPValidationError, LoginResponse]]
+    """
+
+    kwargs = _get_kwargs(
+        body=body,
+    )
+
+    response = client.get_httpx_client().request(
+        **kwargs,
+    )
+
+    return _build_response(client=client, response=response)
+
+
+def sync(
+    *,
+    client: Union[AuthenticatedClient, Client],
+    body: LoginRequest,
+) -> Optional[Union[HTTPValidationError, LoginResponse]]:
+    """Login
+
+     Login with email and password, return JWT tokens.
+    This JWT can be used directly with MinIO STS.
+
+    Args:
+        body (LoginRequest):
+
+    Raises:
+        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+        httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+    Returns:
+        Union[HTTPValidationError, LoginResponse]
+    """
+
+    return sync_detailed(
+        client=client,
+        body=body,
+    ).parsed
+
+
+async def asyncio_detailed(
+    *,
+    client: Union[AuthenticatedClient, Client],
+    body: LoginRequest,
+) -> Response[Union[HTTPValidationError, LoginResponse]]:
+    """Login
+
+     Login with email and password, return JWT tokens.
+    This JWT can be used directly with MinIO STS.
+
+    Args:
+        body (LoginRequest):
+
+    Raises:
+        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+        httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+    Returns:
+        Response[Union[HTTPValidationError, LoginResponse]]
+    """
+
+    kwargs = _get_kwargs(
+        body=body,
+    )
+
+    response = await client.get_async_httpx_client().request(**kwargs)
+
+    return _build_response(client=client, response=response)
+
+
+async def asyncio(
+    *,
+    client: Union[AuthenticatedClient, Client],
+    body: LoginRequest,
+) -> Optional[Union[HTTPValidationError, LoginResponse]]:
+    """Login
+
+     Login with email and password, return JWT tokens.
+    This JWT can be used directly with MinIO STS.
+
+    Args:
+        body (LoginRequest):
+
+    Raises:
+        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+        httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+    Returns:
+        Union[HTTPValidationError, LoginResponse]
+    """
+
+    return (
+        await asyncio_detailed(
+            client=client,
+            body=body,
+        )
+    ).parsed
