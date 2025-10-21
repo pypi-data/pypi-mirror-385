c86122625c722e7b185f65a48011fa057899b05a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 06:05:43 +0000

    Add package markitdown-pro/1.3.5/markitdown_pro-1.3.5.tar.gz

---
+++ packages/markitdown-pro/markitdown_pro-1.3.5.tar.gz/markitdown_pro-1.3.5/markitdown_pro/common/schemas.py
@@ -0,0 +1,20 @@
+import enum
+from io import BytesIO
+
+
+class NamedBytesIO(BytesIO):
+    def __init__(self, data: bytes, name: str):
+        super().__init__(data)
+        self.name = name
+
+
+class ExtensionCategory(enum.Enum):
+    IMAGE = "image"
+    AUDIO = "audio"
+    VIDEO = "video"
+    PDF = "pdf"
+    OFFICE = "office"
+    TABULAR = "tabular"
+    LIGHTWEIGHT_TEXT = "lightweight_text"
+    FORMATTED_DOCS = "formatted_docs"
+    OTHER = "other"
