d4b7a17a2e98cc8201bf4677b8e4ce47d7d3c80a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 04:05:45 +0000

    Add package tiferet/1.1.10/tiferet-1.1.10-py3-none-any.whl

---
+++ packages/tiferet/tiferet-1.1.10-py3-none-any.whl/tiferet/data/__init__.py
@@ -0,0 +1,15 @@
+"""Tiferet Data Transfer Objects Exports"""
+
+# *** exports
+
+# ** app
+from .settings import (
+    DataObject
+)
+from .app import (
+    AppAttributeYamlData,
+    AppInterfaceYamlData
+)
+from .cli import (
+    CliCommandYamlData,
+)
