7fbf469b112531c6fa9a77dab2756d14329ce014
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 18:07:57 +0000

    Add package gcsim-pypi/2.39.0/gcsim_pypi-2.39.0-py3-none-any.whl

---
+++ packages/gcsim-pypi/gcsim_pypi-2.39.0-py3-none-any.whl/gcsim_pypi/aliases/__init__.py
@@ -0,0 +1,9 @@
+from .artifacts import *
+from .characters import *
+from .weapons import *
+
+__all__ = [
+    "ARTIFACT_ALIASES",
+    "CHARACTER_ALIASES",
+    "WEAPON_ALIASES",
+]
---
+++ packages/gcsim-pypi/gcsim_pypi-2.39.0-py3-none-any.whl/gcsim_pypi/availability/__init__.py
@@ -0,0 +1,9 @@
+from .artifacts import *
+from .characters import *
+from .weapons import *
+
+__all__ = [
+    "AVAILABLE_ARTIFACTS",
+    "AVAILABLE_CHARACTERS",
+    "AVAILABLE_WEAPONS",
+]
