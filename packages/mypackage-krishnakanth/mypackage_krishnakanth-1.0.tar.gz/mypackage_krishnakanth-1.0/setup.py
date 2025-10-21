482cd76ee9b05a36aeede7ff8dadb12f8d54e0b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 18:08:48 +0000

    Add package mypackage-krishnakanth/1.0/mypackage_krishnakanth-1.0.tar.gz

---
+++ packages/mypackage-krishnakanth/mypackage_krishnakanth-1.0.tar.gz/mypackage_krishnakanth-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mypackage_krishnakanth',
+    version='1.0',
+    packages=['mypackage_krishnakanth'],
+    include_package_data=True,
+)
