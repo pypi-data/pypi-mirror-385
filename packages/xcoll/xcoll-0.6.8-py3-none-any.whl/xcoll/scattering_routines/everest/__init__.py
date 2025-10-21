0352c84a31563f960ae23a56d0a363b733f42f33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 04:05:08 +0000

    Add package xcoll/0.6.8/xcoll-0.6.8-py3-none-any.whl

---
+++ packages/xcoll/xcoll-0.6.8-py3-none-any.whl/xcoll/scattering_routines/everest/__init__.py
@@ -0,0 +1,7 @@
+# copyright ############################### #
+# This file is part of the Xcoll package.   #
+# Copyright (c) CERN, 2024.                 #
+# ######################################### #
+
+from .materials import GeneralMaterial, Material, CrystalMaterial
+from .everest import EverestEngine
