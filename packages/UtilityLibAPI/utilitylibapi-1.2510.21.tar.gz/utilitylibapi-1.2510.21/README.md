0b138cea2af33b56e652fc8e3e5ebf5e3828986c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 16:06:18 +0000

    Add package UtilityLibAPI/1.2510.21/utilitylibapi-1.2510.21.tar.gz

---
+++ packages/UtilityLibAPI/utilitylibapi-1.2510.21.tar.gz/utilitylibapi-1.2510.21/README.md
@@ -0,0 +1,8 @@
+#UtilityLibAPI Classes
+
+## History of version
+Version 0.1.1: 2025/10/13<BR>
+Fixed export class path<BR>
+
+Version 0.1.0: 2025/10/13<BR>
+Create UtilityLibAPI -- EnDeCodeLib + SessionVARLib<BR>
---
+++ packages/UtilityLibAPI/utilitylibapi-1.2510.21.tar.gz/utilitylibapi-1.2510.21/src/UtilityLibAPI/__init__.py
@@ -0,0 +1,8 @@
+from .EnDeCodeLib   import CLASS_EnDeCodeLib
+from .SessionVARLib import CLASS_SessionVAR
+from .SendMailLib   import CLASS_SendMail
+
+__all__ = ["CLASS_EnDeCodeLib", 
+           "CLASS_SessionVAR", 
+           "CLASS_MailSender"
+          ]
