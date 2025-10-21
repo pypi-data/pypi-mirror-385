c9398f69c54006880fd703ba8feb50fa2f5b0a1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 06:06:14 +0000

    Add package linak-controller/1.3.2/linak_controller-1.3.2.tar.gz

---
+++ packages/linak-controller/linak_controller-1.3.2.tar.gz/linak_controller-1.3.2/CHANGELOG.md
@@ -0,0 +1,225 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+## [1.3.2] - 2025-10-20
+
+### Fixed
+
+- Fix underflow and overflow heights throwing errors
+
+## [1.3.1] - 2025-10-20
+
+### Fixed
+
+- Logging issues when used in server mode
+
+## [1.3.0] - 2025-10-20
+
+### Breaking
+
+- If using the http or tcp server the JSON payload should now be a `command` object and not a `config` object:
+  ```bash
+  # HTTP
+  curl -X POST http://127.0.0.1:9123 --data '{"key": "move_to", "value": 640}'
+  
+  # TCP
+  echo '{"key": "move_to", "value": 640}' | nc -w 1 127.0.0.1 9123
+  ```
+- Minimum python version is now 3.9.
+
+### Changed
+
+- Refactor global config and command to fix [100](https://github.com/rhyst/linak-controller/issues/100) and [85](https://github.com/rhyst/linak-controller/issues/85)
+- Added support for Python 3.14 [103](https://github.com/rhyst/linak-controller/pull/104) by @markfeathers
+- Using UV for dependencies
+
+### Fixed 
+
+- Server port argument [99](https://github.com/rhyst/linak-controller/pull/99) by @gomi-source
+- Allow using FIFO as config file [101](https://github.com/rhyst/linak-controller/pull/101) by @jayrhynas
+- Fixed custom heights [103](https://github.com/rhyst/linak-controller/pull/103) by @markfeathers
+
+
+## [1.2.0] - 2024-12-03
+
+### Added 
+
+- Added http endpoint in addition to web socket endpoint in server mode
+
+### Changes
+
+- Added support for Python 3.13
+- Update bleak dependency
+
+## [1.1.1] - 2023-12-01
+
+### Changed
+
+- Removed redundant move to method
+
+### Fixed
+
+- Configurable move command period should allow float [76](https://github.com/rhyst/linak-controller/pull/76) by @tomsb
+- Fixed "ValueError: Characteristic notifications already started" [77](https://github.com/rhyst/linak-controller/issues/77)
+
+## [1.1.0] - 2023-11-03
+
+### Added
+
+- Configurable move command period (relating to [48](https://github.com/rhyst/linak-controller/issues/48))
+
+### Changed
+
+- Removed redundant move timeout config
+
+## [1.0.3] - 2023-11-01
+
+### Added
+
+- Docker recipe by @voruti
+
+### Fixed
+
+- Fixed stuttering issues [48](https://github.com/rhyst/linak-controller/issues/48) by @tomsb
+
+## [1.0.2] - 2023-10-31
+
+### Changed
+
+- Copy config from old config location if it exists and new one does not
+
+### Fixed
+
+- Swapped `|` operator for `Union` in `gatt.py` to fix python 3.8 compatibility
+
+## [1.0.1] - 2023-10-31
+
+### Changed
+
+- Added `idasen-controller` as a script to maintain some backwards compatibility
+
+## [1.0.0] - 2023-10-31
+
+Rename `idasen-controller` to `linak-controller`.
+
+---
+
+This package was previously named `idasen-controller`
+
+## [2.2.0] - 2023-10-31
+
+Final release of `idasen-controller` to point at renamed packaged `linak-controller`.
+
+## [2.1.0] - 2023-10-31
+
+### Added
+
+- Add setting a user ID on first connection to support DPG1C and DPG1M
+- Allow fetching base height from desk by setting config value to null
+
+### Changed
+
+- Python version changed to >=3.8 <3.13 to satisfy dependencies
+- Bleak updated to 0.21.1
+- Refactor into seperate files and helper classes
+- Removed `print-exceptions` option
+- Removed max height option as it was unused
+- Removed movement range option as it was unused
+
+## [2.0.2] - 2023-03-13
+
+### Changed
+
+- Update aiohttp dependency to 3.8.4
+- Update bleak dependency to 0.19.5
+- Use poetry for dependency management
+- Removed old options from RECIPES.md
+
+
+## [2.0.1] - 2022-07-27
+
+### Fixed
+
+- Removed unnecessary unsubscribe command that was leftover from previous refactor and causing a `bleak.exc.BleakDBusError: [org.bluez.Error.Failed] No notify session started` error.
+
+## [2.0.0] - 2022-03-08
+
+### Changed
+
+- Replaced `--sit/--stand` commands with a more configurable list of favourites that can be actived with `--move-to <favourite-name>`
+
+## [1.1.0] - 2022-03-08
+
+### Added
+
+- Client/server mode now prints heights on the client as well as the server
+
+### Changed
+
+- Updated to use the REFERENCE_INPUT characteristic which allows you to specify a height to move to. Makes the movement accurate to the millimetre and greatly simplifies code. Inspired by this project: https://github.com/pfilipp/idasen-controller
+
+## [1.0.8] - 2022-01-07
+
+### Added
+
+- Add options allowing configuring base height and movement range (via PR from subraizada3)
+
+### Changed
+
+- Remove redundant scanning code when connecting which should improve time to connection
+- Remove pickling code as that was an attempt to speed up the redundant scanning process
+
+### Fixed
+
+- More compatible shebang (via PR from subraizada3)
+
+## [1.0.7] - 2022-01-07
+
+### Changed
+
+- Remove unneeded depenedencies which will hopefully make windows installs a bit smoother.
+
+## [1.0.6] - 2021-03-30
+
+### Changed
+
+- Changed check for pickling connection to `IS_LINUX` after confirmation that you cannot pickle OSX bluetooth connection objects.
+
+## [1.0.5] - 2021-02-11
+
+### Changed
+
+- Fix `--scan-timeout` flag.
+
+## [1.0.4] - 2021-02-11
+
+### Changed
+
+- Ensure mac addresses are upper case because apparently this matters on some devices.
+
+## [1.0.3] - 2021-02-11
+
+### Changed
+
+- Fix to prevent the script from hiding connection errors on subsequent failed attempts.
+
+## [1.0.2] - 2021-01-27
+
+No changes, just a reupload to fix bad Pypi upload of 1.0.1.
+
+## [1.0.1] - 2021-01-27
+
+### Added
+
+- This changelog.
+
+### Changed
+
+- Script will retry connection if it fails with a pickled connection. This will help in situations where the adapter name changes for example.
+
+## [1.0.0] - 2021-01-27
+
+### Added
+
+- Initial pip release.
---
+++ packages/linak-controller/linak_controller-1.3.2.tar.gz/linak_controller-1.3.2/linak_controller/config.py
@@ -0,0 +1,237 @@
+"""
+Config parsing for this script. Parses command line arguments and config.yaml.
+"""
+
+import os
+import sys
+import shutil
+import argparse
+import yaml
+from appdirs import user_config_dir
+from typing import Optional, TypedDict
+from enum import Enum
+
+
+class CommandAction(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        setattr(namespace, "command", self.const)
+        setattr(namespace, self.dest, values)
+
+
+class Commands(str, Enum):
+    watch = "watch"
+    move_to = "move_to"
+    scan_adapter = "scan_adapter"
+    server = "server"
+    tcp_server = "tcp_server"
+
+
+class Config(TypedDict):
+    mac_address: Optional[str]
+    base_height: Optional[int]
+    adapter_name: str
+    scan_timeout: int
+    connection_timeout: int
+    server_address: str
+    server_port: int
+    favourites: dict
+    forward: bool
+    move_command_period: float
+
+default_config = Config(
+    {
+        "mac_address": None,
+        "base_height": None,
+        "adapter_name": "hci0",
+        "scan_timeout": 5,
+        "connection_timeout": 10,
+        "server_address": "127.0.0.1",
+        "server_port": 9123,
+        "favourites": {},
+        "forward": False,
+        "move_command_period": 0.4,
+    }
+)
+
+class Command(TypedDict):
+    key: Optional[Commands]
+    value: Optional[str]
+
+
+def get_config() -> tuple[Config, Command]:
+    config = default_config.copy()
+
+    OLD_CONFIG_DIR = user_config_dir("idasen-controller")
+    OLD_CONFIG_PATH = os.path.join(OLD_CONFIG_DIR, "config.yaml")
+
+    DEFAULT_CONFIG_DIR = user_config_dir("linak-controller")
+    DEFAULT_CONFIG_PATH = os.path.join(DEFAULT_CONFIG_DIR, "config.yaml")
+
+    # Default config
+    if not os.path.isfile(DEFAULT_CONFIG_PATH):
+        os.makedirs(os.path.dirname(DEFAULT_CONFIG_PATH), exist_ok=True)
+        if os.path.isfile(OLD_CONFIG_PATH):
+            shutil.copyfile(OLD_CONFIG_PATH, DEFAULT_CONFIG_PATH)
+        else:
+            shutil.copyfile(
+                os.path.join(os.path.dirname(__file__), "example", "config.yaml"),
+                DEFAULT_CONFIG_PATH,
+            )
+
+    parser = argparse.ArgumentParser(description="")
+
+    # Config via command line options
+
+    parser.add_argument(
+        "--mac-address",
+        dest="mac_address",
+        type=str,
+        help="Mac address of the Linak desk",
+    )
+    parser.add_argument(
+        "--base-height",
+        dest="base_height",
+        type=int,
+        help="The height of tabletop above ground at lowest position (mm)",
+    )
+    parser.add_argument(
+        "--adapter",
+        dest="adapter_name",
+        type=str,
+        help="The bluetooth adapter device name",
+    )
+    parser.add_argument(
+        "--scan-timeout",
+        dest="scan_timeout",
+        type=int,
+        help="The timeout for bluetooth scan (seconds)",
+    )
+    parser.add_argument(
+        "--connection-timeout",
+        dest="connection_timeout",
+        type=int,
+        help="The timeout for bluetooth connection (seconds)",
+    )
+    parser.add_argument(
+        "--move-command-period",
+        dest="move_command_period",
+        type=float,
+        help="The period between each move command (seconds)",
+    )
+    parser.add_argument(
+        "--forward",
+        dest="forward",
+        action="store_true",
+        help="Forward any commands to a server",
+    )
+    parser.add_argument(
+        "--server-address",
+        dest="server_address",
+        type=str,
+        help="The address the server should run at",
+    )
+    parser.add_argument(
+        "--server-port",
+        dest="server_port",
+        type=int,
+        help="The port the server should run on",
+    )
+    parser.add_argument(
+        "--config",
+        dest="config",
+        type=str,
+        help="File path to the config file (Default: {})".format(DEFAULT_CONFIG_PATH),
+        default=DEFAULT_CONFIG_PATH,
+    )
+
+    # Command to run
+
+    cmd = parser.add_mutually_exclusive_group()
+    cmd.add_argument(
+        "--watch",
+        dest="command",
+        action="store_const",
+        const=Commands.watch,
+        help="Watch for changes to desk height and speed and print them",
+    )
+    cmd.add_argument(
+        "--move-to",
+        dest="move_to",
+        action=CommandAction,
+        type=str,
+        const=Commands.move_to,
+        help="Move desk to specified height (mm) or to a favourite position",
+    )
+    cmd.add_argument(
+        "--scan",
+        dest="command",
+        action="store_const",
+        const=Commands.scan_adapter,
+        help="Scan for devices using the configured adapter",
+    )
+    cmd.add_argument(
+        "--server",
+        dest="command",
+        action="store_const",
+        const=Commands.server,
+        help="Run as a server to accept forwarded commands",
+    )
+    cmd.add_argument(
+        "--tcp-server",
+        dest="command",
+        action="store_const",
+        const=Commands.tcp_server,
+        help="Run as a simple TCP server to accept forwarded commands",
+    )
+
+    args = {k: v for k, v in vars(parser.parse_args()).items() if v is not None}
+
+    if args.get("move_to"):
+        args["command"] = Commands.move_to
+
+    # Overwrite config from config.yaml
+    config_file = {}
+    config_file_path = os.path.join(args["config"])
+    if (
+        config_file_path
+        and os.path.exists(config_file_path)
+        and not os.path.isdir(config_file_path)
+    ):
+        with open(config_file_path, "r") as stream:
+            try:
+                config_file = yaml.safe_load(stream)
+            except yaml.YAMLError as exc:
+                print("Reading config.yaml failed")
+                exit(1)
+    else:
+        print("No config file found")
+
+    for key in config:
+        if key in config_file:
+            config[key] = config_file[key]
+
+    # Overwrite config from command line args
+    for key in config:
+        if key in args:
+            config[key] = args[key]
+
+    if not config["mac_address"]:
+        parser.error("Mac address must be provided")
+
+    config["mac_address"] = config["mac_address"].upper()
+
+    IS_WINDOWS = sys.platform == "win32"
+
+    if IS_WINDOWS:
+        # Windows doesn't use this parameter so rename it so it looks nice for the logs
+        config["adapter_name"] = "default adapter"
+
+    # Parse command
+    command = Command(
+        {
+            "key": args.get("command"),
+            "value": args.get("move_to"),
+        }
+    )
+
+    return config, command
