cd68a5c934cbdd780f215ed4b4fb859793350eff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 04:05:46 +0000

    Add package sap-gui-engine/0.1.1b1/sap_gui_engine-0.1.1b1-py3-none-any.whl

---
+++ packages/sap-gui-engine/sap_gui_engine-0.1.1b1-py3-none-any.whl/sap_gui_engine/managers/__init__.py
@@ -0,0 +1,5 @@
+from .connection_manager import SAPConnectionManager
+from .window_manager import SAPWindowManager
+from .launcher import SAPLauncher
+
+__all__ = ["SAPConnectionManager", "SAPWindowManager", "SAPLauncher"]
