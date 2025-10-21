a0783a36aec4a1dadc8aef62a120ae8d533ac84b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 09:06:09 +0000

    Add package envent/0.1.0/envent-0.1.0.tar.gz

---
+++ packages/envent/envent-0.1.0.tar.gz/envent-0.1.0/README.md
@@ -0,0 +1,5 @@
+# envent
+
+This is a placeholder package for osmosis.ai projects.
+
+This package is reserved for future use by Osmosis.ai.
---
+++ packages/envent/envent-0.1.0.tar.gz/envent-0.1.0/envent/__init__.py
@@ -0,0 +1,7 @@
+"""
+envent - Placeholder package for osmosis.ai projects
+"""
+
+__version__ = "0.1.0"
+__author__ = "Osmosis.ai"
+__description__ = "Placeholder package for osmosis.ai projects"
