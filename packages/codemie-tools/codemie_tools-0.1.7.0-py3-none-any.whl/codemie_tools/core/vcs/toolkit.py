3837aace40963f0d89e29d23dc07ba4e84d9ec80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 03:20:57 +0000

    Add package codemie-tools/0.1.7.0/codemie_tools-0.1.7.0-py3-none-any.whl

---
+++ packages/codemie-tools/codemie_tools-0.1.7.0-py3-none-any.whl/codemie_tools/core/vcs/toolkit.py
@@ -0,0 +1,23 @@
+from typing import List
+
+from codemie_tools.base.models import ToolKit, ToolSet, Tool
+from ...base.base_toolkit import DiscoverableToolkit
+from .github.tools import GithubTool
+from .github.tools_vars import GITHUB_TOOL
+from .gitlab.tools import GitlabTool
+from .gitlab.tools_vars import GITLAB_TOOL
+
+
+class VcsToolkitUI(ToolKit):
+    toolkit: ToolSet = ToolSet.VCS
+    tools: List[Tool] = [
+        Tool.from_metadata(GITHUB_TOOL, tool_class=GithubTool),
+        Tool.from_metadata(GITLAB_TOOL, tool_class=GitlabTool),
+    ]
+
+
+class VcsToolkit(DiscoverableToolkit):
+
+    @classmethod
+    def get_definition(cls):
+        return VcsToolkitUI()
