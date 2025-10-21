b35423a11f7f4995812d5914ed596415b139ff12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 05:06:34 +0000

    Add package sap-gui-engine/0.1.1b3/sap_gui_engine-0.1.1b3.tar.gz

---
+++ packages/sap-gui-engine/sap_gui_engine-0.1.1b3.tar.gz/sap_gui_engine-0.1.1b3/src/sap_gui_engine/__init__.py
@@ -0,0 +1,5 @@
+from sap_gui_engine.sap_gui_engine import SAPGuiEngine
+from sap_gui_engine.sap_element import SAPElement
+from sap_gui_engine.vkey import VKey
+
+__all__ = ["SAPGuiEngine", "SAPElement", "VKey"]
