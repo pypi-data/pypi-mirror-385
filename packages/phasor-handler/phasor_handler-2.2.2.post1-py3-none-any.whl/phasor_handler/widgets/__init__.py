8ac769a42a0d63b46e01078ef036bb4e26ed0340
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 17:12:56 +0000

    Add package phasor-handler/2.2.2.post1/phasor_handler-2.2.2.post1-py3-none-any.whl

---
+++ packages/phasor-handler/phasor_handler-2.2.2.post1-py3-none-any.whl/phasor_handler/widgets/__init__.py
@@ -0,0 +1,5 @@
+from .analysis.view import AnalysisWidget
+from .conversion.view import ConversionWidget
+from .registration.view import RegistrationWidget
+
+__all__ = ["AnalysisWidget", "ConversionWidget", "RegistrationWidget"]
