0d675a5b4ef343a402d0167e868348eec981fd4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 03:20:38 +0000

    Add package game-state/2.0.3/game_state-2.0.3.tar.gz

---
+++ packages/game-state/game_state-2.0.3.tar.gz/game_state-2.0.3/tests/test_manager.py
@@ -0,0 +1,70 @@
+from __future__ import annotations
+
+from typing import Tuple, Type
+
+import pytest
+
+from src.game_state import State, StateManager
+from src.game_state.errors import StateError, StateLoadError
+
+
+@pytest.fixture
+def scenario() -> Tuple[StateManager, Type[State], Type[State]]:
+    class StateOne(State, state_name="Test 1"): ...
+
+    class StateTwo(State): ...
+
+    manager = StateManager(...)  # pyright: ignore[reportArgumentType]
+
+    return manager, StateOne, StateTwo
+
+
+def test_load_states(
+    scenario: Tuple[StateManager, Type[State], Type[State]],
+) -> None:
+    manager = scenario[0]
+    state_1 = scenario[1]
+    state_2 = scenario[2]
+
+    class NotAState: ...
+
+    manager.load_states(state_1, state_2)
+
+    with pytest.raises(StateLoadError):
+        manager.load_states(state_1)
+
+    with pytest.raises(StateError):
+        manager.load_states(NotAState)  # pyright:ignore[reportArgumentType]
+
+    assert len(manager.state_map) == 2, (
+        "Loaded 2 states, did not receive 2 states back."
+    )
+    assert state_1.state_name in manager.state_map, (
+        f"Expected {state_1.state_name} in state map."
+    )
+    assert state_2.state_name in manager.state_map, (
+        f"Expected {state_2.state_name} in state map."
+    )
+
+
+def test_change_states(
+    scenario: Tuple[StateManager, Type[State], Type[State]],
+) -> None:
+    manager = scenario[0]
+    state_1 = scenario[1]
+    state_2 = scenario[2]
+
+    manager.load_states(state_1, state_2)
+
+    assert manager.current_state is None, (
+        "Got a non-None value while no state was updated to."
+    )
+
+    manager.change_state(state_1.state_name)
+
+    assert manager.current_state.state_name == state_1.state_name, (  # pyright:ignore[reportAttributeAccessIssue]
+        "Received wrong state instance upon changing."
+    )
+
+    with pytest.raises(StateError):
+        manager.change_state("Invalid State Name")
