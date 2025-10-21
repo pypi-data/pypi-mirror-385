c964fbff3cb17c116fb706c483ed8ba2080555da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 16:05:50 +0000

    Add package pyimagedl/0.1.5/pyimagedl-0.1.5-py3-none-any.whl

---
+++ packages/pyimagedl/pyimagedl-0.1.5-py3-none-any.whl/imagedl/modules/sources/__init__.py
@@ -0,0 +1,21 @@
+'''initialize'''
+from .base import BaseImageClient
+from .bing import BingImageClient
+from .i360 import I360ImageClient
+from .baidu import BaiduImageClient
+from .google import GoogleImageClient
+from ..utils import BaseModuleBuilder
+from .yandex import YandexImageClient
+from .pixabay import PixabayImageClient
+
+
+'''ImageClientBuilder'''
+class ImageClientBuilder(BaseModuleBuilder):
+    REGISTERED_MODULES = {
+        'BingImageClient': BingImageClient, 'BaiduImageClient': BaiduImageClient, 'GoogleImageClient': GoogleImageClient, 
+        'I360ImageClient': I360ImageClient, 'PixabayImageClient': PixabayImageClient, 'YandexImageClient': YandexImageClient,
+    }
+
+
+'''BuildImageClient'''
+BuildImageClient = ImageClientBuilder().build
