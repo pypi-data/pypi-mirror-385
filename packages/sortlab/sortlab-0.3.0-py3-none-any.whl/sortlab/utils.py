64f927da73686de7be509d0bf05471b4f8d46464
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 16:06:05 +0000

    Add package sortlab/0.3.0/sortlab-0.3.0-py3-none-any.whl

---
+++ packages/sortlab/sortlab-0.3.0-py3-none-any.whl/sortlab/utils.py
@@ -0,0 +1,9 @@
+def to_celsius(fahrenheit) -> float:
+    """Convert Fahrenheit to Celsius."""
+    return (fahrenheit - 32) * 5.0 / 9.0
+
+def to_fahrenheit(celsius) -> float:
+    """Convert Celsius to Fahrenheit."""
+    return (celsius * 9.0 / 5.0) + 32
+
+    
