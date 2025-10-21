d22421efd0dea9851c92dd30d2c7be82998779eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 15:06:16 +0000

    Add package DigitalArticle/0.1.1/digitalarticle-0.1.1-py3-none-any.whl

---
+++ packages/DigitalArticle/digitalarticle-0.1.1-py3-none-any.whl/digitalarticle/__init__.py
@@ -0,0 +1,9 @@
+"""
+Digital Article CLI Package
+
+Provides command-line tools for starting the Digital Article backend and frontend servers.
+"""
+
+__version__ = "1.0.0"
+__author__ = "Digital Article Team"
+__description__ = "Command-line tools for Digital Article notebook application"
