1ded5142188102e06b5c1ef62d022a4da70a59ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 06:05:45 +0000

    Add package nadap/2.0.1/nadap-2.0.1-py3-none-any.whl

---
+++ packages/nadap/nadap-2.0.1-py3-none-any.whl/nadap/types/bool_false.py
@@ -0,0 +1,41 @@
+"""
+Bool type classes
+"""
+
+# pylint: disable=too-few-public-methods
+
+from nadap.base import UNDEFINED
+import nadap.types.bool
+
+DOC_DT_NAME = "Bool-False"
+DOC_DT_DESCRIPTION = """
+A **bool_false** data type tests data for being an instance of
+python's built-in class `bool`.
+It has a pre-defined default value set to `False`.
+"""
+DOC_DT_FEATURES = """
+- Supports **Referencing Feature**
+"""
+DOC_DT_YAML_EXAMPLE = """
+type: bool_false
+description: "Example for a bool_false definition"
+
+reference:
+  key: ref_key
+  mode: producer
+  producer_scope: global
+"""
+
+
+class BoolFalse(nadap.types.bool.Bool):
+    """
+    Bool datatype class with false as default value
+    """
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        if self.default_value is UNDEFINED:
+            self.default_value = False
+
+
+DOC_DT_CLASS = BoolFalse  # pylint: disable=invalid-name
