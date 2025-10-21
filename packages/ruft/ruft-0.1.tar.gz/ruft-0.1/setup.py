a33476414f18240a484e07836ebc3799e7743ceb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 18:09:34 +0000

    Add package ruft/0.1/ruft-0.1.tar.gz

---
+++ packages/ruft/ruft-0.1.tar.gz/ruft-0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+def readme():
+  with open('README.md', 'r') as f:
+    return f.read()
+
+setup(
+  name='ruft',
+  version='0.1',
+  author='Olegs Verhodubs',
+  author_email='oleg.verhodub@inbox.lv',
+  description='This is a prototype package for generating rules from raw text',
+  install_requires=['nltk>=3.9.2'],
+  classifiers=[
+    'Programming Language :: Python :: 3.13',
+    'License :: OSI Approved :: Apache Software License',
+    'Operating System :: OS Independent'
+  ],
+  keywords='rules text natural_language_processing python',
+  python_requires='>=3.13'
+)
