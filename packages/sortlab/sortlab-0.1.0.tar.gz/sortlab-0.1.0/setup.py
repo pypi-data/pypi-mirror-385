7b4e495a4d47645f2be9ecc92b904235f6091c23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 16:05:55 +0000

    Add package sortlab/0.1.0/sortlab-0.1.0.tar.gz

---
+++ packages/sortlab/sortlab-0.1.0.tar.gz/sortlab-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sortlab',
+    version='0.1.0',
+    description='A simple Python sorting library',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Lingeshwaran K V',
+    packages=find_packages(),
+    install_requires=[
+
+    ],
+)
