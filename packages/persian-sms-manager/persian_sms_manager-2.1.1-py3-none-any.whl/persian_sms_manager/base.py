d338dac6b6d4d7c2442bcd6f13a4bda947679cf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 04:05:25 +0000

    Add package persian-sms-manager/2.1.1/persian_sms_manager-2.1.1-py3-none-any.whl

---
+++ packages/persian-sms-manager/persian_sms_manager-2.1.1-py3-none-any.whl/persian_sms_manager/base.py
@@ -0,0 +1,72 @@
+class BaseSMSService:
+    """
+    An base class for SMS services.
+
+    Attributes:
+        user_mobile (str): The mobile number of the user.
+    """
+
+    def __init__(self, user_mobile: str, **kwargs: any) -> None:
+        """
+        Initializes the BaseSMSService with the user's mobile number.
+
+        Args:
+            user_mobile (str): The mobile number of the user.
+            **kwargs (Any): Additional keyword arguments (not used in this base class).
+        """
+        self.user_mobile: str = user_mobile
+
+    def send_message(self, message: str) -> bool:
+        """
+        Send a message to the user's mobile number.
+
+        Args:
+            message (str): The message content to send.
+
+        Returns:
+            bool: True if the message was successfully sent, otherwise False.
+        """
+        raise NotImplementedError("This method should be overridden by subclasses")
+
+    def send_otp_code(self, title_type: str) -> bool:
+        """
+        Send an OTP code with a specific title type to the user's mobile number.
+
+        Args:
+            title_type (str): The type or title of the OTP message.
+
+        Returns:
+            bool: True if the OTP code was successfully sent, otherwise False.
+        """
+        raise NotImplementedError("This method should be overridden by subclasses")
+
+    def check_otp_code_existed(self) -> bool:
+        """
+        Check if an OTP code exists for the user's mobile number.
+
+        Returns:
+            bool: True if the OTP code exists, otherwise False.
+        """
+        raise NotImplementedError("This method should be overridden by subclasses")
+
+    def send_auto_otp_code(self) -> bool:
+        """
+        Send an auto-generated OTP code to the user's mobile number.
+
+        Returns:
+            bool: True if the OTP code was successfully sent, otherwise False.
+        """
+        raise NotImplementedError("This method should be overridden by subclasses")
+
+    def check_auto_otp_code(self, otp_code: str) -> bool:
+        """
+        Verify the auto-generated OTP code sent to the user's mobile number.
+
+        Args:
+            otp_code (str): The OTP code to verify.
+
+        Returns:
+            bool: True if the OTP code is valid, otherwise False.
+        """
+        raise NotImplementedError("This method should be overridden by subclasses")
+
---
+++ packages/persian-sms-manager/persian_sms_manager-2.1.1-py3-none-any.whl/persian_sms_manager/manager.py
@@ -0,0 +1,37 @@
+from typing import Type, Dict, Any
+from .raygansms import RayganSMSService
+
+class SMSManager:
+    """
+    Manages different SMS services and provides a unified interface for creating service instances.
+
+    Attributes:
+        services (Dict[str, Type[RayganSMSService]]): A dictionary mapping service names to service classes.
+    """
+    
+    services: Dict[str, Type[RayganSMSService]] = {
+        "raygansms": RayganSMSService,
+        "ippanel": IPPanelService,
+    }
+
+    def __new__(cls, service_name: str, user_mobile: str, **kwargs: Any):
+        """
+        Retrieves an instance of the specified SMS service based on the service name.
+
+        Args:
+            service_name (str): The name of the SMS service to retrieve.
+            user_mobile (str): The mobile number of the user.
+            **kwargs (Any): Additional keyword arguments passed to the SMS service constructor.
+
+        Returns:
+            An instance of the specified SMS service.
+
+        Raises:
+            ValueError: If the provided service name is invalid.
+        """
+        service_class = cls.services.get(service_name.lower())
+        if not service_class:
+            raise ValueError(f"Invalid service name: {service_name}, available services: {list(cls.services.keys())}")
+
+        return service_class(user_mobile, **kwargs)
+
