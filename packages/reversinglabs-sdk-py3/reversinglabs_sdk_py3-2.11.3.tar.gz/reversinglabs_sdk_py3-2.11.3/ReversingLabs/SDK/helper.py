5903facb43fb6757bc482b1767b3d1ef9e022f2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 14:05:55 +0000

    Add package reversinglabs-sdk-py3/2.11.3/reversinglabs_sdk_py3-2.11.3.tar.gz

---
+++ packages/reversinglabs-sdk-py3/reversinglabs_sdk_py3-2.11.3.tar.gz/reversinglabs_sdk_py3-2.11.3/ReversingLabs/SDK/helper.py
@@ -0,0 +1,204 @@
+"""
+author: Mislav Sever
+
+Helper
+A Python module containing common helper functions and variables for ReversingLabsSDK.
+"""
+
+import codecs
+import binascii
+from functools import wraps
+from http import HTTPStatus
+import inspect
+from warnings import warn
+
+from ReversingLabs.SDK import __version__
+
+
+MD5 = "md5"
+SHA1 = "sha1"
+SHA256 = "sha256"
+SHA512 = "sha512"
+
+HASH_LENGTH_MAP = {
+    32: MD5,
+    40: SHA1,
+    64: SHA256,
+    128: SHA512
+}
+
+DEFAULT_USER_AGENT = f"ReversingLabs SDK v{__version__}"
+ADVANCED_SEARCH_SORTING_CRITERIA = ("sha1", "firstseen", "threatname", "sampletype", "filecount", "size")
+
+
+class WrongInputError(Exception):
+    def __init__(self, message="This input type is not allowed"):
+        super(WrongInputError, self).__init__(message)
+
+
+class NotFoundError(Exception):
+    def __init__(self, response_object, message="Not found. No reference was found for this input"):
+        super(NotFoundError, self).__init__(message)
+
+        self.response_object = response_object
+
+
+class NoFileTypeError(Exception):
+    def __init__(self, response_object, message="There is no determinable file type"):
+        super(NoFileTypeError, self).__init__(message)
+
+        self.response_object = response_object
+
+
+class UnauthorizedError(Exception):
+    def __init__(self, response_object, message="The provided credentials are invalid"):
+        super(UnauthorizedError, self).__init__(message)
+
+        self.response_object = response_object
+
+
+class ForbiddenError(Exception):
+    def __init__(self, response_object,
+                 message="The provided credentials do not have the required rights to access this resource"):
+        super(ForbiddenError, self).__init__(message)
+
+        self.response_object = response_object
+
+
+class BadRequestError(Exception):
+    def __init__(self, response_object, message="Bad request created"):
+        super(BadRequestError, self).__init__(message)
+
+        self.response_object = response_object
+
+
+class RequestTimeoutError(Exception):
+    def __init__(self, response_object, message="Request timed out"):
+        super(RequestTimeoutError, self).__init__(message)
+
+        self.response_object = response_object
+
+
+class ConflictError(Exception):
+    def __init__(self, response_object, message="Can't complete the request due to a conflict"):
+        super(ConflictError, self).__init__(message)
+
+        self.response_object = response_object
+
+
+class RequestTooLargeError(Exception):
+    def __init__(self, response_object, message="The request is too large"):
+        super(RequestTooLargeError, self).__init__(message)
+
+        self.response_object = response_object
+
+
+class InternalServerError(Exception):
+    def __init__(self, response_object, message="Internal server error"):
+        super(InternalServerError, self).__init__(message)
+
+        self.response_object = response_object
+
+
+class BadGatewayError(Exception):
+    def __init__(self, response_object, message="The server received an invalid response from another server"):
+        super(BadGatewayError, self).__init__(message)
+
+        self.response_object = response_object
+
+
+class ServiceUnavailableError(Exception):
+    def __init__(self, response_object, message="Service unavailable"):
+        super(ServiceUnavailableError, self).__init__(message)
+
+        self.response_object = response_object
+
+
+class NotAllowedError(Exception):
+    def __init__(self, response_object, message="This method is not allowed"):
+        super(NotAllowedError, self).__init__(message)
+
+        self.response_object = response_object
+
+
+class TooManyRequestsError(Exception):
+    def __init__(self, response_object, message="Too many requests. Your quota limit might be reached"):
+        super(TooManyRequestsError, self).__init__(message)
+
+        self.response_object = response_object
+
+
+class NotAcceptableError(Exception):
+    def __init__(self, response_object, message="This content is not acceptable"):
+        super(NotAcceptableError, self).__init__(message)
+
+        self.response_object = response_object
+
+
+RESPONSE_CODE_ERROR_MAP = {
+    HTTPStatus.UNAUTHORIZED: UnauthorizedError,
+    HTTPStatus.FORBIDDEN: ForbiddenError,
+    HTTPStatus.NOT_FOUND: NotFoundError,
+    HTTPStatus.METHOD_NOT_ALLOWED: NotAllowedError,
+    HTTPStatus.NOT_ACCEPTABLE: NotAcceptableError,
+    HTTPStatus.TOO_MANY_REQUESTS: TooManyRequestsError,
+    HTTPStatus.BAD_REQUEST: BadRequestError,
+    HTTPStatus.REQUEST_TIMEOUT: RequestTimeoutError,
+    HTTPStatus.CONFLICT: ConflictError,
+    HTTPStatus.REQUEST_ENTITY_TOO_LARGE: RequestTooLargeError,
+    HTTPStatus.INTERNAL_SERVER_ERROR: InternalServerError,
+    HTTPStatus.BAD_GATEWAY: BadGatewayError,
+    HTTPStatus.SERVICE_UNAVAILABLE: ServiceUnavailableError
+}
+
+
+def validate_hashes(hash_input, allowed_hash_types):
+    """Checks if the hash input is of the allowed hash types.
+        :param hash_input: a list of hashes
+        :type hash_input:list[str]
+        :param allowed_hash_types: allowed hash types
+        :type allowed_hash_types: tuple
+    """
+    if not hash_input:
+        raise WrongInputError("At least one hash needs to be supplied as input.")
+
+    for hash_string in hash_input:
+        try:
+            codecs.decode(hash_string, "hex")
+        except (TypeError, binascii.Error):
+            raise WrongInputError("The given hash input string is not a valid hexadecimal value.")
+
+        hashing_algorithm = HASH_LENGTH_MAP.get(len(hash_string), None)
+        if hashing_algorithm not in allowed_hash_types:
+            raise WrongInputError(
+                "Only hash strings of the following types are allowed as input values: {allowed}".format(
+                    allowed=allowed_hash_types
+                ))
+
+
+def deprecated_args(dpr_args: list):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            arg_names = list(inspect.getfullargspec(func).args)
+            arg_names.extend(dpr_args)
+            args_kv = dict(zip(arg_names, args))
+            new_args = list(args)
+            for k in dpr_args:
+                if k not in kwargs and k in args_kv:
+                    if k == "sample_sha1":
+                        warn(f"DEPRECATION WARNING - Parameter sample_sha1 is deprecated. "
+                             f"Start using sample_hash instead.", Warning)
+
+                    else:
+                        warn(f"DEPRECATION WARNING - Parameter {k} is deprecated. "
+                             f"Start using it through optional_parameters.", Warning)
+                    arg_idx = arg_names.index(k)
+                    del new_args[arg_idx]
+                    del arg_names[arg_idx]
+                    kwargs[k] = args_kv[k]
+            return func(*new_args, **kwargs)
+
+        return wrapper
+
+    return decorator
---
+++ packages/reversinglabs-sdk-py3/reversinglabs_sdk_py3-2.11.3.tar.gz/reversinglabs_sdk_py3-2.11.3/tests/test_tiscale.py
@@ -0,0 +1,145 @@
+import pytest
+from unittest import mock
+from ReversingLabs.SDK.tiscale import TitaniumScale
+from ReversingLabs.SDK.helper import WrongInputError, DEFAULT_USER_AGENT
+
+
+def test_tiscale_object():
+	invalid_host = "my.host"
+	valid_host = f"https://{invalid_host}"
+	token = "my_mock_token"
+
+	tiscale = TitaniumScale(
+		host=valid_host,
+		token=token,
+		verify=True
+	)
+
+	assert tiscale._url == valid_host + "{endpoint}"
+
+	with pytest.raises(WrongInputError, match=r"host parameter must contain a protocol definition at the beginning."):
+		TitaniumScale(host=invalid_host, token=token)
+
+
+@pytest.fixture
+def requests_mock():
+	with mock.patch('ReversingLabs.SDK.tiscale.requests', autospec=True) as requests_mock:
+		yield requests_mock
+
+
+class TestTitaniumScale:
+	host = "https://my.host"
+	token = "token"
+
+	headers = {"User-Agent": DEFAULT_USER_AGENT, "Authorization": f"Token {token}"}
+
+	@classmethod
+	def setup_class(cls):
+		cls.tiscale = TitaniumScale(cls.host, token=cls.token)
+
+	def test_sample_from_path(self):
+		with pytest.raises(WrongInputError, match=r"file_path must be a string."):
+			self.tiscale.upload_sample_from_path(file_path=123)
+
+	def test_sample_from_file(self):
+		with pytest.raises(WrongInputError, match=r"file_source parameter must be a file open in 'rb' mode."):
+			self.tiscale.upload_sample_from_file(file_source="/path/to/file")
+
+	def test_get_results(self):
+		with pytest.raises(WrongInputError, match=r"full_report parameter must be boolean."):
+			self.tiscale.get_results(task_url="/task/url", full_report=3)
+
+	def test_list_tasks(self, requests_mock):
+		self.tiscale.list_processing_tasks(age=10, custom_token="custom")
+
+		query_params = {
+			"age": 10,
+			"token": "Token custom"
+		}
+
+		expected_url = f"{self.host}/api/tiscale/v1/task"
+
+		self.headers["User-Agent"] = f"{DEFAULT_USER_AGENT}; {self.tiscale.__class__.__name__} list_processing_tasks"
+
+		requests_mock.get.assert_called_with(
+			url=expected_url,
+			verify=True,
+			proxies=None,
+			headers=self.headers,
+			params=query_params
+		)
+
+	def test_task_info(self, requests_mock):
+		self.tiscale.get_processing_task_info(
+			task_id=1,
+			full=True
+		)
+
+		query_params = {
+			"full": "true",
+			"v13": "false"
+		}
+
+		expected_url = f"{self.host}/api/tiscale/v1/task/1"
+
+		self.headers["User-Agent"] = f"{DEFAULT_USER_AGENT}; {self.tiscale.__class__.__name__} get_processing_task_info"
+
+		requests_mock.get.assert_called_with(
+			url=expected_url,
+			verify=True,
+			proxies=None,
+			headers=self.headers,
+			params=query_params
+		)
+
+	def test_delete_task(self, requests_mock):
+		self.tiscale.delete_processing_task(
+			task_id=1
+		)
+
+		expected_url = f"{self.host}/api/tiscale/v1/task/1"
+
+		self.headers["User-Agent"] = f"{DEFAULT_USER_AGENT}; {self.tiscale.__class__.__name__} delete_processing_task"
+
+		requests_mock.delete.assert_called_with(
+			url=expected_url,
+			verify=True,
+			proxies=None,
+			headers=self.headers
+		)
+
+	def test_wrong_task_id(self, requests_mock):
+		with pytest.raises(WrongInputError, match=r"task_id parameter must be integer."):
+			self.tiscale.delete_processing_task(task_id="123")
+
+		assert not requests_mock.delete.called
+
+	def test_delete_multiple(self, requests_mock):
+		self.tiscale.delete_multiple_tasks(age=10)
+
+		query_params = {"age": 10}
+
+		expected_url = f"{self.host}/api/tiscale/v1/task"
+
+		self.headers["User-Agent"] = f"{DEFAULT_USER_AGENT}; {self.tiscale.__class__.__name__} delete_multiple_tasks"
+
+		requests_mock.delete.assert_called_with(
+			url=expected_url,
+			verify=True,
+			proxies=None,
+			headers=self.headers,
+			params=query_params
+		)
+
+	def test_yara_id(self, requests_mock):
+		self.tiscale.get_yara_id()
+
+		self.headers["User-Agent"] = f"{DEFAULT_USER_AGENT}; {self.tiscale.__class__.__name__} get_yara_id"
+
+		requests_mock.get.assert_called_with(
+			url=f"{self.host}/api/tiscale/v1/yara",
+			verify=True,
+			proxies=None,
+			headers=self.headers
+		)
+
