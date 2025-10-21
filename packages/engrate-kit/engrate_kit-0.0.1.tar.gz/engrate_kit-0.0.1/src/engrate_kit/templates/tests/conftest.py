83ca02cb6c50031876d4170a850cf7270f37edf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 04:04:57 +0000

    Add package engrate-kit/0.0.1/engrate_kit-0.0.1.tar.gz

---
+++ packages/engrate-kit/engrate_kit-0.0.1.tar.gz/engrate_kit-0.0.1/src/engrate_kit/templates/tests/conftest.py
@@ -0,0 +1,19 @@
+import pytest
+from fastapi.testclient import TestClient
+from fastmcp.client import Client as MCPClient
+from fastmcp.client.transports import FastMCPTransport
+
+from {{app_module_name}}.app import app
+
+
+@pytest.fixture
+def client():
+    """Inject a FastAPI test client."""
+    return TestClient(app)
+
+
+@pytest.fixture
+def mcp_client():
+    """Inject a FastAPI test client."""
+    assert app.mcp_server is not None
+    return MCPClient(transport=FastMCPTransport(app.mcp_server))
