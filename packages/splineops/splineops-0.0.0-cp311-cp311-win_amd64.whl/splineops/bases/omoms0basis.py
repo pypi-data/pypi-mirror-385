2ef12ac0b6537133e7b5abcde84460d8c8f49c85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 17:12:55 +0000

    Add package splineops/0.0.0/splineops-0.0.0-cp311-cp311-win_amd64.whl

---
+++ packages/splineops/splineops-0.0.0-cp311-cp311-win_amd64.whl/splineops/bases/omoms0basis.py
@@ -0,0 +1,6 @@
+# splineops/src/splineops/bases/omoms0basis.py
+
+from splineops.bases.bspline0basis import BSpline0Basis, BSpline0SymBasis
+
+OMOMS0Basis = BSpline0Basis
+OMOMS0SymBasis = BSpline0SymBasis
