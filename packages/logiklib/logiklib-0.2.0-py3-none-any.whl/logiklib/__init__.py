4f04152313ea65e5d854e7e226e4f5483672ccf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 07:06:41 +0000

    Add package logiklib/0.2.0/logiklib-0.2.0-py3-none-any.whl

---
+++ packages/logiklib/logiklib-0.2.0-py3-none-any.whl/logiklib/__init__.py
@@ -0,0 +1,9 @@
+__version__ = "0.2.0"
+
+
+def register_part_data(fpga, package_name, part_name):
+    fpga.set_dataroot(
+        package_name,
+        f"https://github.com/siliconcompiler/logiklib/releases/download/v{__version__}/{part_name}_cad.tar.gz",
+        f'v{__version__}'
+    )
