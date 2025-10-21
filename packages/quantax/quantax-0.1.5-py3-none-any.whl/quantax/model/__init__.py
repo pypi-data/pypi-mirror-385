65017171a006734e1cb6ec9e526ab0a47128818d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 06:06:06 +0000

    Add package quantax/0.1.5/quantax-0.1.5-py3-none-any.whl

---
+++ packages/quantax/quantax-0.1.5-py3-none-any.whl/quantax/model/__init__.py
@@ -0,0 +1,16 @@
+from .shallow_nets import SingleDense, RBM_Dense, SingleConv, RBM_Conv
+from .conv_nets import ResConv, ResGConv
+from .transformer import ConvTransformer
+from .sign_nets import SgnNet, MarshallSign, StripeSign, Neel120
+from .triangular_nets import Triangular_Neighbor_Conv, Triangular_ResConv
+from .fermion_mf import (
+    GeneralDet,
+    RestrictedDet,
+    UnrestrictedDet,
+    MultiDet,
+    GeneralPf,
+    SingletPair,
+    MultiPf,
+    PartialPair,
+)
+from .neural_fermion import NeuralJastrow
