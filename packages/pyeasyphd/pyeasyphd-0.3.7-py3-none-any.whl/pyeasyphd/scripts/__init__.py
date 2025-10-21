80584d2793999399e4a4c9cc4bd37baf73e09708
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 16:06:28 +0000

    Add package pyeasyphd/0.3.7/pyeasyphd-0.3.7-py3-none-any.whl

---
+++ packages/pyeasyphd/pyeasyphd-0.3.7-py3-none-any.whl/pyeasyphd/scripts/__init__.py
@@ -0,0 +1,25 @@
+__all__ = [
+    "run_article_md_daily_notes",
+    "run_article_tex_submit",
+    "run_beamer_tex_weekly_reports",
+    "run_search_for_screen",
+    "run_search_for_files",
+    "run_compare_after_search",
+    "run_generate_c_Yearly",
+    "run_generate_j_e_Weekly",
+    "run_generate_j_Weekly",
+    "run_generate_j_Monthly",
+    "run_generate_j_Yearly",
+]
+
+from .run_article_md import run_article_md_daily_notes
+from .run_article_tex import run_article_tex_submit
+from .run_beamer_tex import run_beamer_tex_weekly_reports
+from .run_generate import (
+    run_generate_c_Yearly,
+    run_generate_j_e_Weekly,
+    run_generate_j_Monthly,
+    run_generate_j_Weekly,
+    run_generate_j_Yearly,
+)
+from .run_search import run_compare_after_search, run_search_for_files, run_search_for_screen
