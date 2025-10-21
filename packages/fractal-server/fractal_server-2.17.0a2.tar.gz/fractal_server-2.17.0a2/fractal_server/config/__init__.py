22d2e4959def030013f899570850782758b3c28f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 03:21:31 +0000

    Add package fractal-server/2.17.0a2/fractal_server-2.17.0a2.tar.gz

---
+++ packages/fractal-server/fractal_server-2.17.0a2.tar.gz/fractal_server-2.17.0a2/fractal_server/config/__init__.py
@@ -0,0 +1,28 @@
+from ._database import DatabaseSettings
+from ._email import EmailSettings
+from ._email import PublicEmailSettings  # noqa F401
+from ._init_data import InitDataSettings
+from ._main import Settings
+from ._oauth import OAuthSettings
+
+
+def get_db_settings(db_settings=DatabaseSettings()) -> DatabaseSettings:
+    return db_settings
+
+
+def get_settings(settings=Settings()) -> Settings:
+    return settings
+
+
+def get_email_settings(email_settings=EmailSettings()) -> EmailSettings:
+    return email_settings
+
+
+def get_init_data_settings(
+    init_data_settings=InitDataSettings(),
+) -> InitDataSettings:
+    return init_data_settings
+
+
+def get_oauth_settings(oauth_settings=OAuthSettings()) -> OAuthSettings:
+    return oauth_settings
