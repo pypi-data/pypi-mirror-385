b1a8095447aadae2014a99736423a7e569677696
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 08:06:25 +0000

    Add package openassetpricing/0.0.2/openassetpricing-0.0.2-py3-none-any.whl

---
+++ packages/openassetpricing/openassetpricing-0.0.2-py3-none-any.whl/examples/example_usage.py
@@ -0,0 +1,62 @@
+import openassetpricing as oap
+
+
+# List available release versions
+oap.list_release()
+
+# Initialize OpenAP with most recent release
+openap = oap.OpenAP()
+
+# ==========
+# List available portfolios
+# ==========
+openap.list_port()
+
+
+
+# ==========
+# Download SignalDoc.csv in pandas dataframe
+# ==========
+df = openap.dl_signal_doc('pandas')
+
+
+
+# ==========
+# Portfolios -> Full Sets OP -> PredictorPortsFull.csv
+# ==========
+
+# Download entier file in polars dataframe
+df = openap.dl_port('op', 'polars')
+
+# Download specific predictors (can be single or multiple predictors)
+df = openap.dl_port('op', 'polars', ['AM'])
+df = openap.dl_port('op', 'polars', ['AM', 'Mom12m'])
+
+
+
+# ==========
+# Portfolios -> Full Sets Alt -> PredictorAltPorts_Deciles.zip
+# Portfolios -> Full Sets Alt -> PredictorAltPorts_DecilesVW.zip
+# Portfolios -> Full Sets Alt -> PredictorAltPorts_LiqScreen_NYSEonly.zip
+# ==========
+
+# Download entier file
+df = openap.dl_port('deciles_ew', 'polars')
+df = openap.dl_port('deciles_vw', 'polars')
+df = openap.dl_port('nyse', 'pandas')
+
+# Download specific predictors (can be single or multiple predictors)
+df = openap.dl_port('deciles_ew', 'polars', ['Accruals'])
+df = openap.dl_port('deciles_ew', 'polars', ['BM', 'Mom6m'])
+
+
+
+# ==========
+# Firm Level Characteristics
+# ==========
+
+# Download all firm characteristics (signed)
+df = openap.dl_all_signals('polars')
+
+# Download specific firm characteristics
+df = openap.dl_signal('polars', ['BM'])
---
+++ packages/openassetpricing/openassetpricing-0.0.2-py3-none-any.whl/openassetpricing/urls.py
@@ -0,0 +1,21 @@
+# Data is from Chen and Zimmermann: https://www.openassetpricing.com/
+
+# Version 2.00: data release - 2025.10
+release202510_url = (
+    'https://drive.google.com/drive/folders/1qQDuTsnyvWfEJR6nPBQZ8xxlq6bkLG_y')
+
+# Version 1.41: data release - 2024.10
+release202410_url = (
+    'https://drive.google.com/drive/folders/1SSoHGbwgyhRwUCzLE0YWvUlS0DjLCd4k')
+
+# Version 1.40: data release - 2024.08
+release202408_url = (
+    'https://drive.google.com/drive/folders/1-PqsR-tOjv3-U9DRHw85X-VznYlu-Sfc')
+
+# Version 1.30: data release - 2023.08
+release2023_url = (
+    'https://drive.google.com/drive/folders/1EP6oEabyZRamveGNyzYU0u6qJ-N43Qfq')
+
+# Version 1.20: data release - 2022.03
+release2022_url = (
+    'https://drive.google.com/drive/folders/1O18scg9iBTiBaDiQFhoGxdn4FdsbMqGo')
