26954d648a89f6251169f00f3fdc1847ff724551
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 08:07:04 +0000

    Add package tvscreener/0.0.15/tvscreener-0.0.15.tar.gz

---
+++ packages/tvscreener/tvscreener-0.0.15.tar.gz/tvscreener-0.0.15/tvscreener/core/forex.py
@@ -0,0 +1,15 @@
+from tvscreener.core.base import Screener, default_sort_forex
+from tvscreener.field.forex import ForexField
+from tvscreener.util import get_url
+
+
+class ForexScreener(Screener):
+    def __init__(self):
+        super().__init__()
+        subtype = "forex"
+        self.url = get_url(subtype)
+        self.markets = set(subtype)
+        self.specific_fields = ForexField  # {**self.fields, **tvdata.forex['columns']}
+        # self.add_filter("sector", FilterOperation.IN_RANGE, ['Major', 'Minor'])
+        self.sort_by(default_sort_forex)
+        self.add_misc("symbols", {"query": {"types": ["forex"]}})
