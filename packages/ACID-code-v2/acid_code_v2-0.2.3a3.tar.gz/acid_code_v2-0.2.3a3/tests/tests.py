77716388bf0463bb97ae5fea0b15d3da6eb814b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 18:09:00 +0000

    Add package ACID-code-v2/0.2.3a3/acid_code_v2-0.2.3a3.tar.gz

---
+++ packages/ACID-code-v2/acid_code_v2-0.2.3a3.tar.gz/acid_code_v2-0.2.3a3/tests/tests.py
@@ -0,0 +1,44 @@
+#%%
+from astropy.io import fits
+import os, glob, importlib, sys
+import numpy as np
+import matplotlib.pyplot as plt
+os.chdir(os.path.dirname(__file__))
+os.chdir("..")  # ensures we are in the main directory
+try:
+    import ACID_code_v2 as acid
+except:
+    SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+    PROJECT_ROOT = os.path.dirname(SCRIPT_DIR)
+    sys.path.append(PROJECT_ROOT)
+    from src import ACID_code_v2 as acid
+    print("pip module failed to import, imported from local instead")
+importlib.reload(acid)
+
+def test_run_e2ds():
+
+    e2ds_files = glob.glob('tests/data/*e2ds_A*.fits')
+    linelist = 'example/example_linelist.txt'
+    save_path = 'no save'
+
+    velocities = np.arange(-25, 25, 0.82)
+
+    # run ACID on e2ds files
+    ACID_results_e2ds = acid.ACID_HARPS(e2ds_files, linelist, vgrid = velocities, save_path = save_path,
+                                        order_range = np.arange(41, 43), nsteps=2000)
+
+def test_run_s1d():
+
+    s1d_files = glob.glob('tests/data/*s1d_A*.fits')
+    linelist = 'example/example_linelist.txt'
+    save_path = 'no save'
+
+    velocities = np.arange(-25, 25, 0.82)
+
+    # run ACID on s1d files
+    ACID_results_s1d = acid.ACID_HARPS(s1d_files, linelist, vgrid = velocities, save_path = save_path,
+                                       order_range = np.arange(41, 43), file_type = 's1d', nsteps=2000)
+
+test_run_e2ds()
+test_run_s1d()
+print("All tests passed!")
