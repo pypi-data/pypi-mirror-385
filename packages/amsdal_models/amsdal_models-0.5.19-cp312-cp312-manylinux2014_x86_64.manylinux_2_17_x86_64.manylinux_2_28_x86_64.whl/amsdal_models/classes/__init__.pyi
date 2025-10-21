c50366bf4ead4cbeb9e3f702b02ae44cf6b52878
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 03:20:39 +0000

    Add package amsdal_models/0.5.19/amsdal_models-0.5.19-cp312-cp312-manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/amsdal_models/amsdal_models-0.5.19-cp312-cp312-manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_28_x86_64.whl/amsdal_models/classes/__init__.pyi
@@ -0,0 +1,4 @@
+from amsdal_models.classes.external_model import ExternalModel as ExternalModel
+from amsdal_models.classes.model import LegacyModel as LegacyModel, Model as Model
+
+__all__ = ['ExternalModel', 'LegacyModel', 'Model']
