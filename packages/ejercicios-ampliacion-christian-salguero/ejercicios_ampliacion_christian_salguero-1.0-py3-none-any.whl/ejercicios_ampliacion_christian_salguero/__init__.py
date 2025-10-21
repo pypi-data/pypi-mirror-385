9cd2a9c0462ff59901737366d02b8659d335da5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 03:20:52 +0000

    Add package ejercicios-ampliacion-christian-salguero/1.0/ejercicios_ampliacion_christian_salguero-1.0-py3-none-any.whl

---
+++ packages/ejercicios-ampliacion-christian-salguero/ejercicios_ampliacion_christian_salguero-1.0-py3-none-any.whl/ejercicios_ampliacion_christian_salguero/__init__.py
@@ -0,0 +1,1 @@
+from .ejercicios_ampliacion_seleccion_repeticion import ejercicio1, ejercicio2, ejercicio3, ejercicio4, ejercicio5, ejercicio6, ejercicio7, ejercicio8, ejercicio9, ejercicio10, ejercicio11, ejercicio12, ejercicio13, ejercicio14, ejercicio15, ejercicio16, ejercicio17, ejercicio18, ejercicio19, ejercicio20
