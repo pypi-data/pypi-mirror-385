74ab2598471f1746a896a5be980a64dec44af874
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 17:13:07 +0000

    Add package teleporter/2.0.0/teleporter-2.0.0.tar.gz

---
+++ packages/teleporter/teleporter-2.0.0.tar.gz/teleporter-2.0.0/setup.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+import setuptools
+
+from teleporter import __version__
+
+with open(Path(__file__).parent / 'README.md') as f:
+    long_description = '\n' + f.read()
+
+setuptools.setup(
+    name='teleporter',
+    version=__version__,
+    author='sozercanye',
+    description='Serializer and deserializer for Telegram Android sessions.',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
+    url='https://github.com/sozercanye/teleporter',
+    packages=setuptools.find_packages(),
+    license='MIT',
+    install_requires=['aiofiles~=25.1.0'],
+    python_requires='>=3.9'
+)
