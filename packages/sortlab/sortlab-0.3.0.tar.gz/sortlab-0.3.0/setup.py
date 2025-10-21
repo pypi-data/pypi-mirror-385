3b8b369790136b077aa8ed6892716fa6bb1024aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-21 16:06:05 +0000

    Add package sortlab/0.3.0/sortlab-0.3.0.tar.gz

---
+++ packages/sortlab/sortlab-0.3.0.tar.gz/sortlab-0.3.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sortlab',
+    version='0.3.0',
+    description='A simple Python sorting library',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Lingeshwaran K V',
+    python_requires='>=3.6',
+    packages=find_packages(),
+    install_requires=[
+
+    ],
+)
---
+++ packages/sortlab/sortlab-0.3.0.tar.gz/sortlab-0.3.0/sortlab/__init__.py
@@ -0,0 +1,3 @@
+from .utils import to_celsius, to_fahrenheit
+
+__all__ = ["to_celsius", "to_fahrenheit"]
