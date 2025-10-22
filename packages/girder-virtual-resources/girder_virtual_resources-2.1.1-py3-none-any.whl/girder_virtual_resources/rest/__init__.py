cbcb502dde71ec67f5d075751484733990d5f403
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-22 05:06:12 +0000

    Add package girder-virtual-resources/2.1.1/girder_virtual_resources-2.1.1-py3-none-any.whl

---
+++ packages/girder-virtual-resources/girder_virtual_resources-2.1.1-py3-none-any.whl/girder_virtual_resources/rest/__init__.py
@@ -0,0 +1,217 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import base64
+import copy
+import datetime
+import os
+import pathlib
+
+from girder.api.rest import Resource
+from girder.constants import AccessType
+from girder.exceptions import ValidationException
+from girder.models.folder import Folder
+from girder.models.upload import Upload
+
+
+def bail_if_exists(path):
+    if path.exists():
+        raise ValidationException(
+            "A folder or file with that name already exists here.", "name"
+        )
+
+
+def ensure_unique_path(dirname, name):
+    checkName = (dirname / name).exists()
+    new_name = name
+    n = 0
+    while checkName:
+        n += 1
+        new_name = "%s (%d)" % (name, n)
+        checkName = (dirname / new_name).exists()
+    return dirname / new_name
+
+
+def validate_event(level=AccessType.READ, validate_admin=False):
+    def validation(func):
+        def wrapper(self, event):
+            params = event.info.get("params", {})
+            if {"isMapping", "fsPath"} & set(params) and validate_admin:
+                folder = Folder().load(event.info["id"], force=True)
+                update = False
+
+                if params.get("isMapping") is not None:
+                    update = True
+                    folder["isMapping"] = bool(params["isMapping"])
+                if params.get("fsPath") is not None:
+                    update = True
+                    folder["fsPath"] = params["fsPath"]
+
+                if update:
+                    self.requireAdmin(
+                        self.getCurrentUser(), "Must be admin to setup virtual folders."
+                    )
+                    folder = Folder().filter(Folder().save(folder), self.getCurrentUser())
+                    pathlib.Path(folder["fsPath"]).mkdir(
+                        mode=0o755, parents=True, exist_ok=True
+                    )
+                    event.preventDefault().addResponse(folder)
+                    return
+
+            if "uploadId" in params:
+                upload = Upload().load(params["uploadId"])
+                try:
+                    parent_id = str(upload["parentId"])
+                    parent_type = upload["parentType"]
+                except KeyError:
+                    parent_id = str(upload["fileId"])
+                    parent_type = None
+            else:
+                parent_id = params.get("parentId")
+                parent_type = params.get("parentType") or "folder"
+
+            obj_id = event.info.get("id")
+            folder_id = params.get("folderId")
+            item_id = params.get("itemId")
+            any_parent_id = parent_id or folder_id or item_id
+
+            path = None
+            if obj_id:
+                path, root_id = VirtualObject.path_from_id(obj_id)
+            elif any_parent_id and any_parent_id.startswith("wtlocal:"):
+                path, root_id = VirtualObject.path_from_id(any_parent_id)
+            elif (parent_id and parent_type == "folder") or folder_id:
+                path, root_id = VirtualObject.path_from_id(parent_id or folder_id)
+
+            if path:
+                path = pathlib.Path(path)
+                if path.is_absolute():
+                    user = self.getCurrentUser()
+                    root = Folder().load(root_id, level=level, user=user, exc=True)
+                    func(self, event, path, root, user=user)
+
+        return wrapper
+
+    return validation
+
+
+class VirtualObject(Resource):
+    def __init__(self):
+        super(VirtualObject, self).__init__()
+
+    @staticmethod
+    def generate_id(path, root_id):
+        if isinstance(path, pathlib.Path):
+            path = path.as_posix()
+        path += "|" + str(root_id)
+        return "wtlocal:" + base64.b64encode(path.encode()).decode()
+
+    @staticmethod
+    def path_from_id(object_id):
+        if str(object_id).startswith("wtlocal:"):
+            decoded = base64.b64decode(object_id[8:]).decode()
+            path, root_id = decoded.split("|")
+        else:
+            root_folder = Folder().load(object_id, force=True) or {}
+            path = root_folder.get("fsPath")  # only exists on virtual folders
+            root_id = str(root_folder.get("_id"))
+        if path:
+            path = pathlib.Path(path)
+        return path, root_id
+
+    def is_file(self, path, root_id):
+        if not path.is_file():
+            raise ValidationException(
+                "Invalid ObjectId: %s" % self.generate_id(path, root_id), field="id"
+            )
+
+    def is_symlink(self, path, root_id):
+        if not path.is_symlink():
+            raise ValidationException(
+                "Invalid ObjectId: %s" % self.generate_id(path, root_id), field="id"
+            )
+
+    def is_dir(self, path, root_id):
+        if not path.is_dir():
+            raise ValidationException(
+                "Invalid ObjectId: %s" % self.generate_id(path, root_id), field="id"
+            )
+
+    def vFolder(self, path, root):
+        self.is_dir(path, root["_id"])
+        stat = path.stat()
+
+        if path == pathlib.Path(root["fsPath"]):
+            # We want actual mtime/ctime from disk
+            root.update({
+                "created": datetime.datetime.fromtimestamp(stat.st_ctime, datetime.timezone.utc),
+                "updated": datetime.datetime.fromtimestamp(stat.st_mtime, datetime.timezone.utc),
+            })
+            return root
+
+        if path.parent == pathlib.Path(root["fsPath"]):
+            parentId = root["_id"]
+        else:
+            parentId = self.generate_id(path.parent.as_posix(), root["_id"])
+
+        return {
+            "_id": self.generate_id(path.as_posix(), root["_id"]),
+            "_modelType": "folder",
+            "access": copy.deepcopy(root.get("access", {"users": [], "groups": []})),
+            "name": path.parts[-1],
+            "parentId": parentId,
+            "parentCollection": "folder",
+            "creatorId": None,
+            "created": datetime.datetime.fromtimestamp(stat.st_ctime, datetime.timezone.utc),
+            "updated": datetime.datetime.fromtimestamp(stat.st_mtime, datetime.timezone.utc),
+            "size": stat.st_size,
+            "public": root.get("public", False),
+            "lowerName": path.parts[-1].lower(),
+        }
+
+    def vItem(self, path, root):
+        self.is_file(path, root["_id"])
+        stat = path.stat()
+        return {
+            "_id": self.generate_id(path.as_posix(), root["_id"]),
+            "_modelType": "item",
+            "name": path.parts[-1],
+            "folderId": self.generate_id(path.parent.as_posix(), root["_id"]),
+            "creatorId": None,
+            "created": datetime.datetime.fromtimestamp(stat.st_ctime, datetime.timezone.utc),
+            "updated": datetime.datetime.fromtimestamp(stat.st_mtime, datetime.timezone.utc),
+            "size": stat.st_size,
+            "lowerName": path.parts[-1].lower(),
+        }
+
+    def vLink(self, path, root):
+        self.is_symlink(path, root["_id"])
+        stat = path.lstat()
+        return {
+            "_id": self.generate_id(path.as_posix(), root["_id"]),
+            "_modelType": None,
+            "name": path.parts[-1],
+            "folderId": self.generate_id(path.parent.as_posix(), root["_id"]),
+            "creatorId": None,
+            "created": datetime.datetime.fromtimestamp(stat.st_ctime, datetime.timezone.utc),
+            "updated": datetime.datetime.fromtimestamp(stat.st_mtime, datetime.timezone.utc),
+            "size": stat.st_size,
+            "lowerName": path.parts[-1].lower(),
+            "linkTarget": os.readlink(path)
+        }
+
+    def vFile(self, path, root):
+        self.is_file(path, root["_id"])
+        stat = path.stat()
+        return {
+            "_id": self.generate_id(path.as_posix(), root["_id"]),
+            "_modelType": "file",
+            "assetstoreId": None,
+            "creatorId": root["creatorId"],
+            "mimeType": "application/octet-stream",
+            "name": path.parts[-1],
+            "size": stat.st_size,
+            "exts": [_[1:] for _ in path.suffixes],
+            "created": datetime.datetime.fromtimestamp(stat.st_ctime, datetime.timezone.utc),
+            "updated": datetime.datetime.fromtimestamp(stat.st_mtime, datetime.timezone.utc),
+            "itemId": self.generate_id(path.as_posix(), root["_id"]),
+        }
---
+++ packages/girder-virtual-resources/girder_virtual_resources-2.1.1-py3-none-any.whl/girder_virtual_resources/rest/virtual_item.py
@@ -0,0 +1,205 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import pathlib
+import shutil
+from operator import itemgetter
+
+from girder import events
+from girder.api import access
+from girder.constants import AccessType, TokenScope
+from girder.exceptions import GirderException, ValidationException
+from girder.models.file import File
+from girder.models.folder import Folder
+from girder.models.item import Item
+
+import pymongo
+
+from . import VirtualObject, bail_if_exists, ensure_unique_path, validate_event
+
+
+class VirtualItem(VirtualObject):
+    def __init__(self):
+        super(VirtualItem, self).__init__()
+        self.resourceName = "virtual_item"
+        name = "virtual_resources"
+
+        events.bind("model.item.validate", name, self.validate_item)
+        events.bind("rest.get.item.before", name, self.get_child_items)
+        events.bind("rest.post.item.before", name, self.create_item)
+        events.bind("rest.get.item/:id.before", name, self.get_item_info)
+        events.bind("rest.put.item/:id.before", name, self.rename_item)
+        events.bind("rest.delete.item/:id.before", name, self.remove_item)
+        events.bind("rest.post.item/:id/copy.before", name, self.copy_item)
+        # events.bind("rest.get.item/:id/download.before", name, self.file_download)  # in Vfile
+        events.bind("rest.get.item/:id/files.before", name, self.get_child_files)
+        # PUT/DELETE /item/:id/metadata
+        events.bind("rest.get.item/:id/rootpath.before", name, self.item_root_path)
+
+    @staticmethod
+    def validate_item(event):
+        doc = event.info
+        if isinstance(doc.get("_id"), str) and doc.get("_id").startswith("wtlocal:"):
+            return
+        parent = Folder().load(doc["folderId"], force=True, exc=True)
+        if parent.get("isSymlink"):
+            raise ValidationException(
+                "You may not place items under a symlink folder.", field="folderId"
+            )
+
+    @access.public(scope=TokenScope.DATA_READ)
+    def get_child_items(self, event):
+        params = event.info["params"]
+        folder_id = params.get("folderId")
+        if folder_id and not folder_id.startswith("wtlocal:"):
+            parent = Folder().load(params["folderId"], force=True, exc=True)
+            if parent.get("isSymlink") and parent.get("symlinkTargetId"):
+                event.info["params"]["folderId"] = str(parent["symlinkTargetId"])
+        self._get_child_items(event)
+
+    @access.public(scope=TokenScope.DATA_READ)
+    @validate_event(level=AccessType.READ)
+    def _get_child_items(self, event, path, root, user=None):
+        params = event.info["params"]
+
+        self.is_dir(path, root["_id"])
+        offset = int(params.get("offset", 0))
+        limit = int(params.get("limit", 50))
+        name = params.get("name")
+        sort_key = params.get("sort", "lowerName")
+        reverse = int(params.get("sortdir", pymongo.ASCENDING)) == pymongo.DESCENDING
+
+        if name:
+            if (path / name).is_file():
+                items = [self.vItem(path / name, root)]
+            else:
+                items = []
+        else:
+            items = [self.vItem(obj, root) for obj in path.iterdir() if obj.is_file()]
+        items = sorted(items, key=itemgetter(sort_key), reverse=reverse)
+        upper_bound = limit + offset if limit > 0 else None
+        response = [
+            Item().filter(item, user=user) for item in items[offset:upper_bound]
+        ]
+        event.preventDefault().addResponse(response)
+
+    @access.user(scope=TokenScope.DATA_WRITE)
+    @validate_event(level=AccessType.WRITE)
+    def create_item(self, event, path, root, user=None):
+        params = event.info["params"]
+        new_path = path / params["name"]
+        try:
+            path.mkdir(parents=True, exist_ok=True)
+            with new_path.open(mode="x"):
+                os.utime(new_path.as_posix())
+        except FileExistsError:
+            raise ValidationException(
+                "An item with that name already exists here.", "name"
+            )
+        event.preventDefault().addResponse(
+            Item().filter(self.vItem(new_path, root), user=user)
+        )
+
+    @access.public(scope=TokenScope.DATA_READ)
+    @validate_event(level=AccessType.READ)
+    def get_item_info(self, event, path, root, user=None):
+        event.preventDefault().addResponse(
+            Item().filter(self.vItem(path, root), user=user)
+        )
+
+    @access.user(scope=TokenScope.DATA_WRITE)
+    @validate_event(level=AccessType.WRITE)
+    def rename_item(self, event, path, root, user=None):
+        self.is_file(path, root["_id"])
+        source = self.vItem(path, root)
+
+        params = event.info.get("params", {})
+        name = params.get("name", path.name)
+        parentId = params.get("folderId", source["folderId"])
+
+        if parentId == source["folderId"]:
+            new_path = path.with_name(name)
+            bail_if_exists(new_path)
+            path.rename(new_path)
+        else:
+            dst_path, dst_root_id = self.path_from_id(parentId)
+            if not dst_path:
+                raise GirderException("Folder {} is not a mapping.".format(parentId))
+            # Check whether the user can write to the destination
+            Folder().load(dst_root_id, user=user, level=AccessType.WRITE, exc=True)
+            self.is_dir(dst_path, dst_root_id)
+            new_path = dst_path / name
+            bail_if_exists(new_path)
+            shutil.move(path.as_posix(), new_path.as_posix())
+
+        event.preventDefault().addResponse(
+            Item().filter(self.vItem(new_path, root), user=user)
+        )
+
+    @access.user(scope=TokenScope.DATA_WRITE)
+    @validate_event(level=AccessType.WRITE)
+    def remove_item(self, event, path, root, user=None):
+        self.is_file(path, root["_id"])
+        path.unlink()
+        event.preventDefault().addResponse({"message": "Deleted item %s." % path.name})
+
+    @access.user(scope=TokenScope.DATA_WRITE)
+    @validate_event(level=AccessType.WRITE)
+    def copy_item(self, event, path, root, user=None):
+        self.is_file(path, root["_id"])
+        source = self.vItem(path, root)
+        name = event.info["params"].get("name") or path.name
+
+        folder_id = event.info["params"].get("folderId", source["folderId"])
+        if str(folder_id).startswith("wtlocal:"):
+            new_dirname, new_root_id = self.path_from_id(folder_id)
+        else:
+            new_root = Folder().load(folder_id, force=True, exc=True)
+            try:
+                new_dirname = pathlib.Path(new_root["fsPath"])
+            except KeyError:
+                raise GirderException("Folder {} is not a mapping.".format(folder_id))
+            new_root_id = str(new_root["_id"])
+
+        if new_root_id != str(root["_id"]):
+            new_root = Folder().load(
+                new_root["_id"], user=user, level=AccessType.WRITE, exc=True
+            )  # Check ACLs
+        else:
+            new_root = root
+
+        new_path = ensure_unique_path(new_dirname, name)
+        shutil.copy(path.as_posix(), new_path.as_posix())
+        event.preventDefault().addResponse(
+            Item().filter(self.vItem(new_path, new_root), user=user)
+        )
+
+    @access.public(scope=TokenScope.DATA_READ)
+    @validate_event(level=AccessType.READ)
+    def get_child_files(self, event, path, root, user=None):
+        event.preventDefault().addResponse(
+            [File().filter(self.vFile(path, root), user=user)]
+        )
+
+    @access.public(scope=TokenScope.DATA_READ)
+    @validate_event(level=AccessType.READ)
+    def item_root_path(self, event, path, root, user=None):
+        root_path = pathlib.Path(root["fsPath"])
+        response = [
+            {"type": "item", "object": Item().filter(self.vItem(path, root), user=user)}
+        ]
+        path = path.parent
+        while path != root_path:
+            response.append(
+                {
+                    "type": "folder",
+                    "object": Folder().filter(self.vFolder(path, root), user=user),
+                }
+            )
+            path = path.parent
+
+        response.append({"type": "folder", "object": Folder().filter(root, user=user)})
+        girder_rootpath = Folder().parentsToRoot(root, user=user)
+        response += girder_rootpath[::-1]
+        response.pop(0)
+        event.preventDefault().addResponse(response[::-1])
