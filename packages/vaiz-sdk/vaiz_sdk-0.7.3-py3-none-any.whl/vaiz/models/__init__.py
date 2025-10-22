24ac82b82113963c0cf25a98fb631c31336fa3b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-22 04:07:32 +0000

    Add package vaiz-sdk/0.7.3/vaiz_sdk-0.7.3-py3-none-any.whl

---
+++ packages/vaiz-sdk/vaiz_sdk-0.7.3-py3-none-any.whl/vaiz/models/__init__.py
@@ -0,0 +1,107 @@
+from .base import TaskFollower, TaskPriority, CustomField, VaizBaseModel
+from .tasks import Task, TaskResponse, CreateTaskRequest, EditTaskRequest, TaskFile, TaskUploadFile, TaskCustomField, GetHistoryRequest, GetHistoryResponse, HistoryItem, HistoryData, GetHistoryPayload, GetTasksRequest, GetTasksResponse, GetTasksPayload
+from .boards import Board, BoardResponse, BoardsResponse, CustomFieldType, CreateBoardTypeRequest, CreateBoardTypeResponse, EditBoardTypeRequest, EditBoardTypeResponse, CreateBoardGroupRequest, CreateBoardGroupResponse, EditBoardGroupRequest, EditBoardGroupResponse, CreateBoardCustomFieldRequest, CreateBoardCustomFieldResponse, EditBoardCustomFieldRequest, EditBoardCustomFieldResponse
+from .profile import Profile, ProfileResponse
+from .projects import Project, ProjectsResponse, ProjectResponse
+from .milestones import Milestone, MilestonesResponse, CreateMilestoneRequest, CreateMilestoneResponse, GetMilestoneResponse, EditMilestoneRequest, EditMilestoneResponse, ToggleMilestoneRequest, ToggleMilestoneResponse
+from .upload import UploadedFile, UploadFileResponse
+from .documents import GetDocumentRequest, ReplaceDocumentRequest, ReplaceDocumentResponse, Document, GetDocumentsRequest, GetDocumentsResponse, GetDocumentsPayload, CreateDocumentRequest, CreateDocumentResponse, CreateDocumentPayload
+from .comments import Comment, CommentReaction, PostCommentRequest, PostCommentResponse, ReactToCommentRequest, ReactToCommentResponse, GetCommentsRequest, GetCommentsResponse, EditCommentRequest, EditCommentResponse, DeleteCommentRequest, DeleteCommentResponse
+from .enums import CommentReactionType, COMMENT_REACTION_METADATA, EAvatarMode, Kind
+
+__all__ = [
+    # Base models
+    'VaizBaseModel',
+    'TaskFollower',
+    'TaskPriority',
+    'CustomField',
+    
+    # Task models
+    'Task',
+    'TaskResponse',
+    'CreateTaskRequest',
+    'EditTaskRequest',
+    'TaskFile',
+    'TaskUploadFile',
+    'TaskCustomField',
+    'GetHistoryRequest',
+    'GetHistoryResponse',
+    'HistoryItem',
+    'HistoryData',
+    'GetHistoryPayload',
+    'GetTasksRequest',
+    'GetTasksResponse',
+    'GetTasksPayload',
+    
+    # Board models
+    'Board',
+    'BoardResponse',
+    'BoardsResponse',
+    'CustomFieldType',
+    'CreateBoardTypeRequest',
+    'CreateBoardTypeResponse',
+    'EditBoardTypeRequest',
+    'EditBoardTypeResponse',
+    'CreateBoardGroupRequest',
+    'CreateBoardGroupResponse',
+    'EditBoardGroupRequest',
+    'EditBoardGroupResponse',
+    'CreateBoardCustomFieldRequest',
+    'CreateBoardCustomFieldResponse',
+    'EditBoardCustomFieldRequest',
+    'EditBoardCustomFieldResponse',
+    
+    # Profile models
+    'Profile',
+    'ProfileResponse',
+    
+    # Project models
+    'Project',
+    'ProjectsResponse',
+    'ProjectResponse',
+    
+    # Milestone models
+    'Milestone',
+    'MilestonesResponse',
+    'CreateMilestoneRequest',
+    'CreateMilestoneResponse',
+    'GetMilestoneResponse',
+    'EditMilestoneRequest',
+    'EditMilestoneResponse',
+    'ToggleMilestoneRequest',
+    'ToggleMilestoneResponse',
+    
+    # Upload models
+    'UploadedFile',
+    'UploadFileResponse',
+    
+    # Document models
+    'Document',
+    'GetDocumentRequest',
+    'ReplaceDocumentRequest',
+    'ReplaceDocumentResponse',
+    'GetDocumentsRequest',
+    'GetDocumentsResponse',
+    'GetDocumentsPayload',
+    'CreateDocumentRequest',
+    'CreateDocumentResponse',
+    'CreateDocumentPayload',
+    
+    # Comment models
+    'Comment',
+    'CommentReaction',
+    'PostCommentRequest',
+    'PostCommentResponse',
+    'ReactToCommentRequest',
+    'ReactToCommentResponse',
+    'GetCommentsRequest',
+    'GetCommentsResponse',
+    'EditCommentRequest',
+    'EditCommentResponse',
+    'DeleteCommentRequest',
+    'DeleteCommentResponse',
+    'CommentReactionType',
+    'COMMENT_REACTION_METADATA',
+    'EAvatarMode',
+    'Kind',
+] 
