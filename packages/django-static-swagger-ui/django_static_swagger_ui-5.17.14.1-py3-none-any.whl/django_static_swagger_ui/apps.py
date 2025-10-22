7ad593ead2e7652f28d83e24657c3c6c98bcbe47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-22 04:08:05 +0000

    Add package django-static-swagger-ui/5.17.14.1/django_static_swagger_ui-5.17.14.1-py3-none-any.whl

---
+++ packages/django-static-swagger-ui/django_static_swagger_ui-5.17.14.1-py3-none-any.whl/django_static_swagger_ui/apps.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+from __future__ import absolute_import, division, generators, nested_scopes, print_function, unicode_literals, with_statement
+
+from django.apps import AppConfig
+
+
+class DjangoStaticSwaggerUiConfig(AppConfig):
+    name = 'django_static_swagger_ui'
