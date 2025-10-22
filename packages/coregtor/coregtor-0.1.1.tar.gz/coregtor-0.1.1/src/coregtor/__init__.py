128ca8a44b0aa79c15518531b0b2372d0eb6b179
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-22 04:07:22 +0000

    Add package coregtor/0.1.1/coregtor-0.1.1.tar.gz

---
+++ packages/coregtor/coregtor-0.1.1.tar.gz/coregtor-0.1.1/src/coregtor/__init__.py
@@ -0,0 +1,5 @@
+from coregtor._expression import read,create_model_input
+from coregtor._forest import create_model,tree_paths
+import coregtor.figure as figure
+
+__all__ = ["read","create_model_input","create_model","tree_paths","figure"]
