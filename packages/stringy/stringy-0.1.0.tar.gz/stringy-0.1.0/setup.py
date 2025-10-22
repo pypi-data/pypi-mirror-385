1af508995ebc1a003a9ed4f7b01663277abc7b92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-22 07:08:52 +0000

    Add package stringy/0.1.0/stringy-0.1.0.tar.gz

---
+++ packages/stringy/stringy-0.1.0.tar.gz/stringy-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='stringy',
+    version='0.1.0',
+    description='Basic manipulation for text',
+    author='EDCode',
+    author_email='ed.code.tech@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent'
+    ],
+    python_requires='>=3.6'
+)
