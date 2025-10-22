937ddb9fb863d4977c050a7f4bcfae5578d00de9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-22 04:07:42 +0000

    Add package reyserver/1.1.78/reyserver-1.1.78-py3-none-any.whl

---
+++ packages/reyserver/reyserver-1.1.78-py3-none-any.whl/reyserver/__init__.py
@@ -0,0 +1,24 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@Time    : 2023-02-19
+@Author  : Rey
+@Contact : reyxbo@163.com
+@Explain : Backend server method set.
+
+Modules
+-------
+rall : All methods.
+rauth : Authentication methods.
+rbase : Base methods.
+rclient : Client methods.
+rfile : File methods.
+rpublic : Public methods.
+rredirect : Redirect methods.
+rserver : Server methods.
+rtest : Test methods.
+"""
+
+
+from .rserver import Server
---
+++ packages/reyserver/reyserver-1.1.78-py3-none-any.whl/reyserver/rall.py
@@ -0,0 +1,19 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@Time    : 2024-01-11
+@Author  : Rey
+@Contact : reyxbo@163.com
+@Explain : All methods.
+"""
+
+
+from .rauth import *
+from .rbase import *
+from .rclient import *
+from .rfile import *
+from .rpublic import *
+from .rredirect import *
+from .rserver import *
+from .rtest import *
