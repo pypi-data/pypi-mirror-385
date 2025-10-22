a11dd5f1b542c6ad5d6875c6a094e022a15cdffd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-22 03:25:29 +0000

    Add package efel/5.7.20/efel-5.7.20-cp310-cp310-macosx_10_9_x86_64.whl

---
+++ packages/efel/efel-5.7.20-cp310-cp310-macosx_10_9_x86_64.whl/efel/cppcore/DependencyTree.h
@@ -0,0 +1,54 @@
+/* Copyright (c) 2015, EPFL/Blue Brain Project                                   
+ *                                                                               
+ * This file is part of eFEL <https://github.com/BlueBrain/eFEL>                 
+ *                                                                               
+ * This library is free software; you can redistribute it and/or modify it under 
+ * the terms of the GNU Lesser General Public License version 3.0 as published   
+ * by the Free Software Foundation.                                              
+ *                                                                               
+ * This library is distributed in the hope that it will be useful, but WITHOUT   
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more 
+ * details.                                                                      
+ *                                                                               
+ * You should have received a copy of the GNU Lesser General Public License      
+ * along with this library; if not, write to the Free Software Foundation, Inc., 
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.                   
+ */        
+
+#ifndef __DEPENDENCYTREE_H
+#define __DEPENDENCYTREE_H
+
+#include "types.h"
+
+#include <list>
+#include <map>
+#include <string>
+#include <utility>
+#include <vector>
+
+using std::list;
+using std::map;
+using std::string;
+using std::vector;
+
+class cTree {
+  vector<string> strDependencyFile;
+  vector<string> vecFeature;
+
+ public:
+  string ErrorStr;
+  list<string> FinalList;
+  list<string> ChildList;
+  cTree() {};
+  cTree(const char *strFileName);
+  int setFeaturePointers(map<string, feature2function *> &mapFptrLib,
+                         feature2function *FptrTable,
+                         map<string, vector<feature_function > > *FptrLookup);
+  int getChilds(string strLine, list<string> &childs);
+  int getDependency(const string& strLine);
+  void AddUniqueItem(const string& strFeature);
+  int getAllParents(vector<string> &vecFeature);
+};
+
+#endif
