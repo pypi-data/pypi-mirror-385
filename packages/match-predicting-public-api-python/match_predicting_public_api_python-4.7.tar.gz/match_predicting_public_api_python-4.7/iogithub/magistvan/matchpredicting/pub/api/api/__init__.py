2ecde89e1e49e7d31ee6fb24989c4bc9632f724b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-22 09:07:43 +0000

    Add package match-predicting-public-api-python/4.7/match_predicting_public_api_python-4.7.tar.gz

---
+++ packages/match-predicting-public-api-python/match_predicting_public_api_python-4.7.tar.gz/match_predicting_public_api_python-4.7/iogithub/magistvan/matchpredicting/pub/api/api/__init__.py
@@ -0,0 +1,8 @@
+# flake8: noqa
+
+# import apis into api package
+from iogithub.magistvan.matchpredicting.pub.api.api.dummy_api import DummyApi
+from iogithub.magistvan.matchpredicting.pub.api.api.import_api import ImportApi
+from iogithub.magistvan.matchpredicting.pub.api.api.testing_api import TestingApi
+from iogithub.magistvan.matchpredicting.pub.api.api.training_api import TrainingApi
+
