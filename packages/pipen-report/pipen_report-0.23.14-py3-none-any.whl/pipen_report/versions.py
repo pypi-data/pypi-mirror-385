af5c943db630c79a0ad1d92d7ae3b3166adc216e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-22 07:07:17 +0000

    Add package pipen-report/0.23.14/pipen_report-0.23.14-py3-none-any.whl

---
+++ packages/pipen-report/pipen_report-0.23.14-py3-none-any.whl/pipen_report/versions.py
@@ -0,0 +1,15 @@
+"""Provides pipen and pipen-report versions"""
+
+from pipen import __version__ as pipen_version
+
+# pipen-report version
+__version__ = "0.23.14"
+
+version_str = f"""
+    <a href="https://github.com/pwwang/pipen" target="_blank">
+        pipen v{pipen_version}
+    </a> and
+    <a href="https://github.com/pwwang/pipen-report" target="_blank">
+        pipen-report v{__version__}
+    </a>
+"""
