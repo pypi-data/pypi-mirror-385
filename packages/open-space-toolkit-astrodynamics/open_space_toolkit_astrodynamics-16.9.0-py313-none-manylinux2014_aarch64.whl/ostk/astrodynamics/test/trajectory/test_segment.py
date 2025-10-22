9a333ef30329df81219435013f39d61cc68f3e36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-22 05:06:49 +0000

    Add package open-space-toolkit-astrodynamics/16.9.0/open_space_toolkit_astrodynamics-16.9.0-py313-none-manylinux2014_aarch64.whl

---
+++ packages/open-space-toolkit-astrodynamics/open_space_toolkit_astrodynamics-16.9.0-py313-none-manylinux2014_aarch64.whl/ostk/astrodynamics/test/trajectory/test_segment.py
@@ -0,0 +1,507 @@
+# Apache License 2.0
+
+import pytest
+import numpy as np
+
+from ostk.physics import Environment
+from ostk.physics.time import Instant
+from ostk.physics.time import DateTime
+from ostk.physics.time import Scale
+from ostk.physics.time import Duration
+from ostk.physics.coordinate import Frame
+from ostk.physics.environment.object.celestial import Earth
+from ostk.physics.unit import Angle
+
+from ostk.astrodynamics.flight.system import SatelliteSystem
+from ostk.astrodynamics import Dynamics
+from ostk.astrodynamics.dynamics import CentralBodyGravity
+from ostk.astrodynamics.dynamics import PositionDerivative
+from ostk.astrodynamics.dynamics import Thruster
+from ostk.astrodynamics.guidance_law import ConstantThrust
+from ostk.astrodynamics.trajectory import LocalOrbitalFrameFactory
+from ostk.astrodynamics.trajectory import State
+from ostk.astrodynamics.trajectory import Segment
+from ostk.astrodynamics.event_condition import InstantCondition
+from ostk.astrodynamics.trajectory.state import CoordinateSubset
+from ostk.astrodynamics.trajectory.state import CoordinateBroker
+from ostk.astrodynamics.trajectory.state.coordinate_subset import CartesianPosition
+from ostk.astrodynamics.trajectory.state.coordinate_subset import CartesianVelocity
+from ostk.astrodynamics.trajectory.state import NumericalSolver
+
+
+@pytest.fixture
+def environment() -> Environment:
+    return Environment.default()
+
+
+@pytest.fixture
+def state() -> State:
+    instant: Instant = Instant.date_time(DateTime(2018, 1, 1, 0, 0, 0), Scale.UTC)
+    coordinates: list[float] = [
+        7500000.0,
+        0.0,
+        0.0,
+        0.0,
+        5335.865450622126,
+        5335.865450622126,
+        300.0,
+    ]
+    frame: Frame = Frame.GCRF()
+    coordinate_broker: CoordinateBroker = CoordinateBroker(
+        [
+            CartesianPosition.default(),
+            CartesianVelocity.default(),
+            CoordinateSubset.mass(),
+        ]
+    )
+    return State(instant, coordinates, frame, coordinate_broker)
+
+
+@pytest.fixture
+def central_body_gravity() -> CentralBodyGravity:
+    return CentralBodyGravity(Earth.spherical())
+
+
+@pytest.fixture
+def position_derivative() -> PositionDerivative:
+    return PositionDerivative()
+
+
+@pytest.fixture
+def dynamics(
+    position_derivative: PositionDerivative, central_body_gravity: CentralBodyGravity
+) -> list:
+    return [
+        position_derivative,
+        central_body_gravity,
+    ]
+
+
+@pytest.fixture
+def local_orbital_frame_factory() -> LocalOrbitalFrameFactory:
+    return LocalOrbitalFrameFactory.VNC(Frame.GCRF())
+
+
+@pytest.fixture
+def numerical_solver() -> NumericalSolver:
+    return NumericalSolver.default_conditional()
+
+
+@pytest.fixture
+def end_instant(state: State) -> Instant:
+    return state.get_instant() + Duration.minutes(15.0)
+
+
+@pytest.fixture
+def instant_condition(end_instant: Instant) -> InstantCondition:
+    return InstantCondition(InstantCondition.Criterion.AnyCrossing, end_instant)
+
+
+@pytest.fixture
+def name() -> str:
+    return "A Segment"
+
+
+@pytest.fixture
+def coast_duration_segment(
+    name: str,
+    instant_condition: InstantCondition,
+    dynamics: list,
+    numerical_solver: NumericalSolver,
+) -> Segment:
+    return Segment.coast(name, instant_condition, dynamics, numerical_solver)
+
+
+@pytest.fixture
+def maneuver_segment(
+    name: str,
+    instant_condition: InstantCondition,
+    dynamics: list,
+    numerical_solver: NumericalSolver,
+    thruster_dynamics: Thruster,
+) -> Segment:
+    return Segment.maneuver(
+        name=name,
+        event_condition=instant_condition,
+        thruster_dynamics=thruster_dynamics,
+        dynamics=dynamics,
+        numerical_solver=numerical_solver,
+    )
+
+
+@pytest.fixture
+def maximum_allowed_angular_offset() -> Angle:
+    return Angle.degrees(180.0)
+
+
+@pytest.fixture
+def constant_local_orbital_frame_direction_maneuver_segment(
+    name: str,
+    instant_condition: InstantCondition,
+    thruster_dynamics: Thruster,
+    dynamics: list,
+    numerical_solver: NumericalSolver,
+    local_orbital_frame_factory: LocalOrbitalFrameFactory,
+) -> Segment:
+    return Segment.constant_local_orbital_frame_direction_maneuver(
+        name=name,
+        event_condition=instant_condition,
+        thruster_dynamics=thruster_dynamics,
+        dynamics=dynamics,
+        numerical_solver=numerical_solver,
+        local_orbital_frame_factory=local_orbital_frame_factory,
+    )
+
+
+@pytest.fixture
+def constant_local_orbital_frame_direction_maneuver_segment_with_maximum_allowed_angular_offset(
+    name: str,
+    instant_condition: InstantCondition,
+    thruster_dynamics: Thruster,
+    dynamics: list,
+    numerical_solver: NumericalSolver,
+    local_orbital_frame_factory: LocalOrbitalFrameFactory,
+    maximum_allowed_angular_offset: Angle,
+) -> Segment:
+    return Segment.constant_local_orbital_frame_direction_maneuver(
+        name=name,
+        event_condition=instant_condition,
+        thruster_dynamics=thruster_dynamics,
+        dynamics=dynamics,
+        numerical_solver=numerical_solver,
+        local_orbital_frame_factory=local_orbital_frame_factory,
+        maximum_allowed_angular_offset=maximum_allowed_angular_offset,
+    )
+
+
+@pytest.fixture
+def instants(state: State) -> list[Instant]:
+    return [state.get_instant(), state.get_instant() + Duration.minutes(1.0)]
+
+
+@pytest.fixture
+def thruster_dynamics() -> Thruster:
+    return Thruster(
+        satellite_system=SatelliteSystem.default(),
+        guidance_law=ConstantThrust.intrack(),
+    )
+
+
+@pytest.fixture
+def segment_solution(dynamics: list[Dynamics], state: State) -> Segment.Solution:
+    return Segment.Solution(
+        name="A Segment",
+        dynamics=dynamics,
+        states=[
+            state,
+        ],
+        condition_is_satisfied=True,
+        segment_type=Segment.Type.Coast,
+    )
+
+
+class TestSegmentSolution:
+    def test_constructors(
+        self,
+        segment_solution: Segment.Solution,
+    ):
+        assert segment_solution is not None
+        assert segment_solution.name is not None
+        assert segment_solution.dynamics is not None
+        assert segment_solution.states is not None
+        assert segment_solution.condition_is_satisfied is not None
+        assert segment_solution.segment_type is not None
+
+    def test_getters_and_accessors(
+        self,
+        segment_solution: Segment.Solution,
+    ):
+        assert segment_solution.access_start_instant() is not None
+        assert segment_solution.access_end_instant() is not None
+        assert segment_solution.get_interval() is not None
+        assert segment_solution.get_initial_mass() is not None
+        assert segment_solution.get_final_mass() is not None
+        assert segment_solution.get_propagation_duration() is not None
+
+    def test_compute_delta_v(
+        self,
+        segment_solution: Segment.Solution,
+    ):
+        assert segment_solution.compute_delta_v(1500.0) is not None
+
+    def test_compute_delta_mass(
+        self,
+        segment_solution: Segment.Solution,
+    ):
+        assert segment_solution.compute_delta_mass() is not None
+
+    def test_extract_maneuvers(
+        self,
+        segment_solution: Segment.Solution,
+    ):
+        assert segment_solution.extract_maneuvers(Frame.GCRF()) is not None
+
+    def test_calculate_states_at(
+        self,
+        segment_solution: Segment.Solution,
+    ):
+        assert (
+            segment_solution.calculate_states_at(
+                [
+                    segment_solution.states[0].get_instant(),
+                ],
+                NumericalSolver.default_conditional(),
+            )
+            is not None
+        )
+
+    def get_dynamics_contribution(
+        self,
+        segment_solution: Segment.Solution,
+    ):
+        assert (
+            segment_solution.get_dynamics_contribution(
+                segment_solution.dynamics[0], Frame.GCRF()
+            )
+            is not None
+        )
+
+    def get_dynamics_acceleration_contribution(
+        self,
+        segment_solution: Segment.Solution,
+    ):
+        assert (
+            segment_solution.get_dynamics_acceleration_contribution(
+                segment_solution.dynamics[0], Frame.GCRF()
+            )
+            is not None
+        )
+
+    def get_all_dynamics_contributions(
+        self,
+        segment_solution: Segment.Solution,
+    ):
+        assert segment_solution.get_all_dynamics_contributions(Frame.GCRF()) is not None
+
+
+class TestSegment:
+    def test_get_name(self, coast_duration_segment: Segment, name: str):
+        assert coast_duration_segment.get_name() == name
+
+    def test_get_event_condition(
+        self,
+        instant_condition: InstantCondition,
+        coast_duration_segment: Segment,
+    ):
+        assert coast_duration_segment.get_event_condition() == instant_condition
+
+    def test_get_dynamics(
+        self,
+        dynamics: list,
+        coast_duration_segment: Segment,
+    ):
+        assert len(coast_duration_segment.get_dynamics()) == len(dynamics)
+
+    def test_get_numerical_solver(
+        self,
+        numerical_solver: NumericalSolver,
+        coast_duration_segment: Segment,
+    ):
+        assert coast_duration_segment.get_numerical_solver() == numerical_solver
+
+    def test_get_type(
+        self,
+        coast_duration_segment: Segment,
+    ):
+        assert coast_duration_segment.get_type() == Segment.Type.Coast
+
+    def test_coast(
+        self,
+        name: str,
+        instant_condition: InstantCondition,
+        dynamics: list,
+        numerical_solver: NumericalSolver,
+    ):
+        assert (
+            Segment.coast(name, instant_condition, dynamics, numerical_solver) is not None
+        )
+
+    def test_maneuver(
+        self,
+        name: str,
+        instant_condition: InstantCondition,
+        thruster_dynamics: ConstantThrust,
+        dynamics: list,
+        numerical_solver: NumericalSolver,
+    ):
+        assert (
+            Segment.maneuver(
+                name=name,
+                event_condition=instant_condition,
+                thruster_dynamics=thruster_dynamics,
+                dynamics=dynamics,
+                numerical_solver=numerical_solver,
+            )
+            is not None
+        )
+
+    def test_constant_local_orbital_frame_direction_maneuver(
+        self,
+        name: str,
+        instant_condition: InstantCondition,
+        thruster_dynamics: ConstantThrust,
+        dynamics: list,
+        numerical_solver: NumericalSolver,
+        local_orbital_frame_factory: LocalOrbitalFrameFactory,
+        maximum_allowed_angular_offset: Angle,
+    ):
+        assert (
+            Segment.constant_local_orbital_frame_direction_maneuver(
+                name=name,
+                event_condition=instant_condition,
+                thruster_dynamics=thruster_dynamics,
+                dynamics=dynamics,
+                numerical_solver=numerical_solver,
+                local_orbital_frame_factory=local_orbital_frame_factory,
+            )
+            is not None
+        )
+
+        assert (
+            Segment.constant_local_orbital_frame_direction_maneuver(
+                name=name,
+                event_condition=instant_condition,
+                thruster_dynamics=thruster_dynamics,
+                dynamics=dynamics,
+                numerical_solver=numerical_solver,
+                local_orbital_frame_factory=local_orbital_frame_factory,
+                maximum_allowed_angular_offset=maximum_allowed_angular_offset,
+            )
+            is not None
+        )
+
+    def test_create_solution(
+        self,
+        dynamics: list,
+        state: State,
+    ):
+        segment_solution: Segment.Solution = Segment.Solution(
+            name="A Segment",
+            dynamics=dynamics,
+            states=[
+                state,
+            ],
+            condition_is_satisfied=True,
+            segment_type=Segment.Type.Coast,
+        )
+
+        assert segment_solution is not None
+
+    def test_solve_maneuver_segment(
+        self,
+        state: State,
+        end_instant: Instant,
+        maneuver_segment: Segment,
+        instants: list[Instant],
+        numerical_solver: NumericalSolver,
+    ):
+        solution: Segment.Solution = maneuver_segment.solve(state)
+
+        assert (
+            pytest.approx(
+                float((solution.states[-1].get_instant() - end_instant).in_seconds()),
+                abs=1e-7,
+            )
+            == 0.0
+        )
+
+        assert solution.name is not None
+        assert solution.dynamics is not None
+        assert len(solution.states) > 0
+        assert solution.condition_is_satisfied is True
+        assert solution.segment_type is not None
+
+        assert solution.access_start_instant() is not None
+        assert solution.access_end_instant() is not None
+
+        assert solution.get_initial_mass() is not None
+        assert solution.get_final_mass() is not None
+        assert solution.get_propagation_duration() is not None
+
+        assert solution.compute_delta_v(1500.0) is not None
+        assert solution.compute_delta_mass() is not None
+
+        assert solution.extract_maneuvers(state.get_frame()) is not None
+        assert len(solution.extract_maneuvers(state.get_frame())) == 1
+
+        propagated_states = solution.calculate_states_at(
+            instants,
+            numerical_solver,
+        )
+
+        assert propagated_states is not None
+        assert len(propagated_states) == len(instants)
+
+        first_dynamics_contribution = solution.get_dynamics_contribution(
+            solution.dynamics[0], state.get_frame()
+        )
+
+        second_dynamics_contribution = solution.get_dynamics_contribution(
+            solution.dynamics[1], state.get_frame()
+        )
+
+        third_dynamics_contribution = solution.get_dynamics_contribution(
+            solution.dynamics[2], state.get_frame()
+        )
+
+        assert first_dynamics_contribution is not None
+        assert second_dynamics_contribution is not None
+        assert third_dynamics_contribution is not None
+        assert first_dynamics_contribution.shape == (len(solution.states), 3)
+        assert second_dynamics_contribution.shape == (len(solution.states), 3)
+        assert third_dynamics_contribution.shape == (len(solution.states), 4)
+
+        all_contributions = solution.get_all_dynamics_contributions(state.get_frame())
+        assert len(all_contributions) == len(solution.dynamics)
+
+        assert all_contributions is not None
+        assert isinstance(all_contributions, dict)
+        assert np.array_equal(
+            all_contributions[solution.dynamics[0]], first_dynamics_contribution
+        )
+        assert np.array_equal(
+            all_contributions[solution.dynamics[1]], second_dynamics_contribution
+        )
+        assert np.array_equal(
+            all_contributions[solution.dynamics[2]], third_dynamics_contribution
+        )
+
+        acceleration_contribution = solution.get_dynamics_acceleration_contribution(
+            solution.dynamics[1], state.get_frame()
+        )
+        assert acceleration_contribution is not None
+        assert acceleration_contribution.shape == (len(solution.states), 3)
+        assert isinstance(acceleration_contribution, np.ndarray)
+
+        with pytest.raises(Exception):
+            solution.get_dynamics_acceleration_contribution(
+                solution.dynamics[0], state.get_frame()
+            )
+
+    def test_solve_constant_local_orbital_frame_direction_maneuver_segment(
+        self,
+        state: State,
+        constant_local_orbital_frame_direction_maneuver_segment: Segment,
+        constant_local_orbital_frame_direction_maneuver_segment_with_maximum_allowed_angular_offset: Segment,
+    ):
+        solution: Segment.Solution = (
+            constant_local_orbital_frame_direction_maneuver_segment.solve(state)
+        )
+        assert solution is not None
+
+        solution_with_maximum_allowed_angular_offset: Segment.Solution = (
+            constant_local_orbital_frame_direction_maneuver_segment_with_maximum_allowed_angular_offset.solve(
+                state
+            )
+        )
+        assert solution_with_maximum_allowed_angular_offset is not None
---
+++ packages/open-space-toolkit-astrodynamics/open_space_toolkit_astrodynamics-16.9.0-py313-none-manylinux2014_aarch64.whl/ostk/astrodynamics/utilities.py
@@ -0,0 +1,248 @@
+# Apache License 2.0
+
+from __future__ import annotations
+
+from datetime import datetime
+from datetime import timezone
+
+from ostk.physics import Environment
+from ostk.physics.time import Scale
+from ostk.physics.time import Instant
+from ostk.physics.time import Duration
+from ostk.physics.time import Interval
+from ostk.physics.unit import Length
+from ostk.physics.coordinate.spherical import LLA
+from ostk.physics.coordinate.spherical import AER
+from ostk.physics.coordinate import Position
+from ostk.physics.coordinate import Frame
+from ostk.physics.environment.object.celestial import Earth
+from ostk.physics.environment.gravitational import Earth as EarthGravitationalModel
+
+from ostk.astrodynamics import Trajectory
+from ostk.astrodynamics.trajectory import State
+
+
+def lla_from_state(state: State) -> LLA:
+    """
+    Return latitude (degrees), longitude (degrees), altitude (meters) float list from a state.
+
+    Args:
+        state (State): A state.
+
+    Returns:
+        LLA: The LLA.
+    """
+
+    return lla_from_position(state.get_position(), state.get_instant())
+
+
+def lla_from_position(
+    position: Position,
+    instant: Instant | None = None,
+) -> LLA:
+    """
+    Return LLA from position and instant.
+
+    Args:
+        position (Position): A position.
+        instant (Instant): An instant.
+
+    Returns:
+        LLA: The LLA.
+    """
+
+    if position.access_frame() != Frame.ITRF():
+        if instant is None:
+            raise ValueError(
+                "Instant must be provided if position is not expressed in ECEF."
+            )
+        position: Position = position.in_frame(Frame.ITRF(), instant)
+
+    return LLA.cartesian(
+        position.get_coordinates(),
+        EarthGravitationalModel.EGM2008.equatorial_radius,
+        EarthGravitationalModel.EGM2008.flattening,
+    )
+
+
+def position_from_lla(lla: LLA) -> Position:
+    """
+    Return position from lla.
+
+    Args:
+        lla (LLA): A LLA.
+
+    Returns:
+        Position: The position.
+    """
+
+    return Position.meters(
+        lla.to_cartesian(
+            EarthGravitationalModel.EGM2008.equatorial_radius,
+            EarthGravitationalModel.EGM2008.flattening,
+        ),
+        Frame.ITRF(),
+    )
+
+
+def compute_aer(
+    instant: Instant,
+    from_position: Position,
+    to_position: Position,
+    environment: Environment,
+) -> AER:
+    """
+    Return AER from Instant and Positions (observer, target).
+
+    Args:
+        instant (Instant): An instant.
+        from_position (Position): An observer position.
+        to_position (Position): A target position.
+        environment (Environment): An environment.
+
+    Returns:
+        AER: The AER.
+    """
+
+    from_lla: LLA = lla_from_position(from_position, instant)
+
+    earth: Earth = environment.access_celestial_object_with_name("Earth")
+    ned_frame: Frame = earth.get_frame_at(from_lla, Earth.FrameType.NED)
+
+    from_position_NED: Position = from_position.in_frame(ned_frame, instant)
+    to_position_NED: Position = to_position.in_frame(ned_frame, instant)
+
+    return AER.from_position_to_position(from_position_NED, to_position_NED, True)
+
+
+def compute_time_lla_aer_coordinates(
+    state: State,
+    from_position: Position,
+    environment: Environment,
+) -> tuple[datetime, float, float, float, float, float, float]:
+    """
+    Return [datetime, latitude, longitude, altitude, azimuth, elevation, range] from State and observer Position.
+
+    Args:
+        state (State): A state.
+        from_position (Position): An observer position.
+        environment (Environment): An environment.
+
+    Returns:
+        tuple[datetime, float, float, float, float, float, float]: The [datetime, latitude, longitude, altitude, azimuth, elevation, range].
+    """
+
+    instant: Instant = state.get_instant()
+
+    lla: LLA = lla_from_state(state)
+
+    aer: AER = compute_aer(
+        instant,
+        from_position,
+        state.get_position().in_frame(Frame.ITRF(), state.get_instant()),
+        environment,
+    )
+
+    return (
+        instant.get_date_time(Scale.UTC).replace(tzinfo=timezone.utc),
+        float(lla.get_latitude().in_degrees()),
+        float(lla.get_longitude().in_degrees()),
+        float(lla.get_altitude().in_meters()),
+        float(aer.get_azimuth().in_degrees()),
+        float(aer.get_elevation().in_degrees()),
+        float(aer.get_range().in_meters()),
+    )
+
+
+def compute_trajectory_geometry(
+    trajectory: Trajectory,
+    interval: Interval,
+    step: Duration = Duration.minutes(1.0),
+) -> list[LLA]:
+    """
+    Return list of LLA along a Trajectory during the Interval at the provided step.
+
+    Args:
+        trajectory (Trajectory): A trajectory.
+        interval (Interval): An interval.
+        step (Duration): A step. Defaults to 1.0 minutes.
+
+    Returns:
+        list[LLA]: The list of LLA.
+    """
+
+    return [
+        lla_from_state(state)
+        for state in trajectory.get_states_at(interval.generate_grid(step))
+    ]
+
+
+def compute_ground_track(
+    trajectory: Trajectory,
+    interval: Interval,
+    step: Duration = Duration.minutes(1.0),
+) -> list[LLA]:
+    """
+    Return list of LLA along a Trajectory ground track (altitude set to 10.0 meters) during the Interval at the provided step.
+
+    Args:
+        trajectory (Trajectory): A trajectory.
+        interval (Interval): An interval.
+        step (Duration): A step. Defaults to 1.0 minutes.
+
+    Returns:
+        list[LLA]: The list of LLA.
+    """
+
+    ground_llas: list[LLA] = []
+    for state in trajectory.get_states_at(interval.generate_grid(step)):
+        lla: LLA = lla_from_state(state)
+
+        ground_lla: LLA = LLA(
+            lla.get_latitude(),
+            lla.get_longitude(),
+            Length.meters(10.0),
+        )
+
+        ground_llas.append(ground_lla)
+
+    return ground_llas
+
+
+def convert_state(
+    state: State,
+) -> tuple[str, float, float, float, float, float, float, float, float, float]:
+    """
+    Convert a State into dataframe-ready values.
+    - Instant (str)
+    - Modified Julian Date (float)
+    - Position X [meters] (float)
+    - Position Y [meters] (float)
+    - Position Z [meters] (float)
+    - Velocity X [meters/second] (float)
+    - Velocity Y [meters/second] (float)
+    - Velocity Z [meters/second] (float)
+    - Latitude [degrees] (float)
+    - Longitude [degrees] (float)
+    - Altitude [meters] (float)
+
+    Args:
+        state (State): A state.
+
+    Returns:
+        tuple[str, float, float, float, float, float, float, float, float, float]: The dataframe-ready values.
+    """
+
+    lla: LLA = lla_from_state(state)
+
+    instant: Instant = state.get_instant()
+
+    return (
+        str(instant.to_string()),
+        float(instant.get_modified_julian_date(Scale.UTC)),
+        *state.get_position().get_coordinates().transpose().tolist(),
+        *state.get_velocity().get_coordinates().transpose().tolist(),
+        float(lla.get_latitude().in_degrees()),
+        float(lla.get_longitude().in_degrees()),
+        float(lla.get_altitude().in_meters()),
+    )
