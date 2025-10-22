8d0581ec49b84d8bb0f5cbe9b2496d2c3d5ef187
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-22 06:06:21 +0000

    Add package letta-evals/0.4.0/letta_evals-0.4.0.tar.gz

---
+++ packages/letta-evals/letta_evals-0.4.0.tar.gz/letta_evals-0.4.0/letta_evals/__init__.py
@@ -0,0 +1,27 @@
+"""Letta Evals Kit - Evaluation framework for Letta AI agents."""
+
+from importlib.metadata import PackageNotFoundError
+from importlib.metadata import version as _pkg_version
+
+from letta_evals.graders.rubric import RubricGrader
+from letta_evals.graders.tool import ToolGrader
+from letta_evals.models import GateSpec, RunnerResult, Sample, SuiteSpec
+from letta_evals.runner import Runner, run_suite
+from letta_evals.targets.agent import AgentTarget
+
+try:
+    __version__: str = _pkg_version("letta-evals")
+except PackageNotFoundError:
+    __version__ = "0.4.0"
+
+__all__ = [
+    "Sample",
+    "SuiteSpec",
+    "RunnerResult",
+    "GateSpec",
+    "Runner",
+    "run_suite",
+    "AgentTarget",
+    "ToolGrader",
+    "RubricGrader",
+]
