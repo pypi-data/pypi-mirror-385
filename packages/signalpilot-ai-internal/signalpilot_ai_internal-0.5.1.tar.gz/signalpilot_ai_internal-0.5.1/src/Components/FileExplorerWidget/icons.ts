d2420b4d6bfb3beb929db5415ff116c8179ff8ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-22 06:06:15 +0000

    Add package signalpilot-ai-internal/0.5.1/signalpilot_ai_internal-0.5.1.tar.gz

---
+++ packages/signalpilot-ai-internal/signalpilot_ai_internal-0.5.1.tar.gz/signalpilot_ai_internal-0.5.1/src/Components/FileExplorerWidget/icons.ts
@@ -0,0 +1,31 @@
+import { LabIcon } from '@jupyterlab/ui-components';
+import reloadIcon from '../../../style/icons/files/reload.svg';
+import arrowDownIcon from '../../../style/icons/files/arrow-down.svg';
+import openLinkIcon from '../../../style/icons/files/open-link.svg';
+import magicIcon from '../../../style/icons/files/magic.svg';
+import folderIcon from '../../../style/icons/files/folder.svg';
+
+export const RELOAD_ICON = new LabIcon({
+  name: 'signalpilot-ai-internal:reload-icon',
+  svgstr: reloadIcon
+});
+
+export const ARROW_DOWN_ICON = new LabIcon({
+  name: 'signalpilot-ai-internal:arrow-down-icon',
+  svgstr: arrowDownIcon
+});
+
+export const OPEN_LINK_ICON = new LabIcon({
+  name: 'signalpilot-ai-internal:open-link-icon',
+  svgstr: openLinkIcon
+});
+
+export const MAGIC_ICON = new LabIcon({
+  name: 'signalpilot-ai-internal:magic-icon',
+  svgstr: magicIcon
+});
+
+export const FOLDER_ICON = new LabIcon({
+  name: 'signalpilot-ai-internal:folder-icon',
+  svgstr: folderIcon
+});
