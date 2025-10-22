a986d662ed3fa25eec60504414701d0c2a8a354b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-22 09:07:13 +0000

    Add package pypearl/0.6.17/pypearl-0.6.17-cp313-cp313-macosx_14_0_arm64.whl

---
+++ packages/pypearl/pypearl-0.6.17-cp313-cp313-macosx_14_0_arm64.whl/pypearl/__init__.py
@@ -0,0 +1,4 @@
+# pypearl/__init__.py
+from ._pypearl import add, ArrayD1, ArrayD2, ArrayI1, ArrayI2, Layer, ReLU, Softmax, CCE, SGD, Model, breed_models, copy_model, TestActivation, ReLU64, Linear64, Sigmoid64, LeakyReLU64, Step64, Softmax64, ReverseReLU64, CCE64, ndarray
+
+__all__ = ["add", "ArrayD1", "ArrayD2", "ArrayI1", "ArrayI2", "Layer", "ReLU", "Softmax", "CCE", "SGD", "Model", "breed_models", "copy_model", "TestActivation", "ReLU64", "Linear64", "Sigmoid64", "LeakyReLU64", "Step64", "Softmax64", "ReverseReLU64", "CCE64", "ndarray"]
