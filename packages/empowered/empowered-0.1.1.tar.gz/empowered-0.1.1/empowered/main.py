47fdb614564f1ae262ce731f882b2d2890a3ce20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-22 11:06:47 +0000

    Add package empowered/0.1.1/empowered-0.1.1.tar.gz

---
+++ packages/empowered/empowered-0.1.1.tar.gz/empowered-0.1.1/empowered/main.py
@@ -0,0 +1,20 @@
+from dotenv import load_dotenv
+from getpass import getpass
+from logger_setup import set_logger, get_logger
+from empowered.models.sql.sql_client import SQLClient
+import os
+import requests
+
+
+def main():
+    set_logger()
+    load_dotenv()
+
+    # Call ACS1 (acs_id=1)
+    resp = requests.get("http://127.0.0.1:8000/years_available/1")
+    print(resp.status_code)
+    print(resp.text)
+
+
+if __name__ == "__main__":
+    main()
