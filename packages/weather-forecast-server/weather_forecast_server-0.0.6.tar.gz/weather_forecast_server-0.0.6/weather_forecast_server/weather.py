5b7a5e74e653f7c068aaf922e382a2e28d425df6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-22 03:24:55 +0000

    Add package weather-forecast-server/0.0.6/weather_forecast_server-0.0.6.tar.gz

---
+++ packages/weather-forecast-server/weather_forecast_server-0.0.6.tar.gz/weather_forecast_server-0.0.6/weather_forecast_server/weather.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+@author:XuMing(xuming624@qq.com)
+@description: 
+"""
+import requests
+from loguru import logger
+
+
+def get_weather(city: str = None) -> str:
+    """
+    Get weather forecast information for a specified city using wttr.in service.
+
+    Parameters:
+        city: str, city name, e.g., "Beijing", "New York", "Tokyo", "武汉"
+        If None, it will return the weather for the current city.
+    Returns:
+        str: weather forecast information in Markdown format.
+    """
+    try:
+        endpoint = "https://wttr.in"
+        # Get text format weather data
+        if city:
+            response = requests.get(f"{endpoint}/{city}")
+        else:
+            response = requests.get(endpoint)
+        response.raise_for_status()
+        text_result = response.text
+        logger.debug(f"Weather data for {city}: \n{text_result}")
+        return text_result
+    except Exception as e:
+        msg = f"Error getting weather for {city}: {str(e)}"
+        logger.error(msg)
+        return msg
+
+
+if __name__ == '__main__':
+    c = "武汉"
+    weather_info = get_weather(c)
+    print(weather_info)
+    c = ''
+    weather_info = get_weather(c)
+    print(weather_info)
