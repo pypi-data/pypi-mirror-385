2de7dc745839c6d50504cccaf60f9215eb23b1e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-22 03:25:39 +0000

    Add package StreamDataPanel/0.1.18/streamdatapanel-0.1.18-py3-none-any.whl

---
+++ packages/StreamDataPanel/streamdatapanel-0.1.18-py3-none-any.whl/StreamDataPanel/__init__.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+#coding = utf-8
+import eel
+import os
+import logging
+
+from .configEdit import configLoad
+from .api import *
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+CONFIG = configLoad()
+
+APP_CONFIG = CONFIG['APP_CONFIG']
+
+EEL_CONFIG = CONFIG['EEL_CONFIG']
+
+def thread_target(func, *func_args, **func_kwargs):
+    try:
+        func(*func_args, **func_kwargs)
+    except BaseException as e: 
+        if isinstance(e, (SystemExit, KeyboardInterrupt)):
+            logging.info(f"Thread received shutdown signal ({e.__class__.__name__}).")
+        else:
+            logging.error(f"Error in background thread: {e}", exc_info=True)
+    finally:
+        logging.info(f"Thread finished.")
+
+@eel.expose
+def get_initial_config():
+    logging.info("Initializing...")
+    return APP_CONFIG
+
+def run_app(dev: bool=False):
+    if dev:
+        port = EEL_CONFIG['PORT_DEV']
+        logging.info(f"Running in Development Mode, Port: {port}")
+        eel.init('')
+        eel.start('', mode=None, port=port, host='localhost')
+    else:
+        port = EEL_CONFIG['PORT']
+        size = tuple(EEL_CONFIG['SIZE'])
+        eel.init(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'web'))
+        eel.start('index.html', size=size, mode='default', port=port)
+
+def init_app(dev: bool=False):
+    from threading import Thread
+    app = Thread(target=thread_target, args=(run_app,), kwargs={'dev': dev}, daemon=True)
+    app.start()
+
+def init_simulate():
+    from .apiTest import simulate_all
+    start_api()
+    simulate_all()
+
+def test():
+    from time import sleep
+    from threading import Thread
+    
+    init_app()
+    init_simulate()
+
+    try:
+        logging.info('Running test...')
+        sleep(300)
+    except KeyboardInterrupt:
+        logging.info("Test terminated.")
+
