784922c107adc4c36aec45883a400f0eb0a49b6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-22 07:09:15 +0000

    Add package nominal-api/0.964.0/nominal_api-0.964.0-py3-none-any.whl

---
+++ packages/nominal-api/nominal_api-0.964.0-py3-none-any.whl/nominal_api/module_internal/__init__.py
@@ -0,0 +1,23 @@
+# coding=utf-8
+from .._impl import (
+    module_internal_BatchGetResolvedModuleDefinitionsRequest as BatchGetResolvedModuleDefinitionsRequest,
+    module_internal_BatchGetResolvedModuleDefinitionsResponse as BatchGetResolvedModuleDefinitionsResponse,
+    module_internal_BatchGetUnresolvedModuleDefinitionsRequest as BatchGetUnresolvedModuleDefinitionsRequest,
+    module_internal_BatchGetUnresolvedModuleDefinitionsResponse as BatchGetUnresolvedModuleDefinitionsResponse,
+    module_internal_InternalModuleService as InternalModuleService,
+    module_internal_ModuleApplicationReference as ModuleApplicationReference,
+    module_internal_ModuleComputeDefinition as ModuleComputeDefinition,
+    module_internal_ResolvedModuleVersionDefinition as ResolvedModuleVersionDefinition,
+)
+
+__all__ = [
+    'BatchGetResolvedModuleDefinitionsRequest',
+    'BatchGetResolvedModuleDefinitionsResponse',
+    'BatchGetUnresolvedModuleDefinitionsRequest',
+    'BatchGetUnresolvedModuleDefinitionsResponse',
+    'ModuleApplicationReference',
+    'ModuleComputeDefinition',
+    'ResolvedModuleVersionDefinition',
+    'InternalModuleService',
+]
+
