955c24ccac5514cad7c7a662ee06dcee6d48f0ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-22 05:06:52 +0000

    Add package CB2325NumericaG6/0.0.4/cb2325numericag6-0.0.4-py3-none-any.whl

---
+++ packages/CB2325NumericaG6/cb2325numericag6-0.0.4-py3-none-any.whl/CB2325NumericaG6/aproximacao.py
@@ -0,0 +1,6 @@
+
+
+
+def ajuste_linear(x: list[float], y: list[float]) -> list[float]:
+    # TODO: Implement this
+    raise NotImplementedError
---
+++ packages/CB2325NumericaG6/cb2325numericag6-0.0.4-py3-none-any.whl/CB2325NumericaG6/raizes.py
@@ -0,0 +1,5 @@
+from typing import Literal, Callable
+
+def raiz(f: Callable, a, b, tol: float, method: Literal["secante", "bissecao", "newton-raphson"]):
+    # TODO: Implement this
+    raise NotImplementedError
