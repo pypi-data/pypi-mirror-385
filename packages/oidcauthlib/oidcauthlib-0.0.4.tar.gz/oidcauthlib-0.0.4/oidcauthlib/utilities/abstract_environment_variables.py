dd55666f26601bd94d37051a0a72b6303e9a3942
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-22 11:07:39 +0000

    Add package oidcauthlib/0.0.4/oidcauthlib-0.0.4.tar.gz

---
+++ packages/oidcauthlib/oidcauthlib-0.0.4.tar.gz/oidcauthlib-0.0.4/oidcauthlib/utilities/abstract_environment_variables.py
@@ -0,0 +1,28 @@
+from typing import Optional
+from abc import ABC, abstractmethod
+
+
+class AbstractEnvironmentVariables(ABC):
+    @staticmethod
+    def str2bool(v: str | None) -> bool:
+        return v is not None and str(v).lower() in ("yes", "true", "t", "1", "y")
+
+    @property
+    @abstractmethod
+    def oauth_cache(self) -> str:
+        pass
+
+    @property
+    @abstractmethod
+    def mongo_db_auth_cache_collection_name(self) -> Optional[str]:
+        pass
+
+    @property
+    @abstractmethod
+    def mongo_db_cache_disable_delete(self) -> Optional[bool]:
+        pass
+
+    @property
+    @abstractmethod
+    def auth_providers(self) -> Optional[list[str]]:
+        pass
