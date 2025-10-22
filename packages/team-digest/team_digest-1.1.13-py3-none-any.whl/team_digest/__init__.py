f5361fd5a4dcc8852d22718e3596b031693dfba5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-22 15:07:58 +0000

    Add package team-digest/1.1.13/team_digest-1.1.13-py3-none-any.whl

---
+++ packages/team-digest/team_digest-1.1.13-py3-none-any.whl/team_digest/__init__.py
@@ -0,0 +1,10 @@
+﻿# src/team_digest/__init__.py
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except Exception:
+    from importlib_metadata import version, PackageNotFoundError  # for Py<3.8 backport if needed
+
+try:
+    __version__ = version("team-digest")  # must match your PyPI project name
+except PackageNotFoundError:
+    __version__ = "0+unknown"
