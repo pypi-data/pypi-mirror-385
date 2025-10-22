fa0373103fe3c6d516fdae7ec2b822d6b8d7a43f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-22 15:08:25 +0000

    Add package govee-cloud/20251021235946.dev0/govee_cloud-20251021235946.dev0.tar.gz

---
+++ packages/govee-cloud/govee_cloud-20251021235946.dev0.tar.gz/govee_cloud-20251021235946.dev0/src/util/__init__.py
@@ -0,0 +1,26 @@
+import logging
+from types import SimpleNamespace
+
+from aiohttp import ClientSession, TraceRequestStartParams, TraceRequestEndParams
+
+logger = logging.getLogger("govee-cloud")
+
+
+async def on_request_start(
+    session: ClientSession, context: SimpleNamespace, params: TraceRequestStartParams
+) -> None:
+    logger.info("making %s request to %s", params.method, params.url)
+
+
+async def on_request_end(
+    session: ClientSession, context: SimpleNamespace, params: TraceRequestEndParams
+) -> None:
+    data = await params.response.read()
+    data = data.decode("utf-8") if data else None
+    logger.info(
+        "%s request to %s completed with status %s and data %s",
+        params.method,
+        params.url,
+        params.response.status,
+        data,
+    )
