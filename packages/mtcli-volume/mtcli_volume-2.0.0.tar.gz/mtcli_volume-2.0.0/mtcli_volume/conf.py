0d1bd7a57ab144611bb9d6f5555961c431503362
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-22 05:05:58 +0000

    Add package mtcli-volume/2.0.0/mtcli_volume-2.0.0.tar.gz

---
+++ packages/mtcli-volume/mtcli_volume-2.0.0.tar.gz/mtcli_volume-2.0.0/mtcli_volume/conf.py
@@ -0,0 +1,10 @@
+import os
+
+from mtcli.conf import config
+
+SYMBOL = os.getenv("SYMBOL", config["DEFAULT"].get("symbol", fallback="WIN$N"))
+DIGITOS = int(os.getenv("DIGITOS", config["DEFAULT"].getint("digitos", fallback=0)))
+PERIOD = os.getenv("PERIOD", config["DEFAULT"].get("period", fallback="M1"))
+BARS = int(os.getenv("BARS", config["DEFAULT"].getint("bars", fallback=566)))
+STEP = float(os.getenv("STEP", config["DEFAULT"].getfloat("step", fallback=100)))
+VOLUME = os.getenv("VOLUME", config["DEFAULT"].get("volume", fallback="tick"))
