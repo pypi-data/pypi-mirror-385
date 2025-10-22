eebdc8f3aeabb8aac7d4fe557e57ac8e0c99540c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-22 07:07:53 +0000

    Add package databricks-mcp/0.4.0/databricks_mcp-0.4.0-py3-none-any.whl

---
+++ packages/databricks-mcp/databricks_mcp-0.4.0-py3-none-any.whl/databricks_mcp/__init__.py
@@ -0,0 +1,4 @@
+from databricks_mcp.mcp import DatabricksMCPClient
+from databricks_mcp.oauth_provider import DatabricksOAuthClientProvider
+
+__all__ = ["DatabricksOAuthClientProvider", "DatabricksMCPClient"]
