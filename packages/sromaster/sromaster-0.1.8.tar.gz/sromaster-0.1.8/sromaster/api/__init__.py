3df8885d0219f9b7def5d010c82c2824cc00d88b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-22 09:07:43 +0000

    Add package sromaster/0.1.8/sromaster-0.1.8.tar.gz

---
+++ packages/sromaster/sromaster-0.1.8.tar.gz/sromaster-0.1.8/sromaster/api/__init__.py
@@ -0,0 +1,11 @@
+# flake8: noqa
+
+# import apis into api package
+from sromaster.api.customers_api import CustomersApi
+from sromaster.api.im_insurers_api import ImInsurersApi
+from sromaster.api.im_mailing_api import ImMailingApi
+from sromaster.api.im_sros_api import ImSrosApi
+from sromaster.api.parser_api import ParserApi
+from sromaster.api.parser_write_api import ParserWriteApi
+from sromaster.api.users_api import UsersApi
+
