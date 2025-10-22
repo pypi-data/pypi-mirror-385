4fb31ac94a1fc31c47525e28b038f26e57f533e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-22 04:08:09 +0000

    Add package es2/1.1.0rc4/es2-1.1.0rc4-cp310-cp310-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/es2/es2-1.1.0rc4-cp310-cp310-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl/evi/include/EVI/Utils.hpp
@@ -0,0 +1,28 @@
+////////////////////////////////////////////////////////////////////////////////
+//                                                                            //
+// Copyright (C) 2021-2024, CryptoLab Inc. All rights reserved.               //
+//                                                                            //
+// This software and/or source code may be commercially used and/or           //
+// disseminated only with the written permission of CryptoLab Inc,            //
+// or in accordance with the terms and conditions stipulated in the           //
+// agreement/contract under which the software and/or source code has been    //
+// supplied by CryptoLab Inc. Any unauthorized commercial use and/or          //
+// dissemination of this file is strictly prohibited and will constitute      //
+// an infringement of copyright.                                              //
+//                                                                            //
+////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+#include "EVI/Enums.hpp"
+#include "EVI/Query.hpp"
+
+#include <string>
+
+namespace evi {
+
+class EVI_API Utils {
+public:
+    static SealMode stringToSealMode(const std::string &str);
+    static ParameterPreset stringToPreset(const std::string &str);
+};
+} // namespace evi
