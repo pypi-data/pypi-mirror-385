3409d84da59def69fd66b81d0f865796ea2ec09e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-22 09:07:20 +0000

    Add package rubti-beancount-import/1.2/rubti_beancount_import-1.2-py3-none-any.whl

---
+++ packages/rubti-beancount-import/rubti_beancount_import-1.2-py3-none-any.whl/rubti_beancount_import/bbva.py
@@ -0,0 +1,145 @@
+from datetime import datetime
+from decimal import Decimal
+from pathlib import Path
+
+import beangulp
+import pandas as pd
+from beancount.core import amount, data
+from beangulp.testing import main
+
+import rubti_beancount_import.utils as utils
+
+IGNORE_DESCRIPTION = ("Pago con tarjeta", "Otros")
+
+
+class BBVAImporter(beangulp.Importer):
+    """Beancount importer for Excel sheet for checking accounts from Spanish BBVA bank"""
+
+    _expected_header = pd.Index(
+        [
+            "Unnamed: 0",
+            "Fecha",
+            "F.Valor",
+            "Concepto",
+            "Movimiento",
+            "Importe",
+            "Divisa",
+            "Disponible",
+            "Divisa.1",
+            "Observaciones",
+        ],
+        dtype="object",
+    )
+    _new_expected_header = pd.Index(
+        [
+            "Unnamed: 0",
+            "F.Valor",
+            "Fecha",
+            "Concepto",
+            "Movimiento",
+            "Importe",
+            "Divisa",
+            "Disponible",
+            "Divisa.1",
+            "Observaciones",
+        ],
+        dtype="object",
+    )
+    ledger_account: str
+    account_number: str
+    currency: str
+    _excel_header_line: int = 4
+    _acc_map: utils.AccountMapper
+
+    def __init__(
+        self,
+        account: str,
+        account_number: str,
+        account_mapping: str = None,
+        currency: str = "EUR",
+        tags: set = frozenset(),
+    ) -> None:
+        self.ledger_account = account
+        self.account_number = account_number
+        self.currency = currency
+        self._acc_map = utils.AccountMapper(account_mapping)
+        self.tags = tags
+
+    def identify(self, filepath) -> bool:
+        if Path(filepath).suffix.lower() != ".xlsx":
+            return False
+        try:
+            raw_content = pd.read_excel(filepath, header=self._excel_header_line)
+        except:
+            return False
+        c = raw_content.columns
+        return c.equals(self._expected_header) or c.equals(self._new_expected_header)
+
+    def account(self, filepath):
+        return self.ledger_account
+
+    def extract(self, filepath, existing=None):
+        if existing:
+            entries = existing
+        entries = []
+        raw_content = pd.read_excel(filepath, header=self._excel_header_line)
+        for ind, row in raw_content.iterrows():
+            meta = data.new_metadata(filename=filepath, lineno=row.name)
+            units = amount.Amount(
+                Decimal(str(round(row["Importe"], 2))), currency=self.currency
+            )
+            postings = [utils.create_posting(self.ledger_account, units, meta)]
+
+            payee = row["Concepto"]
+            search_key = payee
+
+            if self._acc_map.known(search_key):
+                postings.append(
+                    utils.create_posting(
+                        self._acc_map.account(search_key), -units, None
+                    )
+                )
+
+            if self._acc_map.payee(search_key):
+                payee = self._acc_map.payee(search_key)
+
+            narration = row["Movimiento"]
+            if narration in IGNORE_DESCRIPTION:
+                narration = None
+            if self._acc_map.narration(search_key):
+                narration = self._acc_map.narration(search_key)
+            if narration is None:
+                narration = payee
+                payee = None
+
+            try:
+                date = row["Fecha"].date()
+            except AttributeError:
+                date = datetime.strptime(row["Fecha"], "%d/%m/%Y").date()
+            entries.append(
+                utils.create_transaction(
+                    postings,
+                    date,
+                    meta,
+                    payee,
+                    narration,
+                    tags=self.tags,
+                )
+            )
+        return entries
+
+    def filename(self, filepath):
+        return f"BBVA_{self.account_number}.xlsx"
+
+    def date(self, filepath):
+        return max(map(lambda entry: entry.date, self.extract(filepath)))
+
+
+if __name__ == "__main__":
+    importer = BBVAImporter(
+        "Assets:ES:BBVA:Checking",
+        "ES12345678901234567890",
+        "./tests/test_mapping.yaml",
+        tags={"share-Example"},
+    )
+    main(importer)
---
+++ packages/rubti-beancount-import/rubti_beancount_import-1.2-py3-none-any.whl/rubti_beancount_import/master_card.py
@@ -0,0 +1,128 @@
+import csv
+from datetime import datetime
+
+import beangulp
+from beancount.core import amount, data
+from beangulp import mimetypes
+from beangulp.testing import main
+
+import rubti_beancount_import.utils as utils
+
+DEFAULT_FIELDS = (
+    "Umsatz getätigt von",
+    "Belegdatum",
+    "Buchungsdatum",
+    "Originalbetrag",
+    "Originalwährung",
+    "Umrechnungskurs",
+    "Buchungsbetrag",
+    "Buchungswährung",
+    "Transaktionsbeschreibung",
+    "Transaktionsbeschreibung Zusatz",
+    "Buchungsreferenz",
+    "Gebührenschlüssel",
+    "Länderkennzeichen",
+    "BAR-Entgelt+Buchungsreferenz",
+    "AEE+Buchungsreferenz",
+    "Abrechnungskennzeichen",
+)
+
+
+class SpkMasterCardImporter(beangulp.Importer):
+    last_four_digits: str
+    ledger_account: str
+    currency: str
+    _date_format = "%d.%m.%y"
+    _file_encoding = "ISO-8859-1"
+    _acc_map: utils.AccountMapper
+
+    def __init__(
+        self,
+        account: str,
+        last_four_digits: str,
+        account_mapping: str = None,
+        currency: str = "EUR",
+    ) -> None:
+        self.ledger_account = account
+        self.last_four_digits = last_four_digits
+        self.currency = currency
+        self._acc_map = utils.AccountMapper(account_mapping)
+
+    def identify(self, filepath) -> bool:
+        mimetype, encoding = mimetypes.guess_type(filepath)
+        if mimetype != "text/csv":
+            return False
+
+        with open(filepath, encoding=self._file_encoding) as f:
+            header = f.readline().strip()
+            csv_row = f.readline().strip()
+
+        expected_header = ";".join([f'"{field}"' for field in DEFAULT_FIELDS])
+
+        header_match = header == expected_header
+        card_number_match = (
+            csv_row.split(";")[0].replace('"', "")[-4:] == self.last_four_digits
+        )
+        return header_match and card_number_match
+
+    def account(self, filepath):
+        return self.ledger_account
+
+    def extract(self, filepath, existing=None):
+        if existing:
+            entries = existing
+        else:
+            entries = []
+        index = 0
+        with open(filepath, encoding=self._file_encoding) as f:
+            for index, row in enumerate(
+                csv.DictReader(f, delimiter=";", quotechar='"')
+            ):
+                meta = data.new_metadata(filename=filepath, lineno=index)
+                date: datetime = datetime.strptime(
+                    row["Buchungsdatum"], self._date_format
+                ).date()
+                narration = row["Transaktionsbeschreibung"]
+                units = amount.Amount(
+                    utils.format_amount(row["Buchungsbetrag"]), currency=self.currency
+                )
+                postings = [utils.create_posting(self.ledger_account, units, meta)]
+
+                if self._acc_map.known(narration):
+                    postings.append(
+                        utils.create_posting(
+                            self._acc_map.account(narration), -units, None
+                        )
+                    )
+
+                payee = None
+                if self._acc_map.payee(narration):
+                    payee = self._acc_map.payee(narration)
+                if self._acc_map.narration(narration):
+                    narration = self._acc_map.narration(narration)
+
+                entries.append(
+                    utils.create_transaction(
+                        postings,
+                        date,
+                        meta,
+                        payee,
+                        narration,
+                    )
+                )
+        return entries
+
+    def filename(self, filepath):
+        return f"MasterCard_{self.last_four_digits}.csv"
+
+    def date(self, filepath):
+        return max(map(lambda entry: entry.date, self.extract(filepath)))
+
+
+if __name__ == "__main__":
+    importer = SpkMasterCardImporter(
+        "Liabilities:DE:MasterCard:Silver-4932",
+        "4932",
+        account_mapping="./tests/test_mapping.yaml",
+    )
+    main(importer)
