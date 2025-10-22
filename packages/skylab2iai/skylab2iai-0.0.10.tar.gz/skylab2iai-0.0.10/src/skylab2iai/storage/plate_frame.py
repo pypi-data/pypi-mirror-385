cb31f3d7bc0df3d3fd76e613ccb519b024a7498a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-22 15:09:29 +0000

    Add package skylab2iai/0.0.10/skylab2iai-0.0.10.tar.gz

---
+++ packages/skylab2iai/skylab2iai-0.0.10.tar.gz/skylab2iai-0.0.10/src/skylab2iai/storage/plate_frame.py
@@ -0,0 +1,58 @@
+from io import UnsupportedOperation
+from typing import final
+
+import pandas as pd
+
+from .sql_connection import _SqlStorage
+
+
+@final
+class _SkylabPlateStorage:
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(_SkylabPlateStorage, cls).__new__(cls)
+            cls._instance._connection = None
+        return cls._instance
+
+    def __init__(self):
+        if self._connection is None:
+            self._connection = _SqlStorage()
+
+    @property
+    def __connection(self):
+        return self._connection
+
+    def get_plate_frame(self, name: str):
+        return pd.read_sql_query("SELECT * FROM plate_frame WHERE name = ?", self.__connection.db, params=(name,))
+
+    def get_plate_frames(self):
+        return pd.read_sql_query("SELECT * FROM plate_frame", self.__connection.db)
+
+    def get_plate_frames_by_plate(self, plate_name: str):
+        return pd.read_sql_query("SELECT * FROM plate_frame WHERE PLATE_ID = ?", self.__connection.db,
+                                 params=(plate_name,))
+
+    def _avoid_sql_injection(self, query: str):
+        if query.__contains__("--"):
+            raise UnsupportedOperation("SQL injection is not allowed")
+
+    def __avoid_sql_delete(self, query: str):
+        if query.__contains__("DELETE"):
+            raise UnsupportedOperation("Delete operation is not allowed")
+
+    def __avoid_sql_update(self, query: str):
+        if query.__contains__("UPDATE"):
+            raise UnsupportedOperation("Update operation is not allowed")
+
+    def __avoid_sql_insert(self, query: str):
+        if query.__contains__("INSERT"):
+            raise UnsupportedOperation("Insert operation is not allowed")
+
+    def get_from_custom_query(self, query: str, params: tuple = None):
+        self._avoid_sql_injection(query)
+        self.__avoid_sql_delete(query)
+        self.__avoid_sql_update(query)
+        self.__avoid_sql_insert(query)
+        return pd.read_sql_query(query, self.__connection.db, params=params)
---
+++ packages/skylab2iai/skylab2iai-0.0.10.tar.gz/skylab2iai-0.0.10/src/skylab2iai/storage/sql_connection.py
@@ -0,0 +1,22 @@
+import sqlite3
+from pathlib import Path
+from typing import final
+
+
+@final
+class _SqlStorage:
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(_SqlStorage, cls).__new__(cls)
+            cls._instance.db = None
+        return cls._instance
+
+    def __init__(self):
+        if self.db is None:
+            db_path = Path(__file__).parent / 'skylab-data.db'
+            self.db = sqlite3.connect(str(db_path))
+
+    def cursor(self):
+        return self.db.cursor()
