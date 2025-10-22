cb43e3f56b189210e9edd7cc419a2f130449e964
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-22 05:06:13 +0000

    Add package obstools-ipgp/1.1.0/obstools_ipgp-1.1.0-py3-none-any.whl

---
+++ packages/obstools-ipgp/obstools_ipgp-1.1.0-py3-none-any.whl/obstools_ipgp/__init__.py
@@ -0,0 +1,14 @@
+name = "obstools_ipgp"
+"""
+Command-line programs for evaluating and processing OBS data
+
+Data must be stored in SeisComp Data Structure (SDSP format
+Metadata must be stored in StationXML format
+
+command-line programs:
+     obstest: Plot figures for evaluating instrumentation tests
+     plotPSDs: Plot individual and all-station Power Spectral Densities
+     date_extent: Coming!
+     drift_correl: Coming!
+     toSEGY: Coming!
+"""
