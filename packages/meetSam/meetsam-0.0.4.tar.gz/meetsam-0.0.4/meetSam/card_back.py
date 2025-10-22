d2453295b9730d194f81cc6c2e5a825b77b6ae04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-22 07:09:15 +0000

    Add package meetSam/0.0.4/meetsam-0.0.4.tar.gz

---
+++ packages/meetSam/meetsam-0.0.4.tar.gz/meetsam-0.0.4/meetSam/card_back.py
@@ -0,0 +1,17 @@
+def name():
+    return "Samuel Mehalko"
+
+def title():
+    return "Sr. Principle Embedded Software Engineer"
+
+def company():
+    return "Northrop Grumman Corporation"
+
+def email():
+    return "samuel.mehalko@ngc.com"
+
+def site():
+    return "meetsam.readthedocs.io"
+
+def installation():
+    return "pip install meetSam"
