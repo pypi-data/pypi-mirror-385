5c643ed9621a114d6e976f212aa6a8975a6f01ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-22 05:06:12 +0000

    Add package connector-sdk-types/0.28.0/connector_sdk_types-0.28.0-py3-none-any.whl

---
+++ packages/connector-sdk-types/connector_sdk_types-0.28.0-py3-none-any.whl/connector_sdk_types/serializers/__init__.py
@@ -0,0 +1,17 @@
+from .field import (
+    AnnotatedField,
+    Discriminator,
+    FieldType,
+    HiddenField,
+    SecretField,
+    SemanticType,
+)
+
+__all__ = [
+    "AnnotatedField",
+    "Discriminator",
+    "FieldType",
+    "HiddenField",
+    "SecretField",
+    "SemanticType",
+]
