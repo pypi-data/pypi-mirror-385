352266cc5b9fddef598590353a1d92d7f09a626d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-22 05:06:11 +0000

    Add package ethyca-fides/2.72.3/ethyca_fides-2.72.3.tar.gz

---
+++ packages/ethyca-fides/ethyca_fides-2.72.3.tar.gz/ethyca_fides-2.72.3/src/fides/api/schemas/taxonomy_extensions.py
@@ -0,0 +1,51 @@
+"""
+Fides-specific extensions to the pydantic models of taxonomy elements as defined in fideslang.
+"""
+
+from typing import List, Optional
+
+from fideslang.models import DataCategory as BaseDataCategory
+from fideslang.models import DataSubject as BaseDataSubject
+from fideslang.models import DataSubjectRights
+from fideslang.models import DataUse as BaseDataUse
+from fideslang.models import DefaultModel
+from fideslang.validation import FidesKey
+from pydantic import BaseModel, Field
+
+active_field = Field(
+    default=True, description="Indicates whether the resource is currently 'active'."
+)
+
+
+class DataUse(BaseDataUse):
+    active: bool = active_field
+
+
+class DataCategory(BaseDataCategory):
+    active: bool = active_field
+
+
+class DataSubject(BaseDataSubject):
+    active: bool = active_field
+
+
+class TaxonomyCreateOrUpdateBase(DefaultModel, BaseModel):
+    name: Optional[str] = None
+    description: Optional[str] = None
+    active: Optional[bool] = True
+    fides_key: Optional[FidesKey] = None
+    tags: Optional[List[str]] = None
+    organization_fides_key: Optional[FidesKey] = "default_organization"
+
+
+class DataUseCreateOrUpdate(TaxonomyCreateOrUpdateBase):
+    parent_key: Optional[FidesKey] = None
+
+
+class DataCategoryCreateOrUpdate(TaxonomyCreateOrUpdateBase):
+    parent_key: Optional[FidesKey] = None
+
+
+class DataSubjectCreateOrUpdate(TaxonomyCreateOrUpdateBase):
+    rights: Optional[DataSubjectRights] = None
+    automated_decisions_or_profiling: Optional[bool] = None
