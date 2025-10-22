a6d9460d8d1b0f73b84f1fd5c9dc95719cb29697
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-22 09:08:22 +0000

    Add package sbxpy/0.8.1/sbxpy-0.8.1.tar.gz

---
+++ packages/sbxpy/sbxpy-0.8.1.tar.gz/sbxpy-0.8.1/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sbxpy",
+    version="0.8.1",
+    author="Luis Guzmán",
+    author_email="lgguzman890414@gmail.com",
+    description="This is the module  create all request used to communicate with SbxCloud",
+    long_description="This is the module  create all request used to communicate with SbxCloud",
+    long_description_content_type="text/markdown",
+    url="https://github.com/sbxcloud/sbxcloudpython",
+    packages=setuptools.find_packages(),
+    install_requires=[
+              'aiohttp>=3.7.3',
+              'redis>=4.5.2',
+              'pydantic>2.1.0',
+              'deepmerge>=1.1.0'
+          ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
