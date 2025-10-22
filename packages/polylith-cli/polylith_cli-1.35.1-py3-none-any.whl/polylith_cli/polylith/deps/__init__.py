665e95e14de872d62a13142f8735ed0456042edd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-22 07:07:27 +0000

    Add package polylith-cli/1.35.1/polylith_cli-1.35.1-py3-none-any.whl

---
+++ packages/polylith-cli/polylith_cli-1.35.1-py3-none-any.whl/polylith_cli/polylith/deps/__init__.py
@@ -0,0 +1,3 @@
+from polylith_cli.polylith.deps.core import get_brick_imports
+from polylith_cli.polylith.deps.report import print_brick_deps, print_deps
+__all__ = ['get_brick_imports', 'print_brick_deps', 'print_deps']
