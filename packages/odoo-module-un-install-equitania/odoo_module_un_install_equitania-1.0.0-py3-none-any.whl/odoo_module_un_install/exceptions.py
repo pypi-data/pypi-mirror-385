240d28f0611fea429a30d178daad051cfb688d6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-22 05:06:53 +0000

    Add package odoo-module-un-install-equitania/1.0.0/odoo_module_un_install_equitania-1.0.0-py3-none-any.whl

---
+++ packages/odoo-module-un-install-equitania/odoo_module_un_install_equitania-1.0.0-py3-none-any.whl/odoo_module_un_install/exceptions.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Copyright 2014-now Equitania Software GmbH - Pforzheim - Germany
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# Date: 24.06.2025
+
+"""Custom exceptions for Odoo module management operations."""
+
+
+class OdooConnectionError(Exception):
+    """Raised when connection to Odoo server fails."""
+    pass
+
+
+class PathDoesNotExistError(Exception):
+    """Raised when a specified path does not exist."""
+    pass
+
+
+class ModuleDependencyError(Exception):
+    """Raised when module dependencies prevent operation."""
+    pass
+
+
+class ModuleNotFoundError(Exception):
+    """Raised when a module could not be found in Odoo."""
+    pass
