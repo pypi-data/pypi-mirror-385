9017ccb40d24bcea036fed1745733078deb998c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-22 03:24:38 +0000

    Add package runestone-petljadoc/3.1.2.8/runestone_petljadoc-3.1.2.8-py2.py3-none-any.whl

---
+++ packages/runestone-petljadoc/runestone_petljadoc-3.1.2.8-py2.py3-none-any.whl/runestone/assess/js/mchoice-i18n.hu.js
@@ -0,0 +1,7 @@
+$.i18n().load({
+    "hu": {
+        "msg_mchoice_correct_answer": "Helyes!",
+        "msg_mchoice_incorrect_answer": "Nem helyes!",
+        "msg_mchoice_check_me": "Ellenőrizd le!"
+    }
+});
