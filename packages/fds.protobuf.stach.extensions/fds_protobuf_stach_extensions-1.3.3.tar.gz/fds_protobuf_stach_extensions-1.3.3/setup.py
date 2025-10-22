a6d8e82c30549e7a76cb56aa471beb8cbfa102c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-22 03:24:44 +0000

    Add package fds.protobuf.stach.extensions/1.3.3/fds_protobuf_stach_extensions-1.3.3.tar.gz

---
+++ packages/fds.protobuf.stach.extensions/fds_protobuf_stach_extensions-1.3.3.tar.gz/fds_protobuf_stach_extensions-1.3.3/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+REQUIRES = ["fds.protobuf.stach<2.0.0", "fds.protobuf.stach.v2<2.0.0", "pandas<3.0.0", "numpy<3.0.0"]
+
+setuptools.setup(
+    name="fds.protobuf.stach.extensions",
+    version="1.3.3",
+    author="Analytics API",
+    author_email="analytics.api.support@factset.com",
+    description="FactSet stach extensions",
+    long_description = "Extension package to help convert stach format data to simpler data structures",
+    url="https://github.com/factset/stach-extensions",
+    packages=setuptools.find_packages(exclude=["test", "tests"]),
+    install_requires=REQUIRES,
+    include_package_data=True,
+    license="Apache License 2.0",
+    python_requires=">=3.9",
+)
