2d5fee9af69c2155ce6e23b45f684b9de7b3aff0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-22 03:25:29 +0000

    Add package ahbicht/0.16.4/ahbicht-0.16.4.tar.gz

---
+++ packages/ahbicht/ahbicht-0.16.4.tar.gz/ahbicht-0.16.4/json_schemas/generate_json_schemas.py
@@ -0,0 +1,71 @@
+"""
+This script is run in the tox 'json_schemas' environment.
+It creates json schema files as described in the README.md in the same directory.
+"""
+
+import json
+import pathlib
+from typing import Type
+
+from marshmallow import Schema, fields
+from marshmallow_jsonschema import JSONSchema  # type:ignore[import]
+
+from ahbicht.json_serialization.tree_schema import TokenSchema  # , TreeSchema
+from ahbicht.models.categorized_key_extract import CategorizedKeyExtractSchema
+from ahbicht.models.condition_nodes import EvaluatedFormatConstraintSchema
+from ahbicht.models.content_evaluation_result import ContentEvaluationResultSchema
+from ahbicht.models.evaluation_results import (
+    AhbExpressionEvaluationResultSchema,
+    FormatConstraintEvaluationResultSchema,
+    RequirementConstraintEvaluationResultSchema,
+)
+from ahbicht.models.mapping_results import (
+    ConditionKeyConditionTextMappingSchema,
+    PackageKeyConditionExpressionMappingSchema,
+)
+
+schema_types: list[Type[Schema]] = [
+    RequirementConstraintEvaluationResultSchema,
+    FormatConstraintEvaluationResultSchema,
+    EvaluatedFormatConstraintSchema,
+    AhbExpressionEvaluationResultSchema,
+    ConditionKeyConditionTextMappingSchema,
+    PackageKeyConditionExpressionMappingSchema,
+    ContentEvaluationResultSchema,
+    TokenSchema,
+    CategorizedKeyExtractSchema,
+    # TreeSchema
+    # As of 2021-11 the TreeSchema fails, probably because of recursion or the lambda:
+    # (<class 'AttributeError'>, AttributeError("'function' object has no attribute 'fields'"), ....)
+]
+json_schema = JSONSchema()
+for schema_type in schema_types:
+    this_directory = pathlib.Path(__file__).parent.absolute()
+    file_name = schema_type.__name__ + ".json"  # pylint:disable=invalid-name
+    file_path = this_directory / file_name
+    schema_instance = schema_type()
+    if "requirement_indicator" in schema_instance.fields:
+        # workaround: in the schemas we want the requirement indicator to appear as simple string
+        # the schema used internally is just a workaround
+        for field_dict in [
+            schema_instance.fields,
+            schema_instance.load_fields,
+            schema_instance.dump_fields,
+            schema_instance.declared_fields,
+        ]:
+            field_dict["requirement_indicator"] = fields.String(name="requirement_indicator")
+
+    json_schema_dict = json_schema.dump(schema_instance)
+    # We want our JSON schemas to be compatible with a typescript code generator:
+    # https://github.com/bcherny/json-schema-to-typescript/
+    # However there's an unresolved bug: The root level of the schema must not contain any '$ref' key.
+    # https://github.com/bcherny/json-schema-to-typescript/issues/132
+    # The workaround goes like this:
+    result = json_schema_dict.copy()
+    result["type"] = "object"
+    result["title"] = schema_type.__name__
+    result["properties"] = {"$ref": result["$ref"] + "/properties"}
+    del result["$ref"]
+
+    with open(file_path, "w", encoding="utf-8") as json_schema_file:
+        json.dump(result, json_schema_file, ensure_ascii=False, sort_keys=True, indent=4)
---
+++ packages/ahbicht/ahbicht-0.16.4.tar.gz/ahbicht-0.16.4/src/ahbicht/json_serialization/tree_schema.py
@@ -0,0 +1,110 @@
+"""
+Schemata for the JSON serialization of expressions.
+"""
+
+from typing import Optional, Union
+
+from lark import Token, Tree
+from marshmallow import Schema, fields, post_load, pre_dump
+
+# in the classes/schemata we don't care about if there aren't enough public versions.
+# We also don't care about unused kwargs, or no self-use.
+# pylint: disable=too-few-public-methods,unused-argument
+
+
+class _TokenOrTree:
+    """
+    A class that is easily serializable as dictionary and allows us to _not_ use the marshmallow-union package.
+    """
+
+    def __init__(self, token: Optional[Token] = None, tree: Optional[Tree] = None) -> None:
+        self.token = token
+        self.tree = tree
+
+    token: Optional[Token]
+    tree: Optional[Tree]
+
+
+class _TokenOrTreeSchema(Schema):
+    """
+    A schema that represents data of the kind Union[str,Tree]
+    There is a python package for that: https://github.com/adamboche/python-marshmallow-union
+    but is only has 15 stars; not sure if it's worth the dependency
+    """
+
+    token = fields.Nested(
+        lambda: TokenSchema(), allow_none=True, required=False  # pylint: disable=unnecessary-lambda
+    )  # fields.String(dump_default=False, required=False, allow_none=True)
+    # disable unnecessary lambda warning because of circular imports
+    tree = fields.Nested(
+        lambda: TreeSchema(), dump_default=False, required=False, allow_none=True  # pylint: disable=unnecessary-lambda
+    )
+
+    @post_load
+    def deserialize(self, data, **kwargs) -> Union[str, Tree, Token]:
+        """
+        convert a dictionary back to a string, Tree or Token
+        :param data:
+        :param kwargs:
+        :return:
+        """
+        if "tree" in data and data["tree"]:
+            if not isinstance(data["tree"], Tree):
+                return Tree(**data["tree"])
+            return data["tree"]
+        if "token" in data and data["token"]:
+            return data["token"]
+        return data
+
+    @pre_dump
+    def prepare_tree_for_serialization(self, data, **kwargs) -> _TokenOrTree:
+        """
+        Create a string of tree object
+        :param data:
+        :param kwargs:
+        :return:
+        """
+        if isinstance(data, Tree):
+            return _TokenOrTree(token=None, tree=data)
+        if isinstance(data, Token):
+            return _TokenOrTree(token=data, tree=None)
+        raise NotImplementedError(f"Data type of {data} is not implemented for JSON serialization")
+
+
+class TokenSchema(Schema):
+    """
+    A schema that represents a lark Token and allows (de-)serializing is as JSON
+    """
+
+    value = fields.String(dump_default=False, allow_none=True)
+    type = fields.String(dump_default=False, data_key="type")
+
+    @post_load
+    def deserialize(self, data, **kwargs) -> Token:
+        """
+        converts the barely typed data dictionary into an actual Tree
+        :param data:
+        :param kwargs:
+        :return:
+        """
+        return Token(data["type"], data["value"])
+
+
+class TreeSchema(Schema):
+    """
+    A schema that represents a lark tree and allows (de-)serializing it as JSON
+    """
+
+    data = fields.String(data_key="type")  # for example 'or_composition', 'and_composition', 'condition_key'
+    # disable lambda warning. I don't know how to resolve this circular imports
+    children = fields.List(fields.Nested(lambda: _TokenOrTreeSchema()))  # pylint: disable=unnecessary-lambda
+
+    @post_load
+    def deserialize(self, data, **kwargs) -> Tree:
+        """
+        converts the barely typed data dictionary into an actual Tree
+        :param data:
+        :param kwargs:
+        :return:
+        """
+        return Tree(**data)
