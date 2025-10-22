bcb0cb85282394bd0f58e79f570810350ef57174
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-22 03:25:40 +0000

    Add package IduStorage/1.1.3/idustorage-1.1.3.tar.gz

---
+++ packages/IduStorage/idustorage-1.1.3.tar.gz/idustorage-1.1.3/src/idustorage/storage/interfaces/storage_interface.py
@@ -0,0 +1,26 @@
+import datetime
+from typing import Any
+
+from idustorage.storage.interfaces.cacheable import Cacheable
+
+
+class StorageInterface:
+    """Abstract class for a storage service"""
+
+    def save(self, cacheable: Cacheable, name: str, ext: str, date: datetime.datetime, *args) -> str:
+        pass
+
+    def retrieve_cached_file(self, pattern: str, ext: str, date: datetime.datetime, *args):
+        pass
+    
+    def open(self, cacheable: Cacheable, name: str):
+        pass
+
+    def get_actuality(self) -> str:
+        pass
+
+    def set_actuality(self, val: str) -> str:
+        pass
+
+    def delete_existing_cache(self, filename: str):
+        pass
