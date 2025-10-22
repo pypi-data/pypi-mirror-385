aab968086cae1df5bebd881224623b438dc475cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-22 15:09:11 +0000

    Add package agentql/1.15.0/agentql-1.15.0-py3-none-any.whl

---
+++ packages/agentql/agentql-1.15.0-py3-none-any.whl/agentql/tools/_shared/tetra/__init__.py
@@ -0,0 +1,5 @@
+from .browser_profile import BrowserProfile
+from .browser_session import BrowserSession
+from .user_agent_preset import UserAgentPreset
+
+__all__ = ["BrowserProfile", "BrowserSession", "UserAgentPreset"]
---
+++ packages/agentql/agentql-1.15.0-py3-none-any.whl/agentql/tools/async_api/__init__.py
@@ -0,0 +1,13 @@
+from agentql.ext.playwright.tools.async_api._pagination.pagination_tool import paginate
+from agentql.tools._shared.tetra import BrowserProfile, BrowserSession, UserAgentPreset
+from agentql.tools.async_api._query_document.query_document import query_document
+from agentql.tools.async_api._tetra.tetra import create_browser_session
+
+__all__ = [
+    "paginate",
+    "query_document",
+    "create_browser_session",
+    "BrowserProfile",
+    "UserAgentPreset",
+    "BrowserSession",
+]
