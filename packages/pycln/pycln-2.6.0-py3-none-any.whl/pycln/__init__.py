5cf8e9d91465a224c9dd6e7460bb0adb050e8fe9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-22 09:07:18 +0000

    Add package pycln/2.6.0/pycln-2.6.0-py3-none-any.whl

---
+++ packages/pycln/pycln-2.6.0-py3-none-any.whl/pycln/__init__.py
@@ -0,0 +1,33 @@
+import io
+import os
+import sys
+from importlib.metadata import metadata
+from pathlib import Path
+
+import typer
+
+#: Add vendor directory to module search path
+VENDOR_PATH = Path(__file__).parent.parent.joinpath("vendor")
+sys.path.append(str(VENDOR_PATH))
+
+
+#: Fixes `UnicodeEncodeError` in non-utf8 terminals.
+#: For more info: https://github.com/hadialqattan/pycln/issues/54
+UTF8 = "utf-8"
+if "pytest" not in sys.modules:
+    sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding=UTF8)  # pragma: nocover
+    sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding=UTF8)  # pragma: nocover
+
+ISWIN = os.name == "nt"
+
+pycln = metadata("pycln")
+__name__ = str(pycln["Name"])
+__doc__ = str(pycln["Summary"])
+__version__ = str(pycln["Version"])
+
+
+def version_callback(value: bool):
+    """Show the version and exit with 0."""
+    if value:
+        typer.echo(f"{__name__}, version {__version__}")
+        raise typer.Exit(0)
---
+++ packages/pycln/pycln-2.6.0-py3-none-any.whl/pycln/__main__.py
@@ -0,0 +1,6 @@
+"""Entry point to run Pycln as a module."""
+
+from . import __name__
+from .cli import app
+
+app(prog_name=__name__)
