99d251b59cdd44d4718491e8a07bb69556c85f29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-22 06:06:27 +0000

    Add package rolfedh-doc-utils/0.1.29/rolfedh_doc_utils-0.1.29-py3-none-any.whl

---
+++ packages/rolfedh-doc-utils/rolfedh_doc_utils-0.1.29-py3-none-any.whl/doc_utils/version.py
@@ -0,0 +1,8 @@
+"""Version information for doc-utils."""
+
+# This should match the version in pyproject.toml
+__version__ = "0.1.29"
+
+def get_version():
+    """Return the current version string."""
+    return __version__
